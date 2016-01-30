2fd0474268069e063f3abf40b3b23880e1d6b61f
AnyLine <server@anyline.org>
2016-01-30 10:26:07 +0800

    no commit message

--- anyline_core/src/org/anyline/util/VerifyCodeUtil.java
+++ anyline_core/src/org/anyline/util/VerifyCodeUtil.java
@@ -262,13 +262,13 @@ public class VerifyCodeUtil {
 
 	}
 
-	public static void main(String[] args) throws IOException {
-		File dir = new File("F:/verifies");
-		int w = 200, h = 80;
-		for (int i = 0; i < 50; i++) {
-			String verifyCode = getRandomCode(4);
-			File file = new File(dir, verifyCode + ".jpg");
-			outputImage(w, h, file, verifyCode);
-		}
-	}
+//	public static void main(String[] args) throws IOException {
+//		File dir = new File("F:/verifies");
+//		int w = 200, h = 80;
+//		for (int i = 0; i < 50; i++) {
+//			String verifyCode = getRandomCode(4);
+//			File file = new File(dir, verifyCode + ".jpg");
+//			outputImage(w, h, file, verifyCode);
+//		}
+//	}
 }
