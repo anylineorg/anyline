c44317582b12fdbe1001acbff21fc6d22eddaca2
AnyLine <server@anyline.org>
2016-02-17 19:08:11 +0800

    no commit message

--- anyline_core/src/org/anyline/struts/result/TemplateResult.java
+++ anyline_core/src/org/anyline/struts/result/TemplateResult.java
@@ -27,7 +27,6 @@ import org.apache.struts2.dispatcher.StrutsResultSupport;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.Result;
-
 public class TemplateResult extends StrutsResultSupport {
 	private static final long serialVersionUID = 0xe4a6cfd5319c8fc5L;
 	private String contentPage = null;
@@ -53,33 +52,40 @@ public class TemplateResult extends StrutsResultSupport {
 				contentPage = dir + contentPage;
 			}
 		}
+		
 		HttpServletRequest request = ServletActionContext.getRequest();
-		String templet = ConfigTable.getString("TEMPLET_FILE_PATH_WEB");
-		if (WebUtil.isWap(request)) {
-			templet = ConfigTable.getString("TEMPLET_FILE_PATH_WAP");
+		String template = ""; 
+		int idx = contentPage.indexOf("/page/");
+		if(idx > 0){
+			template = contentPage.substring(0,idx)+"/template/default.jsp";
 		}
-		if (null == templet) {
-			templet = ConfigTable.getString("TEMPLET_FILE_PATH");
+		if(null == template){
+			template = ConfigTable.getString("TEMPLET_FILE_PATH_WEB");
+			if (WebUtil.isWap(request)) {
+				template = ConfigTable.getString("TEMPLET_FILE_PATH_WAP");
+			}
+			if (null == template) {
+				template = ConfigTable.getString("TEMPLET_FILE_PATH");
+			}
 		}
 		PageContext pageContext = ServletActionContext.getPageContext();
 		if (pageContext != null) {
-			pageContext.include(templet);
+			pageContext.include(template);
 		} else {
 			HttpServletResponse response = ServletActionContext.getResponse();
-			RequestDispatcher dispatcher = request
-					.getRequestDispatcher(templet);
+			RequestDispatcher dispatcher = request.getRequestDispatcher(template);
 			request.setAttribute("content_page", contentPage);
 			if (dispatcher == null) {
 				response.sendError(404,
 						(new StringBuilder()).append("result '")
-								.append(templet).append("' not found")
+								.append(template).append("' not found")
 								.toString());
 				return;
 			}
 			if (!response.isCommitted()
 					&& request
 							.getAttribute("javax.servlet.include.servlet_path") == null) {
-				request.setAttribute("struts.view_uri", templet);
+				request.setAttribute("struts.view_uri", template);
 				request.setAttribute("struts.request_uri",
 						request.getRequestURI());
 				dispatcher.forward(request, response);
