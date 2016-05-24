4794f2d2a080f9fe2b9312a2ffd07943a79f0e63
AnyLine <server@anyline.org>
2016-05-24 11:13:11 +0800

    no commit message

--- anyline_core/src/org/anyline/dao/impl/BatchInsertStore.java
+++ anyline_core/src/org/anyline/dao/impl/BatchInsertStore.java
@@ -1,16 +1,16 @@
 package org.anyline.dao.impl;
 
-import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class BatchInsertStore {
-	private ConcurrentHashMap<String,Vector<Object>> map = new ConcurrentHashMap<String,Vector<Object>>();
+	private ConcurrentHashMap<String,ConcurrentLinkedDeque<String>> map = new ConcurrentHashMap<String,ConcurrentLinkedDeque<String>>();
 	
 	public void addData(String table, String cols, String data){
-		String key = table + "_" + cols;
-		Vector<Object> rows = map.get(key);
+		String key = table + "(" + cols +")";
+		ConcurrentLinkedDeque<String> rows = map.get(key);
 		if(null == rows){
-			rows = new Vector<Object>();
+			rows = new ConcurrentLinkedDeque<String>();
 			map.put(key, rows);
 		}
 		rows.add(data);
