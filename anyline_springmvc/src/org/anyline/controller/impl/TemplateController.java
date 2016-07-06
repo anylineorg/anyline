e887b6b02fbca2d7477a15ac85b5e4a48435f4ed
anyline <server@anyline.org>
2016-07-06 15:15:13 +0800

    数据样式模板

--- anyline_springmvc/src/org/anyline/controller/impl/TemplateController.java
+++ anyline_springmvc/src/org/anyline/controller/impl/TemplateController.java
@@ -16,6 +16,13 @@
  *          AnyLine以及一切衍生库 不得用于任何与网游相关的系统
  */
 package org.anyline.controller.impl;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,9 +34,12 @@ import org.anyline.util.ConfigTable;
 import org.anyline.util.DESUtil;
 import org.anyline.util.HttpUtil;
 import org.anyline.util.WebUtil;
+import org.anyline.util.regular.RegularUtil;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.ModelAndView;
  
  
@@ -67,6 +77,98 @@ public class TemplateController extends AnylineController {
 		html = HttpUtil.escape(html);
 		return success(html);
 	}
+	/**
+	 * 加载样式模板
+	 * 与load区别是{KEY}形式的变量KEY会加密
+	 * 并有可能调用controller方法为模板准备数据(参考parseTemplate)
+	 * @return
+	 */
+	@RequestMapping("load_style")
+	@ResponseBody
+	public String loadDataTemplate(HttpServletRequest request, HttpServletResponse response){
+		String template_path = getParam("path", false , true);
+		if(null == template_path || template_path.isEmpty()){
+			template_path = "default.jsp";
+		}
+		if(template_path.endsWith("/")){
+			template_path = template_path+"default.jsp";
+		}
+		String content = parseTemplate(request, response,template_path);
+		LOG.info("样式模板:"+template_path);
+		try{
+			List<List<String>> vars= RegularUtil.fetch(content, "{([\\w.]+)}");//RegularUtil.REGEX_VARIABLE
+			for(List<String> var:vars){
+				String fullVar = var.get(0);
+				String simVar = var.get(1).toUpperCase().trim();
+				if("ROW_NUMBER".equals(simVar)){
+					continue;
+				}
+				if(simVar.contains(".")){
+					String pre = simVar.substring(0,simVar.lastIndexOf("."));
+					String suf = simVar.substring(simVar.lastIndexOf(".")+1);
+					simVar = pre + "." + WebUtil.encryptKey(suf).toUpperCase(); 
+				}else{
+					simVar = WebUtil.encryptKey(simVar).toUpperCase();
+				}
+				content = content.replace(fullVar, "{"+simVar+"}");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		content = HttpUtil.escape(content);
+		return success(content);
+	}
+	
+	private String parseTemplate(HttpServletRequest request, HttpServletResponse response, String path){
+		String result = "";
+		 //构造填充数据的Map 
+		if(null == path){
+			return result;
+		}
+		Map map = new HashMap();
+		int idx = path.indexOf(":");
+		if(idx > 0){
+			String cm = path.substring(idx+1);
+			path = path.substring(0, idx);
+			idx = cm.lastIndexOf(".");
+			String clazz = cm.substring(0,idx);
+			String method = cm.substring(idx+1);
+			String arg = method.substring(method.indexOf("(")+1, method.indexOf(")"));
+			method = method.substring(0, method.indexOf("("));
+			try{
+				Class c = Class.forName(clazz);
+				ApplicationContext ac = WebApplicationContextUtils.getRequiredWebApplicationContext(request.getSession().getServletContext());
+				
+				Object instance = ac.getBean(c);
+				Method m = c.getMethod(method, String.class);
+				if(null != m){
+					map = (Map)m.invoke(instance, arg);
+					if(null != map){
+						Set keys = map.keySet();
+						for(Object key:keys){
+							request.setAttribute(key.toString(), map.get(key));
+						}
+					}
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}
+        try{
+        	File dir = new File(ConfigTable.getWebRoot());
+        	File file = new File(dir, path);
+        	if(!file.exists()){
+        		//文件不存在
+        		return result;
+        	}
+        	result = WebUtil.parseJsp(request, response, path);
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
+		return result;
+	}
+	
 	/**
 	 * 创建显示视图(page目录下)
 	 * @param name
