180851b92b91c959842a63ad8c2af5037d676263
anyline <server@anyline.org>
2016-07-14 18:36:43 +0800

    分页标签

--- anyline_core/src/org/anyline/util/ConfigTable.java
+++ anyline_core/src/org/anyline/util/ConfigTable.java
@@ -139,7 +139,7 @@ public class ConfigTable {
 	}
 	public static String getString(String key, String def){
 		String val = getString(key);
-		if(null == def){
+		if(null == val){
 			val = def;
 		}
 		return val;
