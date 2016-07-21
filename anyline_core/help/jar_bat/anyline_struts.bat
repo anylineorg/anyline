439d832a6661dbe62c8e33aa9ece3ea9e0834801
anyline <server@anyline.org>
2016-07-21 23:04:48 +0800

    jar

--- anyline_core/help/jar_bat/anyline_struts.bat
+++ anyline_core/help/jar_bat/anyline_struts.bat
@@ -6,6 +6,9 @@ rmdir  /s/q anyline_struts\org
 mkdir anyline_struts\org
 xcopy E:\develop\git\osc\anyline\anyline_struts\bin\org anyline_struts\org /s /e
       
+del anyline_struts\struts-anyline.xml 
+xcopy E:\develop\web\anyline_core\WEB-INF\classes\struts-anyline.xml anyline_struts /s /e
+
 cd anyline_struts
 jar -cvf ..\anyline_struts.jar *
 cd ..
