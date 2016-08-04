edffcbcf0431c9a92f7b7629d0c35bd7c0dad511
anyline <server@anyline.org>
2016-08-04 15:48:22 +0800

    缓存更新

--- anyline_core/help/jar_bat/anyline_core.bat
+++ anyline_core/help/jar_bat/anyline_core.bat
@@ -11,9 +11,6 @@ rmdir  /s/q anyline_core\META-INF
 mkdir anyline_core\META-INF
 xcopy E:\develop\web\anyline_core\WEB-INF\tld anyline_core\META-INF /s /e
 
-del anyline_core\struts-anyline.xml 
-xcopy E:\develop\web\anyline_core\WEB-INF\classes\struts-anyline.xml anyline_core /s /e
-
 cd anyline_core
 jar -cvf ..\anyline_core.jar *
 cd ..
