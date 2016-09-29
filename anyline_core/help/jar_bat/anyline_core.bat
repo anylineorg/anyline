4a96ee8c6edde5aac27bf3a4c29a703279b9a72c
server <server@anyline.org>
2016-09-29 10:20:26 +0800

    jar script

--- anyline_core/help/jar_bat/anyline_core.bat
+++ anyline_core/help/jar_bat/anyline_core.bat
@@ -1,15 +1,15 @@
-E:
-cd E:\jar_bat
+D:
+cd D:\jar_bat
 del anyline_core.jar
 
 rmdir  /s/q anyline_core\org
 mkdir anyline_core\org
-xcopy E:\develop\web\anyline_core\WEB-INF\classes\org anyline_core\org /s /e
+xcopy D:\develop\web\anyline_core\WEB-INF\classes\org anyline_core\org /s /e
 
 
 rmdir  /s/q anyline_core\META-INF
 mkdir anyline_core\META-INF
-xcopy E:\develop\web\anyline_core\WEB-INF\tld anyline_core\META-INF /s /e
+xcopy D:\develop\web\anyline_core\WEB-INF\tld anyline_core\META-INF /s /e
 
 cd anyline_core
 jar -cvf ..\anyline_core.jar *
--- anyline_core/help/jar_bat/anyline_springmvc.bat
+++ anyline_core/help/jar_bat/anyline_springmvc.bat
@@ -1,10 +1,10 @@
-E:
-cd E:\jar_bat
+D:
+cd D:\jar_bat
 del anyline_springmvc.jar
 
 rmdir  /s/q anyline_springmvc\org
 mkdir anyline_springmvc\org
-xcopy E:\develop\git\osc\anyline\anyline_springmvc\bin\org anyline_springmvc\org /s /e
+xcopy D:\develop\git\anyline\anyline_springmvc\bin\org anyline_springmvc\org /s /e
 
 cd anyline_springmvc
 jar -cvf ..\anyline_springmvc.jar *
