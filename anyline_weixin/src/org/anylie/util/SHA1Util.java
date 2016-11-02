c78c3578b8d9199d023008869fcee1e80e41ce77
server <server@anyline.org>
2016-11-02 21:36:42 +0800

    weixin init

---
+++ anyline_weixin/src/org/anylie/util/SHA1Util.java
@@ -0,0 +1,29 @@
+package org.anylie.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA1Util {
+	private static MessageDigest digest = null;
+	public static String encrypt(String src){
+		try {
+			if(null == digest){
+				digest = MessageDigest.getInstance("SHA-1");
+			}
+			digest.update(src.getBytes());
+			byte messageDigest[] = digest.digest();
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < messageDigest.length; i++) {
+				String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+				if (shaHex.length() < 2) {
+					hexString.append(0);
+				}
+				hexString.append(shaHex);
+			}
+			return hexString.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
