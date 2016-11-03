c2ae046125b242823baf27a303e11b62061a6679
server <server@anyline.org>
2016-11-03 15:20:25 +0800

    datasource table schema兼容

--- anyline_weixin/src/org/anyline/weixin/util/WXConfig.java
+++ anyline_weixin/src/org/anyline/weixin/util/WXConfig.java
@@ -16,19 +16,51 @@ public class WXConfig {
 	/**
 	 * 服务号相关信息
 	 */
-	 public static String APP_ID = "";//AppID(应用ID)
-	 public static String APP_SECRECT = "";//AppSecret(应用密钥)
-	 public static String MCH_ID = "";//商户号
-	 public static String SIGN_TYPE = "";//签名加密方式
-	 public static String SERVER_TOKEN = "";//服务号的配置token
-	 public static String CERT_PATH = "";//微信支付证书存放路径地址
-	//微信支付统一接口的回调action
-	 public static String PAY_NOTIFY_URL = "";
-	//微信支付成功支付后跳转的地址
-	 public static String PAY_CALLBACK_URL = "";
-	 //oauth2授权时回调action
-	 public static String OAUTH2_REDIRECT_URI = "";
+	 public static String APP_ID = ""				; //AppID(应用ID)
+	 public static String APP_SECRECT = ""			; //AppSecret(应用密钥)
+	 public static String MCH_ID = ""				; //商户号
+	 public static String SIGN_TYPE = ""			; //签名加密方式
+	 public static String SERVER_TOKEN = ""			; //服务号的配置token
+	 public static String CERT_PATH = ""			; //微信支付证书存放路径地址
+	 public static String PAY_NOTIFY_URL = ""		; //微信支付统一接口的回调action
+	 public static String PAY_CALLBACK_URL = ""		; //微信支付成功支付后跳转的地址
+	 public static String OAUTH2_REDIRECT_URI = ""	; //oauth2授权时回调action
 	 
+	 /**
+	 * 微信基础接口地址
+	 */
+	 //获取token接口(GET)
+	 public final static String TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+	 //oauth2授权接口(GET)
+	 public final static String OAUTH2_URL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
+	 //刷新access_token接口（GET）
+	 public final static String REFRESH_TOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=APPID&grant_type=refresh_token&refresh_token=REFRESH_TOKEN";
+	// 菜单创建接口（POST）
+	 public final static String MENU_CREATE_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
+	// 菜单查询（GET）
+	 public final static String MENU_GET_URL = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token=ACCESS_TOKEN";
+	// 菜单删除（GET）
+	public final static String MENU_DELETE_URL = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN";
+	/**
+	 * 微信支付接口地址
+	 */
+	//微信支付统一接口(POST)
+	public final static String UNIFIED_ORDER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+	//微信退款接口(POST)
+	public final static String REFUND_URL = "https://api.mch.weixin.qq.com/secapi/pay/refund";
+	//订单查询接口(POST)
+	public final static String CHECK_ORDER_URL = "https://api.mch.weixin.qq.com/pay/orderquery";
+	//关闭订单接口(POST)
+	public final static String CLOSE_ORDER_URL = "https://api.mch.weixin.qq.com/pay/closeorder";
+	//退款查询接口(POST)
+	public final static String CHECK_REFUND_URL = "https://api.mch.weixin.qq.com/pay/refundquery";
+	//对账单接口(POST)
+	public final static String DOWNLOAD_BILL_URL = "https://api.mch.weixin.qq.com/pay/downloadbill";
+	//短链接转换接口(POST)
+	public final static String SHORT_URL = "https://api.mch.weixin.qq.com/tools/shorturl";
+	//接口调用上报接口(POST)
+	public final static String REPORT_URL = "https://api.mch.weixin.qq.com/payitil/report";
+		
 	static{
 		init();
 		debug();
@@ -97,40 +129,4 @@ public class WXConfig {
 	public static void main(String args[]){
 		debug();
 	}
-	
-	 
-	/**
-	 * 微信基础接口地址
-	 */
-	 //获取token接口(GET)
-	 public final static String TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
-	 //oauth2授权接口(GET)
-	 public final static String OAUTH2_URL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
-	 //刷新access_token接口（GET）
-	 public final static String REFRESH_TOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=APPID&grant_type=refresh_token&refresh_token=REFRESH_TOKEN";
-	// 菜单创建接口（POST）
-	 public final static String MENU_CREATE_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
-	// 菜单查询（GET）
-	 public final static String MENU_GET_URL = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token=ACCESS_TOKEN";
-	// 菜单删除（GET）
-	public final static String MENU_DELETE_URL = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN";
-	/**
-	 * 微信支付接口地址
-	 */
-	//微信支付统一接口(POST)
-	public final static String UNIFIED_ORDER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
-	//微信退款接口(POST)
-	public final static String REFUND_URL = "https://api.mch.weixin.qq.com/secapi/pay/refund";
-	//订单查询接口(POST)
-	public final static String CHECK_ORDER_URL = "https://api.mch.weixin.qq.com/pay/orderquery";
-	//关闭订单接口(POST)
-	public final static String CLOSE_ORDER_URL = "https://api.mch.weixin.qq.com/pay/closeorder";
-	//退款查询接口(POST)
-	public final static String CHECK_REFUND_URL = "https://api.mch.weixin.qq.com/pay/refundquery";
-	//对账单接口(POST)
-	public final static String DOWNLOAD_BILL_URL = "https://api.mch.weixin.qq.com/pay/downloadbill";
-	//短链接转换接口(POST)
-	public final static String SHORT_URL = "https://api.mch.weixin.qq.com/tools/shorturl";
-	//接口调用上报接口(POST)
-	public final static String REPORT_URL = "https://api.mch.weixin.qq.com/payitil/report";
 }
