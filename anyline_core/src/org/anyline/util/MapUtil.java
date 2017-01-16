142229261327a97e8fc8830a19e4d1fa89d39da4
Administrator <Administrator@zhang>
2017-01-16 14:41:12 +0800

    Map Util

--- anyline_core/src/org/anyline/util/MapUtil.java
+++ anyline_core/src/org/anyline/util/MapUtil.java
@@ -18,19 +18,38 @@ public class MapUtil {
 	 * @param lon2
 	 * @return
 	 */
-	public static double distance(double lat1, double lon1, double lat2, double lon2) {
-		double radLat1 = rad(lat1);
-		double radLat2 = rad(lat2);
-		double a = radLat1 - radLat2;
-		double b = rad(lon1) - rad(lon2);
-		double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
-				+ Math.cos(radLat1) * Math.cos(radLat2)
-				* Math.pow(Math.sin(b / 2), 2)));
-		s = s * EARTH_RADIUS;
-		s = Math.round(s * 10000d) / 10000d;
-		s = s * 1000;
-		BigDecimal decimal = new BigDecimal(s);  
-		s = decimal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();  
-		return s;
+	public static double distance(double lon1, double lat1, double lon2, double lat2) {
+		try{
+			double radLat1 = rad(lat1);
+			double radLat2 = rad(lat2);
+			double a = radLat1 - radLat2;
+			double b = rad(lon1) - rad(lon2);
+			double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+					+ Math.cos(radLat1) * Math.cos(radLat2)
+					* Math.pow(Math.sin(b / 2), 2)));
+			s = s * EARTH_RADIUS;
+			s = Math.round(s * 10000d) / 10000d;
+			s = s * 1000;
+			BigDecimal decimal = new BigDecimal(s);  
+			s = decimal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();  
+			return s;
+		}catch(Exception e){
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	public static double distance(String lon1, String lat1, String lon2, String lat2) {
+		double distance = -1;
+		try{
+			distance = distance(
+					BasicUtil.parseDouble(lon1, -1.0),
+					BasicUtil.parseDouble(lat1, -1.0),
+					BasicUtil.parseDouble(lon2, -1.0),
+					BasicUtil.parseDouble(lat2, -1.0)
+					);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return distance;
 	}
 }
