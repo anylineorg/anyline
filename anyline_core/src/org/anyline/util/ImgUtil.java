ab403edcf91a22887b20c6c0e3485f0ec6cb1b2c
Administrator <Administrator@zhang>
2017-01-17 21:02:41 +0800

    ImgUtil

---
+++ anyline_core/src/org/anyline/util/ImgUtil.java
@@ -0,0 +1,322 @@
+package org.anyline.util;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.color.ColorSpace;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
+import java.awt.image.CropImageFilter;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.ImageFilter;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 图片处理工具类：<br>
+ * 功能：缩放图像、切割图像、图像类型转换、彩色转黑白、文字水印、图片水印等
+ */
+public class ImgUtil {
+	private static Logger log = Logger.getLogger(ImgUtil.class);
+    /**
+     * 几种常见的图片格式
+     */
+    public static String IMAGE_TYPE_GIF = "gif";// 图形交换格式
+    public static String IMAGE_TYPE_JPG = "jpg";// 联合照片专家组
+    public static String IMAGE_TYPE_JPEG = "jpeg";// 联合照片专家组
+    public static String IMAGE_TYPE_BMP = "bmp";// 英文Bitmap（位图）的简写，它是Windows操作系统中的标准图像文件格式
+    public static String IMAGE_TYPE_PNG = "png";// 可移植网络图形
+    public static String IMAGE_TYPE_PSD = "psd";// Photoshop的专用格式Photoshop
+
+//    public static void main(String args[]){
+//    	File dir = new File("D:\\imgs");
+//    	List<File> files = FileUtil.getAllChildrenFile(dir);
+//    	for(File file:files){
+//    		String src = file.getAbsolutePath();
+//    		File tar = new File(src.replace("imgs", "imgs_960"));
+//    		try{
+//    			if(!tar.exists())
+//    			ImgUtil.scale(file, tar, 960, 436, false);
+//    		}catch(Exception e){
+//    			e.printStackTrace();
+//    		}
+//    	}
+//    }
+
+    /**
+     * 缩放图像（按比例缩放）
+     * @param srcImageFile 源图像文件地址
+     * @param result 缩放后的图像地址
+     * @param scale 缩放比例
+     */
+    public final static void scale(File src, File tar, float scale) {
+        try {
+            BufferedImage img = ImageIO.read(src); // 读入文件
+            int width = img.getWidth(); // 得到源图宽
+            int height = img.getHeight(); // 得到源图长
+            width = (int)(width * scale);
+            height = (int)(height * scale);
+            Image image = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+            BufferedImage tag = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics g = tag.getGraphics();
+            g.drawImage(image, 0, 0, null); // 绘制缩小后的图
+            g.dispose();
+            File dir = tar.getParentFile();
+            if(!dir.exists()){
+            	dir.mkdirs();
+            }
+            ImageIO.write(tag, "JPEG", tar);// 输出到文件流
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 缩放图像（按高度和宽度缩放）
+     * @param srcImageFile 源图像文件地址
+     * @param result 缩放后的图像地址
+     * @param height 缩放后的高度
+     * @param width 缩放后的宽度
+     * @param fill 比例不对时是否需要补白：true为补白; false为不补白;
+     */
+    public final static void scale(File src, File tar, int width, int height, boolean fill) {
+    	long fr = System.currentTimeMillis();
+        try {
+            double ratio = 0.0; // 缩放比例
+            BufferedImage bi = ImageIO.read(src);
+            Image itemp = bi.getScaledInstance(width, height, bi.SCALE_SMOOTH);
+            // 计算比例
+            if ((bi.getHeight() > height) || (bi.getWidth() > width)) {
+                if (bi.getHeight() > bi.getWidth()) {
+                    ratio = (new Integer(height)).doubleValue() / bi.getHeight();
+                } else {
+                    ratio = (new Integer(width)).doubleValue() / bi.getWidth();
+                }
+                AffineTransformOp op = new AffineTransformOp(AffineTransform .getScaleInstance(ratio, ratio), null);
+                itemp = op.filter(bi, null);
+            }
+            if (fill) {//补白
+                BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                Graphics2D g = image.createGraphics();
+                g.setColor(Color.white);
+                g.fillRect(0, 0, width, height);
+                if (width == itemp.getWidth(null)){
+                    g.drawImage(itemp, 0, (height - itemp.getHeight(null)) / 2, itemp.getWidth(null), itemp.getHeight(null), Color.white, null);
+                }else{
+                    g.drawImage(itemp, (width - itemp.getWidth(null)) / 2, 0, itemp.getWidth(null), itemp.getHeight(null), Color.white, null);
+                }
+                g.dispose();
+                itemp = image;
+            }
+            File dir = tar.getParentFile();
+            if(!dir.exists()){
+            	dir.mkdirs();
+            }
+            ImageIO.write((BufferedImage) itemp, "JPEG", tar);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.warn("[压缩图片][耗时:"+(System.currentTimeMillis()-fr)+"][source:"+src+"][target:"+tar+"]");
+    }
+    
+    /**
+     * 图像切割(按指定起点坐标和宽高切割)
+     * @param srcImageFile 源图像地址
+     * @param result 切片后的图像地址
+     * @param x 目标切片起点坐标X
+     * @param y 目标切片起点坐标Y
+     * @param width 目标切片宽度
+     * @param height 目标切片高度
+     */
+    public final static void cut(File src, File tar, int x, int y, int width, int height) {
+        try {
+            // 读取源图像
+            BufferedImage bi = ImageIO.read(src);
+            int srcWidth = bi.getHeight(); // 源图宽度
+            int srcHeight = bi.getWidth(); // 源图高度
+            if (srcWidth > 0 && srcHeight > 0) {
+                Image image = bi.getScaledInstance(srcWidth, srcHeight, Image.SCALE_DEFAULT);
+                // 四个参数分别为图像起点坐标和宽高
+                // 即: CropImageFilter(int x,int y,int width,int height)
+                ImageFilter cropFilter = new CropImageFilter(x, y, width, height);
+                Image img = Toolkit.getDefaultToolkit().createImage(new FilteredImageSource(image.getSource(), cropFilter));
+                BufferedImage tag = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                Graphics g = tag.getGraphics();
+                g.drawImage(img, 0, 0, width, height, null); // 绘制切割后的图
+                g.dispose();
+                // 输出为文件
+
+                File dir = tar.getParentFile();
+                if(!dir.exists()){
+                	dir.mkdirs();
+                }
+                ImageIO.write(tag, "JPEG", tar);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * 图像切割（指定切片的行数和列数）
+     * @param srcImageFile 源图像地址
+     * @param descDir 切片目标文件夹
+     * @param rows 目标切片行数。默认2，必须是范围 [1, 20] 之内
+     * @param cols 目标切片列数。默认2，必须是范围 [1, 20] 之内
+     */
+    public final static void cut2(File src, File dir, int rows, int cols) {
+        try {
+            if(rows<=0||rows>20) rows = 2; // 切片行数
+            if(cols<=0||cols>20) cols = 2; // 切片列数
+            // 读取源图像
+            BufferedImage bi = ImageIO.read(src);
+            int srcWidth = bi.getHeight(); // 源图宽度
+            int srcHeight = bi.getWidth(); // 源图高度
+            if (srcWidth > 0 && srcHeight > 0) {
+                Image img;
+                ImageFilter cropFilter;
+                Image image = bi.getScaledInstance(srcWidth, srcHeight, Image.SCALE_DEFAULT);
+                int destWidth = srcWidth; // 每张切片的宽度
+                int destHeight = srcHeight; // 每张切片的高度
+                // 计算切片的宽度和高度
+                if (srcWidth % cols == 0) {
+                    destWidth = srcWidth / cols;
+                } else {
+                    destWidth = (int) Math.floor(srcWidth / cols) + 1;
+                }
+                if (srcHeight % rows == 0) {
+                    destHeight = srcHeight / rows;
+                } else {
+                    destHeight = (int) Math.floor(srcWidth / rows) + 1;
+                }
+                // 循环建立切片
+                // 改进的想法:是否可用多线程加快切割速度
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < cols; j++) {
+                        // 四个参数分别为图像起点坐标和宽高
+                        // 即: CropImageFilter(int x,int y,int width,int height)
+                        cropFilter = new CropImageFilter(j * destWidth, i * destHeight, destWidth, destHeight);
+                        img = Toolkit.getDefaultToolkit().createImage(new FilteredImageSource(image.getSource(), cropFilter));
+                        BufferedImage tag = new BufferedImage(destWidth,  destHeight, BufferedImage.TYPE_INT_RGB);
+                        Graphics g = tag.getGraphics();
+                        g.drawImage(img, 0, 0, null); // 绘制缩小后的图
+                        g.dispose();
+                        // 输出为文件
+
+                        if(!dir.exists()){
+                        	dir.mkdirs();
+                        }
+                        ImageIO.write(tag, "JPEG", new File(dir,  "_r" + i + "_c" + j + ".jpg"));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 图像切割（指定切片的宽度和高度）
+     * @param srcImageFile 源图像地址
+     * @param descDir 切片目标文件夹
+     * @param destWidth 目标切片宽度。默认200
+     * @param destHeight 目标切片高度。默认150
+     */
+    public final static void cut3(File src, File dir, int destWidth, int destHeight) {
+        try {
+            if(destWidth<=0) destWidth = 200; // 切片宽度
+            if(destHeight<=0) destHeight = 150; // 切片高度
+            // 读取源图像
+            BufferedImage bi = ImageIO.read(src);
+            int srcWidth = bi.getHeight(); // 源图宽度
+            int srcHeight = bi.getWidth(); // 源图高度
+            if (srcWidth > destWidth && srcHeight > destHeight) {
+                Image img;
+                ImageFilter cropFilter;
+                Image image = bi.getScaledInstance(srcWidth, srcHeight, Image.SCALE_DEFAULT);
+                int cols = 0; // 切片横向数量
+                int rows = 0; // 切片纵向数量
+                // 计算切片的横向和纵向数量
+                if (srcWidth % destWidth == 0) {
+                    cols = srcWidth / destWidth;
+                } else {
+                    cols = (int) Math.floor(srcWidth / destWidth) + 1;
+                }
+                if (srcHeight % destHeight == 0) {
+                    rows = srcHeight / destHeight;
+                } else {
+                    rows = (int) Math.floor(srcHeight / destHeight) + 1;
+                }
+                // 循环建立切片
+                // 改进的想法:是否可用多线程加快切割速度
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < cols; j++) {
+                        // 四个参数分别为图像起点坐标和宽高
+                        // 即: CropImageFilter(int x,int y,int width,int height)
+                        cropFilter = new CropImageFilter(j * destWidth, i * destHeight,
+                                destWidth, destHeight);
+                        img = Toolkit.getDefaultToolkit().createImage(
+                                new FilteredImageSource(image.getSource(),
+                                        cropFilter));
+                        BufferedImage tag = new BufferedImage(destWidth,
+                                destHeight, BufferedImage.TYPE_INT_RGB);
+                        Graphics g = tag.getGraphics();
+                        g.drawImage(img, 0, 0, null); // 绘制缩小后的图
+                        g.dispose();
+
+                        if(!dir.exists()){
+                        	dir.mkdirs();
+                        }
+                        // 输出为文件
+                        ImageIO.write(tag, "JPEG", new File(dir, "_r" + i + "_c" + j + ".jpg"));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 图像类型转换：GIF->JPG、GIF->PNG、PNG->JPG、PNG->GIF(X)、BMP->PNG
+     * @param srcImageFile 源图像地址
+     * @param formatName 包含格式非正式名称的 String：如JPG、JPEG、GIF等
+     * @param destImageFile 目标图像地址
+     */
+    public final static void convert(File src, String formatName, String destImageFile) {
+        try {
+            src.canRead();
+            src.canWrite();
+            BufferedImage img = ImageIO.read(src);
+            ImageIO.write(img, formatName, new File(destImageFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 彩色转为黑白 
+     * @param srcImageFile 源图像地址
+     * @param destImageFile 目标图像地址
+     */
+    public final static void gray(File src, File tar) {
+        try {
+            BufferedImage img = ImageIO.read(src);
+            ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+            ColorConvertOp op = new ColorConvertOp(cs, null);
+            img = op.filter(img, null);
+            ImageIO.write(img, "JPEG", tar);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
