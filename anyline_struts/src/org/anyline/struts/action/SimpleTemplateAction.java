52db7d8921c5035c995ea7f6f304448e9260ba91
admin <admin@anyline.org>
2017-02-26 13:30:42 +0800

    常用业务方法

---
+++ anyline_struts/src/org/anyline/struts/action/SimpleTemplateAction.java
@@ -0,0 +1,141 @@
+/* 
+ * Copyright 2006-2015 www.anyline.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *          AnyLine以及一切衍生库 不得用于任何与网游相关的系统
+ */
+package org.anyline.struts.action;
+
+import org.anyline.config.http.ConfigStore;
+import org.anyline.entity.DataRow;
+import org.anyline.entity.DataSet;
+import org.anyline.util.BasicUtil;
+import org.anyline.util.BeanUtil;
+import org.anyline.util.ConfigTable;
+import org.anyline.util.WebUtil;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+@ParentPackage("anyline-default")
+@Namespace("/al/tmp")
+@Scope("prototype")
+public class SimpleTemplateAction extends TemplateAction {
+
+	/**
+	 * 常用业务方法
+	 */
+
+	protected String table = null;
+	protected String view = null;
+	protected String[] columns = null;
+	protected String[] conditions = null;
+	protected String[] orders = null;
+	protected int page = 0;
+	private void init(){
+		if(BasicUtil.isEmpty(table)){
+			table = (String)BeanUtil.getFieldValue(this, "table");
+		}
+		if(BasicUtil.isEmpty(view)){
+			view = (String)BeanUtil.getFieldValue(this, "view");
+		}
+		if(BasicUtil.isEmpty(columns)){
+			String str = (String)BeanUtil.getFieldValue(this, "columns");
+			if(BasicUtil.isNotEmpty(str)){
+				columns = str.split(",");
+			}
+		}
+		if(BasicUtil.isEmpty(conditions)){
+			String str = (String)BeanUtil.getFieldValue(this, "conditions");
+			if(BasicUtil.isNotEmpty(str)){
+				conditions = str.split(",");
+			}
+		}
+		if(BasicUtil.isEmpty(orders)){
+			String str = (String)BeanUtil.getFieldValue(this, "orders");
+			if(BasicUtil.isNotEmpty(str)){
+				orders = str.split(",");
+			}
+		}
+		if(page == 0){
+			String str = (String)BeanUtil.getFieldValue(this, "page");
+			if(BasicUtil.isEmpty(str)){
+				page =  ConfigTable.getInt("PAGE_DEFAULT_VOL", 10);
+			}else{
+				if("false".equalsIgnoreCase(str)){
+					page = -1;
+				}else if("true".equalsIgnoreCase(str)){
+					page =  ConfigTable.getInt("PAGE_DEFAULT_VOL", 10);
+				}else{
+					page = BasicUtil.parseInt(BeanUtil.getFieldValue(this, "page")+"", ConfigTable.getInt("PAGE_DEFAULT_VOL", 10));
+				}
+			}
+		}
+	}
+
+	@Actions({ @Action(value = "a", results = { @Result(type = "template", location = "/${dir}/info.jsp") }) })
+	public String add(){
+		init();
+		return success();
+	}
+	@Actions({ @Action(value = "l", results = { @Result(type = "template", location = "/${dir}/list.jsp") }) })
+	public String list(){
+		init();
+		ConfigStore config = parseConfig(true);
+		if(BasicUtil.isNotEmpty(orders)){
+			for(String item:orders){
+				config.order(item);
+			}
+		}
+		DataSet set = service.query(view,config);
+		WebUtil.encrypt(set, DataRow.PRIMARY_KEY);
+		request.setAttribute("set",set);
+		return success();
+	}
+	@Actions({ 
+		@Action(value = "v", results = { @Result(type = "template", location = "/${dir}/view.jsp") }) ,
+		@Action(value = "u", results = { @Result(type = "template", location = "/${dir}/info.jsp") }) 
+		})
+	public String info() {
+		init();
+		String pk = DataRow.PRIMARY_KEY.toLowerCase();
+		DataRow row = service.queryRow(view, parseConfig("+"+pk+":"+pk+"-+"+":"+pk+"++"));
+		WebUtil.encrypt(row, DataRow.PRIMARY_KEY);
+		request.setAttribute("row", row);
+		return success();
+	}
+	
+	@Actions({ @Action(value = "s", results = { @Result(type = "redirectAction", params={"actionName","v","cd","${data}"}) }) })
+	public String save(){
+		init();
+		DataRow row = entityRow(columns);
+		service.save(table, row);
+		data = WebUtil.encryptValue(row.getPrimaryValue()+"");
+		return success();
+	}
+
+	@Actions({ @Action(value = "d", results = { @Result(type = "json") }) })
+	public String delete(){
+		init();
+		String pk = DataRow.PRIMARY_KEY.toLowerCase();
+		DataRow row = service.queryRow(table, parseConfig("+"+pk+":"+pk+"-+"+":"+pk+"++"));
+		if(null == row){
+			return fail("数据不存在");
+		}
+		service.delete(row);
+		return success();
+	}
+}
