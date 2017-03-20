bcf81a9dffc0c422345515da7fa6f36f7e2ba17e
admin <admin@anyline.org>
2017-03-20 23:27:23 +0800

    支付宝支付简化版

--- anyline_alipay/src/org/anyline/alipay/util/AliPayConfig.java
+++ anyline_alipay/src/org/anyline/alipay/util/AliPayConfig.java
@@ -12,41 +12,15 @@ import org.dom4j.io.SAXReader;
 
 public class AliPayConfig {
 	private static Logger log = Logger.getLogger(AliPayConfig.class);
-	// 合作身份者ID，签约账号，以2088开头由16位纯数字组成的字符串，查看地址：https://b.alipay.com/order/pidAndKey.htm
-	public static String PARTNER = "";
-	// 收款支付宝账号，以2088开头由16位纯数字组成的字符串，一般情况下收款账号就是签约账号
-	public static String SELLER_ID = "";
-	// MD5密钥，安全检验码，由数字和字母组成的32位字符串，查看地址：https://b.alipay.com/order/pidAndKey.htm
-    public static String KEY = "";
-	// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String NOTIFY_URL = "";
-	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String CALLBACK_URL = "";
-	// 签名方式
-	public static String SIGN_TYPE = "MD5";
-	// 调试用，创建TXT日志文件夹路径，见AlipayCore.java类中的logResult(String sWord)打印方法。
-	public static String LOG_PATH = "d:\\log";
-	// 字符编码格式 目前支持 gbk 或 utf-8
-	public static String INPUT_CHARSET = "utf-8";
-	// 支付类型 ，无需修改
-	public static String PAYMENT_TYPE = "1";
-	// 调用的接口名，无需修改
-	public static String SERVICE_WEB = "create_direct_pay_by_user";
-	public static String SERVICE_WAP = "alipay.wap.create.direct.pay.by.user";
-    /**
-     * 支付宝提供给商家的服务接入网关URL(新)
-     */
-    public static final String ALIPAY_GATEWAY_NEW = "https://mapi.alipay.com/gateway.do?";
-	//应用公钥
-	public static String APP_PUBLIC_KEY = "";
-	//应用私钥
+
 	public static String APP_PRIVATE_KEY = "";
-	//支付宝公钥
-	public static String ALIPAY_PUBLIC_KEY= "";
-//↓↓↓↓↓↓↓↓↓↓ 请在这里配置防钓鱼信息，如果没开通防钓鱼功能，为空即可 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-	public static String ANTI_PHISHING_KEY = "";
-	// 客户端的IP地址 非局域网的外网IP地址，如：221.0.0.1
-	public static String EXTER_INVOKE_IP = "";
+	public static String ALIPAY_PUBLIC_KEY = "";
+	public static String APP_ID = "";
+	public static String DATA_FORMAT = "json";
+	public static String ENCODE = "utf-8";
+	public static String SIGN_TYPE = "RSA";
+	public static String NOTIFY_URL= "";
+	
 	static{
 		init();
 		debug();
@@ -82,34 +56,21 @@ public class AliPayConfig {
 				Element propertyElement = itrProperty.next();
 				String key = propertyElement.attributeValue("key");
 				String value = propertyElement.getTextTrim();
-				if("PARTNER".equalsIgnoreCase(key)){
-					AliPayConfig.PARTNER = value;
-				}else if("SELLER_ID".equalsIgnoreCase(key)){
-					AliPayConfig.SELLER_ID = value;
-				}else if("KEY".equalsIgnoreCase(key)){
-					AliPayConfig.KEY = value;
-				}else if("APP_PUBLIC_KEY".equalsIgnoreCase(key)){
-					AliPayConfig.APP_PUBLIC_KEY = value;
-				}else if("APP_PRIVATE_KEY".equalsIgnoreCase(key)){
+				if("APP_PRIVATE_KEY".equalsIgnoreCase(key)){
 					AliPayConfig.APP_PRIVATE_KEY = value;
 				}else if("ALIPAY_PUBLIC_KEY".equalsIgnoreCase(key)){
 					AliPayConfig.ALIPAY_PUBLIC_KEY = value;
-				}else if("NOTIFY_URL".equalsIgnoreCase(key)){
-					AliPayConfig.NOTIFY_URL = value;
-				}else if("CALLBACK_URL".equalsIgnoreCase(key)){
-					AliPayConfig.CALLBACK_URL = value;
+				}else if("APP_ID".equalsIgnoreCase(key)){
+					AliPayConfig.APP_ID = value;
+				}else if("DATA_FORMAT".equalsIgnoreCase(key)){
+					AliPayConfig.DATA_FORMAT = value;
+				}else if("ENCODE".equalsIgnoreCase(key)){
+					AliPayConfig.ENCODE = value;
 				}else if("SIGN_TYPE".equalsIgnoreCase(key)){
 					AliPayConfig.SIGN_TYPE = value;
-				}else if("LOG_PATH".equalsIgnoreCase(key)){
-					AliPayConfig.LOG_PATH = value;
-				}else if("INPUT_CHARSET".equalsIgnoreCase(key)){
-					AliPayConfig.INPUT_CHARSET = value;
-				}else if("ANTI_PHISHING_KEY".equalsIgnoreCase(key)){
-					AliPayConfig.ANTI_PHISHING_KEY = value;
-				}else if("EXTER_INVOKE_IP".equalsIgnoreCase(key)){
-					AliPayConfig.EXTER_INVOKE_IP = value;
+				}else if("NOTIFY_URL".equalsIgnoreCase(key)){
+					AliPayConfig.NOTIFY_URL = value;
 				}
-				
 				if(ConfigTable.isDebug()){
 					log.info("[解析支付宝配置文件] [" + key + " = " + value+"]");
 				}
--- anyline_alipay/src/org/anyline/alipay/util/AliPayUtil.java
+++ anyline_alipay/src/org/anyline/alipay/util/AliPayUtil.java
@@ -7,114 +7,70 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.domain.AlipayTradeAppPayModel;
+import com.alipay.api.request.AlipayTradeAppPayRequest;
+import com.alipay.api.response.AlipayTradeAppPayResponse;
 
 public class AliPayUtil {
+	public static AlipayClient client = null;
+	static {
+		client = new DefaultAlipayClient(
+				"https://openapi.alipay.com/gateway.do", AliPayConfig.APP_ID,
+				AliPayConfig.APP_PRIVATE_KEY, AliPayConfig.DATA_FORMAT,
+				AliPayConfig.ENCODE, AliPayConfig.ALIPAY_PUBLIC_KEY,
+				AliPayConfig.SIGN_TYPE);
+	}
 
 	/**
 	 * app支付
-	 * @param subject 支付标题
-	 * @param body 支付明细
-	 * @param price 支付价格
-	 * @param order 系统订单号
+	 * 
+	 * @param subject
+	 *            支付标题
+	 * @param body
+	 *            支付明细
+	 * @param price
+	 *            支付价格(元)
+	 * @param order
+	 *            系统订单号
 	 * @return
 	 */
-	public static String createAppOrder(String subject, String body, String price, String order) {
-		String result = "partner=" + "\"" + AliPayConfig.PARTNER + "\""			; // 签约合作者身份ID
-		result += "&seller_id=" + "\"" + AliPayConfig.SELLER_ID + "\""			; // 签约卖家支付宝账号
-		result += "&out_trade_no=" + "\"" + order + "\""						; // 商家网站唯一订单号
-		result += "&subject=" + "\"" + subject + "\""							; // 商品名称
-		result += "&body=" + "\"" + body + "\""									; // 商品详情
-		result += "&total_fee=" + "\"" + price + "\""							; // 商品金额
-		result += "&notify_url=" + "\"" + AliPayConfig.NOTIFY_URL + "\""		; // 服务器异步通知页面路径
-		result += "&service=\"mobile.securitypay.pay\""							; // 服务接口名称， 固定值
-		result += "&payment_type=\"1\""											; // 支付类型， 固定值
-		result += "&_input_charset=\"utf-8\""									; // 参数编码， 固定值
-		result += "&it_b_pay=\"30m\""											; // 有效支付时间
-		result += "&return_url=\""+AliPayConfig.CALLBACK_URL+"\""				; // 支付宝处理完请求后，当前页面跳转到商家指定页面的路径，可空
-		String sign = SignUtils.sign(result, AliPayConfig.APP_PRIVATE_KEY);
+	public static String createAppOrder(String subject, String body,
+			String price, String order) {
+		String result = "";
+		AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
+		AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
+		model.setBody(body);
+		model.setSubject(subject);
+		model.setOutTradeNo(order);
+		model.setTimeoutExpress("30m");
+		model.setTotalAmount(price);
+		request.setBizModel(model);
+		request.setNotifyUrl(AliPayConfig.NOTIFY_URL);
 		try {
-			sign = URLEncoder.encode(sign, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
+			AlipayTradeAppPayResponse response = client.sdkExecute(request);
+			result = response.getBody();
+		} catch (AlipayApiException e) {
 			e.printStackTrace();
 		}
-		result = result + "&sign=\"" + sign + "\"&sign_type=\"RSA\"";
+
 		return result;
 	}
+
 	/**
 	 * html支付
+	 * 
 	 * @param subject
 	 * @param body
 	 * @param price
 	 * @param order
 	 * @return
 	 */
-	public static String createHtmlOrder(String subject, String body, String price, String order){
-		String result ="";
-		Map<String,String> params = createDefaultParam();
-		params.put("out_trade_no", order);
-		params.put("subject", subject);
-		params.put("total_fee", price);
-		params.put("body", body);	
-		params = signParam(params);
-		
-		List<String> keys = new ArrayList<String>(params.keySet());
-        StringBuffer sbHtml = new StringBuffer();
-        sbHtml.append("<form id=\"alipaysubmit\" name=\"alipaysubmit\" action=\"" + AliPayConfig.ALIPAY_GATEWAY_NEW + "_input_charset=" + AliPayConfig.INPUT_CHARSET + "\" method=\"POST\">");
-        for (int i = 0; i < keys.size(); i++) {
-            String name = (String) keys.get(i);
-            String value = (String) params.get(name);
-
-            sbHtml.append("<input type=\"hidden\" name=\"" + name + "\" value=\"" + value + "\"/>");
-        }
-        sbHtml.append("<input type=\"submit\" value=\"Pay\" style=\"display:none;\"></form>");
-        sbHtml.append("<script>document.forms['alipaysubmit'].submit();</script>");
-
+	public static String createHtmlOrder(String subject, String body,
+			String price, String order) {
+		String result = "";
 		return result;
-	}
-	 private static Map<String, String> signParam(Map<String, String> params) {
-	        //除去数组中的空值和签名参数
-	        Map<String, String> result = AlipayCore.paraFilter(params);
-	        //生成签名结果
-	        String sign = buildRequestMysign(result);
-	        //签名结果与签名方式加入请求提交参数组中
-	        result.put("sign", sign);
-	        params.put("sign", sign);
-	        result.put("sign_type", AliPayConfig.SIGN_TYPE);
-
-	        return result;
-	    }
-	  /**
-	     * 生成签名结果
-	     * @param sPara 要签名的数组
-	     * @return 签名结果字符串
-	     */
-		public static String buildRequestMysign(Map<String, String> sPara) {
-	    	String prestr = AlipayCore.createLinkString(sPara); //把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
-	        String mysign = "";
-	        if(AliPayConfig.SIGN_TYPE.equalsIgnoreCase("MD5") ) {
-	        	mysign = MD5.sign(prestr, AliPayConfig.KEY, AliPayConfig.INPUT_CHARSET);
-	        }
-	        return mysign;
-	    }
-	/**
-	 * 封装默认参数
-	 * @return
-	 */
-	public static Map<String,String> createDefaultParam(){
-		return createDefaultParam(null);
-	}
-	public static Map<String,String> createDefaultParam(Map<String,String> params){
-		if(null == params){
-			params = new HashMap<String,String>();
-		}
-    	params.put("partner", AliPayConfig.PARTNER);
-    	params.put("seller_id", AliPayConfig.SELLER_ID);
-    	params.put("_input_charset", AliPayConfig.INPUT_CHARSET);
-    	params.put("payment_type", AliPayConfig.PAYMENT_TYPE);
-    	params.put("notify_url", AliPayConfig.NOTIFY_URL);
-    	params.put("return_url", AliPayConfig.CALLBACK_URL);
-    	params.put("anti_phishing_key", AliPayConfig.ANTI_PHISHING_KEY);
-    	params.put("exter_invoke_ip", AliPayConfig.EXTER_INVOKE_IP);
-    	return params;
 	}
 }
