e475f9a1ff8cd7513f5ab7a24adb96918befe885
admin <admin@anyline.org>
2017-04-27 10:07:09 +0800

    阿里云短信多帐号发送

--- anyline_sms/src/org/anyline/sms/SMSConfig.java
+++ anyline_sms/src/org/anyline/sms/SMSConfig.java
@@ -1,8 +1,12 @@
 package org.anyline.sms;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.anyline.util.BasicUtil;
 import org.anyline.util.ConfigTable;
@@ -14,14 +18,17 @@ import org.dom4j.io.SAXReader;
 
 public class SMSConfig {
 	private static Logger log = Logger.getLogger(SMSConfig.class);
+	private static Hashtable<String, SMSConfig> instances = new Hashtable<String,SMSConfig>();
+	private Map<String,String> kvs = new HashMap<String,String>();
+	
 	//服务器配置
-	public static String ACCESS_KEY= "";
-	public static String ACCESS_SECRET = "";
-	public static String SMS_SIGN = "";
+	public String ACCESS_KEY= "";
+	public String ACCESS_SECRET = "";
+	public String SMS_SIGN = "";
 	//客户端配置
-	public static String SMS_SERVER = "";
-	public static String CLIENT_APP= "";
-	public static String CLIENT_SECRET= "";
+	public String SMS_SERVER = "";
+	public String CLIENT_APP= "";
+	public String CLIENT_SECRET= "";
 	static{
 		init();
 		debug();
@@ -29,6 +36,16 @@ public class SMSConfig {
 	public static void init() {
 		//加载配置文件
 		loadConfig();
+	}
+
+	public static SMSConfig getInstance(){
+		return instances.get("default");
+	}
+	public static SMSConfig getInstance(String key){
+		return instances.get(key);
+	}
+	public String getString(String key){
+		return kvs.get(key);
 	}
 	/**
 	 * 加载配置文件
@@ -58,30 +75,49 @@ public class SMSConfig {
 			SAXReader reader = new SAXReader();
 			Document document = reader.read(file);
 			Element root = document.getRootElement();
-			for(Iterator<Element> itrProperty=root.elementIterator("property"); itrProperty.hasNext();){
-				Element propertyElement = itrProperty.next();
-				String key = propertyElement.attributeValue("key");
-				String value = propertyElement.getTextTrim();
-				if("ACCESS_KEY".equalsIgnoreCase(key)){
-					ACCESS_KEY = value;
-				}else if("ACCESS_SECRET".equalsIgnoreCase(key)){
-					ACCESS_SECRET = value;
-				}else if("SMS_SIGN".equalsIgnoreCase(key)){
-					SMS_SIGN = value;
-				}else if("SMS_SERVER".equalsIgnoreCase(key)){
-					SMS_SERVER = value;
-				}else if("CLIENT_APP".equalsIgnoreCase(key)){
-					CLIENT_APP = value;
-				}else if("CLIENT_SECRET".equalsIgnoreCase(key)){
-					CLIENT_SECRET = value;
+
+			for(Iterator<Element> itrConfig=root.elementIterator("config"); itrConfig.hasNext();){
+				SMSConfig config = new SMSConfig();
+				Element configElement = itrConfig.next();
+				String configKey = configElement.attributeValue("key");
+				if(BasicUtil.isEmpty(configKey)){
+					configKey = "default";
 				}
-				
-				if(ConfigTable.isDebug()){
-					log.info("[解析SMS配置文件] [" + key + " = " + value+"]");
+				Map<String,String> kvs = new HashMap<String,String>();
+				for(Iterator<Element> itrProperty=configElement.elementIterator("property"); itrProperty.hasNext();){
+					Element propertyElement = itrProperty.next();
+					String key = propertyElement.attributeValue("key");
+					String value = propertyElement.getTextTrim();
+					if(ConfigTable.isDebug()){
+						log.info("[解析SMS配置文件][key = "  + key + "][" + key + " = " + value+"]");
+					}
+					kvs.put(key, value);
 				}
+				config.kvs = kvs;
+				config.setFieldValue();
+				instances.put(configKey, config);
 			}
 		}catch(Exception e){
 			log.error("配置文件解析异常:"+e);
+		}
+	}
+
+	private void setFieldValue(){
+		Field[] fields = this.getClass().getDeclaredFields();
+		for(Field field:fields){
+			if(field.getType().getName().equals("java.lang.String")){
+				String name = field.getName();
+				try {
+					String value = kvs.get(name);
+					if(BasicUtil.isNotEmpty(value)){
+						field.set(this, kvs.get(name));
+					}
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	private static void debug(){
