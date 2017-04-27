1944a31af78a46bbd8615dbff7bcdd498f565cef
admin <admin@anyline.org>
2017-04-27 10:07:09 +0800

    阿里云短信多帐号发送

--- anyline_sms/src/org/anyline/sms/SMSClient.java
+++ anyline_sms/src/org/anyline/sms/SMSClient.java
@@ -1,9 +1,11 @@
 package org.anyline.sms;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
+import org.anyline.util.BasicUtil;
 import org.anyline.util.BeanUtil;
 import org.anyline.util.ConfigTable;
 import org.anyline.util.HttpClientUtil;
@@ -17,32 +19,40 @@ import org.apache.log4j.Logger;
  */
 public class SMSClient {
 	private static Logger log = Logger.getLogger(SMSClient.class);
-    
-	private static String sms_server = SMSConfig.SMS_SERVER;
-	private String app = SMSConfig.CLIENT_APP;
-	private String secret = SMSConfig.CLIENT_SECRET;
-	private static SMSClient defaultClient = null;
-	public synchronized static SMSClient defaultClient(){
-		if(null == defaultClient){
-			defaultClient = new SMSClient();
-			if(ConfigTable.isDebug()){
-				log.warn("[SMS][create new client]");
-			}
-		}
-		return defaultClient;
-	}
+    private SMSConfig config = null;
+	private static Hashtable<String,SMSClient> instances = new Hashtable<String,SMSClient>();
 	
+//	private static String sms_server = SMSConfig.SMS_SERVER;
+//	private String app = SMSConfig.CLIENT_APP;
+//	private String secret = SMSConfig.CLIENT_SECRET;
+
+	public static SMSClient getInstance(){
+		return getInstance("default");
+	}
+	public static SMSClient getInstance(String key){
+		if(BasicUtil.isEmpty(key)){
+			key = "default";
+		}
+		SMSClient client = instances.get(key);
+		if(null == client){
+			client = new SMSClient();
+			SMSConfig config = SMSConfig.getInstance(key);
+			client.config = config;
+			instances.put(key, client);
+		}
+		return client;
+	}
 	public SMSResult send(String sign, String template, String mobile, Map<String, String> params) {
 		SMSResult result = null;
 		try{
 			Map<String,String> map = new HashMap<String,String>();
-			map.put("_client_app", app);
-			map.put("_client_secret", secret);
+			map.put("_client_app", config.CLIENT_APP);
+			map.put("_client_secret", config.CLIENT_SECRET);
 			map.put("_sms_sign", sign);
 			map.put("_sms_template", template);
 			map.put("_sms_mobile", mobile);
 			map.put("_sms_param", BeanUtil.map2json(params));
-			String txt = HttpClientUtil.get(sms_server, "UTF-8", map).getText();
+			String txt = HttpClientUtil.get(config.SMS_SERVER, "UTF-8", map).getText();
 			result = BeanUtil.json2oject(txt, SMSResult.class);
 			if(ConfigTable.isDebug()){
 				log.warn("[SMS SEND][mobile:"+mobile+"][result:"+txt+"]");
@@ -67,10 +77,10 @@ public class SMSClient {
 	}
 
 	public SMSResult send(String template, String mobile, Map<String, String> params) {
-		return send(SMSConfig.SMS_SIGN, template, mobile, params);
+		return send(config.SMS_SIGN, template, mobile, params);
 	}
 	public SMSResult send(String template, List<String> mobile, Map<String, String> params) {
-		return send(SMSConfig.SMS_SIGN, template, mobile, params);
+		return send(config.SMS_SIGN, template, mobile, params);
 	}
 
 }
