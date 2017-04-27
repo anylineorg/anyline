2ece2791f201dd7e61f4e596f536239dc55becac
admin <admin@anyline.org>
2017-04-27 11:08:06 +0800

    环信多账号发送

--- anyline_easemob/src/org/anyline/easemob/EasemobConfig.java
+++ anyline_easemob/src/org/anyline/easemob/EasemobConfig.java
@@ -1,10 +1,14 @@
 package org.anyline.easemob;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-import org.anyline.entity.DataRow;
+import org.anyline.util.BasicUtil;
 import org.anyline.util.ConfigTable;
 import org.anyline.util.FileUtil;
 import org.apache.log4j.Logger;
@@ -15,12 +19,16 @@ import org.dom4j.io.SAXReader;
 
 public class EasemobConfig {
 	private static Logger log = Logger.getLogger(EasemobConfig.class);
-	public static String HOST = "";
-	public static String CLIENT_ID ="";
-	public static String CLIENT_SECRET ="";
-	public static String ORG_NAME ="";
-	public static String APP_NAME ="";
-
+	private static Hashtable<String,EasemobConfig> instances = new Hashtable<String,EasemobConfig>();
+	private Map<String,String> kvs = new HashMap<String,String>();
+	
+	public String HOST = "";
+	public String CLIENT_ID ="";
+	public String CLIENT_SECRET ="";
+	public String ORG_NAME ="";
+	public String APP_NAME ="";
+	
+	
 		
 	static{
 		init();
@@ -29,6 +37,15 @@ public class EasemobConfig {
 	public static void init() {
 		//加载配置文件
 		loadConfig();
+	}
+	public static EasemobConfig getInstance(){
+		return getInstance("default");
+	}
+	public static EasemobConfig getInstance(String key){
+		if(BasicUtil.isEmpty(key)){
+			key = "default";
+		}
+		return instances.get(key);
 	}
 	/**
 	 * 加载配置文件
@@ -56,32 +73,50 @@ public class EasemobConfig {
 			SAXReader reader = new SAXReader();
 			Document document = reader.read(file);
 			Element root = document.getRootElement();
-			for(Iterator<Element> itrProperty=root.elementIterator("property"); itrProperty.hasNext();){
-				Element propertyElement = itrProperty.next();
-				String key = propertyElement.attributeValue("key");
-				String value = propertyElement.getTextTrim();
-				if("CLIENT_ID".equalsIgnoreCase(key)){
-					EasemobConfig.CLIENT_ID = value;
-				}else if("CLIENT_SECRET".equalsIgnoreCase(key)){
-					EasemobConfig.CLIENT_SECRET = value;
-				}else if("ORG_NAME".equalsIgnoreCase(key)){
-					EasemobConfig.ORG_NAME = value;
-				}else if("APP_NAME".equalsIgnoreCase(key)){
-					EasemobConfig.APP_NAME = value;
-				}else if("HOST".equalsIgnoreCase(key)){
-					EasemobConfig.HOST = value;
+			for(Iterator<Element> itrConfig=root.elementIterator("config"); itrConfig.hasNext();){
+				EasemobConfig config = new EasemobConfig();
+				Element configElement = itrConfig.next();
+				String configKey = configElement.attributeValue("key");
+				if(BasicUtil.isEmpty(configKey)){
+					configKey = "default";
 				}
-				if(ConfigTable.isDebug()){
-					log.info("[解析环信配置文件] [" + key + " = " + value+"]");
+				Map<String,String> kvs = new HashMap<String,String>();
+				for(Iterator<Element> itrProperty=configElement.elementIterator("property"); itrProperty.hasNext();){
+					Element propertyElement = itrProperty.next();
+					String key = propertyElement.attributeValue("key");
+					String value = propertyElement.getTextTrim();
+					log.info("[解析环信配置文件][key = " + configKey + "] [" + key + " = " + value+"]");
+					kvs.put(key, value);
 				}
+				config.kvs = kvs;
+				config.setFieldValue();
+				instances.put(configKey, config);
 			}
 		}catch(Exception e){
 			log.error("配置文件解析异常:"+e);
 		}
 	}
-	private static void debug(){
+	private void setFieldValue(){
+		Field[] fields = this.getClass().getDeclaredFields();
+		for(Field field:fields){
+			if(field.getType().getName().equals("java.lang.String")){
+				String name = field.getName();
+				try {
+					String value = kvs.get(name);
+					if(BasicUtil.isNotEmpty(value)){
+						field.set(this, kvs.get(name));
+					}
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
-	public static void main(String args[]){
-		debug();
+	public String getString(String key){
+		return kvs.get(key);
+	}
+	private static void debug(){
 	}
 }
