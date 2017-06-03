e4b08b89389f8d48d9edd0da42534558378f4bff
admin <admin@anyline.org>
2017-06-03 19:34:23 +0800

    QQ支付

--- anyline_qq/src/org/anyline/qq/pay/QQPayConfig.java
+++ anyline_qq/src/org/anyline/qq/pay/QQPayConfig.java
@@ -21,9 +21,10 @@ public class QQPayConfig extends BasicConfig{
 	 * 服务号相关信息
 	 */
 	public String APP_ID = ""				; //AppID(应用ID)
+	public String APP_KEY = ""				; //APPKEY(应用密钥)
 	public String APP_SECRECT = ""			; //AppSecret(应用密钥)
 	public String API_SECRECT = ""			; //商家平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置
-	public String PAY_MCH_ID = ""			; //商家号
+	public String MCH_ID = ""				; //商家号
 	public String SIGN_TYPE = ""			; //签名加密方式
 	public String SERVER_TOKEN = ""			; //服务号的配置token
 	public String CERT_PATH = ""			; //支付证书存放路径地址
--- anyline_qq/src/org/anyline/qq/pay/QQPayUtil.java
+++ anyline_qq/src/org/anyline/qq/pay/QQPayUtil.java
@@ -1,7 +1,12 @@
 package org.anyline.qq.pay;
 
+import java.util.Base64;
 import java.util.Hashtable;
 import java.util.Map;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.anyline.qq.pay.entity.QQPayOrder;
 import org.anyline.qq.pay.entity.QQPayOrderResult;
@@ -43,7 +48,7 @@ public class QQPayUtil {
 			order.setAppid(config.APP_ID);
 		}
 		if(BasicUtil.isEmpty(order.getMch_id())){
-			order.setMch_id(config.PAY_MCH_ID);
+			order.setMch_id(config.MCH_ID);
 		}
 		if(BasicUtil.isEmpty(order.getNotify_url())){
 			order.setNotify_url(config.PAY_NOTIFY_URL);
@@ -74,7 +79,7 @@ public class QQPayUtil {
 	}
 	/**
 	 * 签名
-	 * 
+	 * 统一下单签名
 	 * @param params
 	 * @return
 	 */
@@ -84,5 +89,39 @@ public class QQPayUtil {
 		sign += "&key=" + config.API_SECRECT;
 		sign = MD5Util.crypto(sign).toUpperCase();
 		return sign;
+	}
+
+	/**
+	 * APP 调用起支付签名
+	 * @param nonce	随机串
+	 * @param token 预支付ID
+	 * @return
+	 */
+	public String appSign(String nonce, String token){
+		String result = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("appId=").append(config.APP_ID);
+        stringBuilder.append("&bargainorId=").append(config.MCH_ID);
+        stringBuilder.append("&nonce=").append(nonce);
+        stringBuilder.append("&pubAcc=").append("");
+        stringBuilder.append("&tokenId=").append(token);
+
+        try {
+			byte[] byteKey = (QQPayConfig.getInstance().APP_KEY+"&").getBytes("UTF-8");
+        // 根据给定的字节数组构造一个密钥,第二参数指定一个密钥算法的名称
+        SecretKey secretKey = new SecretKeySpec(byteKey, "HmacSHA1");
+        // 生成一个指定 Mac 算法 的 Mac 对象
+        Mac mac = Mac.getInstance("HmacSHA1");
+        // 用给定密钥初始化 Mac 对象
+        mac.init(secretKey);
+        byte[] byteSrc = stringBuilder.toString().getBytes("UTF-8");
+        // 完成 Mac 操作
+        byte[] dst = mac.doFinal(byteSrc);
+        // Base64
+        result = Base64.getEncoder().encodeToString(dst);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
