0c140b28b75e7dfff3624425dea86fbbea069645
admin <admin@anyline.org>
2017-09-07 18:13:30 +0800

    微信H5支付

--- anyline_core/src/org/anyline/util/BasicConfig.java
+++ anyline_core/src/org/anyline/util/BasicConfig.java
@@ -14,6 +14,31 @@ import org.dom4j.io.SAXReader;
 
 public class BasicConfig {
 	protected static Logger log = Logger.getLogger(BasicConfig.class);
+	public static final String TRADE_TYPE_JSAPI 		= "JSAPI"	;//公众号支付	
+	public static final String TRADE_TYPE_NATIVE 		= "NATIVE"	;//原生扫码支付
+	public static final String TRADE_TYPE_APP 			= "APP"		;//app支付
+	public static final String TRADE_TYPE_MICROPAY 		= "MICROPAY";//刷卡支付
+	public static final String TRADE_TYPE_WAP 			= "MWEB"	;//H5支付
+	/**
+	 * 微信支付接口地址
+	 */
+	//微信支付统一接口(POST)
+	public final static String UNIFIED_ORDER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+	//微信退款接口(POST)
+	public final static String REFUND_URL = "https://api.mch.weixin.qq.com/secapi/pay/refund";
+	//订单查询接口(POST)
+	public final static String CHECK_ORDER_URL = "https://api.mch.weixin.qq.com/pay/orderquery";
+	//关闭订单接口(POST)
+	public final static String CLOSE_ORDER_URL = "https://api.mch.weixin.qq.com/pay/closeorder";
+	//退款查询接口(POST)
+	public final static String CHECK_REFUND_URL = "https://api.mch.weixin.qq.com/pay/refundquery";
+	//对账单接口(POST)
+	public final static String DOWNLOAD_BILL_URL = "https://api.mch.weixin.qq.com/pay/downloadbill";
+	//短链接转换接口(POST)
+	public final static String SHORT_URL = "https://api.mch.weixin.qq.com/tools/shorturl";
+	//接口调用上报接口(POST)
+	public final static String REPORT_URL = "https://api.mch.weixin.qq.com/payitil/report";
+
 	protected Map<String,String> kvs = new HashMap<String,String>();
 	protected static Hashtable<String,BasicConfig> parseFile(Class T, File file, Hashtable<String,BasicConfig> instances){
 		if(null == file || !file.exists()){
