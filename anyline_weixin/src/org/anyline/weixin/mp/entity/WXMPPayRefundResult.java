fe90bfd3c7b9165d61489c722f836520d8675c0c
zhang <zhang@anyline.org>
2018-06-12 17:05:47 +0800

    微信(公众号 APP)退款过程日志

--- anyline_weixin/src/org/anyline/weixin/mp/entity/WXMPPayRefundResult.java
+++ anyline_weixin/src/org/anyline/weixin/mp/entity/WXMPPayRefundResult.java
@@ -5,4 +5,13 @@ import org.anyline.weixin.entity.RefundResult;
 
 
 public class WXMPPayRefundResult  extends RefundResult{
+	public WXMPPayRefundResult(){
+		super();
+	}
+	public WXMPPayRefundResult(boolean result){
+		super(result, null);
+	}
+	public WXMPPayRefundResult(boolean result, String msg){
+		super(result, msg);
+	}
 }
