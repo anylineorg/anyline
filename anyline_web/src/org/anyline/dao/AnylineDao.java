b63b6ee727629c0eec57662f019da060bb8158ad
admin <admin@anyline.org>
2018-07-12 21:07:41 +0800

    数据源切换方式

--- anyline_web/src/org/anyline/dao/AnylineDao.java
+++ anyline_web/src/org/anyline/dao/AnylineDao.java
@@ -20,10 +20,6 @@
 package org.anyline.dao;
 
 import java.util.Collection;
-import java.util.List;
-
-import javax.sql.DataSource;
-
 import org.anyline.config.db.Procedure;
 import org.anyline.config.db.SQL;
 import org.anyline.config.http.ConfigStore;
@@ -35,19 +31,13 @@ public interface AnylineDao{
 	 * @param sql
 	 * @return
 	 */
-	public DataSet query(DataSource ds, SQL sql, ConfigStore configs, String ... conditions);
 	public DataSet query(SQL sql, ConfigStore configs, String ... conditions);
-	public DataSet query(DataSource ds, SQL sql, String ... conditions);
 	public DataSet query(SQL sql, String ... conditions);
 
-	public int count(DataSource ds, SQL sql, ConfigStore configs, String ... conditions);
 	public int count(SQL sql, ConfigStore configs, String ... conditions);
-	public int count(DataSource ds, SQL sql, String ... conditions);
 	public int count(SQL sql, String ... conditions);
 	
-	public boolean exists(DataSource ds, SQL sql, ConfigStore configs, String ... conditions);
 	public boolean exists(SQL sql, ConfigStore configs, String ... conditions);
-	public boolean exists(DataSource ds, SQL sql, String ... conditions);
 	public boolean exists(SQL sql, String ... conditions);
 	/**
 	 * 更新
@@ -59,9 +49,7 @@ public interface AnylineDao{
 	 * 			表
 	 * @return
 	 */
-	public int update(DataSource ds, String dst, Object data, String ... columns);
 	public int update(String dst, Object data, String ... columns);
-	public int update(DataSource ds, Object data, String ... columns);
 	public int update(Object data, String ... columns);
 	
 	/**
@@ -76,23 +64,15 @@ public interface AnylineDao{
 	 * 			表
 	 * @return
 	 */
-	public int insert(DataSource ds, String dst, Object data, boolean checkPrimary, String ... columns);
 	public int insert(String dst, Object data, boolean checkPrimary, String ... columns);
-	public int insert(DataSource ds, Object data, boolean checkPrimary, String ... columns);
 	public int insert(Object data, boolean checkPrimary, String ... columns);
-	public int insert(DataSource ds, String dst, Object data, String ... columns);
 	public int insert(String dst, Object data, String ... columns);
-	public int insert(DataSource ds, Object data, String ... columns);
 	public int insert(Object data, String ... columns);
 	
 
-	public int batchInsert(DataSource ds, String dst, Object data, boolean checkPrimary, String ... columns);
 	public int batchInsert(String dst, Object data, boolean checkPrimary, String ... columns);
-	public int batchInsert(DataSource ds, Object data, boolean checkPrimary, String ... columns);
 	public int batchInsert(Object data, boolean checkPrimary, String ... columns);
-	public int batchInsert(DataSource ds, String dst, Object data, String ... columns);
 	public int batchInsert(String dst, Object data, String ... columns);
-	public int batchInsert(DataSource ds, Object data, String ... columns);
 	public int batchInsert(Object data, String ... columns);
 	/**
 	 * 保存(insert|update)
@@ -101,21 +81,15 @@ public interface AnylineDao{
 	 * @param columns
 	 * @return
 	 */
-	public int save(DataSource ds, String dst, Object data, boolean checkPrimary, String ... columns);
 	public int save(String dst, Object data, boolean checkPrimary, String ... columns);
-	public int save(DataSource ds, Object data, boolean checkPrimary, String ... columns);
 	public int save(Object data, boolean checkPrimary, String ... columns);
-	public int save(DataSource ds, String dst, Object data, String ... columns);
 	public int save(String dst, Object data, String ... columns);
-	public int save(DataSource ds, Object data, String ... columns);
 	public int save(Object data, String ... columns);
 
 
-	public int execute(DataSource ds, SQL sql, ConfigStore configs, String ... conditions);
-	public int execute(DataSource ds, SQL sql, String ... conditions);
-	
 	public int execute(SQL sql, ConfigStore configs, String ... conditions);
 	public int execute(SQL sql, String ... conditions);
+	
 	
 
 
@@ -126,7 +100,6 @@ public interface AnylineDao{
 	 * @param outputs
 	 * @return
 	 */
-	public boolean executeProcedure(DataSource ds, Procedure procedure);
 	public boolean executeProcedure(Procedure procedure);
 	/**
 	 * 根据存储过程查询
@@ -134,9 +107,8 @@ public interface AnylineDao{
 	 * @param inputs
 	 * @return
 	 */
-	public DataSet queryProcedure(DataSource ds, Procedure procedure);
 	public DataSet queryProcedure(Procedure procedure);
-	public int delete(DataSource ds, String dest, Object data, String ... columns);
-	public int delete(DataSource ds, String table, String key, Collection<Object> values);
-	public int delete(DataSource ds, String table, String key, String ... values);
+	public int delete(String dest, Object data, String ... columns);
+	public int delete(String table, String key, Collection<Object> values);
+	public int delete(String table, String key, String ... values);
 }
