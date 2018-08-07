81eb143168db4021a7d2b3360a43af10eae46a35
admin <admin@anyline.org>
2018-08-07 08:04:38 +0800

    多版本数据库支持

--- anyline_core/src/org/anyline/util/SpringContextUtil.java
+++ anyline_core/src/org/anyline/util/SpringContextUtil.java
@@ -1,17 +1,20 @@
 package org.anyline.util;
 
 
+import java.util.Map;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SpringContextUtil implements ApplicationContextAware {
 	
 	private static ApplicationContext applicationContext; 
 	
 	@Override
-	public void setApplicationContext(ApplicationContext ac)
-			throws BeansException {
+	public void setApplicationContext(ApplicationContext ac) throws BeansException {
 		 SpringContextUtil.applicationContext = ac;  
 	}
 	
@@ -21,5 +24,11 @@ public class SpringContextUtil implements ApplicationContextAware {
 	
     public static Object getBean(String name) throws BeansException {  
         return applicationContext.getBean(name);  
-    }  
+    } 
+    public static <T> Map<String,T> getBeans(Class<T> clazz) throws BeansException{
+    	return applicationContext.getBeansOfType(clazz);
+    } 
+    public static <T> T getBean(Class<T> clazz) throws BeansException{
+    	return applicationContext.getBean(clazz);
+    } 
 }
