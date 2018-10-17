aef7321164fd92750fb75dfe3bfc8408db5375d4
admin <admin@anyline.org>
2018-10-17 11:20:48 +0800

    config

--- anyline_weixin/src/org/anyline/weixin/mp/util/WXMPConfig.java
+++ anyline_weixin/src/org/anyline/weixin/mp/util/WXMPConfig.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.Hashtable;
 import java.util.List;
 
+import org.anyline.entity.DataRow;
 import org.anyline.util.BasicConfig;
 import org.anyline.util.BasicUtil;
 import org.anyline.util.ConfigTable;
@@ -12,23 +13,25 @@ import org.anyline.util.FileUtil;
 
 public class WXMPConfig extends BasicConfig{
 	private static Hashtable<String,BasicConfig> instances = new Hashtable<String,BasicConfig>();
-	
+	public static void main(String args[]){
+		parse(WXMPConfig.class, "", new DataRow(),null);
+	}
 	/**
 	 * 服务号相关信息
 	 */
-	public String APP_ID = ""					; //AppID(应用ID)
-	public String APP_SECRECT = ""				; //AppSecret(应用密钥)
-	public String SIGN_TYPE = ""				; //签名加密方式
-	public String SERVER_TOKEN = ""				; //服务号的配置token
-	//public String CERT_PATH = ""				; //微信支付证书存放路径地址
-	public String OAUTH_REDIRECT_URL = ""		; //oauth2授权时回调action
-	public String WEB_SERVER = ""				; 
-	public String PAY_API_SECRECT = ""			; //微信商家平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置
-	public String PAY_MCH_ID = ""				; //商家号
-	public String PAY_NOTIFY_URL = ""			; //微信支付统一接口的回调action
-	public String PAY_CALLBACK_URL = ""			; //微信支付成功支付后跳转的地址
-	public String PAY_KEY_STORE_FILE = ""		; //证书文件
-	public String PAY_KEY_STORE_PASSWORD = ""	; //证书密码
+	public String APP_ID 					= "" ; //AppID(应用ID)
+	public String APP_SECRECT 				= "" ; //AppSecret(应用密钥)
+	public String SIGN_TYPE 				= "" ; //签名加密方式
+	public String SERVER_TOKEN 				= "" ; //服务号的配置token
+	//public String CERT_PATH 				= "" ; //微信支付证书存放路径地址
+	public String OAUTH_REDIRECT_URL 		= "" ; //oauth2授权时回调action
+	public String WEB_SERVER 				= "" ; 
+	public String PAY_API_SECRECT 			= "" ; //微信商家平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置
+	public String PAY_MCH_ID 				= "" ; //商家号
+	public String PAY_NOTIFY_URL 			= "" ; //微信支付统一接口的回调action
+	public String PAY_CALLBACK_URL 			= "" ; //微信支付成功支付后跳转的地址
+	public String PAY_KEY_STORE_FILE 		= "" ; //证书文件
+	public String PAY_KEY_STORE_PASSWORD 	= "" ; //证书密码
 	
 	
 	static{
@@ -39,7 +42,6 @@ public class WXMPConfig extends BasicConfig{
 		//加载配置文件
 		loadConfig();
 	}
-
 	public static WXMPConfig getInstance(){
 		return getInstance("default");
 	}
