316fd6ec928803c9eec0a03e4d2ad217271910c7
admin <admin@anyline.org>
2018-10-18 08:47:05 +0800

    交易参数说明

--- anyline_weixin/src/org/anyline/weixin/entity/TradeOrder.java
+++ anyline_weixin/src/org/anyline/weixin/entity/TradeOrder.java
@@ -3,30 +3,28 @@ package org.anyline.weixin.entity;
 import org.anyline.weixin.WXBasicConfig.TRADE_TYPE;
 
 public class TradeOrder {
-	//配置文件中已设置
-	protected String appid;
-	protected String mch_id;
-	protected String notify_url;
-	//必选
-	protected String nonce_str;
-	protected String sign;
-	protected String body;
-	protected String out_trade_no;		//流水号　notify时原样回传　用来对应TRADE_CALL(常用格式 CALL_ID + _ + ORDER_ID)
-	protected String total_fee;			//支付金额(分)
-	protected String spbill_create_ip;
-	protected String trade_type;
-	
-	//可选
-	protected String device_info;
-	protected String sign_type;
-	protected String detail;
-	protected String attach;
-	protected String fee_type;
-	protected String time_start;
-	protected String time_expire;
-	protected String goods_tag;
-	protected String limit_pay;
-	protected String scene_info;
+	protected String appid				; //公众号ID(conf)	是	String(32)	wxd678efh567hg6787	微信支付分配的公众账号ID（企业号corpid即为此appId）
+	protected String mch_id				; //商户号(conf)		是	String(32)	1230000109	微信支付分配的商户号
+	protected String nonce_str			; //随机字符串(auto)	是	String(32)	5K8264ILTKCH16CQ2502SI8ZNMTM67VS	随机字符串，长度要求在32位以内。推荐随机数生成算法
+	protected String sign				; //签名(auto)		是	String(32)	C380BEC2BFD727A4B6845133519F3AD6	通过签名算法计算得出的签名值，详见签名生成算法
+	protected String body				; //商品描述			是	String(128)	腾讯充值中心-QQ会员充值	商品简单描述，该字段请按照规范传递，具体请见参数规定
+	protected String out_trade_no		; //商户订单号			是	String(32)	notify时原样回传　用来对应TRADE_CALL(常用格式 CALL_ID + _ + ORDER_ID) 20150806125346	商户系统内部订单号，要求32个字符内，只能是数字、大小写字母_-|* 且在同一个商户号下唯一。详见商户订单号
+	protected String total_fee			; //标价金额			是	Int			88	订单总金额，单位为分，详见支付金额
+	protected String spbill_create_ip	; //终端IP			是	String(16)	123.12.12.123	APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP。
+	protected String notify_url			; //通知地址			是	String(256)	http://www.weixin.qq.com/wxpay/pay.php	异步接收微信支付结果通知的回调地址，通知url必须为外网可访问的url，不能携带参数。
+	protected String trade_type			; //交易类型			是	String(16)	JSAPI	 JSAPI 公众号支付 NATIVE 扫码支付 APP APP支付
+	protected String sign_type			; //签名类型			否	String(32)	MD5	签名类型，默认为MD5，支持HMAC-SHA256和MD5。
+	protected String device_info		; //设备号			否	String(32)	013467007045764	自定义参数，可以为终端设备号(门店号或收银设备ID)，PC网页或公众号内支付可以传"WEB"
+	protected String detail				; //商品详情			否	String(6000)商品详细描述，对于使用单品优惠的商户，改字段必须按照规范上传，详见“单品优惠参数说明”
+	protected String attach				; //附加数据			否	String(127)	深圳分店	附加数据，在查询API和支付通知中原样返回，可作为自定义参数使用。
+	protected String fee_type			; //标价币种			否	String(16)	CNY	符合ISO 4217标准的三位字母代码，默认人民币：CNY，详细列表请参见货币类型
+	protected String time_start			; //交易起始时间		否	String(14)	20091225091010	订单生成时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010。其他详见时间规则
+	protected String time_expire		; //交易结束时间		否	String(14)	20091227091010	订单失效时间，格式为yyyyMMddHHmmss，如2009年12月27日9点10分10秒表示为20091227091010。订单失效时间是针对订单号而言的，由于在请求支付的时候有一个必传参数prepay_id只有两小时的有效期，所以在重入时间超过2小时的时候需要重新请求下单接口获取新的prepay_id。其他详见时间规则建议：最短失效时间间隔大于1分钟
+	protected String goods_tag			; //订单优惠标记		否	String(32)	WXG	订单优惠标记，使用代金券或立减优惠功能时需要的参数，说明详见代金券或立减优惠
+	protected String product_id			; //商品ID			否	String(32)	12235413214070356458058	trade_type=NATIVE时（即扫码支付），此参数必传。此参数为二维码中包含的商品ID，商户自行定义。
+	protected String limit_pay			; //指定支付方式		否	String(32)	no_credit	上传此参数no_credit--可限制用户不能使用信用卡支付
+	protected String openid				; //用户标识			否	String(128)	oUpF8uMuAJO_M2pxb1Q9zNjWeS6o	trade_type=JSAPI时（即公众号支付），此参数必传，此参数为微信用户在商户对应appid下的唯一标识。openid如何获取，可参考【获取openid】。企业号请使用【企业号OAuth2.0接口】获取企业号内成员userid，再调用【企业号userid转openid接口】进行转换
+	protected String scene_info			; //+场景信息			否	String(256)	{"store_info" : {"id": "SZTX001","name": "腾大餐厅","area_code": "440305","address": "科技园中一路腾讯大厦" }}该字段用于上报场景信息，目前支持上报实际门店信息。该字段为JSON对象数据，对象格式为{"store_info":{"id": "门店ID","name": "名称","area_code": "编码","address": "地址" }} ，字段详细说明请点击行前的+展开
 	public String getAppid() {
 		return appid;
 	}
