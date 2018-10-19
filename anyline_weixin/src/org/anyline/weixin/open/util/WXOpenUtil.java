a66450b25e361b9e9bbf1d7131dac2d3b1a5018f
admin <admin@anyline.org>
2018-10-19 10:51:34 +0800

    微信支付到银行卡config

--- anyline_weixin/src/org/anyline/weixin/open/util/WXOpenUtil.java
+++ anyline_weixin/src/org/anyline/weixin/open/util/WXOpenUtil.java
@@ -12,6 +12,7 @@ import org.anyline.util.ConfigTable;
 import org.anyline.util.HttpClientUtil;
 import org.anyline.util.HttpUtil;
 import org.anyline.util.SimpleHttpUtil;
+import org.anyline.weixin.mp.util.WXMPConfig;
 import org.anyline.weixin.open.entity.WXOpenPayRefund;
 import org.anyline.weixin.open.entity.WXOpenPayRefundResult;
 import org.anyline.weixin.open.entity.WXOpenPayTradeOrder;
@@ -30,7 +31,12 @@ public class WXOpenUtil {
 	public WXOpenUtil(WXOpenConfig config){
 		this.config = config;
 	}
-	
+
+	public WXOpenUtil(String key, DataRow config){
+		WXOpenConfig conf = WXOpenConfig.parse(key, config);
+		this.config = conf;
+		instances.put(key, this);
+	}
 	public static WXOpenUtil getInstance(){
 		return getInstance("default");
 	}
