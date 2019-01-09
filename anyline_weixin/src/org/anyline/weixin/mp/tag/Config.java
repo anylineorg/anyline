b95a679828923f64d9c357fdd772ec96bacd4fab
admin <admin@DESKTOP-7EUHITR>
2019-01-09 20:39:35 +0800

    config

--- anyline_weixin/src/org/anyline/weixin/mp/tag/Config.java
+++ anyline_weixin/src/org/anyline/weixin/mp/tag/Config.java
@@ -47,48 +47,50 @@ public class Config extends BaseBodyTag {
 		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
 		try{
 			WXMPUtil util = WXMPUtil.getInstance(key);
-			//request.getHeader("Referer") http://www.anyline.org/mbr/od/v?cd=v79v01f3c102114ddf5bf7a4c25daeb6ff0d007934692a7a0a39e8f95b41fd3e88f3
-			String server = util.getConfig().WEB_SERVER;
-			String url = "";
-			if(BasicUtil.isEmpty(server)){
-				server = HttpUtil.getHostUrl(request.getHeader("Referer"));
-			}
-			url =  FileUtil.mergePath(server , request.getAttribute("javax.servlet.forward.request_uri")+"");
-			String param = request.getQueryString();
-			if(BasicUtil.isNotEmpty(param)){
-				url += "?" + param;
-			}
-			Map<String,Object> map = util.jsapiSign(url);
-			
-			String config = "<script language=\"javascript\">\n";
-			if(debug){
-				String alert = "请注意url,经过代理的应用有可能造成域名不符(如localhost,127.0.0.1等),请在anyline-weixin-mp.xml中配置WEB_SERVER=http://www.xx.com\\n,并在微信后台设置服务器IP白名单";
-				alert += "SIGN SRC: appId=" + util.getConfig().APP_ID + ",noncestr="+map.get("noncestr")
-						+",jsapi_ticket="+map.get("jsapi_ticket")+",url="+url+",timestamp="+map.get("timestamp");
-				config += "alert(\""+alert+"\");\n";
-			}
-			config += "wx.config({\n";
-			config += "debug:"+debug+",\n";
-			config += "appId:'"+util.getConfig().APP_ID+"',\n";
-			config += "timestamp:"+map.get("timestamp")+",\n";
-			config += "nonceStr:'"+map.get("noncestr") + "',\n";
-			config += "signature:'"+map.get("sign")+"',\n";
-			config += "jsApiList:[";
-			String apiList[] = apis.split(",");
-			int size = apiList.length;
-			for(int i=0; i<size; i++){
-				String api = apiList[i];
-				api = api.replace("'", "").replace("\"", "");
-				if(i>0){
-					config += ",";
+			if(null != util){
+				//request.getHeader("Referer") http://www.anyline.org/mbr/od/v?cd=v79v01f3c102114ddf5bf7a4c25daeb6ff0d007934692a7a0a39e8f95b41fd3e88f3
+				String server = util.getConfig().WEB_SERVER;
+				String url = "";
+				if(BasicUtil.isEmpty(server)){
+					server = HttpUtil.getHostUrl(request.getHeader("Referer"));
 				}
-				config += "'" + api + "'";
+				url =  FileUtil.mergePath(server , request.getAttribute("javax.servlet.forward.request_uri")+"");
+				String param = request.getQueryString();
+				if(BasicUtil.isNotEmpty(param)){
+					url += "?" + param;
+				}
+				Map<String,Object> map = util.jsapiSign(url);
+				
+				String config = "<script language=\"javascript\">\n";
+				if(debug){
+					String alert = "请注意url,经过代理的应用有可能造成域名不符(如localhost,127.0.0.1等),请在anyline-weixin-mp.xml中配置WEB_SERVER=http://www.xx.com\\n,并在微信后台设置服务器IP白名单";
+					alert += "SIGN SRC: appId=" + util.getConfig().APP_ID + ",noncestr="+map.get("noncestr")
+							+",jsapi_ticket="+map.get("jsapi_ticket")+",url="+url+",timestamp="+map.get("timestamp");
+					config += "alert(\""+alert+"\");\n";
+				}
+				config += "wx.config({\n";
+				config += "debug:"+debug+",\n";
+				config += "appId:'"+util.getConfig().APP_ID+"',\n";
+				config += "timestamp:"+map.get("timestamp")+",\n";
+				config += "nonceStr:'"+map.get("noncestr") + "',\n";
+				config += "signature:'"+map.get("sign")+"',\n";
+				config += "jsApiList:[";
+				String apiList[] = apis.split(",");
+				int size = apiList.length;
+				for(int i=0; i<size; i++){
+					String api = apiList[i];
+					api = api.replace("'", "").replace("\"", "");
+					if(i>0){
+						config += ",";
+					}
+					config += "'" + api + "'";
+				}
+				config += "]\n";
+				config += "});\n";
+				config += "</script>";
+				JspWriter out = pageContext.getOut();
+				out.println(config);
 			}
-			config += "]\n";
-			config += "});\n";
-			config += "</script>";
-			JspWriter out = pageContext.getOut();
-			out.println(config);
 		} catch (Exception e) {
 			e.printStackTrace();
 			if(ConfigTable.isDebug()){
