e98b6ef3021752754c68fc4fb2ebc1168311655b
zhangpeiqin <zhangpeiqin@qihoinfo.com>
2019-05-28 10:15:03 +0800

    环信配置文件重新加载

--- anyline-easemob/src/main/java/org/anyline/easemob/EasemobConfig.java
+++ anyline-easemob/src/main/java/org/anyline/easemob/EasemobConfig.java
@@ -1,20 +1,14 @@
 package org.anyline.easemob;
 
 import java.io.File;
-import java.lang.reflect.Field;
-import java.util.HashMap;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
+import org.anyline.entity.PageNaviConfig;
 import org.anyline.util.BasicConfig;
 import org.anyline.util.BasicUtil;
 import org.anyline.util.ConfigTable;
 import org.anyline.util.FileUtil;
-import org.dom4j.Document;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
 
 
 public class EasemobConfig extends BasicConfig{
@@ -40,25 +34,19 @@ public class EasemobConfig extends BasicConfig{
 		if(BasicUtil.isEmpty(key)){
 			key = "default";
 		}
+
+		if(ConfigTable.getReload() > 0 && (System.currentTimeMillis() - EasemobConfig.lastLoadTime)/1000 > ConfigTable.getReload() ){
+			//重新加载
+			loadConfig();
+		}
 		return (EasemobConfig)instances.get(key);
 	}
 	/**
 	 * 加载配置文件
 	 */
 	private synchronized static void loadConfig() {
-		try {
-
-			File dir = new File(ConfigTable.getWebRoot() , "WEB-INF/classes");
-			List<File> files = FileUtil.getAllChildrenFile(dir, "xml");
-			for(File file:files){
-				if("anyline-easemob.xml".equals(file.getName())){
-					parseFile(EasemobConfig.class, file, instances);
-				}
-			}
-			
-		} catch (Exception e) {
-			log.error("配置文件解析异常:"+e);
-		}
+		loadConfig(instances, EasemobConfig.class, "anyline-easemob.xml");
+		EasemobConfig.lastLoadTime = System.currentTimeMillis();
 	}
 	
 	private static void debug(){
