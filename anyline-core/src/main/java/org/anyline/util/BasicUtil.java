82728b30c32407fd74924ead0e884740a03cddbe
zhangpeiqin <zhangpeiqin@qihoinfo.com>
2019-05-28 20:30:20 +0800

    file type regex

--- anyline-core/src/main/java/org/anyline/util/BasicUtil.java
+++ anyline-core/src/main/java/org/anyline/util/BasicUtil.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
