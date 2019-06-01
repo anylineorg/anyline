b142573cfc83b76be933884d92c1eb929b1b5ba3
zhangpeiqin <zhangpeiqin@qihoinfo.com>
2019-06-01 13:27:42 +0800

    config listener

--- anyline-web/src/main/java/org/anyline/config/ConfigListener.java
+++ anyline-web/src/main/java/org/anyline/config/ConfigListener.java
@@ -1,24 +1,27 @@
 package org.anyline.config;
 
-	import java.util.Map;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
 import org.anyline.util.ConfigTable;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-	@Component("al.config.listener")
-	public class ConfigListener implements ApplicationListener<ContextRefreshedEvent> {
-		@Override
-		public void onApplicationEvent(ContextRefreshedEvent event) {
-			if(event.getApplicationContext().getParent() != null){
-				return;
-			}
-//			ServletContext servlet = null;
-//			Map<String,String> configs = ConfigTable.getConfigs();
-//			servlet.setAttribute("al", configs);
-			
+import org.springframework.web.context.WebApplicationContext;
+
+@Component("al.config.listener")
+public class ConfigListener implements
+		ApplicationListener<ContextRefreshedEvent> {
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		if (event.getApplicationContext().getParent() != null) {
+			return;
 		}
+		WebApplicationContext webApplicationContext = (WebApplicationContext) event.getApplicationContext();
+		ServletContext servlet = webApplicationContext.getServletContext();
+		Map<String, String> configs = ConfigTable.getConfigs();
+		servlet.setAttribute("al", configs);
+
 	}
+}
