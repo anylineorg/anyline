83f35e3b84b2bd040f91ddafb8f6299ff0d108f9
zhang <zh@anyline.org>
2019-06-07 14:42:25 +0800

    config listener

--- anyline-web/src/main/java/org/anyline/config/ConfigListener.java
+++ anyline-web/src/main/java/org/anyline/config/ConfigListener.java
@@ -6,24 +6,30 @@ import javax.servlet.ServletContext;
 
 import org.anyline.util.ConfigTable;
 import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
 @Component("anyline.config.listener")
-public class ConfigListener implements ApplicationListener<ContextRefreshedEvent> {
-	private Logger log = Logger.getLogger(ConfigListener.class); 
+public class ConfigListener implements
+		ApplicationListener<ContextRefreshedEvent> {
+	private Logger log = Logger.getLogger(ConfigListener.class);
+
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		if (event.getApplicationContext().getParent() != null) {
 			return;
 		}
-		WebApplicationContext webApplicationContext = (WebApplicationContext) event.getApplicationContext();
-		ServletContext servlet = webApplicationContext.getServletContext();
-		Map<String, String> configs = ConfigTable.getConfigs();
-		String key = ConfigTable.getString("SERVLET_ATTRIBUTE_KEY", "al");
-		servlet.setAttribute(key, configs);
-		log.warn("[配置文件加载至servlet context][key:"+key+"]");
+		ApplicationContext app = event.getApplicationContext();
+		if (app instanceof WebApplicationContext) {
+			WebApplicationContext webApplicationContext = (WebApplicationContext) app;
+			ServletContext servlet = webApplicationContext.getServletContext();
+			Map<String, String> configs = ConfigTable.getConfigs();
+			String key = ConfigTable.getString("SERVLET_ATTRIBUTE_KEY", "al");
+			servlet.setAttribute(key, configs);
+			log.warn("[配置文件加载至servlet context][key:" + key + "]");
+		}
 	}
 }
