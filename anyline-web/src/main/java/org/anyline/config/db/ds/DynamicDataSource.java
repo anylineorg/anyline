1e838cf24fcf96d108f655d8b5248c80d3a5ad3a
zhang <zh@anyline.org>
2019-07-02 08:53:17 +0800

    动态数据源注册

--- anyline-web/src/main/java/org/anyline/config/db/ds/DynamicDataSource.java
+++ anyline-web/src/main/java/org/anyline/config/db/ds/DynamicDataSource.java
@@ -1,11 +1,24 @@
 package org.anyline.config.db.ds;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 public class DynamicDataSource extends AbstractRoutingDataSource {
 
+    // 保存动态创建的数据源
+    private static final Map<String,DataSource> dsMap = new HashMap<String,DataSource>();
+	/**
+	 * 获取当前线程数据源
+	 */
 	@Override
 	protected Object determineCurrentLookupKey() {
 		return DataSourceHolder.getDataSource();
 	}
+	public void addDataSource(String key, DataSource ds) {
+        dsMap.put(key, ds);
+    }
 }
