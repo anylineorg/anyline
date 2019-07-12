92f0e2d21ec44a6301a00318f0c4f941a8c7181b
zhang <zh@anyline.org>
2019-07-12 18:26:24 +0800

    文件下载中间过程

--- anyline-core/src/main/java/org/anyline/util/DownloadProcessImpl.java
+++ anyline-core/src/main/java/org/anyline/util/DownloadProcessImpl.java
@@ -10,8 +10,12 @@ public class DownloadProcessImpl implements DownloadProcess{
 	private File local		; //本地文件
 	private long total		; //总大小
 	private long finish		; //已完成
+	private double rate		; //完成百分比rate%
 	private long time		; //已耗时	
 	private long expect		; //预计耗时
+	
+	private double lastLogRate	; //最后一次日志进度
+	private long lastLogTime	; //量后一次日志时间
 	
 	
 	public DownloadProcessImpl(){}
@@ -27,18 +31,17 @@ public class DownloadProcessImpl implements DownloadProcess{
 		this.total = total;
 		this.finish = finish;
 		this.time = time;
+
+		rate = finish*1.0/total*100;
+		expect = total / (finish/time);
 		log();
 	}
 	private void log(){
-	    double lastRate = 0;
-	    long lastTime = 0;
-		double rate = finish*1.0/total*100;
-		if(rate - lastRate  >= 0.5 || System.currentTimeMillis() - lastTime > 1000 * 5 || rate==100){
-			expect = total / (finish/time);
+		if(rate - lastLogRate  >= 0.5 || System.currentTimeMillis() - lastLogTime > 1000 * 5 || rate==100){
     		String process = "[文件下载][进度:"+FileUtil.process(total, finish)+"][耗时:"+DateUtil.conversion(time)+"/"+DateUtil.conversion(expect)+"("+FileUtil.size(finish*1000/time)+"/s)][url:"+url+"][local:"+local.getAbsolutePath()+"]";
     		log.warn(process);
-    		lastRate = rate;
-    		lastTime = System.currentTimeMillis();
+    		lastLogRate = rate;
+    		lastLogTime = System.currentTimeMillis();
 		}
 	}
 	public String getUrl() {
