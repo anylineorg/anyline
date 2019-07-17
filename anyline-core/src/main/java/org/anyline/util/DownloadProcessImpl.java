6e835de4568be5614eb0c4e9f9b2b7b37808558d
al <server@anyline.org>
2019-07-17 17:13:16 +0800

    default download progress

--- anyline-core/src/main/java/org/anyline/util/DownloadProcessImpl.java
+++ anyline-core/src/main/java/org/anyline/util/DownloadProcessImpl.java
@@ -10,37 +10,52 @@ public class DownloadProcessImpl implements DownloadProcess{
 	private File local		; //本地文件
 	private long total		; //总大小
 	private long finish		; //已完成
-	private double rate		; //完成百分比rate%
-	private long time		; //已耗时	
-	private long expect		; //预计耗时
+	private long start		; //开始时间
+	private long expend		; //已耗时
+	private long expect		; //预计剩余时间
 	
 	private double lastLogRate	; //最后一次日志进度
 	private long lastLogTime	; //量后一次日志时间
-	
-	
-	public DownloadProcessImpl(){}
+	private String message		; //进度提示
+	private double process		; //完成百分比process%
+	public DownloadProcessImpl(){
+		this.start = System.currentTimeMillis();
+	}
 	public DownloadProcessImpl(String url, File local){
 		this.url = url;
 		this.local = local;
+		this.start = System.currentTimeMillis();
+		this.start = System.currentTimeMillis();
 	}
 	@Override
-	public void process(long total, long finish, long time) {
-    	if(finish > total){
-    		finish = total;
-    	}
-		this.total = total;
-		this.finish = finish;
-		this.time = time;
+	public void process(long finish) {
+		this.finish += finish;
+		process = this.finish*1.0/total*100;
 
-		rate = finish*1.0/total*100;
-		expect = total / (finish/time);
-		log();
+    	if(this.finish > total){
+    		this.finish = total;
+    		process = 100;
+    	}
+		expend = System.currentTimeMillis() - start;//已耗时
+		if(expend>0){
+			expect = (long)(total / (this.finish*1.0/expend) - expend);	//剩余时间=预计总耗时-已耗时
+			if(process == 100){
+				expect = 0;
+			}
+			log();
+		}
+	}
+	public void setProcess(double process){
+		this.process = process;
 	}
 	private void log(){
-		if(rate - lastLogRate  >= 0.5 || System.currentTimeMillis() - lastLogTime > 1000 * 5 || rate==100){
-    		String process = "[文件下载][进度:"+FileUtil.process(total, finish)+"][耗时:"+DateUtil.conversion(time)+"/"+DateUtil.conversion(expect)+"("+FileUtil.size(finish*1000/time)+"/s)][url:"+url+"][local:"+local.getAbsolutePath()+"]";
-    		log.warn(process);
-    		lastLogRate = rate;
+		if(process - lastLogRate  >= 0.5 || System.currentTimeMillis() - lastLogTime > 1000 * 5 || process==100){
+			long time = System.currentTimeMillis() - start;
+			message = "[进度:"+FileUtil.progress(total, finish)+"]"
+    				+ "[耗时:"+DateUtil.conversion(time)+"/"+DateUtil.conversion(expect)+"("+FileUtil.conversion(finish*1000/time)+"/s)]";
+    		String txt = "[文件下载]"+ message + "[url:"+url+"][local:"+local.getAbsolutePath()+"]";
+    		log.warn(txt);
+    		lastLogRate = process;
     		lastLogTime = System.currentTimeMillis();
 		}
 	}
@@ -56,5 +71,37 @@ public class DownloadProcessImpl implements DownloadProcess{
 	public void setLocal(File local) {
 		this.local = local;
 	}
-	
+	public void start(){
+		this.start = System.currentTimeMillis();
+	}
+	@Override
+	public void setTotal(long total) {
+		this.total = total;
+	}
+	@Override
+	public long getTotal() {
+		return this.total;
+	}
+	@Override
+	public long getFinish() {
+		return this.finish;
+	}
+	@Override
+	public long getStart() {
+		return this.start;
+	}
+	public long getExpect(){
+		return this.expect;
+	}
+	public long getExpend() {
+		return expend;
+	}
+	@Override
+	public double getProcess() {
+		return process;
+	}
+	@Override
+	public String getMessage() {
+		return message;
+	}
 }
