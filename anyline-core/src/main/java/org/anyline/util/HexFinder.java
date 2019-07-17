14e9aadf654a6801e1baed9b3fb2fcb814bd718e
zhang <zh@anyline.org>
2019-07-17 17:47:41 +0800

    sftp

--- anyline-core/src/main/java/org/anyline/util/HexFinder.java
+++ anyline-core/src/main/java/org/anyline/util/HexFinder.java
@@ -1,8 +1,5 @@
 package org.anyline.util;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class HexFinder {
 	public static String format(byte[] bt) {
@@ -18,19 +15,5 @@ public class HexFinder {
 		}
 		buf.append("\n");
 		return buf.toString();
-	}
-
-	public static byte[] readFile(String file) throws IOException {
-		InputStream is = new FileInputStream(file);
-		int length = is.available();
-		byte bt[] = new byte[length];
-		is.read(bt);
-		return bt;
-	}
-
-	public static void main(String[] agrs) throws IOException {
-		byte[] bt = HexFinder.readFile("d:\\a.sbd");
-		String hexData = HexFinder.format(bt);
-		System.out.println(hexData);
 	}
 }
