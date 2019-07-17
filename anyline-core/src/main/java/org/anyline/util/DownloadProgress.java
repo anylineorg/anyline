adf2b50712b2c1b2c3b89f963c48fd196fcb64a7
zhang <zh@anyline.org>
2019-07-17 19:49:56 +0800

    downloader thread pool

---
+++ anyline-core/src/main/java/org/anyline/util/DownloadProgress.java
@@ -0,0 +1,34 @@
+package org.anyline.util;
+
+
+public interface DownloadProgress {
+	/**
+	 * 加载历史进度
+	 * @param url
+	 * @param thread	线程号
+	 * @param total		本次需下载
+	 * @param past		历史进度
+	 */
+	public void init(String url, String thread, long total, long past);
+	/**
+	 * 每次下载长度
+	 * @param url
+	 * @param thread	线程号
+	 * @param finish	本次循环下载长度
+	 */
+	public void step(String url, String thread, long finish);
+	/**
+	 * 下载完成
+	 * @param url
+	 * @param thread
+	 */
+	public void finish(String url, String thread);
+	/**
+	 * 下载异常
+	 * @param url
+	 * @param thread
+	 * @param code
+	 * @param message
+	 */
+	public void error(String url, String thread, int code, String message);
+}
