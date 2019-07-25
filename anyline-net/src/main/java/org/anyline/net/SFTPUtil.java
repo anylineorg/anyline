dd5e7d84e134eab28d3baad56657ab411f1cddeb
zhang <zh@anyline.org>
2019-07-25 17:03:08 +0800

    ftp util

--- anyline-net/src/main/java/org/anyline/net/SFTPUtil.java
+++ anyline-net/src/main/java/org/anyline/net/SFTPUtil.java
@@ -358,15 +358,18 @@ class SFTPProgressMonitor implements SftpProgressMonitor {
 			displayRate = curRate; 
 			displayTime = System.currentTimeMillis();
 			long delay = System.currentTimeMillis()-startTime;
-			double expect = length / (transfered/delay);
-			String total_title = "[文件下载][进度:" + FileUtil.progress(length, transfered) +"][耗时:"+DateUtil.conversion(delay)+"/"+DateUtil.conversion(expect)+"("+FileUtil.conversion(transfered*1000/delay)+"/s)]";
-			if(null != local){
-				total_title = "[local:"+local+"]" + total_title;
+			double expect = 0;
+			if(delay>0 && transfered>0){
+				expect = length / (transfered/delay);
+				String total_title = "[文件下载][进度:" + FileUtil.progress(length, transfered) +"][耗时:"+DateUtil.conversion(delay)+"/"+DateUtil.conversion(expect)+"("+FileUtil.conversion(transfered*1000/delay)+"/s)]";
+				if(null != local){
+					total_title = "[local:"+local+"]" + total_title;
+				}
+				if(null != remote){
+					total_title = "[remote:"+remote+"]"  + total_title;
+				}
+				log.warn(total_title);
 			}
-			if(null != remote){
-				total_title = "[remote:"+remote+"]"  + total_title;
-			}
-			log.warn(total_title);
 		}
 		transfered = transfered + count;
 		return true;
