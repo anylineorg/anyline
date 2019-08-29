ce9948bb939a4bd70cd0a917304b3df97b50c182
zhang <zh@anyline.org>
2019-08-29 08:26:21 +0800

    视频截图

--- anyline-video/src/main/java/org/anyline/video/VideoUtil.java
+++ anyline-video/src/main/java/org/anyline/video/VideoUtil.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
@@ -17,15 +16,12 @@ import org.bytedeco.javacv.Java2DFrameConverter;
 
 public class VideoUtil {
 	private static Logger log = Logger.getLogger(VideoUtil.class);
-	public static void main(String args[]){
-		frame(new File("D:\\b.m3u8"), new File("D:\\b.jpg"));
-	}
 	/**
 	 * 截取视频获得指定帧的图片
 	 * @param video 视频
 	 * @param out 输入流
 	 */
-	public static boolean frame(File video, OutputStream out) {
+	public static boolean frame(File video, OutputStream out, int index) {
 		if(null == video || null == out || !video.exists()){
 			log.warn("[视频截图][文件异常]");
 			return false;
@@ -34,27 +30,21 @@ public class VideoUtil {
 		FFmpegFrameGrabber ff = new FFmpegFrameGrabber(video);
 		try {
 			ff.start();
-
+			
 			// 截取中间帧图片
-			// int i = 0;
-			// int length = ff.getLengthInFrames();
-			// int middleFrame = length / 2;
-			// middleFrame = 1;
-			// Frame frame = null;
-			// while (i < length) {
-			// frame = ff.grabFrame();
-			// if ((i > middleFrame) && (frame.image != null)) {
-			// break;
-			// }
-			// i++;
-			// }
+			int i = 0;
+			int length = ff.getLengthInFrames();
+			if(index <=0){
+				index= length / 2;
+			}
 			Frame frame = null;
-			while (true) {
+			while (i < length) {
 				frame = ff.grabFrame();
-				if (frame.image != null) {
+				if (i > index && null != frame.image) {
 					break;
 				}
-			}
+				i++;
+			 }
 
 			Java2DFrameConverter converter = new Java2DFrameConverter();
 			BufferedImage srcImage = converter.getBufferedImage(frame);
@@ -81,6 +71,9 @@ public class VideoUtil {
 			}catch(Exception e){}
 		}
 		
+	}
+	public static boolean frame(File video, OutputStream out) {
+		return frame(video, out, -1);
 	}
 	public static boolean frame(File video, File img) {
 		boolean result = false;
