76a793ec44ba12213c4b7fa21b9dbf4a1173261e
nc <nc@anyboot.cn>
2019-09-05 17:35:16 +0800

    variable

--- anyline-nc/src/main/java/org/anyline/nc/NCUtil.java
+++ anyline-nc/src/main/java/org/anyline/nc/NCUtil.java
@@ -1,6 +1,15 @@
 package org.anyline.nc;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.anyline.util.BeanUtil;
+
+import ucar.ma2.InvalidRangeException;
 import ucar.nc2.NetcdfFile;
+import ucar.nc2.Variable;
 
 public class NCUtil {
 	private String file;
@@ -27,4 +36,16 @@ public class NCUtil {
 			return false;
 		}
 	}
+	public List<Variable> getVariables(){
+		return nc.getVariables();
+	}
+	public List<String> getVariableNames(){
+		List<String> list = new ArrayList<String>();
+		List<Variable> variables = getVariables();
+		for(Variable var:variables){
+			list.add(var.getFullName());
+		}
+		return list;
+	}
+	
 }
