7b00c8b584c554c23333dbbc173a9515f9e926f2
zh <zh>
2019-11-05 20:31:59 +0800

    simple http util

--- anyline-core/src/main/java/org/anyline/util/SimpleHttpUtil.java
+++ anyline-core/src/main/java/org/anyline/util/SimpleHttpUtil.java
@@ -25,46 +25,55 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class SimpleHttpUtil {
-	public static String post(String url, String param){
-		return request(url,"POST", param);
+	public static String post(String url, String param) {
+		return request(url, "POST", param);
 	}
-	public static String get(String url, String param){
-		return request(url,"GET", param);
+
+	public static String get(String url, String param) {
+		return request(url, "GET", param);
 	}
+
 	public static String request(String url, String method, String param) {
+		HttpURLConnection conn = null;
+		InputStream is = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
 		try {
-			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+			conn = (HttpURLConnection) new URL(url).openConnection();
 			conn.setDoOutput(true);
 			conn.setDoInput(true);
 			conn.setUseCaches(false);
-			// 设置请求方式（GET/POST）
 			conn.setRequestMethod(method);
-			conn.setRequestProperty("content-type", "application/x-www-form-urlencoded");
-			// 当outputStr不为null时向输出流写数据
+			conn.setRequestProperty("content-type",
+					"application/x-www-form-urlencoded");
 			if (null != param) {
-				OutputStream outputStream = conn.getOutputStream();
-				// 注意编码格式
-				outputStream.write(param.getBytes("UTF-8"));
-				outputStream.close();
+				OutputStream os = conn.getOutputStream();
+				os.write(param.getBytes("UTF-8"));
+				os.close();
 			}
 			// 从输入流读取返回内容
-			InputStream inputStream = conn.getInputStream();
-			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			is = conn.getInputStream();
+			isr = new InputStreamReader(is, "utf-8");
+			br = new BufferedReader(isr);
 			String str = null;
 			StringBuffer buffer = new StringBuffer();
-			while ((str = bufferedReader.readLine()) != null) {
+			while ((str = br.readLine()) != null) {
 				buffer.append(str);
 			}
-			// 释放资源
-			bufferedReader.close();
-			inputStreamReader.close();
-			inputStream.close();
-			inputStream = null;
-			conn.disconnect();
 			return buffer.toString();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				// 释放资源
+				br.close();
+				isr.close();
+				is.close();
+				is = null;
+				conn.disconnect();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		return null;
 	}
