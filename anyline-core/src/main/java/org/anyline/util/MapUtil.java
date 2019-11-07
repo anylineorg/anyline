573f0e8902e16b8d8f41d5c09c5e771edce05d9f
zh <zh>
2019-11-07 13:46:14 +0800

    8.2.0

--- anyline-core/src/main/java/org/anyline/util/MapUtil.java
+++ anyline-core/src/main/java/org/anyline/util/MapUtil.java
@@ -35,7 +35,7 @@ public class MapUtil {
 			s = Math.round(s * 10000d) / 10000d;
 			s = s * 1000;
 //			if(ConfigTable.isDebug()){
-//				log.warn("[距离计算][LON1:"+lon1+"][LAT1:"+lat1+"][LON2:"+lon2+"][LAT2:"+lat2+"][DISTANCE:"+s+"]");
+//				log.warn("\n\t[距离计算][LON1:"+lon1+"][LAT1:"+lat1+"][LON2:"+lon2+"][LAT2:"+lat2+"][DISTANCE:"+s+"]");
 //			}
 			BigDecimal decimal = new BigDecimal(s);  
 			s = decimal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();  
--- anyline-core/src/main/java/org/anyline/util/RuntimeUtil.java
+++ anyline-core/src/main/java/org/anyline/util/RuntimeUtil.java
@@ -13,7 +13,7 @@ public class RuntimeUtil {
 		String result = "";
 		Runtime runTime = Runtime.getRuntime();
         if (runTime == null) {
-            log.warn("[runtime][获取运行时环境失败]");
+            log.warn("\n\t[runtime][获取运行时环境失败]");
             return result;
         }
         try {
@@ -32,7 +32,7 @@ public class RuntimeUtil {
         	e.printStackTrace();
         }
         
-        log.warn("[runtime][cmd:"+cmd+"][result:"+result+"]");
+        log.warn("\n\t[runtime][cmd:"+cmd+"][result:"+result+"]");
 		return result;
 	}
 	public static String run(String cmd){
