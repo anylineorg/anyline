8b2a84d8a78d70a4ef8fa569342d880454446728
zhang <zh>
2019-11-13 13:47:45 +0800

    pom version

--- anyline-core/src/main/java/org/anyline/entity/PageNaviConfig.java
+++ anyline-core/src/main/java/org/anyline/entity/PageNaviConfig.java
@@ -56,6 +56,9 @@ public class PageNaviConfig extends BasicConfig{
 	public String VAR_PAGE_VOL_INDEX		= "last"			;	//位置:last:最后,page:页标之后
 	public String VAR_PAGE_VOL_NUMBERS		= ""				; //用户选择每页多少条 10,20,30,40,50,100 如果设置了此属性将生成"<select class='navi-rows-set'><option value='10'>10 条/页</option>...</select>
 		
+	//位置分布 index:下标,stat:统计,jump:跳转到,vol:每页多少条
+	public String VAR_COMPONENT_LAYOUT		= "{stat}{index}{vol}{jump}";
+	
 	public String EVENT_BEFORE				= "";
 	public String EVENT_AFTER				= "";
 	public String EVENT_REFRESH				= "";
