81e35e521fdc5a02775cea8398e665f10aa34c97
zhang <zh>
2019-12-06 23:05:03 +0800

    doc

--- anyline-ext/src/main/java/org/anyline/util/GifUtil.java
+++ anyline-ext/src/main/java/org/anyline/util/GifUtil.java
@@ -1,145 +1,145 @@
-package org.anyline.util;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.imageio.ImageIO;
-import javax.imageio.spi.ImageReaderSpi;
-import javax.imageio.spi.ImageWriterSpi;
-import javax.imageio.stream.FileImageInputStream;
-import javax.imageio.stream.FileImageOutputStream;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.sun.imageio.plugins.gif.GIFImageReader;
-import com.sun.imageio.plugins.gif.GIFImageReaderSpi;
-import com.sun.imageio.plugins.gif.GIFImageWriter;
-import com.sun.imageio.plugins.gif.GIFImageWriterSpi;
-
-public class GifUtil {
-	private static Logger log = LoggerFactory.getLogger(GifUtil.class);
-    /**
-     * 拆分gif
-     * @param file
-     * @param dir 解压目录
-     * @return
-     */
-    public static List<File> splitGif(File file, File dir){
-    	List<File> files = new ArrayList<File>();
-    	FileImageInputStream in = null;
-    	FileImageOutputStream out = null;
-    	try {
-    		if(!dir.exists()){
-    			dir.mkdirs();
-    		}
-    		in = new FileImageInputStream(file);
-    		ImageReaderSpi readerSpi = new GIFImageReaderSpi();
-    		GIFImageReader gifReader = (GIFImageReader) readerSpi.createReaderInstance();
-    		gifReader.setInput(in);
-    		int size = gifReader.getNumImages(true);
-    		ImageWriterSpi writerSpi = new GIFImageWriterSpi();
-    		GIFImageWriter writer = (GIFImageWriter) writerSpi.createWriterInstance();
-    		for (int i = 0; i < size; i++) {
-    			File target = new File(dir, file.getName().replace(".gif", "_"+i+".jpg"));
-    			out = new FileImageOutputStream(target);
-    			writer.setOutput(out);
-    			writer.write(gifReader.read(i));
-    		}
-    	}catch(Exception e){
-    		e.printStackTrace();
-    	}
-    	return files;
-    }
-    /**
-     * 
-     * @param delay 播放延迟时间  
-     * @param tar
-     * @param srcs
-     */
-    public synchronized static void createGif(int delay, String tar, String ... srcs) {
-    	List<File> list = new ArrayList<File>();
-    	for(String src:srcs){
-    		list.add(new File(src));
-    	}
-    	createGif(delay, new File(tar), list);
-    }
-    public synchronized static void createGif(int delay, String tar, List<String> srcs) {
-    	List<File> list = new ArrayList<File>();
-    	for(String src:srcs){
-    		list.add(new File(src));
-    	}
-    	createGif(delay, new File(tar), list);
-    }
-    public synchronized static void createGif(int delay, File tar, File ... srcs) {
-    	List<File> list = new ArrayList<File>();
-    	for(File src:srcs){
-    		list.add(src);
-    	}
-    	createGif(delay, tar, list);
-    }
-    /**
-     * 
-     * @param delay 播放延迟时间  
-     * @param tar
-     * @param srcs
-     */
-    public synchronized static void createGif(int delay, File tar, List<File> srcs) {  
-        try {
-        	long fr = 0;
-        	File dir = tar.getParentFile();
-        	if(!dir.exists()){
-        		dir.mkdirs();
-        	}
-            AnimatedGifEncoder e = new AnimatedGifEncoder(); 
-            e.setRepeat(0);  
-            e.start(tar.getAbsolutePath());
-            int size = srcs.size();
-            BufferedImage src[] = new BufferedImage[size];  
-            for (int i = 0; i < size; i++) {
-            	File item = srcs.get(i);
-            	fr = System.currentTimeMillis();
-                e.setDelay(delay); //设置播放的延迟时间  
-                src[i] = ImageIO.read(item); // 读入需要播放的jpg文件  
-                e.addFrame(src[i]);  //添加到帧中  
-                if(ConfigTable.isDebug() && log.isWarnEnabled()){
-                	log.warn("[合成 gif][第{}/{}帧][gif:{}][源文件:{}][耗时:{}]",i+1,size,tar.getAbsolutePath(), item.getAbsoluteFile(),System.currentTimeMillis()-fr);
-                }
-            }  
-            e.finish();  
-        } catch (Exception e) {
-            e.printStackTrace();  
-        }  
-    }  
-
-    public synchronized static void createGif(int delay0, int delay1, File tar, List<File> srcs) {  
-        try {
-        	long fr = 0;
-        	File dir = tar.getParentFile();
-        	if(!dir.exists()){
-        		dir.mkdirs();
-        	}
-            AnimatedGifEncoder e = new AnimatedGifEncoder(); 
-            e.setRepeat(0);  
-            e.start(tar.getAbsolutePath());
-            int size = srcs.size();
-            BufferedImage src[] = new BufferedImage[size];  
-            for (int i = 0; i < size; i++) {
-            	File item = srcs.get(i);
-            	fr = System.currentTimeMillis();
-            	int delay = NumberUtil.random(delay0, delay1);
-                e.setDelay(delay); //设置播放的延迟时间  
-                src[i] = ImageIO.read(item); // 读入需要播放的jpg文件  
-                e.addFrame(src[i]);  //添加到帧中  
-                if(ConfigTable.isDebug() && log.isWarnEnabled()){
-                	log.warn("[合成 gif][第{}/{}帧][gif:{}][源文件:{}][耗时:{}]",i+1,size,tar.getAbsoluteFile(),item.getAbsoluteFile(),System.currentTimeMillis()-fr);
-                }
-            }  
-            e.finish();  
-        } catch (Exception e) {
-            e.printStackTrace();  
-        }  
-    }  
-}
+package org.anyline.util; 
+ 
+import java.awt.image.BufferedImage; 
+import java.io.File; 
+import java.util.ArrayList; 
+import java.util.List; 
+ 
+import javax.imageio.ImageIO; 
+import javax.imageio.spi.ImageReaderSpi; 
+import javax.imageio.spi.ImageWriterSpi; 
+import javax.imageio.stream.FileImageInputStream; 
+import javax.imageio.stream.FileImageOutputStream; 
+ 
+import org.slf4j.Logger; 
+import org.slf4j.LoggerFactory; 
+ 
+import com.sun.imageio.plugins.gif.GIFImageReader; 
+import com.sun.imageio.plugins.gif.GIFImageReaderSpi; 
+import com.sun.imageio.plugins.gif.GIFImageWriter; 
+import com.sun.imageio.plugins.gif.GIFImageWriterSpi; 
+ 
+public class GifUtil { 
+	private static Logger log = LoggerFactory.getLogger(GifUtil.class); 
+    /** 
+     * 拆分gif 
+     * @param file  file
+     * @param dir 解压目录 
+     * @return return
+     */ 
+    public static List<File> splitGif(File file, File dir){ 
+    	List<File> files = new ArrayList<File>(); 
+    	FileImageInputStream in = null; 
+    	FileImageOutputStream out = null; 
+    	try { 
+    		if(!dir.exists()){ 
+    			dir.mkdirs(); 
+    		} 
+    		in = new FileImageInputStream(file); 
+    		ImageReaderSpi readerSpi = new GIFImageReaderSpi(); 
+    		GIFImageReader gifReader = (GIFImageReader) readerSpi.createReaderInstance(); 
+    		gifReader.setInput(in); 
+    		int size = gifReader.getNumImages(true); 
+    		ImageWriterSpi writerSpi = new GIFImageWriterSpi(); 
+    		GIFImageWriter writer = (GIFImageWriter) writerSpi.createWriterInstance(); 
+    		for (int i = 0; i < size; i++) { 
+    			File target = new File(dir, file.getName().replace(".gif", "_"+i+".jpg")); 
+    			out = new FileImageOutputStream(target); 
+    			writer.setOutput(out); 
+    			writer.write(gifReader.read(i)); 
+    		} 
+    	}catch(Exception e){ 
+    		e.printStackTrace(); 
+    	} 
+    	return files; 
+    } 
+    /** 
+     *  
+     * @param delay 播放延迟时间   
+     * @param tar  tar
+     * @param srcs  srcs
+     */ 
+    public synchronized static void createGif(int delay, String tar, String ... srcs) { 
+    	List<File> list = new ArrayList<File>(); 
+    	for(String src:srcs){ 
+    		list.add(new File(src)); 
+    	} 
+    	createGif(delay, new File(tar), list); 
+    } 
+    public synchronized static void createGif(int delay, String tar, List<String> srcs) { 
+    	List<File> list = new ArrayList<File>(); 
+    	for(String src:srcs){ 
+    		list.add(new File(src)); 
+    	} 
+    	createGif(delay, new File(tar), list); 
+    } 
+    public synchronized static void createGif(int delay, File tar, File ... srcs) { 
+    	List<File> list = new ArrayList<File>(); 
+    	for(File src:srcs){ 
+    		list.add(src); 
+    	} 
+    	createGif(delay, tar, list); 
+    } 
+    /** 
+     *  
+     * @param delay 播放延迟时间   
+     * @param tar  tar
+     * @param srcs  srcs
+     */ 
+    public synchronized static void createGif(int delay, File tar, List<File> srcs) {   
+        try { 
+        	long fr = 0; 
+        	File dir = tar.getParentFile(); 
+        	if(!dir.exists()){ 
+        		dir.mkdirs(); 
+        	} 
+            AnimatedGifEncoder e = new AnimatedGifEncoder();  
+            e.setRepeat(0);   
+            e.start(tar.getAbsolutePath()); 
+            int size = srcs.size(); 
+            BufferedImage src[] = new BufferedImage[size];   
+            for (int i = 0; i < size; i++) { 
+            	File item = srcs.get(i); 
+            	fr = System.currentTimeMillis(); 
+                e.setDelay(delay); //设置播放的延迟时间   
+                src[i] = ImageIO.read(item); // 读入需要播放的jpg文件   
+                e.addFrame(src[i]);  //添加到帧中   
+                if(ConfigTable.isDebug() && log.isWarnEnabled()){ 
+                	log.warn("[合成 gif][第{}/{}帧][gif:{}][源文件:{}][耗时:{}]",i+1,size,tar.getAbsolutePath(), item.getAbsoluteFile(),System.currentTimeMillis()-fr); 
+                } 
+            }   
+            e.finish();   
+        } catch (Exception e) { 
+            e.printStackTrace();   
+        }   
+    }   
+ 
+    public synchronized static void createGif(int delay0, int delay1, File tar, List<File> srcs) {   
+        try { 
+        	long fr = 0; 
+        	File dir = tar.getParentFile(); 
+        	if(!dir.exists()){ 
+        		dir.mkdirs(); 
+        	} 
+            AnimatedGifEncoder e = new AnimatedGifEncoder();  
+            e.setRepeat(0);   
+            e.start(tar.getAbsolutePath()); 
+            int size = srcs.size(); 
+            BufferedImage src[] = new BufferedImage[size];   
+            for (int i = 0; i < size; i++) { 
+            	File item = srcs.get(i); 
+            	fr = System.currentTimeMillis(); 
+            	int delay = NumberUtil.random(delay0, delay1); 
+                e.setDelay(delay); //设置播放的延迟时间   
+                src[i] = ImageIO.read(item); // 读入需要播放的jpg文件   
+                e.addFrame(src[i]);  //添加到帧中   
+                if(ConfigTable.isDebug() && log.isWarnEnabled()){ 
+                	log.warn("[合成 gif][第{}/{}帧][gif:{}][源文件:{}][耗时:{}]",i+1,size,tar.getAbsoluteFile(),item.getAbsoluteFile(),System.currentTimeMillis()-fr); 
+                } 
+            }   
+            e.finish();   
+        } catch (Exception e) { 
+            e.printStackTrace();   
+        }   
+    }   
+} 
--- anyline-ext/src/main/java/org/anyline/util/WatermarkUtil.java
+++ anyline-ext/src/main/java/org/anyline/util/WatermarkUtil.java
@@ -17,8 +17,8 @@
  */
 
 
-package org.anyline.util;
-
+package org.anyline.util; 
+ 
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Font;
@@ -37,22 +37,22 @@ import javax.swing.ImageIcon;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+ 
 public class WatermarkUtil {
-	protected static final Logger log = LoggerFactory.getLogger(WatermarkUtil.class);
-	private int degree = 0;								//旋转角度
-	private Color color = Color.LIGHT_GRAY;				//水印颜色
-	private String fontName = "宋体";						//字体名称
-	private int fontStyle = Font.BOLD + Font.ITALIC;	//字体样式
-	private int fontSize = 30;							//字体大小
-	private float alpha = 0.3f;							//透明度
-	private float x = 0;								//坐标X如果x<0,x=width+x
+	protected static final Logger log = LoggerFactory.getLogger(WatermarkUtil.class); 
+	private int degree = 0;								//旋转角度 
+	private Color color = Color.LIGHT_GRAY;				//水印颜色 
+	private String fontName = "宋体";						//字体名称 
+	private int fontStyle = Font.BOLD + Font.ITALIC;	//字体样式 
+	private int fontSize = 30;							//字体大小 
+	private float alpha = 0.3f;							//透明度 
+	private float x = 0;								//坐标X如果x<0,x=width+x 
 	private float y = 0;								//坐标Y
 	//xy按百分比
-	
-	public WatermarkUtil(){
-		this.x = 0;
-		this.y = 0;
+	 
+	public WatermarkUtil(){ 
+		this.x = 0; 
+		this.y = 0; 
 	}
 	public WatermarkUtil(int x, int y){
 		this.x = x;
@@ -62,14 +62,14 @@ public class WatermarkUtil {
 		this.x = x;
 		this.y = y;
 	}
-	
-	 /**
-     * 给图片添加水印、可设置水印的旋转角度
-     * @param text
-     * @param src
-     * @param target
-     * @param degree
-     */
+	 
+	 /** 
+     * 给图片添加水印、可设置水印的旋转角度 
+     * @param text  text
+     * @param src  src
+     * @param target  target
+     * @return return
+     */ 
     public boolean markText(String text, File src, File target) {
     	if(null == text || null == src || null == target || !src.exists()){
     		return false;
@@ -79,42 +79,42 @@ public class WatermarkUtil {
     	if(!dir.exists()){
     		dir.mkdirs();
     	}
-        // 主图片的路径
-        InputStream is = null;
+        // 主图片的路径 
+        InputStream is = null; 
         OutputStream os = null;
         BufferedImage buffImg = null;
-        try {
-            Image srcImg = ImageIO.read(src);
+        try { 
+            Image srcImg = ImageIO.read(src); 
             buffImg = new BufferedImage(srcImg.getWidth(null),srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
             int width = buffImg.getWidth();
             int height = buffImg.getHeight();
-           
-            // 得到画笔对象
-            Graphics2D g = buffImg.createGraphics();
-            // 设置对线段的锯齿状边缘处理
-            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null);
-            if (degree != 0) {
-                // 设置水印旋转
-                g.rotate(Math.toRadians(degree),(double) width / 2, (double) height / 2);
-            }
-            // 设置颜色
-            g.setColor(color);
-            // 设置 Font
-            g.setFont(new Font(fontName, fontStyle, fontSize));
+            
+            // 得到画笔对象 
+            Graphics2D g = buffImg.createGraphics(); 
+            // 设置对线段的锯齿状边缘处理 
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR); 
+            g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null); 
+            if (degree != 0) { 
+                // 设置水印旋转 
+                g.rotate(Math.toRadians(degree),(double) width / 2, (double) height / 2); 
+            } 
+            // 设置颜色 
+            g.setColor(color); 
+            // 设置 Font 
+            g.setFont(new Font(fontName, fontStyle, fontSize)); 
             g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,alpha));
-            int[] offset = finalOffset(width,height);
-            g.drawString(text, offset[0], offset[1]);
-            g.dispose();
-            os = new FileOutputStream(target);
+            int[] offset = finalOffset(width,height); 
+            g.drawString(text, offset[0], offset[1]); 
+            g.dispose(); 
+            os = new FileOutputStream(target); 
             // 生成图片// 生成图片
             String format = "jpg";
             if(target.getName().toLowerCase().endsWith("png")){
             	format = "png";
-            }
-            ImageIO.write(buffImg, format, os);
-        } catch (Exception e) {
-            e.printStackTrace();
+            } 
+            ImageIO.write(buffImg, format, os); 
+        } catch (Exception e) { 
+            e.printStackTrace(); 
         } finally {
             try {
                 if (null != buffImg)
@@ -127,66 +127,67 @@ public class WatermarkUtil {
                     is.close();
             } catch (Exception e) {
                 e.printStackTrace();
-            }
-            try {
-                if (null != os)
-                    os.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            } 
+            try { 
+                if (null != os) 
+                    os.close(); 
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
         }
         log.warn("[添加水印][耗时:{}][text:{}][src:{}][target:{}]",DateUtil.conversion(System.currentTimeMillis()-fr),text, src.getAbsoluteFile(), target.getAbsoluteFile());
-        return true;
+        return true; 
+    } 
+    public void markText(String text, String src, String target){ 
+    	markText(text, new File(src), new File(target)); 
+    } 
+    public void markText(String text, File src){ 
+    	markText(text, src, src); 
+    } 
+    public void markText(String text, String src){ 
+    	markText(text, src, src); 
     }
-    public void markText(String text, String src, String target){
-    	markText(text, new File(src), new File(target));
-    }
-    public void markText(String text, File src){
-    	markText(text, src, src);
-    }
-    public void markText(String text, String src){
-    	markText(text, src, src);
-    }
-    /**
-     * 给图片添加水印、可设置水印图片旋转角度
-     * @param icon 水印图片路径
-     * @param src 源图片路径
-     * @param target 目标图片路径
-     * @param degree 水印图片旋转角度
-     */
-    public void markIcon(File icon, int iconWidth, int iconHeight, File src, File target) {
+    /** 
+     * 给图片添加水印、可设置水印图片旋转角度 
+     * @param icon 水印图片路径 
+     * @param iconWidth icon宽度
+     * @param iconHeight icon高度 
+     * @param src 源图片路径 
+     * @param target 目标图片路径 
+     */ 
+    public void markIcon(File icon, int iconWidth, int iconHeight, File src, File target) { 
         OutputStream os = null;
         BufferedImage buffImg = null;
         long fr = System.currentTimeMillis();
         int _x = 0;
         int _y = 0;
         try {
-            Image srcImg = ImageIO.read(src);
+            Image srcImg = ImageIO.read(src); 
             buffImg = new BufferedImage(srcImg.getWidth(null), srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
             int width = buffImg.getWidth();
             int height = buffImg.getHeight();
 
             int icon_width = iconWidth;
             int icon_height = iconHeight;
-            // 得到画笔对象
+            // 得到画笔对象 
             Graphics2D g = buffImg.createGraphics();
             buffImg = g.getDeviceConfiguration()
                     .createCompatibleImage(srcImg.getWidth(null), srcImg.getHeight(null), Transparency.TRANSLUCENT);
             //g.dispose();
             g = buffImg.createGraphics();
-            // 设置对线段的锯齿状边缘处理
-            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null);
-            if (degree !=0) {
-                // 设置水印旋转
-                g.rotate(Math.toRadians(degree),(double) width / 2, (double) height / 2);
-            }
-            // 水印图象的路径 水印一般为gif或者png的，这样可设置透明度
-            ImageIcon imgIcon = new ImageIcon(icon.getAbsolutePath());
-            // 得到Image对象。
-            Image img = imgIcon.getImage();
-            float alpha = 0.5f; // 透明度
-            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,alpha));
+            // 设置对线段的锯齿状边缘处理 
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR); 
+            g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null); 
+            if (degree !=0) { 
+                // 设置水印旋转 
+                g.rotate(Math.toRadians(degree),(double) width / 2, (double) height / 2); 
+            } 
+            // 水印图象的路径 水印一般为gif或者png的，这样可设置透明度 
+            ImageIcon imgIcon = new ImageIcon(icon.getAbsolutePath()); 
+            // 得到Image对象。 
+            Image img = imgIcon.getImage(); 
+            float alpha = 0.5f; // 透明度 
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,alpha)); 
             int[] offset = finalOffset(width,height);
             _x = offset[0]-icon_width;
             _y = offset[1]-icon_height;
@@ -197,27 +198,27 @@ public class WatermarkUtil {
             	_y = 0;
             }
             g.drawImage(img, _x, _y, icon_width, icon_height, null);
-            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
-            g.dispose();
-            os = new FileOutputStream(target);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER)); 
+            g.dispose(); 
+            os = new FileOutputStream(target); 
             // 生成图片
             String format = "jpg";
             if(target.getName().toLowerCase().endsWith("png")){
             	format = "png";
             }
             ImageIO.write(buffImg, format, os);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception e) { 
+            e.printStackTrace(); 
         } finally {
         	try{
         		buffImg.flush();
         	}catch(Exception e){
-        	}
-            try {
-                if (null != os)
-                    os.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+        	} 
+            try { 
+                if (null != os) 
+                    os.close(); 
+            } catch (Exception e) { 
+                e.printStackTrace(); 
             }
         }
         log.warn("[添加水印][耗时:{}][x:{}][y:{}][icon:{}][src:{}][target:{}]",DateUtil.conversion(System.currentTimeMillis()-fr), _x,_y,icon.getAbsoluteFile(), src.getAbsoluteFile(), target.getAbsoluteFile());
@@ -249,8 +250,8 @@ public class WatermarkUtil {
     }
     /**
      * 水印偏移
-     * @param x
-     * @param y
+     * @param x x
+     * @param y y
      */
     public void offset(int x, int y){
     	this.x = x;
@@ -258,56 +259,56 @@ public class WatermarkUtil {
     }
     /**
      * 水印偏移
-     * @param x%
-     * @param y%
+     * @param x  x%
+     * @param y  y%
      */
     public void offset(float x, float y){
     	this.x = x;
     	this.y = y;
     }
-    public void markIcon(String icon, String src, String target){
-    	markIcon(new File(icon), new File(src), new File(target));
-    }
-    public void markIcon(File icon, File src){
-    	markIcon(icon, src, src);
-    }
-    public void markIcon(String icon, String src){
-    	markIcon(icon, src, src);
-    }
-	public Integer getDegree() {
-		return degree;
+    public void markIcon(String icon, String src, String target){ 
+    	markIcon(new File(icon), new File(src), new File(target)); 
+    } 
+    public void markIcon(File icon, File src){ 
+    	markIcon(icon, src, src); 
+    } 
+    public void markIcon(String icon, String src){ 
+    	markIcon(icon, src, src); 
+    } 
+	public Integer getDegree() { 
+		return degree; 
+	} 
+	public void setDegree(Integer degree) { 
+		this.degree = degree; 
+	} 
+	public Color getColor() { 
+		return color; 
+	} 
+	public void setColor(Color color) { 
+		this.color = color; 
+	} 
+	public String getFontName() { 
+		return fontName; 
+	} 
+	public void setFontName(String fontName) { 
+		this.fontName = fontName; 
+	} 
+	public int getFontStyle() { 
+		return fontStyle; 
+	} 
+	public void setFontStyle(int fontStyle) { 
+		this.fontStyle = fontStyle; 
+	} 
+	public int getFontSize() { 
+		return fontSize; 
+	} 
+	public void setFontSize(int fontSize) { 
+		this.fontSize = fontSize; 
+	} 
+	public float getAlpha() { 
+		return alpha; 
+	} 
+	public void setAlpha(float alpha) { 
+		this.alpha = alpha; 
 	}
-	public void setDegree(Integer degree) {
-		this.degree = degree;
-	}
-	public Color getColor() {
-		return color;
-	}
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	public String getFontName() {
-		return fontName;
-	}
-	public void setFontName(String fontName) {
-		this.fontName = fontName;
-	}
-	public int getFontStyle() {
-		return fontStyle;
-	}
-	public void setFontStyle(int fontStyle) {
-		this.fontStyle = fontStyle;
-	}
-	public int getFontSize() {
-		return fontSize;
-	}
-	public void setFontSize(int fontSize) {
-		this.fontSize = fontSize;
-	}
-	public float getAlpha() {
-		return alpha;
-	}
-	public void setAlpha(float alpha) {
-		this.alpha = alpha;
-	}
-}
+} 
