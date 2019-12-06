b5a6be0ec6e240dace82c1a7b9eee250dbf6a676
zhang <zh>
2019-12-06 23:05:09 +0800

    doc

--- anyline-qq/src/main/java/org/anyline/qq/mp/util/QQMPConfig.java
+++ anyline-qq/src/main/java/org/anyline/qq/mp/util/QQMPConfig.java
@@ -1,52 +1,52 @@
-package org.anyline.qq.mp.util;
-
-import java.util.Hashtable;
-
-import org.anyline.util.BasicConfig;
-import org.anyline.util.BasicUtil;
-import org.anyline.util.ConfigTable;
-
-
-public class QQMPConfig extends BasicConfig{
-	private static Hashtable<String,BasicConfig> instances = new Hashtable<String,BasicConfig>();
-	/**
-	 * 服务号相关信息
-	 */
-	public String APP_ID = ""				; //AppID(应用ID)
-	public String API_KEY = ""				; //APPKEY(应用密钥)
-	public String OAUTH_REDIRECT_URL		; //登录成功回调URL
-	static{
-		init();
-		debug();
-	}
-	public static void init() {
-		//加载配置文件
-		loadConfig();
-	}
-
-	public static QQMPConfig getInstance(){
-		return getInstance("default");
-	}
-	public static QQMPConfig getInstance(String key){
-		if(BasicUtil.isEmpty(key)){
-			key = "default";
-		}
-
-		if(ConfigTable.getReload() > 0 && (System.currentTimeMillis() - QQMPConfig.lastLoadTime)/1000 > ConfigTable.getReload() ){
-			//重新加载
-			loadConfig();
-		}
-		return (QQMPConfig)instances.get(key);
-	}
-	/**
-	 * 加载配置文件
-	 * 首先加载anyline-config.xml
-	 * 然后加载anyline开头的xml文件并覆盖先加载的配置
-	 */
-	private synchronized static void loadConfig() {
-		loadConfig(instances, QQMPConfig.class, "anyline-qq-mp.xml");
-		QQMPConfig.lastLoadTime = System.currentTimeMillis();
-	}
-	private static void debug(){
-	}
-}
+package org.anyline.qq.mp.util; 
+ 
+import java.util.Hashtable; 
+ 
+import org.anyline.util.BasicConfig; 
+import org.anyline.util.BasicUtil; 
+import org.anyline.util.ConfigTable; 
+ 
+ 
+public class QQMPConfig extends BasicConfig{ 
+	private static Hashtable<String,BasicConfig> instances = new Hashtable<String,BasicConfig>(); 
+	/** 
+	 * 服务号相关信息 
+	 */ 
+	public String APP_ID = ""				; //AppID(应用ID) 
+	public String API_KEY = ""				; //APPKEY(应用密钥) 
+	public String OAUTH_REDIRECT_URL		; //登录成功回调URL 
+	static{ 
+		init(); 
+		debug(); 
+	} 
+	public static void init() { 
+		//加载配置文件 
+		loadConfig(); 
+	} 
+ 
+	public static QQMPConfig getInstance(){ 
+		return getInstance("default"); 
+	} 
+	public static QQMPConfig getInstance(String key){ 
+		if(BasicUtil.isEmpty(key)){ 
+			key = "default"; 
+		} 
+ 
+		if(ConfigTable.getReload() > 0 && (System.currentTimeMillis() - QQMPConfig.lastLoadTime)/1000 > ConfigTable.getReload() ){ 
+			//重新加载 
+			loadConfig(); 
+		} 
+		return (QQMPConfig)instances.get(key); 
+	} 
+	/** 
+	 * 加载配置文件 
+	 * 首先加载anyline-config.xml 
+	 * 然后加载anyline开头的xml文件并覆盖先加载的配置 
+	 */ 
+	private synchronized static void loadConfig() { 
+		loadConfig(instances, QQMPConfig.class, "anyline-qq-mp.xml"); 
+		QQMPConfig.lastLoadTime = System.currentTimeMillis(); 
+	} 
+	private static void debug(){ 
+	} 
+} 
--- anyline-qq/src/main/java/org/anyline/qq/open/entity/QQPayTradeResult.java
+++ anyline-qq/src/main/java/org/anyline/qq/open/entity/QQPayTradeResult.java
@@ -1,105 +1,105 @@
-package org.anyline.qq.open.entity;
-/**
- * 统一下单返回结果
- */
-public class QQPayTradeResult {
-	private String 	return_code		; //返回状态码
-	private String 	return_msg		; //返回信息
-	private String 	retcode			; //手Q CGI原始错误码
-	private String 	retmsg			; //手Q CGI原始错误信息
-	private String 	appid			; //应用ID
-	private String 	mch_id			; //商户号
-	private String 	sign			; //商户签名算法规则
-	private String 	result_code		; //业务结果SUCCESS/FAIL
-	private String 	err_code		; //错误代码
-	private String 	err_code_desc	; //错误代码描述
-	private String 	nonce_str		; //随机字符串
-	private String 	trade_type		; //支付场景
-	private String 	prepay_id		; //QQ钱包的预支付会话标识
-	private String 	code_url		; //二维码链接
-	
-	public String getReturn_code() {
-		return return_code;
-	}
-	public void setReturn_code(String return_code) {
-		this.return_code = return_code;
-	}
-	public String getReturn_msg() {
-		return return_msg;
-	}
-	public void setReturn_msg(String return_msg) {
-		this.return_msg = return_msg;
-	}
-	public String getRetcode() {
-		return retcode;
-	}
-	public void setRetcode(String retcode) {
-		this.retcode = retcode;
-	}
-	public String getRetmsg() {
-		return retmsg;
-	}
-	public void setRetmsg(String retmsg) {
-		this.retmsg = retmsg;
-	}
-	public String getAppid() {
-		return appid;
-	}
-	public void setAppid(String appid) {
-		this.appid = appid;
-	}
-	public String getMch_id() {
-		return mch_id;
-	}
-	public void setMch_id(String mch_id) {
-		this.mch_id = mch_id;
-	}
-	public String getSign() {
-		return sign;
-	}
-	public void setSign(String sign) {
-		this.sign = sign;
-	}
-	public String getResult_code() {
-		return result_code;
-	}
-	public void setResult_code(String result_code) {
-		this.result_code = result_code;
-	}
-	public String getErr_code() {
-		return err_code;
-	}
-	public void setErr_code(String err_code) {
-		this.err_code = err_code;
-	}
-	public String getErr_code_desc() {
-		return err_code_desc;
-	}
-	public void setErr_code_desc(String err_code_desc) {
-		this.err_code_desc = err_code_desc;
-	}
-	public String getNonce_str() {
-		return nonce_str;
-	}
-	public void setNonce_str(String nonce_str) {
-		this.nonce_str = nonce_str;
-	}
-	public String getTrade_type() {
-		return trade_type;
-	}
-	public void setTrade_type(String trade_type) {
-		this.trade_type = trade_type;
-	}
-	public String getPrepay_id() {
-		return prepay_id;
-	}
-	public void setPrepay_id(String prepay_id) {
-		this.prepay_id = prepay_id;
-	}
-	public String getCode_url() {
-		return code_url;
-	}
-	public void setCode_url(String code_url) {
-		this.code_url = code_url;
-	}
-}
+package org.anyline.qq.open.entity; 
+/** 
+ * 统一下单返回结果 
+ */ 
+public class QQPayTradeResult { 
+	private String 	return_code		; //返回状态码 
+	private String 	return_msg		; //返回信息 
+	private String 	retcode			; //手Q CGI原始错误码 
+	private String 	retmsg			; //手Q CGI原始错误信息 
+	private String 	appid			; //应用ID 
+	private String 	mch_id			; //商户号 
+	private String 	sign			; //商户签名算法规则 
+	private String 	result_code		; //业务结果SUCCESS/FAIL 
+	private String 	err_code		; //错误代码 
+	private String 	err_code_desc	; //错误代码描述 
+	private String 	nonce_str		; //随机字符串 
+	private String 	trade_type		; //支付场景 
+	private String 	prepay_id		; //QQ钱包的预支付会话标识 
+	private String 	code_url		; //二维码链接 
+	 
+	public String getReturn_code() { 
+		return return_code; 
+	} 
+	public void setReturn_code(String return_code) { 
+		this.return_code = return_code; 
+	} 
+	public String getReturn_msg() { 
+		return return_msg; 
+	} 
+	public void setReturn_msg(String return_msg) { 
+		this.return_msg = return_msg; 
+	} 
+	public String getRetcode() { 
+		return retcode; 
+	} 
+	public void setRetcode(String retcode) { 
+		this.retcode = retcode; 
+	} 
+	public String getRetmsg() { 
+		return retmsg; 
+	} 
+	public void setRetmsg(String retmsg) { 
+		this.retmsg = retmsg; 
+	} 
+	public String getAppid() { 
+		return appid; 
+	} 
+	public void setAppid(String appid) { 
+		this.appid = appid; 
+	} 
+	public String getMch_id() { 
+		return mch_id; 
+	} 
+	public void setMch_id(String mch_id) { 
+		this.mch_id = mch_id; 
+	} 
+	public String getSign() { 
+		return sign; 
+	} 
+	public void setSign(String sign) { 
+		this.sign = sign; 
+	} 
+	public String getResult_code() { 
+		return result_code; 
+	} 
+	public void setResult_code(String result_code) { 
+		this.result_code = result_code; 
+	} 
+	public String getErr_code() { 
+		return err_code; 
+	} 
+	public void setErr_code(String err_code) { 
+		this.err_code = err_code; 
+	} 
+	public String getErr_code_desc() { 
+		return err_code_desc; 
+	} 
+	public void setErr_code_desc(String err_code_desc) { 
+		this.err_code_desc = err_code_desc; 
+	} 
+	public String getNonce_str() { 
+		return nonce_str; 
+	} 
+	public void setNonce_str(String nonce_str) { 
+		this.nonce_str = nonce_str; 
+	} 
+	public String getTrade_type() { 
+		return trade_type; 
+	} 
+	public void setTrade_type(String trade_type) { 
+		this.trade_type = trade_type; 
+	} 
+	public String getPrepay_id() { 
+		return prepay_id; 
+	} 
+	public void setPrepay_id(String prepay_id) { 
+		this.prepay_id = prepay_id; 
+	} 
+	public String getCode_url() { 
+		return code_url; 
+	} 
+	public void setCode_url(String code_url) { 
+		this.code_url = code_url; 
+	} 
+} 
