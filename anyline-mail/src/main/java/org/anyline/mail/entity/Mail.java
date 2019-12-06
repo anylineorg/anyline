1db48a0073371ec5b99f1e243dec38944dbe198e
zhang <zh>
2019-12-06 23:05:06 +0800

    doc

--- anyline-mail/src/main/java/org/anyline/mail/entity/Mail.java
+++ anyline-mail/src/main/java/org/anyline/mail/entity/Mail.java
@@ -1,60 +1,60 @@
-package org.anyline.mail.entity;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-public class Mail {
-	private String subject;
-	private String content;
-	private Date sendTime;
-	private String sender;
-	private Date receiveTime;
-	private String receiver;
-	private List<File> attachments = new ArrayList<File>();
-	public String getSubject() {
-		return subject;
-	}
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Date getSendTime() {
-		return sendTime;
-	}
-	public void setSendTime(Date sendTime) {
-		this.sendTime = sendTime;
-	}
-	public String getSender() {
-		return sender;
-	}
-	public void setSender(String sender) {
-		this.sender = sender;
-	}
-	public Date getReceiveTime() {
-		return receiveTime;
-	}
-	public void setReceiveTime(Date receiveTime) {
-		this.receiveTime = receiveTime;
-	}
-	public String getReceiver() {
-		return receiver;
-	}
-	public void setReceiver(String receiver) {
-		this.receiver = receiver;
-	}
-	public List<File> getAttachments() {
-		return attachments;
-	}
-	public void setAttachments(List<File> attachments) {
-		this.attachments = attachments;
-	}
-
-	
-}
+package org.anyline.mail.entity; 
+ 
+import java.io.File; 
+import java.util.ArrayList; 
+import java.util.Date; 
+import java.util.List; 
+ 
+public class Mail { 
+	private String subject; 
+	private String content; 
+	private Date sendTime; 
+	private String sender; 
+	private Date receiveTime; 
+	private String receiver; 
+	private List<File> attachments = new ArrayList<File>(); 
+	public String getSubject() { 
+		return subject; 
+	} 
+	public void setSubject(String subject) { 
+		this.subject = subject; 
+	} 
+	public String getContent() { 
+		return content; 
+	} 
+	public void setContent(String content) { 
+		this.content = content; 
+	} 
+	public Date getSendTime() { 
+		return sendTime; 
+	} 
+	public void setSendTime(Date sendTime) { 
+		this.sendTime = sendTime; 
+	} 
+	public String getSender() { 
+		return sender; 
+	} 
+	public void setSender(String sender) { 
+		this.sender = sender; 
+	} 
+	public Date getReceiveTime() { 
+		return receiveTime; 
+	} 
+	public void setReceiveTime(Date receiveTime) { 
+		this.receiveTime = receiveTime; 
+	} 
+	public String getReceiver() { 
+		return receiver; 
+	} 
+	public void setReceiver(String receiver) { 
+		this.receiver = receiver; 
+	} 
+	public List<File> getAttachments() { 
+		return attachments; 
+	} 
+	public void setAttachments(List<File> attachments) { 
+		this.attachments = attachments; 
+	} 
+ 
+	 
+} 
--- anyline-mail/src/main/java/org/anyline/mail/util/MailConfig.java
+++ anyline-mail/src/main/java/org/anyline/mail/util/MailConfig.java
@@ -1,58 +1,58 @@
-package org.anyline.mail.util;
-
-import java.io.File;
-import java.util.Hashtable;
-
-import org.anyline.util.BasicConfig;
-import org.anyline.util.BasicUtil;
-import org.anyline.util.ConfigTable;
-
-public class MailConfig extends BasicConfig{
-	
-	private static Hashtable<String,BasicConfig> instances = new Hashtable<String,BasicConfig>();
-	private static File configDir;
-	public String ACCOUNT;
-	public String PASSWORD;
-	public String USERNAME;
-	public String PROTOCOL = "smtp";
-	public String HOST;
-	public String PORT;
-	public String ATTACHMENT_DIR = "";	//附件下载地址
-	public boolean AUTO_DOWNLOAD_ATTACHMENT = true;
-	static{
-		init();
-		debug();
-	}
-	public static void init() {
-		//加载配置文件
-		loadConfig();
-	}
-	public static void setConfigDir(File dir){
-		configDir = dir;
-		init();
-	}
-	public static MailConfig getInstance(){
-		return getInstance("default");
-	}
-	public static MailConfig getInstance(String key){
-		if(BasicUtil.isEmpty(key)){
-			key = "default";
-		}
-		if(ConfigTable.getReload() > 0 && (System.currentTimeMillis() - MailConfig.lastLoadTime)/1000 > ConfigTable.getReload() ){
-			//重新加载
-			loadConfig();
-		}
-		return (MailConfig)instances.get(key);
-	}
-	/**
-	 * 加载配置文件
-	 */
-	private synchronized static void loadConfig() {
-		loadConfig(instances, MailConfig.class, "anyline-mail.xml");
-		MailConfig.lastLoadTime = System.currentTimeMillis();
-	}
-	private static void debug(){
-	}
-
-}
-
+package org.anyline.mail.util; 
+ 
+import java.io.File; 
+import java.util.Hashtable; 
+ 
+import org.anyline.util.BasicConfig; 
+import org.anyline.util.BasicUtil; 
+import org.anyline.util.ConfigTable; 
+ 
+public class MailConfig extends BasicConfig{ 
+	 
+	private static Hashtable<String,BasicConfig> instances = new Hashtable<String,BasicConfig>(); 
+	private static File configDir; 
+	public String ACCOUNT; 
+	public String PASSWORD; 
+	public String USERNAME; 
+	public String PROTOCOL = "smtp"; 
+	public String HOST; 
+	public String PORT; 
+	public String ATTACHMENT_DIR = "";	//附件下载地址 
+	public boolean AUTO_DOWNLOAD_ATTACHMENT = true; 
+	static{ 
+		init(); 
+		debug(); 
+	} 
+	public static void init() { 
+		//加载配置文件 
+		loadConfig(); 
+	} 
+	public static void setConfigDir(File dir){ 
+		configDir = dir; 
+		init(); 
+	} 
+	public static MailConfig getInstance(){ 
+		return getInstance("default"); 
+	} 
+	public static MailConfig getInstance(String key){ 
+		if(BasicUtil.isEmpty(key)){ 
+			key = "default"; 
+		} 
+		if(ConfigTable.getReload() > 0 && (System.currentTimeMillis() - MailConfig.lastLoadTime)/1000 > ConfigTable.getReload() ){ 
+			//重新加载 
+			loadConfig(); 
+		} 
+		return (MailConfig)instances.get(key); 
+	} 
+	/** 
+	 * 加载配置文件 
+	 */ 
+	private synchronized static void loadConfig() { 
+		loadConfig(instances, MailConfig.class, "anyline-mail.xml"); 
+		MailConfig.lastLoadTime = System.currentTimeMillis(); 
+	} 
+	private static void debug(){ 
+	} 
+ 
+} 
+ 
