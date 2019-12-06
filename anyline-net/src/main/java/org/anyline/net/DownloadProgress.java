8de728ca86ff2867a6147473a472289eef568f70
zhang <zh>
2019-12-06 23:05:03 +0800

    doc

--- anyline-net/src/main/java/org/anyline/net/DownloadProgress.java
+++ anyline-net/src/main/java/org/anyline/net/DownloadProgress.java
@@ -1,39 +1,39 @@
-package org.anyline.net;
-
-
-public interface DownloadProgress {
-	/**
-	 * 加载历史进度
-	 * @param url
-	 * @param thread	线程号
-	 * @param total		本次需下载
-	 * @param past		历史进度
-	 */
-	public void init(String url, String thread, long total, long past);
-	/**
-	 * 每次下载长度
-	 * @param url
-	 * @param thread	线程号
-	 * @param finish	本次循环下载长度
-	 */
-	public void step(String url, String thread, long finish);
-	/**
-	 * 下载完成
-	 * @param url
-	 * @param thread
-	 */
-	public void finish(String url, String thread);
-	/**
-	 * 下载异常
-	 * @param url
-	 * @param thread
-	 * @param code
-	 * @param message
-	 */
-	public void error(String url, String thread, int code, String message);
-
-	public void setErrorListener(DownloadListener listener);
-	public void setFinishListener(DownloadListener listener);
-	public int getAction();
-	public void stop();
-}
+package org.anyline.net; 
+ 
+ 
+public interface DownloadProgress { 
+	/** 
+	 * 加载历史进度 
+	 * @param url  url
+	 * @param thread	线程号  thread	线程号
+	 * @param total		本次需下载  total		本次需下载
+	 * @param past		历史进度  past		历史进度
+	 */ 
+	public void init(String url, String thread, long total, long past); 
+	/** 
+	 * 每次下载长度 
+	 * @param url  url
+	 * @param thread	线程号  thread	线程号
+	 * @param finish	本次循环下载长度  finish	本次循环下载长度
+	 */ 
+	public void step(String url, String thread, long finish); 
+	/** 
+	 * 下载完成 
+	 * @param url  url
+	 * @param thread  thread
+	 */ 
+	public void finish(String url, String thread); 
+	/** 
+	 * 下载异常 
+	 * @param url  url
+	 * @param thread  thread
+	 * @param code  code
+	 * @param message  message
+	 */ 
+	public void error(String url, String thread, int code, String message); 
+ 
+	public void setErrorListener(DownloadListener listener); 
+	public void setFinishListener(DownloadListener listener); 
+	public int getAction(); 
+	public void stop(); 
+} 
--- anyline-net/src/main/java/org/anyline/net/HttpProxy.java
+++ anyline-net/src/main/java/org/anyline/net/HttpProxy.java
@@ -15,51 +15,51 @@
  *
  *          
  */
-package org.anyline.net;
-
-public 
-class HttpProxy{
-	private String host;
-	private int port;
-	private String user;
-	private String password;
+package org.anyline.net; 
+ 
+public  
+class HttpProxy{ 
+	private String host; 
+	private int port; 
+	private String user; 
+	private String password; 
 
 	public HttpProxy(){
 	}
 
 	public HttpProxy(String host, int port, String user, String password){
 	}
-
-	public String getHost() {
-		return host;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public int getPort() {
-		return port;
-	}
-
-	public void setPort(int port) {
-		this.port = port;
-	}
-
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+ 
+	public String getHost() { 
+		return host; 
+	} 
+ 
+	public void setHost(String host) { 
+		this.host = host; 
+	} 
+ 
+	public int getPort() { 
+		return port; 
+	} 
+ 
+	public void setPort(int port) { 
+		this.port = port; 
+	} 
+ 
+	public String getUser() { 
+		return user; 
+	} 
+ 
+	public void setUser(String user) { 
+		this.user = user; 
+	} 
+ 
+	public String getPassword() { 
+		return password; 
+	} 
+ 
+	public void setPassword(String password) { 
+		this.password = password; 
+	} 
+	 
 }
