569386c13d9c7c28a9b34386b0bbe42eeac1994e
zhang <zh>
2019-12-06 23:05:06 +0800

    doc

--- anyline-jpush/src/main/java/org/anyline/jpush/util/JPushConfig.java
+++ anyline-jpush/src/main/java/org/anyline/jpush/util/JPushConfig.java
@@ -1,46 +1,46 @@
-package org.anyline.jpush.util;
-
-import java.util.Hashtable;
-
-import org.anyline.util.BasicConfig;
-import org.anyline.util.BasicUtil;
-import org.anyline.util.ConfigTable;
-
-public class JPushConfig extends BasicConfig{
-	private static Hashtable<String,BasicConfig> instances = new Hashtable<String,BasicConfig>();
-	public String APP_KEY ="";
-	public String MASTER_SECRET ="";
-		
-	static{
-		init();
-		debug();
-	}
-	public static void init() {
-		//加载配置文件
-		loadConfig();
-	}
-
-	public static JPushConfig getInstance(){
-		return getInstance("default");
-	}
-	public static JPushConfig getInstance(String key){
-		if(BasicUtil.isEmpty(key)){
-			key = "default";
-		}
-
-		if(ConfigTable.getReload() > 0 && (System.currentTimeMillis() - JPushConfig.lastLoadTime)/1000 > ConfigTable.getReload() ){
-			//重新加载
-			loadConfig();
-		}
-		return (JPushConfig)instances.get(key);
-	}
-	/**
-	 * 加载配置文件
-	 */
-	private synchronized static void loadConfig() {
-		loadConfig(instances, JPushConfig.class, "anyline-jpush.xml");
-		JPushConfig.lastLoadTime = System.currentTimeMillis();
-	}
-	private static void debug(){
-	}
+package org.anyline.jpush.util; 
+ 
+import java.util.Hashtable; 
+ 
+import org.anyline.util.BasicConfig; 
+import org.anyline.util.BasicUtil; 
+import org.anyline.util.ConfigTable; 
+ 
+public class JPushConfig extends BasicConfig{ 
+	private static Hashtable<String,BasicConfig> instances = new Hashtable<String,BasicConfig>(); 
+	public String APP_KEY =""; 
+	public String MASTER_SECRET =""; 
+		 
+	static{ 
+		init(); 
+		debug(); 
+	} 
+	public static void init() { 
+		//加载配置文件 
+		loadConfig(); 
+	} 
+ 
+	public static JPushConfig getInstance(){ 
+		return getInstance("default"); 
+	} 
+	public static JPushConfig getInstance(String key){ 
+		if(BasicUtil.isEmpty(key)){ 
+			key = "default"; 
+		} 
+ 
+		if(ConfigTable.getReload() > 0 && (System.currentTimeMillis() - JPushConfig.lastLoadTime)/1000 > ConfigTable.getReload() ){ 
+			//重新加载 
+			loadConfig(); 
+		} 
+		return (JPushConfig)instances.get(key); 
+	} 
+	/** 
+	 * 加载配置文件 
+	 */ 
+	private synchronized static void loadConfig() { 
+		loadConfig(instances, JPushConfig.class, "anyline-jpush.xml"); 
+		JPushConfig.lastLoadTime = System.currentTimeMillis(); 
+	} 
+	private static void debug(){ 
+	} 
 }
