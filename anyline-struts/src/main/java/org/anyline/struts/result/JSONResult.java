c25892cf13c089b38925ae61a02fbd10996a01a6
zhang <zh>
2019-12-06 23:05:11 +0800

    doc

--- anyline-struts/src/main/java/org/anyline/struts/result/JSONResult.java
+++ anyline-struts/src/main/java/org/anyline/struts/result/JSONResult.java
@@ -17,8 +17,8 @@
  */
 
 
-package org.anyline.struts.result;
-
+package org.anyline.struts.result; 
+ 
 import java.io.PrintWriter;
 import java.util.Date;
 import java.util.HashMap;
@@ -41,65 +41,65 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.opensymphony.xwork2.ActionInvocation;
-
-public class JSONResult extends StrutsResultSupport {
-	private static final long serialVersionUID = 1L;
-	private static final Logger log = LoggerFactory.getLogger(JSONResult.class);
-	private boolean result = true;
-	private Object data = null;
+ 
+public class JSONResult extends StrutsResultSupport { 
+	private static final long serialVersionUID = 1L; 
+	private static final Logger log = LoggerFactory.getLogger(JSONResult.class); 
+	private boolean result = true; 
+	private Object data = null; 
 	private String message = null;
 	private String url = null;
-	private String code = null;
-	
-	
+	private String code = null; 
+	 
+	 
     protected void doExecute(String finalLocation, ActionInvocation invocation) throws Exception {
         HttpServletRequest request = (HttpServletRequest) invocation.getInvocationContext().get(HTTP_REQUEST);
-        HttpServletResponse response = (HttpServletResponse) invocation.getInvocationContext().get(HTTP_RESPONSE);
-    	response.setContentType("text/json; charset=UTF-8");
-    	response.setCharacterEncoding("UTF-8");
-        PrintWriter writer = response.getWriter();
-        try {
-        	//提取Action的传入值
-        	data = invocation.getStack().findValue("data");
+        HttpServletResponse response = (HttpServletResponse) invocation.getInvocationContext().get(HTTP_RESPONSE); 
+    	response.setContentType("text/json; charset=UTF-8"); 
+    	response.setCharacterEncoding("UTF-8"); 
+        PrintWriter writer = response.getWriter(); 
+        try { 
+        	//提取Action的传入值 
+        	data = invocation.getStack().findValue("data"); 
         	result = BasicUtil.parseBoolean(invocation.getStack().findValue("result"),true);
         	message = invocation.getStack().findString("msg");
         	url = invocation.getStack().findString("url");
         	code = invocation.getStack().findString("code");
-        	
-        	//转换成JSON格式
-        	//JsonConfig config = new JsonConfig();
+        	 
+        	//转换成JSON格式 
+        	//JsonConfig config = new JsonConfig(); 
         	String dataType   = null; 	//数据类型
-        	Map<String,Object> map = new HashMap<String,Object>();
-        	if(null == data){
-        		message = (String)BasicUtil.nvl(message, "没有返回数据");
-        		data = false;
-        	}else if(data instanceof Iterable){
-				dataType = "list";
-        	}else if (data instanceof DataSet) {
-        		DataSet set = (DataSet)data;
-        		result = set.isSuccess();
-        		message = (String)BasicUtil.nvl(message,set.getMessage());
-				dataType = "list";
+        	Map<String,Object> map = new HashMap<String,Object>(); 
+        	if(null == data){ 
+        		message = (String)BasicUtil.nvl(message, "没有返回数据"); 
+        		data = false; 
+        	}else if(data instanceof Iterable){ 
+				dataType = "list"; 
+        	}else if (data instanceof DataSet) { 
+        		DataSet set = (DataSet)data; 
+        		result = set.isSuccess(); 
+        		message = (String)BasicUtil.nvl(message,set.getMessage()); 
+				dataType = "list"; 
 				data = set.getRows();
-				map.put("navi", set.getNavi());
-			}else if (data instanceof DataRow) {
-				dataType = "map";
-			}else if(data instanceof Map){
-				dataType = "map";
-			}else if(data instanceof String){
-				dataType = "string";
-				data = convertJSONChar(data.toString());
-			}else if(data instanceof Number){
-				dataType = "number";
-				data = convertJSONChar(data.toString());
-			}else{
-				dataType = "map";
-			}
-        	map.put("type", dataType);
-        	map.put("result", result);
+				map.put("navi", set.getNavi()); 
+			}else if (data instanceof DataRow) { 
+				dataType = "map"; 
+			}else if(data instanceof Map){ 
+				dataType = "map"; 
+			}else if(data instanceof String){ 
+				dataType = "string"; 
+				data = convertJSONChar(data.toString()); 
+			}else if(data instanceof Number){ 
+				dataType = "number"; 
+				data = convertJSONChar(data.toString()); 
+			}else{ 
+				dataType = "map"; 
+			} 
+        	map.put("type", dataType); 
+        	map.put("result", result); 
         	map.put("message", message);
         	map.put("data", data);
-        	map.put("url", url);
+        	map.put("url", url); 
         	map.put("success", result);
     		map.put("code", code);
         	map.put("request_time", request.getParameter("_anyline_request_time"));
@@ -108,24 +108,24 @@ public class JSONResult extends StrutsResultSupport {
         	JsonConfig config = new JsonConfig();
     		config.registerJsonValueProcessor(Date.class, new JSONDateFormatProcessor());
     		config.registerJsonValueProcessor(java.sql.Timestamp.class, new JSONDateFormatProcessor());
-    		config.registerJsonValueProcessor(Object.class, new JSONStringFormatProcessor());
+    		config.registerJsonValueProcessor(Object.class, new JSONStringFormatProcessor()); 
         	JSON json = JSONObject.fromObject(map, config);  
-    		String str = json.toString();
-        	writer.print(str);
+    		String str = json.toString(); 
+        	writer.print(str); 
         }catch(Exception e){
-        	e.printStackTrace();
-        }finally {
-            if (writer != null) {
-                writer.flush();
-                writer.close();
-            }
-        }
+        	e.printStackTrace(); 
+        }finally { 
+            if (writer != null) { 
+                writer.flush(); 
+                writer.close(); 
+            } 
+        } 
 	}	
     private String convertJSONChar(String value) {
 		if (null != value) {
 			value = value.replace("\\", "\\\\").replace("\"", "\\\"");
 		}
 		return value;
-	}
-	
-}
+	} 
+	 
+} 
