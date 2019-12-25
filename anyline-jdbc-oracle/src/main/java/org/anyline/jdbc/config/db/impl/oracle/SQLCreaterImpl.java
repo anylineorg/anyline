380f5806a247cb27b3dbf2d25f8b6e8ecc6ddf30
zhang <zh>
2019-12-25 08:39:48 +0800

    oracle

--- anyline-jdbc-oracle/src/main/java/org/anyline/jdbc/config/db/impl/oracle/SQLCreaterImpl.java
+++ anyline-jdbc-oracle/src/main/java/org/anyline/jdbc/config/db/impl/oracle/SQLCreaterImpl.java
@@ -1,15 +1,15 @@
  
-package org.anyline.config.db.impl.oracle; 
+package org.anyline.jdbc.config.db.impl.oracle; 
  
-import org.anyline.config.db.OrderStore; 
-import org.anyline.config.db.SQLCreater; 
-import org.anyline.config.db.impl.BasicSQLCreaterImpl; 
-import org.anyline.config.db.run.RunSQL; 
-import org.anyline.dao.AnylineDao; 
-import org.anyline.entity.PageNavi; 
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.beans.factory.annotation.Qualifier; 
-import org.springframework.stereotype.Repository; 
+import org.anyline.entity.PageNavi;
+import org.anyline.jdbc.config.db.OrderStore;
+import org.anyline.jdbc.config.db.SQLCreater;
+import org.anyline.jdbc.config.db.impl.BasicSQLCreaterImpl;
+import org.anyline.jdbc.config.db.run.RunSQL;
+import org.anyline.jdbc.dao.AnylineDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
  
 @Repository("anyline.jdbc.creater.oracle") 
 public class SQLCreaterImpl extends BasicSQLCreaterImpl implements SQLCreater{ 
