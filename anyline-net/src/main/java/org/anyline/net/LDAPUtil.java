55d98105138514750f08f99dfca3d6fbe70b1f74
zhang <zh>
2019-12-25 18:34:26 +0800

    ldap login

---
+++ anyline-net/src/main/java/org/anyline/net/LDAPUtil.java
@@ -0,0 +1,40 @@
+package org.anyline.net;
+
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LDAPUtil {
+	private static Logger log = LoggerFactory.getLogger(LDAPUtil.class);
+	/**
+	 * 登录
+	 * @param url 登录地址 如 http://ladp.anyline.org:389
+	 * @param account 登录帐号 如 admin@anyline.org
+	 * @param password 登录密码
+	 * @return LdapContext
+	 * @throws NamingException 如果抛出异常表示登录失败
+	 */
+    public LdapContext login(String url, String account, String password) throws NamingException {
+        Hashtable<String, Object> env = new Hashtable<String, Object>();
+        env.put(Context.SECURITY_PRINCIPAL, account);//用户名
+        env.put(Context.SECURITY_CREDENTIALS, password);//密码
+        env.put(Context.PROVIDER_URL, url);//LDAP的地址：端口
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");//LDAP工厂类
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");//认证类型
+        LdapContext ctx = null;
+        try{
+        	 ctx = new InitialLdapContext(env, null);//连接
+        }catch(NamingException e){
+        	log.warn("[ldap login][result:false][msg:{}]", e.getMessage());
+        	throw e;
+        }
+        return ctx;
+    }
+}
