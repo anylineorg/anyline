38aed8459ce97db4db417fe97d895f42c4a6c58c
zhang <zh>
2019-12-25 08:41:18 +0800

    jdbc

---
+++ anyline-jdbc/src/main/java/org/anyline/jdbc/cache/CacheUtil.java
@@ -0,0 +1,310 @@
+/* 
+ * Copyright 2006-2015 www.anyline.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *          
+ */
+package org.anyline.jdbc.cache; 
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+import org.anyline.entity.DataRow;
+import org.anyline.entity.PageNavi;
+import org.anyline.jdbc.config.db.OrderStore;
+import org.anyline.jdbc.config.Config;
+import org.anyline.jdbc.config.ConfigChain;
+import org.anyline.jdbc.config.ConfigStore;
+import org.anyline.util.BasicUtil;
+import org.anyline.util.ConfigTable;
+import org.anyline.util.MD5Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+ 
+public class CacheUtil {
+	private static final Logger log = LoggerFactory.getLogger(CacheUtil.class);
+	private static CacheManager manager = null;
+	private static Hashtable<String,Long> reflushFlag = new Hashtable<String,Long>();		//缓存刷新标记
+	
+	public static CacheManager createManager(){
+		long fr = System.currentTimeMillis();
+		if(null == manager){
+			manager = CacheManager.create();
+	    	if(ConfigTable.isDebug() && log.isWarnEnabled()){
+	    		log.warn("[加载ehcache配置文件][耗时:{}",System.currentTimeMillis() - fr);
+	    		for(String name:manager.getCacheNames()){
+	    			log.warn("[解析ehcache配置文件] [name:{}]",name);
+	    		}
+	    	}
+		}
+		return manager;
+	}
+	
+	public static Cache getCache(String channel){
+		CacheManager manager = createManager();
+		Cache cache = manager.getCache(channel);
+		if(null == cache){
+			manager.addCache(channel);
+		}
+		cache = manager.getCache(channel);
+		return cache;
+	}
+	public static List<String> getCacheNames(){
+		List<String> names = new ArrayList<String>();
+		CacheManager manager = createManager();
+		for(String name:manager.getCacheNames()){
+			names.add(name);
+		}
+		return names;
+	}
+	public static List<Cache> getCaches(){
+		List<Cache> caches = new ArrayList<Cache>();
+		CacheManager manager = createManager();
+		for(String name:manager.getCacheNames()){
+			caches.add(manager.getCache(name));
+		}
+		return caches;
+	}
+	public static Element getElement(String channel, String key){
+		Element result = null;
+		long fr = System.currentTimeMillis();
+		Cache cache = getCache(channel);
+		if(null != cache){
+			result = cache.get(key);
+			if(null == result){
+		    	if(ConfigTable.isDebug() && log.isWarnEnabled()){
+		    		log.warn("[缓存不存在][cnannel:{}][key:{}][生存:-1/{}]",channel, key,cache.getCacheConfiguration().getTimeToLiveSeconds());
+		    	}
+				return null;
+			}
+			if(result.isExpired()){
+		    	if(ConfigTable.isDebug() && log.isWarnEnabled()){
+		    		log.warn("[缓存数据提取成功但已过期][耗时:{}][cnannel:{}][key:{}][命中:{}][生存:{}/{}]",System.currentTimeMillis()-fr,channel,key,result.getHitCount(),(System.currentTimeMillis() - result.getCreationTime())/1000,result.getTimeToLive());
+		    	}
+		    	result = null;
+			}else{
+				if(ConfigTable.isDebug() && log.isWarnEnabled()){
+		    		log.warn("[缓存数据提取成功并有效][耗时:{}][cnannel:{}][key:{}][命中:{}][生存:{}/{}]",System.currentTimeMillis()-fr,channel,key,result.getHitCount(),(System.currentTimeMillis() - result.getCreationTime())/1000,result.getTimeToLive());
+		    	}
+			}
+		}
+		return result;
+	}
+	
+	
+	public static void put(String channel, String key, Object value){
+		Element element = new Element(key, value);
+		put(channel, element);
+	}
+	public static void put(String channel, Element element){
+		Cache cache = getCache(channel);
+		if(null != cache){
+			cache.put(element);
+	    	if(ConfigTable.isDebug() && log.isWarnEnabled()){
+	    		log.warn("[存储缓存数据][channel:{}][key:{}][生存:0/{}]",channel,element.getObjectKey(),cache.getCacheConfiguration().getTimeToLiveSeconds());
+	    	}
+		}
+	}
+	
+	public static boolean remove(String channel, String key){
+		boolean result = true;
+		try{
+			Cache cache = getCache(channel);
+			if(null != cache){
+				cache.remove(key);
+			}
+	    	if(ConfigTable.isDebug() && log.isWarnEnabled()){
+	    		log.warn("[删除缓存数据] [channel:{}][key:{}]",channel, key);
+	    	}
+		}catch(Exception e){
+			result = false;
+		}
+    	return result;
+	}
+	public static boolean clear(String channel){
+		boolean result = true;
+		try{
+			CacheManager manager = createManager();
+			manager.removeCache(channel);
+	    	if(ConfigTable.isDebug() && log.isWarnEnabled()){
+	    		log.warn("[清空缓存数据] [channel:{}]",channel);
+	    	}
+		}catch(Exception e){
+			result = false;
+		}
+		return result;
+	} 
+	/*
+	 * 辅助缓存刷新控制, N秒内只接收一次刷新操作
+	 * 调用刷新方法前,先调用start判断是否可刷新,刷新完成后调用stop
+	 * start与stop使用同一个key,
+	 * 其中两次刷新间隔时间在anyline-config中设置单位秒<property key="key">sec</property>
+	 */
+    /**
+     * 开始刷新
+     * 如果不符合刷新条件返回false
+     * @param key key
+     * @param sec sec
+     * @return return
+     */
+    public static boolean start(String key, int sec){
+    	boolean result = false;
+    	Long fr = reflushFlag.get(key);
+    	long age = -1;			//已生存
+    	if(null == fr){
+    		result = true;
+    	}else{
+	    	age = (System.currentTimeMillis() - fr) / 1000;
+	    	if(age > sec){
+	    		result = true;
+	    	}
+    	}
+    	if(result){
+    		reflushFlag.put(key, System.currentTimeMillis());
+    		if(ConfigTable.isDebug() && log.isWarnEnabled()){
+    			log.warn("[频率控制放行][key:{}][间隔:{}/{}]", key, age, sec);
+    		}
+    	}else{
+    		if(ConfigTable.isDebug() && log.isWarnEnabled()){
+    			log.warn("[频率控制拦截][key:{}][间隔:{}/{}]", key, age, sec);
+    		}
+    	}
+    	return result;
+    }
+    public static boolean start(String key){
+    	int period = ConfigTable.getInt(key, 120);		//两次刷新最小间隔
+    	return start(key, period);
+    }
+    /**
+     * 刷新完成
+     * @param key key
+     * @param sec sec
+     */
+    public static void stop(String key, int sec){
+    	Long fr = reflushFlag.get(key);
+    	if(null == fr){
+    		if(ConfigTable.isDebug() && log.isWarnEnabled()){
+    			log.warn("[频率控制还原完成 有可能key拼写有误][key:{}]",key);
+    		}
+    		return;
+    	}
+    	long age = (System.currentTimeMillis() - fr)/1000;			//已生存
+    	
+    	if(age > sec){
+    		reflushFlag.remove(key);
+    	}
+		if(ConfigTable.isDebug() && log.isWarnEnabled()){
+			log.warn("[频率控制还原完成][key:{}][间隔:{}/{}]", key, age, sec);
+		}
+    }
+    public static void stop(String key){
+    	int period = ConfigTable.getInt(key, 120);					//两次刷新最小间隔
+    	stop(key,period);
+    }
+    public boolean isRun(String key){
+    	if(null == reflushFlag.get(key)){
+    		return false;
+    	}
+    	return true;
+    }
+    /**
+     * 已执行时间
+     * @param key key
+     * @return return
+     */
+    public long getRunTime(String key){
+    	long result = -1;
+    	Long fr = reflushFlag.get(key);
+    	if(null != fr){
+    		return System.currentTimeMillis() - fr;
+    	}
+    	return result;
+    }
+    /**
+     * 创建集中缓存的key
+     * @param table table
+     * @param row row
+     * @return return
+     */
+    public static String crateCachePrimaryKey(String table, DataRow row){
+    	String key = table;
+    	List<String> pks = row.getPrimaryKeys();
+    	if(BasicUtil.isNotEmpty(pks) && null != row){
+    		for(String pk:pks){
+    			String value = row.getString(pk);
+    			key += "|" + pk + "=" + value;
+    		}
+    	}
+    	return key;
+    }
+    /**
+	 * 创建cache key
+	 * @param page 是否需要拼接分页下标
+	 * @param order order
+	 * @param src src
+	 * @param store store
+	 * @param conditions conditions
+	 * @return return
+	 */
+	public static String createCacheElementKey(boolean page, boolean order, String src, ConfigStore store, String ... conditions){
+		conditions = BasicUtil.compressionSpace(conditions);
+		String result = src+"|";
+		if(null != store){
+			ConfigChain chain = store.getConfigChain();
+			if(null != chain){
+				List<Config> configs = chain.getConfigs();
+				if(null != configs){
+					for(Config config:configs){
+						List<Object> values = config.getValues();
+						if(null != values){
+							result += config.toString() + "|";
+						}
+					}	
+				}
+			}
+			PageNavi navi = store.getPageNavi();
+			if(page && null != navi){
+				result += "page=" + navi.getCurPage()+"|first=" + navi.getFirstRow() + "|last="+navi.getLastRow()+"|";
+			}
+			if(order){
+				OrderStore orders = store.getOrders();
+				if(null != orders){
+					result += orders.getRunText("").toUpperCase() +"|";
+				}
+			}
+		}
+		if(null != conditions){
+			for(String condition:conditions){
+				if(BasicUtil.isNotEmpty(condition)){
+					if(condition.trim().toUpperCase().startsWith("ORDER")){
+						if(order){
+							result += condition.toUpperCase() + "|";
+						}
+					}else{
+						result += condition+"|";
+					}
+				}
+			}
+		}
+		if(ConfigTable.isDebug() && log.isWarnEnabled()){
+			log.warn("[create cache key][key:{}]",result);
+		}
+		return MD5Util.crypto(result);
+	} 
+} 
---
+++ anyline-jdbc/src/main/java/org/anyline/jdbc/config/db/sql/xml/impl/XMLSQLImpl.java
@@ -0,0 +1,370 @@
+/* 
+ * Copyright 2006-2015 www.anyline.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *          
+ */
+
+
+package org.anyline.jdbc.config.db.sql.xml.impl; 
+ 
+import java.util.ArrayList; 
+import java.util.List; 
+
+import org.anyline.jdbc.config.ConfigParser;
+import org.anyline.jdbc.config.ParseResult;
+import org.anyline.jdbc.config.db.Condition;
+import org.anyline.jdbc.config.db.ConditionChain;
+import org.anyline.jdbc.config.db.SQL;
+import org.anyline.jdbc.config.db.SQLHelper;
+import org.anyline.jdbc.config.db.SQLVariable;
+import org.anyline.jdbc.config.db.impl.BasicSQL;
+import org.anyline.jdbc.config.db.impl.SQLVariableImpl;
+import org.anyline.jdbc.config.db.sql.xml.XMLSQL;
+import org.anyline.util.BasicUtil; 
+import org.anyline.util.regular.Regular;
+import org.anyline.util.regular.RegularUtil; 
+public class XMLSQLImpl extends BasicSQL implements XMLSQL{ 
+	/*解析XML*/ 
+	private String id; 
+	private String text;
+	private boolean strict = true;	//严格格式, true:不允许添加XML定义之外 的临时查询条件
+	private List<SQLVariable> variables; 
+	 
+	public XMLSQLImpl(){ 
+		super(); 
+		chain = new XMLConditionChainImpl(); 
+	} 
+	public SQL init() { 
+		if(null == variables){ 
+			variables = new ArrayList<SQLVariable>(); 
+		} 
+		for(SQLVariable variable:variables){
+			if(null == variable){
+				continue;
+			} 
+			variable.init(); 
+		} 
+		if(null != chain){ 
+			for(Condition condition:chain.getConditions()){
+				if(null == condition){
+					continue;
+				} 
+				condition.init(); 
+			} 
+		} 
+		return this; 
+	} 
+	public Object clone() throws CloneNotSupportedException{ 
+		XMLSQLImpl clone = (XMLSQLImpl)super.clone(); 
+		clone.chain = (ConditionChain)chain.clone(); 
+		if(null != variables){ 
+			List<SQLVariable> cVariables = new ArrayList<SQLVariable>(); 
+			for(SQLVariable var:variables){
+				if(null == var){
+					continue;
+				} 
+				cVariables.add((SQLVariable)var.clone()); 
+			} 
+			clone.variables = cVariables; 
+		} 
+		return clone; 
+	} 
+	/** 
+	 * 设置SQL 主体文本 
+	 * @param text  text
+	 */ 
+	public SQL setText(String text) { 
+		if(null == text){ 
+			return this; 
+		} 
+		text = text.replaceAll("--.*", "");//过滤注释 
+		this.text = text; 
+		parseText(); 
+		return this; 
+	} 
+ 
+	/** 
+	 * 添加静态文本查询条件 
+	 */ 
+	public SQL addCondition(String condition) { 
+		if(BasicUtil.isEmpty(condition)){ 
+			return this; 
+		} 
+		if(condition.contains(":")){
+			ParseResult parser = ConfigParser.parse(condition,false);
+			 
+			String id = parser.getId(); 
+			String var = null; 
+			Object value = ConfigParser.getValues(parser);//parser.getKey(); 
+			if(id.contains(".")){ 
+				String[] keys = id.split("."); 
+				id = keys[0]; 
+				if(keys.length > 1){ 
+					var = keys[1]; 
+				} 
+			} 
+			setConditionValue(id,var,value); 
+		} 
+		return this; 
+	} 
+	/** 
+	 * 解析 SQL 主体文本 
+	 */ 
+	private void parseText(){ 
+		if(null == text) {
+			return;
+		} 
+		try{
+			 
+			List<List<String>> keys = RegularUtil.fetchs(text, SQL_PARAM_VAIRABLE_REGEX, Regular.MATCH_MODE.CONTAIN);
+			int type = 1 ;
+			if(keys.size() ==0){
+				keys = RegularUtil.fetchs(text, SQL_PARAM_VAIRABLE_REGEX_EL, Regular.MATCH_MODE.CONTAIN);
+				type = 2;
+			} 
+			if(BasicUtil.isNotEmpty(true,keys)){ 
+				//AND CD = :CD 
+				for(int i=0; i<keys.size();i++){
+					List<String> keyItem = keys.get(i); 
+					SQLVariable var = SQLHelper.buildVariable(type, keyItem.get(0), keyItem.get(1), keyItem.get(2), keyItem.get(3));
+					var.setRequired(true);
+					addVariable(var); 
+				}// end for 
+			}else{ 
+				// AND CD = ? 
+				List<String> idxKeys = RegularUtil.fetch(text, "\\?",Regular.MATCH_MODE.CONTAIN,0); 
+				if(BasicUtil.isNotEmpty(true,idxKeys)){ 
+					for(int i=0; i<idxKeys.size(); i++){ 
+						SQLVariable var = new SQLVariableImpl(); 
+						var.setType(SQLVariable.VAR_TYPE_INDEX);
+						var.setRequired(true); 
+						addVariable(var); 
+					} 
+				} 
+			} 
+		}catch(Exception e){ 
+			e.printStackTrace(); 
+		} 
+	} 
+	/** 
+	 * 添加SQL主体变量 
+	 * @param var  var
+	 */ 
+	private void addVariable(SQLVariable var){ 
+		if(null == variables){ 
+			variables = new ArrayList<SQLVariable>(); 
+		}
+		variables.add(var); 
+	} 
+ 
+ 
+	/* *********************************************************************************************************************************** 
+	 *  
+	 * 														赋值 
+	 *  
+	 * ***********************************************************************************************************************************/ 
+	/** 
+	 * 添加查询条件 
+	 * @param	condition 
+	 * 			列名|查询条件ID 
+	 * @param	variable 
+	 * 			变量key 
+	 * @param	value 
+	 * 			值 
+	 */ 
+	public SQL setConditionValue(String condition, String variable, Object value){ 
+		/*不指定变量名时,根据condition为SQL主体变量赋值*/ 
+		if(null != variables && BasicUtil.isEmpty(variable)){ 
+			for(SQLVariable v:variables){ 
+				if(v.getKey().equalsIgnoreCase(condition)){ 
+					v.setValue(value); 
+				} 
+			} 
+		} 
+		/*参数赋值*/ 
+		if(null == condition){ 
+			return this; 
+		} 
+		XMLConditionImpl con = getCondition(condition); 
+		if(null == con){ 
+			return this; 
+		} 
+		variable = BasicUtil.nvl(variable, condition).toString(); 
+		con.setValue(variable, value); 
+		return this; 
+	} 
+	private XMLConditionImpl getCondition(String id){ 
+		if(null == chain){ 
+			return null; 
+		} 
+		for(Condition con:chain.getConditions()){ 
+			if(BasicUtil.isEqual(id, con.getId())){ 
+				return (XMLConditionImpl)con; 
+			} 
+		} 
+		return null; 
+	} 
+	/* *********************************************************************************************************************************** 
+	 *  
+	 * 														生成SQL 
+	 *  
+	 * ***********************************************************************************************************************************/ 
+//	public void appendCondition(StringBuilder builder, String disKey){ 
+//		if(null == chain){ 
+//			return; 
+//		} 
+//		builder.append(chain.getRunText(disKey)); 
+//		addRunValue(chain.getRunValues()); 
+//	} 
+//	/** 
+//	 * 生成运行时SQL 
+//	 */ 
+//	public String getRunText(String diskey){ 
+//		StringBuilder builder = new StringBuilder(); 
+//		builder.append(createRunText()); 
+//		appendCondition(builder, diskey); 
+//		return builder.toString(); 
+//	} 
+ 
+//	/** 
+//	 * 添加分组 
+//	 * @param builder  builder
+//	 */ 
+//	public void appendGroup(StringBuilder builder){ 
+//		if(null != groups && !groups.isEmpty()){ 
+//			int size = groups.size(); 
+//			builder.append(" GROUP BY "); 
+//			for(int i=0; i<size; i++){ 
+//				builder.append(groups.get(i)); 
+//				if(i<size-1){ 
+//					builder.append(","); 
+//				} 
+//			} 
+//		} 
+//	} 
+	/*  
+	 * 创建运行时主体SQL 
+	 * @param text  text
+	 * @return return
+	 */ 
+//	private String createRunText(){ 
+//		initRunValues(); 
+//		String result = text; 
+//		if(null == variables) {
+//			return result;
+//		} 
+// 
+//		for(SQLVariable var:variables){ 
+//			if(var.getType() == SQLVariable.VAR_TYPE_REPLACE){ 
+//				//CD = ::CD 
+//				Object varValue = var.getValues(); 
+//				String value = null; 
+//				if(BasicUtil.isNotEmpty(varValue)){ 
+//					value = varValue.toString(); 
+//				} 
+//				if(null != value){ 
+//					result = result.replace("::"+var.getKey(), value); 
+//				}else{ 
+//					result = result.replace("::"+var.getKey(), "NULL"); 
+//				} 
+//			} 
+//		} 
+//		for(SQLVariable var:variables){ 
+//			if(var.getType() == SQLVariable.VAR_TYPE_KEY_REPLACE){ 
+//				//CD = ':CD' 
+//				List<Object> varValues = var.getValues(); 
+//				String value = null; 
+//				if(BasicUtil.isNotEmpty(true,varValues)){ 
+//					value = (String)varValues.get(0); 
+//				} 
+//				if(null != value){ 
+//					result = result.replace(":"+var.getKey(), value); 
+//				}else{ 
+//					result = result.replace(":"+var.getKey(), ""); 
+//				} 
+//			} 
+//		} 
+//		for(SQLVariable var:variables){ 
+//			if(var.getType() == SQLVariable.VAR_TYPE_KEY){ 
+//				// CD = :CD 
+//				List<Object> varValues = var.getValues(); 
+//				if(BasicUtil.isNotEmpty(true, varValues)){ 
+//					if(var.getCompare() == SQL.COMPARE_TYPE_IN){ 
+//						//多个值IN 
+//						String replaceSrc = ":"+var.getKey(); 
+//						String replaceDst = "";  
+//						for(Object tmp:varValues){ 
+//							addRunValue(tmp); 
+//							replaceDst += " ?"; 
+//						} 
+//						replaceDst = replaceDst.trim().replace(" ", ","); 
+//						result = result.replace(replaceSrc, replaceDst); 
+//					}else{ 
+//						//单个值 
+//						result = result.replace(":"+var.getKey(), "?"); 
+//						addRunValue(varValues.get(0)); 
+//					} 
+//				} 
+//			} 
+//		} 
+//		//添加其他变量值 
+//		for(SQLVariable var:variables){ 
+//			//CD = ? 
+//			if(var.getType() == SQLVariable.VAR_TYPE_INDEX){ 
+//				List<Object> varValues = var.getValues(); 
+//				String value = null; 
+//				if(BasicUtil.isNotEmpty(true, varValues)){ 
+//					value = (String)varValues.get(0); 
+//				} 
+//				addRunValue(value); 
+//			} 
+//		} 
+//		return result; 
+//	} 
+ 
+	public SQL setDataSource(String ds){ 
+		this.id = ds; 
+		return this; 
+	} 
+	public String getDataSource(){ 
+		return id ; 
+	} 
+	public String getSchema(){ 
+		return null; 
+	} 
+	@Override 
+	public String getText() { 
+		return this.text; 
+	} 
+	@Override 
+	public List<SQLVariable> getSQLVariables() { 
+		return this.variables; 
+	}
+	@Override
+	public String getTable() {
+		return null;
+	}
+	@Override
+	public List<String> getColumns() {
+		return null;
+	}
+	public boolean isStrict() {
+		return strict;
+	}
+	public void setStrict(boolean strict) {
+		this.strict = strict;
+	}
+	
+} 
