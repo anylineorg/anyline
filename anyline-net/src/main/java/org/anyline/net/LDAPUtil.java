b64769e4e7ee797973bbf8cf014590a2c69efa1c
zhang <zh>
2019-12-26 16:23:08 +0800

    ldap

--- anyline-net/src/main/java/org/anyline/net/LDAPUtil.java
+++ anyline-net/src/main/java/org/anyline/net/LDAPUtil.java
@@ -13,14 +13,6 @@ import org.slf4j.LoggerFactory;
 
 public class LDAPUtil {
 	private static Logger log = LoggerFactory.getLogger(LDAPUtil.class);
-	/**
-	 * 登录
-	 * @param url 登录地址 如 http://ladp.anyline.org:389
-	 * @param account 登录帐号 如 admin@anyline.org
-	 * @param password 登录密码
-	 * @return boolean
-	 * @throws NamingException 如果抛出异常表示登录失败
-	 */
     public boolean login(String url, String account, String password) {
         try{
         	connect(url, account, password);
@@ -30,6 +22,14 @@ public class LDAPUtil {
         }
         return true;
     }
+	/**
+	 * 连接
+	 * @param url 登录地址 如 http://ladp.anyline.org:389
+	 * @param account 登录帐号 如 admin@anyline.org
+	 * @param password 登录密码
+	 * @return LdapContext
+	 * @throws NamingException 如果抛出异常表示登录失败
+	 */
     public LdapContext connect(String url, String account, String password) throws NamingException {
         Hashtable<String, Object> env = new Hashtable<String, Object>();
         env.put(Context.SECURITY_PRINCIPAL, account);		//用户名
