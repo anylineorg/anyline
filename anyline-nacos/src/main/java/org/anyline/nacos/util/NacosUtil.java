fb0a92eaa55f89357485a317fff211e1c7dc2db3
zhang <zh>
2020-01-07 12:44:40 +0800

    nacos listener

--- anyline-nacos/src/main/java/org/anyline/nacos/util/NacosUtil.java
+++ anyline-nacos/src/main/java/org/anyline/nacos/util/NacosUtil.java
@@ -43,14 +43,13 @@ public class NacosUtil {
 		Properties properties = new Properties();
 		properties.put(PropertyKeyConst.SERVER_ADDR, config.ADDRESS+":"+config.PORT);
 		ConfigService configService = NacosFactory.createConfigService(properties);
-		String content = configService.getConfig(data, group, 5000);
+		String content = configService.getConfig(data, group, config.TIMEOUT);
 		parse(T, content);
 		configService.addListener(data, group, new Listener() {
 			@Override
 			public void receiveConfigInfo(String content) {
 				parse(T, content);
 			}
-
 			@Override
 			public Executor getExecutor() {
 				return null;
@@ -58,6 +57,34 @@ public class NacosUtil {
 		});
 		result = true;
 		return result;
+	}
+
+	/**
+	 * 读取配置文件 并 监听配置更新
+	 * @param group group
+	 * @param data data
+	 * @param listener listent
+	 * @return String
+	 * @throws NacosException NacosException
+	 */
+	public String config(String group, String data, Listener listener) throws NacosException{
+		Properties properties = new Properties();
+		properties.put(PropertyKeyConst.SERVER_ADDR, config.ADDRESS+":"+config.PORT);
+		ConfigService configService = NacosFactory.createConfigService(properties);
+		String content = configService.getConfig(data, group, config.TIMEOUT);
+		if(null != listener) {
+			configService.addListener(data, group, listener);
+		}
+		return content;
+	}
+	public String config(String group, String data){
+		try{
+			Listener listener = null;
+			return config(group, data, listener);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public static void parse(Class<? extends AnylineConfig> T, String content) {
