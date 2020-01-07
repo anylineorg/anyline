2960316bb6c73941666df9d4ad28f03d165fc363
zhang <zh>
2020-01-07 19:18:43 +0800

    nacos listener

--- anyline-nacos/src/main/java/org/anyline/nacos/util/NacosConfig.java
+++ anyline-nacos/src/main/java/org/anyline/nacos/util/NacosConfig.java
@@ -12,6 +12,7 @@ public class NacosConfig extends AnylineConfig{
 	public String ADDRESS;
 	public int PORT = 8848;
 	public int TIMEOUT = 5000;
+	public String NAMESAPCE = "public";
 	public String GROUP = "DEFAULT_GROUP";
 	public boolean AUTO_SCAN = true;
 	static{
