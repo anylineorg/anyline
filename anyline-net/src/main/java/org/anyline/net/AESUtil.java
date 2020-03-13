5da399f1a157083764a664dacf9d60cd01e782b5
zhang <zh@anyline.org>
2020-03-13 21:36:09 +0800

    aes

--- anyline-net/src/main/java/org/anyline/net/AESUtil.java
+++ anyline-net/src/main/java/org/anyline/net/AESUtil.java
@@ -18,68 +18,96 @@
  
 package org.anyline.net; 
  
-import java.security.NoSuchAlgorithmException; 
+import java.security.AlgorithmParameters;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom; 
  
 import javax.crypto.Cipher; 
 import javax.crypto.KeyGenerator; 
-import javax.crypto.SecretKey; 
-import javax.crypto.spec.SecretKeySpec; 
- 
-import org.apache.commons.codec.binary.Base64; 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.anyline.util.Base64Util;
+import org.anyline.util.BasicUtil;
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger; 
 import org.slf4j.LoggerFactory; 
  
  
 public class AESUtil { 
 	private static Logger log = LoggerFactory.getLogger(AESUtil.class); 
-	private static final String KEY_ALGORITHM = "AES"; 
-	private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";// 默认的加密算法 
- 
+	private static final String KEY = "AES";
+	public static enum CIPHER{
+		PKCS5			{public String getCode(){return "AES/ECB/PKCS5Padding";}},
+		PKCS7			{public String getCode(){return "AES/CBC/PKCS7Padding";}};
+		public abstract String getCode();
+	};
 	/** 
 	 * AES 加密操作 
-	 *  
-	 * @param content  content
-	 *            待加密内容 
-	 * @param password  password
-	 *            加密密码 
+	 *
+	 * @param cipher  cipher
+	 * @param content  content 待加密内容
+	 * @param password  password 加密密码
 	 * @return 返回Base64转码后的加密数据 
 	 */ 
-	public static String encrypt(String content, String password) { 
-		try { 
-			Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);// 创建密码器 
-			byte[] byteContent = content.getBytes("utf-8"); 
-			cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(password));// 初始化为加密模式的密码器 
-			byte[] result = cipher.doFinal(byteContent);// 加密 
+	public static String encrypt(CIPHER cipher, String password, String content) {
+		try {
+			Cipher cipherInstance = Cipher.getInstance(cipher.getCode());// 创建密码器
+			byte[] byteContent = content.getBytes("utf-8");
+			cipherInstance.init(Cipher.ENCRYPT_MODE, getSecretKey(password));// 初始化为加密模式的密码器
+			byte[] result = cipherInstance.doFinal(byteContent);// 加密
 			return Base64.encodeBase64String(result);// 通过Base64转码返回 
 		} catch (Exception ex) { 
 			log.error(ex.getMessage()); 
 		} 
 		return null; 
-	} 
+	}
+	public static String encrypt(String password, String content) {
+		return encrypt(CIPHER.PKCS5, password, content);
+	}
  
 	/** 
 	 * AES 解密操作 
-	 *  
+	 *
+	 * @param cipher  cipher
 	 * @param content  content
 	 * @param password  password
 	 * @return return
 	 */ 
-	public static String decrypt(String content, String password) { 
+	public static String decrypt(CIPHER cipher, String password, String vector, String content) {
 		try { 
 			// 实例化 
-			Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM); 
-			// 使用密钥初始化，设置为解密模式 
-			cipher.init(Cipher.DECRYPT_MODE, getSecretKey(password)); 
+			Cipher cipherInstance = Cipher.getInstance(cipher.getCode());
+			// 使用密钥初始化，设置为解密模式
+			if(BasicUtil.isNotEmpty(vector)){
+				AlgorithmParameters algorithmParameters = AlgorithmParameters.getInstance(KEY);
+				algorithmParameters.init(new IvParameterSpec(Base64Util.decode(vector)));
+				Key key = new SecretKeySpec(Base64Util.decode(password), KEY);
+				cipherInstance.init(Cipher.DECRYPT_MODE, key,algorithmParameters);
+			}else {
+				cipherInstance.init(Cipher.DECRYPT_MODE, getSecretKey(password));
+			}
 			// 执行操作 
-			byte[] result = cipher.doFinal(Base64.decodeBase64(content)); 
+			byte[] result = cipherInstance.doFinal(Base64Util.decode(content));
 			return new String(result, "utf-8"); 
-		} catch (Exception ex) { 
+		} catch (Exception ex) {
+			ex.printStackTrace();
 			log.error(ex.getMessage()); 
 		} 
 		return null; 
-	} 
- 
+	}
+	public static String decrypt(CIPHER cipher, String password, String content) {
+		return decrypt(cipher, password, null, content);
+	}
+	public static String decrypt(String password, String vector, String content) {
+		return decrypt(CIPHER.PKCS5, password, vector, content);
+	}
+
+	public static String decrypt(String password, String content) {
+		return decrypt(CIPHER.PKCS5, password, null, content);
+	}
 	/** 
 	 * 生成加密秘钥 
 	 *  
@@ -87,15 +115,16 @@ public class AESUtil {
 	 */ 
 	private static SecretKeySpec getSecretKey(final String password) { 
 		// 返回生成指定算法密钥生成器的 KeyGenerator 对象 
-		KeyGenerator kg = null; 
-		try { 
-			kg = KeyGenerator.getInstance(KEY_ALGORITHM); 
+		KeyGenerator kg = null;
+		try {
+			kg = KeyGenerator.getInstance(KEY);
 			// AES 要求密钥长度为 128 
-			kg.init(128, new SecureRandom(password.getBytes())); 
+			kg.init(128, new SecureRandom(password.getBytes()));
 			// 生成一个密钥 
 			SecretKey secretKey = kg.generateKey(); 
-			return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);// 转换为AES专用密钥 
-		} catch (NoSuchAlgorithmException ex) { 
+			return new SecretKeySpec(secretKey.getEncoded(), KEY);// 转换为AES专用密钥
+		} catch (Exception ex) {
+			ex.printStackTrace();
 			log.error(ex.getMessage()); 
 		} 
 		return null; 
