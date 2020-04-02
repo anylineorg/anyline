b7751cef975a3115ae8fa4df0d24b3929ce6cf66
zhang <zh@anyline.org>
2020-04-02 09:39:13 +0800

    git util

--- anyline-ext/src/main/java/org/anyline/util/GifUtil.java
+++ anyline-ext/src/main/java/org/anyline/util/GifUtil.java
@@ -23,11 +23,11 @@ public class GifUtil {
 	private static Logger log = LoggerFactory.getLogger(GifUtil.class); 
     /** 
      * 拆分gif 
-     * @param file  file
+     * @param file  gif文件
      * @param dir 解压目录 
-     * @return return
+     * @return List<File>
      */ 
-    public static List<File> splitGif(File file, File dir){ 
+    public static List<File> split(File file, File dir){
     	List<File> files = new ArrayList<File>(); 
     	FileImageInputStream in = null; 
     	FileImageOutputStream out = null; 
@@ -55,38 +55,37 @@ public class GifUtil {
     } 
     /** 
      *  
-     * @param delay 播放延迟时间   
-     * @param tar  tar
-     * @param srcs  srcs
+     * @param delay 播放间隔时间
+     * @param tar  生成文件
+     * @param srcs  源图片列表
      */ 
-    public synchronized static void createGif(int delay, String tar, String ... srcs) { 
+    public synchronized static void create(int delay, String tar, String ... srcs) {
     	List<File> list = new ArrayList<File>(); 
     	for(String src:srcs){ 
     		list.add(new File(src)); 
-    	} 
-    	createGif(delay, new File(tar), list); 
+    	}
+		create(delay, new File(tar), list);
     } 
-    public synchronized static void createGif(int delay, String tar, List<String> srcs) { 
+    public synchronized static void create(int delay, String tar, List<String> srcs) {
     	List<File> list = new ArrayList<File>(); 
     	for(String src:srcs){ 
     		list.add(new File(src)); 
-    	} 
-    	createGif(delay, new File(tar), list); 
+    	}
+		create(delay, new File(tar), list);
     } 
-    public synchronized static void createGif(int delay, File tar, File ... srcs) { 
+    public synchronized static void create(int delay, File tar, File ... srcs) {
     	List<File> list = new ArrayList<File>(); 
     	for(File src:srcs){ 
     		list.add(src); 
-    	} 
-    	createGif(delay, tar, list); 
+    	}
+		create(delay, tar, list);
     } 
-    /** 
-     *  
-     * @param delay 播放延迟时间   
-     * @param tar  tar
-     * @param srcs  srcs
+    /**
+	 * @param delay 播放间隔时间(milliseconds)
+	 * @param tar  生成文件
+	 * @param srcs  源图片列表
      */ 
-    public synchronized static void createGif(int delay, File tar, List<File> srcs) {   
+    public synchronized static void create(int delay, File tar, List<File> srcs) {
         try { 
         	long fr = 0; 
         	File dir = tar.getParentFile(); 
@@ -112,10 +111,17 @@ public class GifUtil {
         } catch (Exception e) { 
             e.printStackTrace();   
         }   
-    }   
- 
-    public synchronized static void createGif(int delay0, int delay1, File tar, List<File> srcs) {   
-        try { 
+    }
+
+	/**
+	 *
+	 * @param min 最小间隔时间(milliseconds)
+	 * @param max 最大间隔时间(milliseconds)
+	 * @param tar 生成文件
+	 * @param srcs 源图片集合
+	 */
+	public synchronized static void create(int min, int max, File tar, List<File> srcs) {
+        try {
         	long fr = 0; 
         	File dir = tar.getParentFile(); 
         	if(!dir.exists()){ 
@@ -129,7 +135,7 @@ public class GifUtil {
             for (int i = 0; i < size; i++) { 
             	File item = srcs.get(i); 
             	fr = System.currentTimeMillis(); 
-            	int delay = NumberUtil.random(delay0, delay1); 
+            	int delay = NumberUtil.random(min, max);
                 e.setDelay(delay); //设置播放的延迟时间   
                 src[i] = ImageIO.read(item); // 读入需要播放的jpg文件   
                 e.addFrame(src[i]);  //添加到帧中   
@@ -141,5 +147,5 @@ public class GifUtil {
         } catch (Exception e) { 
             e.printStackTrace();   
         }   
-    }   
+    }
 } 
