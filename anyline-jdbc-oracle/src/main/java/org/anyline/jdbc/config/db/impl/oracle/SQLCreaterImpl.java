8b6353dee6ed5aa32bb63f785df121df16fafbb1
zhang <zh@anyline.org>
2020-04-17 11:21:34 +0800

    jdbc driver type

--- anyline-jdbc-oracle/src/main/java/org/anyline/jdbc/config/db/impl/oracle/SQLCreaterImpl.java
+++ anyline-jdbc-oracle/src/main/java/org/anyline/jdbc/config/db/impl/oracle/SQLCreaterImpl.java
@@ -25,7 +25,15 @@ public class SQLCreaterImpl extends BasicSQLCreaterImpl implements SQLCreater{
 	public SQLCreaterImpl(){ 
 		disKeyFr = "\""; 
 		disKeyTo = "\""; 
-	} 
+	}
+	@Override
+	public String getDisKeyFr(){
+		return disKeyFr;
+	}
+	@Override
+	public String getDisKeyTo(){
+		return disKeyTo;
+	}
 	@Override 
 	public String parseFinalQueryTxt(RunSQL run){ 
 		StringBuilder builder = new StringBuilder(); 
