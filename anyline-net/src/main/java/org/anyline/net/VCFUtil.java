17f85e89cca17213d1f1137a5f844bb2dafe0390
zhang <zh@anyline.org>
2020-05-13 10:51:34 +0800

    VCFUtil

--- anyline-net/src/main/java/org/anyline/net/VCFUtil.java
+++ anyline-net/src/main/java/org/anyline/net/VCFUtil.java
@@ -49,7 +49,6 @@ public class VCFUtil {
      * 编码 UTF8 quoted-printable
      * @param str 需要编码的字符串
      * @return 编码后的字符串
-     * @throws Exception
      */
     public static String encode(String str) {
         if (str != null) {
@@ -105,46 +104,49 @@ public class VCFUtil {
      * 解码 UTF8 quoted-printable
      * @param str 需要解码的字符串
      * @return 解码后的字符串
-     * @throws Exception
      */
-    public static String decode(String str) throws Exception {
+    public static String decode(String str){
         if (str != null) {
-            StringBuffer sb = new StringBuffer(str);
-            for (int i = 0; i < sb.length(); i++) {
-                if (sb.charAt(i) == '\n' && sb.charAt(i - 1) == '=') {
-                    sb.deleteCharAt(i - 1);
-                }
-            }
-            str = sb.toString();
-            byte[] bytes = str.getBytes("US-ASCII");
-            for (int i = 0; i < bytes.length; i++) {
-                byte b = bytes[i];
-                if (b != 95) {
-                    bytes[i] = b;
-                } else {
-                    bytes[i] = 32;
-                }
-            }
-            if (bytes != null) {
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                for (int i = 0; i < bytes.length; i++) {
-                    int b = bytes[i];
-                    if (b == '=') {
-                        try {
-                            int u = Character.digit((char) bytes[++i], 16);
-                            int l = Character.digit((char) bytes[++i], 16);
-                            if (u == -1 || l == -1) {
-                                continue;
-                            }
-                            buffer.write((char) ((u << 4) + l));
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        buffer.write(b);
+            try {
+                StringBuffer sb = new StringBuffer(str);
+                for (int i = 0; i < sb.length(); i++) {
+                    if (sb.charAt(i) == '\n' && sb.charAt(i - 1) == '=') {
+                        sb.deleteCharAt(i - 1);
                     }
                 }
-                str = new String(buffer.toByteArray(), "UTF-8");
+                str = sb.toString();
+                byte[] bytes = str.getBytes("US-ASCII");
+                for (int i = 0; i < bytes.length; i++) {
+                    byte b = bytes[i];
+                    if (b != 95) {
+                        bytes[i] = b;
+                    } else {
+                        bytes[i] = 32;
+                    }
+                }
+                if (bytes != null) {
+                    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                    for (int i = 0; i < bytes.length; i++) {
+                        int b = bytes[i];
+                        if (b == '=') {
+                            try {
+                                int u = Character.digit((char) bytes[++i], 16);
+                                int l = Character.digit((char) bytes[++i], 16);
+                                if (u == -1 || l == -1) {
+                                    continue;
+                                }
+                                buffer.write((char) ((u << 4) + l));
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            buffer.write(b);
+                        }
+                    }
+                    str = new String(buffer.toByteArray(), "UTF-8");
+                }
+            }catch(Exception e){
+                e.printStackTrace();
             }
         }
         return str;
