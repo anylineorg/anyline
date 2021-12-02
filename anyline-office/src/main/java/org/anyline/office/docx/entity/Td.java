ee6aa76d2a77412e0ead5eadefa6ca98fc1d74ed
anylineorg <server@anyline.org>
2021-12-02 13:43:38 +0800

    td

--- anyline-office/src/main/java/org/anyline/office/docx/entity/Td.java
+++ anyline-office/src/main/java/org/anyline/office/docx/entity/Td.java
@@ -6,11 +6,16 @@ import org.dom4j.Element;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Td extends DataRow {
+public class Td {
 
     private Map<String,String> styles = new HashMap();
     private Element src;
-
+    private String text = null;
+    private int colspan = 1;
+    private int rowspan = 1;
+    private String clazz = null;
+    private int merge = -1;
+    private boolean remove = false;
     public Map<String, String> getStyles() {
         return styles;
     }
@@ -25,5 +30,77 @@ public class Td extends DataRow {
 
     public void setSrc(Element src) {
         this.src = src;
+    }
+
+    public int getColspan() {
+        return colspan;
+    }
+    public boolean isEmpty(){
+        if(null != src){
+            return false;
+        }
+        if(null != clazz){
+            return false;
+        }
+        if(null != text){
+            return false;
+        }
+        if(merge >= 0){
+            return false;
+        }
+        if(rowspan >1){
+            return false;
+        }
+        if(colspan >1){
+            return false;
+        }
+        if(isRemove()){
+            return false;
+        }
+        return true;
+    }
+    public void setColspan(int colspan) {
+        this.colspan = colspan;
+    }
+
+    public int getRowspan() {
+        return rowspan;
+    }
+
+    public void setRowspan(int rowspan) {
+        this.rowspan = rowspan;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+
+    public int getMerge() {
+        return merge;
+    }
+
+    public void setMerge(int merge) {
+        this.merge = merge;
+    }
+
+    public boolean isRemove() {
+        return remove;
+    }
+
+    public void setRemove(boolean remove) {
+        this.remove = remove;
     }
 }
