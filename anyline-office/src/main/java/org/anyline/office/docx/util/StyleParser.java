c8cdbec6bf3e84bec22c31cae38ae49eadcf2839
anylineorg <server@anyline.org>
2021-12-03 15:57:36 +0800

    style inherit

--- anyline-office/src/main/java/org/anyline/office/docx/util/StyleParser.java
+++ anyline-office/src/main/java/org/anyline/office/docx/util/StyleParser.java
@@ -14,30 +14,30 @@ import java.util.List;
 import java.util.Map;
 
 public class StyleParser {
-    public static Map<String,Map<String,String>> load(String html){
+    public static Map<String, Map<String, String>> load(String html) {
         String style = html;
-        if(html.contains("<style")){
-            style = RegularUtil.cut(html,"<style",">","</style>");
+        if (html.contains("<style")) {
+            style = RegularUtil.cut(html, "<style", ">", "</style>");
         }
-        Map<String,Map<String,String>> styles = new HashMap<String,Map<String,String>>();
-        if(null != style){
-            while(style.contains("}")){
-                String item = RegularUtil.cut(style,"{begin}","}");
-                if(null == item){
+        Map<String, Map<String, String>> styles = new HashMap<String, Map<String, String>>();
+        if (null != style) {
+            while (style.contains("}")) {
+                String item = RegularUtil.cut(style, "{begin}", "}");
+                if (null == item) {
                     break;
                 }
-                style = style.substring(item.length()+1);
+                style = style.substring(item.length() + 1);
                 item = item.trim();
-                String key = RegularUtil.cut(item,"{begin}","{");
-                String value = RegularUtil.cut(item,"{","{end}");
-                if(null == key || null ==value){
+                String key = RegularUtil.cut(item, "{begin}", "{");
+                String value = RegularUtil.cut(item, "{", "{end}");
+                if (null == key || null == value) {
                     continue;
                 }
                 key = key.trim();
                 value = value.trim();
                 String[] keys = key.split(",");
-                for(String k:keys){
-                    Map<String,String> tmps = parse(value);
+                for (String k : keys) {
+                    Map<String, String> tmps = parse(value);
                     BeanUtil.merge(tmps, styles.get(k));
                     styles.put(k, tmps);
                 }
@@ -46,10 +46,10 @@ public class StyleParser {
         return styles;
     }
 
-    public static Map<String,String> parse(String txt){
-        Map<String,String> styles = new HashMap<String,String>();
+    public static Map<String, String> parse(String txt) {
+        Map<String, String> styles = new HashMap<String, String>();
 
-        if(null != txt) {
+        if (null != txt) {
             txt = BasicUtil.compressionSpace(txt);//多个空格压缩成一个
             String[] items = txt.split(";");
             for (String item : items) {
@@ -59,31 +59,11 @@ public class StyleParser {
                         String k = kv[0].trim();
                         String v = kv[1].trim();
                         styles.put(k, v);
-                        if("border".equalsIgnoreCase(k)){
-                            v = v.replace("solid","single");
+                        if ("border".equalsIgnoreCase(k)) {
+                            v = v.replace("solid", "single");
                             String[] vs = v.split(" ");
                             //border:5px solid red;
-                            if("none".equalsIgnoreCase(v)){
-                                styles.remove("border-top-width");
-                                styles.remove("border-top-style");
-                                styles.remove("border-top-color");
-                                styles.remove("border-right-width");
-                                styles.remove("border-right-style");
-                                styles.remove("border-right-color");
-                                styles.remove("border-bottom-width");
-                                styles.remove("border-bottom-style");
-                                styles.remove("border-bottom-color");
-                                styles.remove("border-left-width");
-                                styles.remove("border-left-style");
-                                styles.remove("border-left-color");
-                                styles.remove("border-insideV-width");
-                                styles.remove("border-insideV-style");
-                                styles.remove("border-insideV-color");
-                                styles.remove("border-insideH-width");
-                                styles.remove("border-insideH-style");
-                                styles.remove("border-insideH-color");
-                            }
-                            else if(vs.length ==3){
+                            if (vs.length == 3) {
                                 styles.put("border-top-width", vs[0]);
                                 styles.put("border-top-style", vs[1]);
                                 styles.put("border-top-color", vs[2]);
@@ -103,9 +83,9 @@ public class StyleParser {
                                 styles.put("border-insideH-style", vs[1]);
                                 styles.put("border-insideH-color", vs[2]);
                             }
-                        }else if("border-width".equalsIgnoreCase(k)){
+                        } else if ("border-width".equalsIgnoreCase(k)) {
                             String[] vs = v.split(" ");
-                            if(vs.length ==2){
+                            if (vs.length == 2) {
                                 styles.put("border-top-width", vs[0]);
                                 styles.put("border-bottom-width", vs[0]);
                                 styles.put("border-insideH-width", vs[0]);
@@ -113,10 +93,10 @@ public class StyleParser {
                                 styles.put("border-right-width", vs[1]);
                                 styles.put("border-insideV-width", vs[1]);
                             }
-                        }else if("border-style".equalsIgnoreCase(k)){
-                            v = v.replace("solid","single");
+                        } else if ("border-style".equalsIgnoreCase(k)) {
+                            v = v.replace("solid", "single");
                             String[] vs = v.split(" ");
-                            if(vs.length ==2){
+                            if (vs.length == 2) {
                                 styles.put("border-top-style", vs[0]);
                                 styles.put("border-bottom-style", vs[0]);
                                 styles.put("border-insideH-style", vs[0]);
@@ -124,9 +104,9 @@ public class StyleParser {
                                 styles.put("border-right-style", vs[1]);
                                 styles.put("border-insideV-style", vs[1]);
                             }
-                        }else if("border-color".equalsIgnoreCase(k)){
+                        } else if ("border-color".equalsIgnoreCase(k)) {
                             String[] vs = v.split(" ");
-                            if(vs.length ==2){
+                            if (vs.length == 2) {
                                 styles.put("border-top-color", vs[0]);
                                 styles.put("border-bottom-color", vs[0]);
                                 styles.put("border-insideH-color", vs[0]);
@@ -134,21 +114,21 @@ public class StyleParser {
                                 styles.put("border-right-color", vs[1]);
                                 styles.put("border-insideV-color", vs[0]);
                             }
-                        }else if("font".equalsIgnoreCase(k)){
-                            if("bold".equalsIgnoreCase(v)){
-                                styles.put("font-weight","700");
+                        } else if ("font".equalsIgnoreCase(k)) {
+                            if ("bold".equalsIgnoreCase(v)) {
+                                styles.put("font-weight", "700");
                             }
-                        }else if("font-weight".equalsIgnoreCase(k)){
-                            if("bold".equalsIgnoreCase(v)){
-                                styles.put("font-weight","700");
+                        } else if ("font-weight".equalsIgnoreCase(k)) {
+                            if ("bold".equalsIgnoreCase(v)) {
+                                styles.put("font-weight", "700");
                             }
-                        }else if("font-style".equalsIgnoreCase(k)){
-                            if("italic".equalsIgnoreCase(v)){
-                                styles.put("italic","true");
+                        } else if ("font-style".equalsIgnoreCase(k)) {
+                            if ("italic".equalsIgnoreCase(v)) {
+                                styles.put("italic", "true");
                             }
-                        }else if("text-decoration".equalsIgnoreCase(k)){
-                            if("underline".equalsIgnoreCase(v)){
-                                styles.put("underline","true");
+                        } else if ("text-decoration".equalsIgnoreCase(k)) {
+                            if ("underline".equalsIgnoreCase(v)) {
+                                styles.put("underline", "true");
                             }
                         }
                     }
@@ -157,12 +137,76 @@ public class StyleParser {
         }
         return styles;
     }
+    public static Map<String,String> inherit(Map<String, String> src, Map<String, String> parent){
+        if(null == src){
+            src = new HashMap<>();
+        }
+
+        if(null != parent){
+            for(String k: parent.keySet()){
+                if(k.contains("font") || k.contains("align")) {
+                    src.put(k, parent.get(k));
+                }
+            }
+        }
+
+        return src;
+    }
+    public static Map<String, String> merge(Map<String, String> src, Map<String, String> copy) {
+        if(null == src){
+            src = new HashMap<>();
+        }
+
+        if(null == copy){
+            copy = new HashMap<>();
+        }
+
+        if ("none".equalsIgnoreCase(copy.get("border"))) {
+            src.remove("border-top-width");
+            src.remove("border-top-style");
+            src.remove("border-top-color");
+            src.remove("border-right-width");
+            src.remove("border-right-style");
+            src.remove("border-right-color");
+            src.remove("border-bottom-width");
+            src.remove("border-bottom-style");
+            src.remove("border-bottom-color");
+            src.remove("border-left-width");
+            src.remove("border-left-style");
+            src.remove("border-left-color");
+            src.remove("border-insideV-width");
+            src.remove("border-insideV-style");
+            src.remove("border-insideV-color");
+            src.remove("border-insideH-width");
+            src.remove("border-insideH-style");
+            src.remove("border-insideH-color");
+        }
+        if ("none".equalsIgnoreCase(copy.get("border-left"))) {
+            src.remove("border-left-width");
+            src.remove("border-left-style");
+            src.remove("border-left-color");
+        }
+        if ("none".equalsIgnoreCase(copy.get("border-right"))) {
+            src.remove("border-right-width");
+            src.remove("border-right-style");
+            src.remove("border-right-color");
+        }
+        if ("none".equalsIgnoreCase(copy.get("border-top"))) {
+            src.remove("border-top-width");
+            src.remove("border-top-style");
+            src.remove("border-top-color");
+        }
+        if ("none".equalsIgnoreCase(copy.get("border-bottom"))) {
+            src.remove("border-bottom-width");
+            src.remove("border-bottom-style");
+            src.remove("border-bottom-color");
+        }
+        BeanUtil.merge(src, copy);
+        return src;
+    }
 
     public static Map<String,String> parse(Map<String,String> src, String txt){
-        if(null == src){
-            src = new HashMap<String,String>();
-        }
-        BeanUtil.merge(src,StyleParser.parse(txt));
-        return src;
+        Map<String,String> copy = StyleParser.parse(txt);
+        return merge(src, copy);
     }
 }
