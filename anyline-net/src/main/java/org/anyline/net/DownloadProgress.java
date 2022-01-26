570efe935c999798ef74c583a099422dda858a2c
anylineorg <server@anyline.org>
2022-01-26 20:29:58 +0800

    下载任务

--- anyline-net/src/main/java/org/anyline/net/DownloadProgress.java
+++ anyline-net/src/main/java/org/anyline/net/DownloadProgress.java
@@ -5,16 +5,16 @@ public interface DownloadProgress {
 	/** 
 	 * 加载历史进度 
 	 * @param url  url
-	 * @param thread	线程号  thread	线程号
-	 * @param total		本次需下载  total		本次需下载
-	 * @param past		历史进度  past		历史进度
+	 * @param thread	线程号
+	 * @param total		本次需下载
+	 * @param past		历史进度
 	 */ 
 	public void init(String url, String thread, long total, long past); 
 	/** 
 	 * 每次下载长度 
 	 * @param url  url
-	 * @param thread	线程号  thread	线程号
-	 * @param finish	本次循环下载长度  finish	本次循环下载长度
+	 * @param thread	线程号
+	 * @param finish	本次循环下载长度
 	 */ 
 	public void step(String url, String thread, long finish); 
 	/** 
