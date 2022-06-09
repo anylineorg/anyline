f08172a13a4a69576f0d927a2b67add806ad820f
deepbit <server@anyline.org>
2022-06-09 18:04:47 +0800

    AlipayTradeQuery

--- anyline-alipay/src/main/java/org/anyline/alipay/entity/AlipayTradeQuery.java
+++ anyline-alipay/src/main/java/org/anyline/alipay/entity/AlipayTradeQuery.java
@@ -4,8 +4,8 @@ package org.anyline.alipay.entity;
  * 
  */ 
 public class AlipayTradeQuery { 
-	private String out_biz_no	; //商户转账唯一订单号：发起转账来源方定义的转账单据ID。 和支付宝转账单据号不能同时为空。当和支付宝转账单据号同时提供时，将用支付宝转账单据号进行查询，忽略本参数。	 
-	private String order_id		; //支付宝转账单据号：和商户转账唯一订单号不能同时为空。当和商户转账唯一订单号同时提供时，将用本参数进行查询，忽略商户转账唯一订单号。 
+	private String out_biz_no	; //商户转账唯一订单号:发起转账来源方定义的转账单据ID. 和支付宝转账单据号不能同时为空.当和支付宝转账单据号同时提供时,将用支付宝转账单据号进行查询,忽略本参数.	 
+	private String order_id		; //支付宝转账单据号:和商户转账唯一订单号不能同时为空.当和商户转账唯一订单号同时提供时,将用本参数进行查询,忽略商户转账唯一订单号. 
 	public String getOut_biz_no() { 
 		return out_biz_no; 
 	} 
