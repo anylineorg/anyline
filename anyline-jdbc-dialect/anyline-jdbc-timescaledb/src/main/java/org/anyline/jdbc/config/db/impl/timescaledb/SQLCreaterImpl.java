f7be13179ecc10b2fd4ef7bc3b794ae8370e6193
deepbit <server@anyline.org>
2022-07-06 08:59:43 +0800

    TimescaleDB

---
+++ anyline-jdbc-dialect/anyline-jdbc-timescaledb/src/main/java/org/anyline/jdbc/config/db/impl/timescaledb/SQLCreaterImpl.java
@@ -0,0 +1,7 @@
+package org.anyline.jdbc.config.db.impl.timescaledb;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.jdbc.creater.timescaledb")
+public class SQLCreaterImpl extends org.anyline.jdbc.config.db.impl.postgresql.SQLCreaterImpl{
+} 
