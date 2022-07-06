77b4a25ed723e2447348ed72c2e8d257abacdfd9
deepbit <server@anyline.org>
2022-07-06 15:16:25 +0800

    config cos

--- anyline-tencent/src/main/java/org/anyline/tencent/cos/tag/COSConfig.java
+++ anyline-tencent/src/main/java/org/anyline/tencent/cos/tag/COSConfig.java
@@ -32,14 +32,14 @@ public class COSConfig extends BaseBodyTag {
                 String yy = DateUtil.format("yy");
                 String mm = DateUtil.format("MM");
                 String dd = DateUtil.format("dd");
-                dir = dir.replace("{yyyy}", yyyy);
-                dir = dir.replace("{yy}", yy);
-                dir = dir.replace("{MM}", mm);
-                dir = dir.replace("{mm}", mm);
-                dir = dir.replace("{dd}", dd);
-                dir = dir.replace("{y}", yyyy);
-                dir = dir.replace("{m}", mm);
-                dir = dir.replace("{d}", dd);
+                dir = dir.replace("${yyyy}", yyyy);
+                dir = dir.replace("${yy}", yy);
+                dir = dir.replace("${MM}", mm);
+                dir = dir.replace("${mm}", mm);
+                dir = dir.replace("${dd}", dd);
+                dir = dir.replace("${y}", yyyy);
+                dir = dir.replace("${m}", mm);
+                dir = dir.replace("${d}", dd);
             }
             Map<String,String> map = util.signature(dir, expire);
             if(BasicUtil.isEmpty(var)){
