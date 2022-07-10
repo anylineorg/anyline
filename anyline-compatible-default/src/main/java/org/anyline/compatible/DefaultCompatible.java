79e00468ae0b42551182c475249a31ce92a58968
deepbit <server@anyline.org>
2022-07-10 18:56:30 +0800

    compatible

--- anyline-compatible-default/src/main/java/org/anyline/compatible/DefaultCompatible.java
+++ anyline-compatible-default/src/main/java/org/anyline/compatible/DefaultCompatible.java
@@ -1,18 +1,50 @@
 package org.anyline.compatible;
 
+import org.anyline.util.BasicUtil;
 import org.anyline.util.BeanUtil;
+import org.anyline.util.ClassUtil;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Component("anyline.compatible.default")
 public class DefaultCompatible implements Compatible{
+    private static Map<String,String> tables = new HashMap<>();
+    private static Map<String,String> columns = new HashMap<>();
     @Override
     public String table(Class clazz) {
-        return "hr_"+clazz.getSimpleName();
+        String key = clazz.getName();
+        Object name = tables.get(key);
+        if(BasicUtil.isNotEmpty(name)){
+            return name.toString();
+        }
+        name = ClassUtil.parseAnnotationFieldValue(clazz, "table", "name");
+        if(BasicUtil.isNotEmpty(name)){
+            tables.put(key, name.toString());
+            return name.toString();
+        }
+        name = ClassUtil.parseAnnotationFieldValue(clazz, "table", "value");
+        if(BasicUtil.isNotEmpty(name)){
+            tables.put(key, name.toString());
+            return name.toString();
+        }
+        name = ClassUtil.parseAnnotationFieldValue(clazz, "tableName", "name");
+        if(BasicUtil.isNotEmpty(name)){
+            tables.put(key, name.toString());
+            return name.toString();
+        }
+        name = ClassUtil.parseAnnotationFieldValue(clazz, "tableName", "value");
+        if(BasicUtil.isNotEmpty(name)){
+            tables.put(key, name.toString());
+            return name.toString();
+        }
+        name = clazz.getSimpleName();
+        tables.put(key, name.toString());
+        return name.toString();
     }
 
     @Override
@@ -27,9 +59,37 @@ public class DefaultCompatible implements Compatible{
         return columns;
     }
 
+    @SuppressWarnings("DuplicatedCode")
     @Override
     public String column(Class clazz, Field field) {
-        return null;
+        String key = clazz.getName()+":"+field.getName();
+        Object name = columns.get(field.getName());
+        if(BasicUtil.isNotEmpty(name)){
+            return name.toString();
+        }
+        name = ClassUtil.parseAnnotationFieldValue(field, "column", "name");
+        if(BasicUtil.isNotEmpty(name)){
+            columns.put(key, name.toString());
+            return name.toString();
+        }
+        name = ClassUtil.parseAnnotationFieldValue(field, "column", "value");
+        if(BasicUtil.isNotEmpty(name)){
+            columns.put(key, name.toString());
+            return name.toString();
+        }
+        name = ClassUtil.parseAnnotationFieldValue(field, "TableField", "name");
+        if(BasicUtil.isNotEmpty(name)){
+            columns.put(key, name.toString());
+            return name.toString();
+        }
+        name = ClassUtil.parseAnnotationFieldValue(field, "TableField", "value");
+        if(BasicUtil.isNotEmpty(name)){
+            columns.put(key, name.toString());
+            return name.toString();
+        }
+        name = field.getName();
+        columns.put(key, name.toString());
+        return name.toString();
     }
 
     @Override
