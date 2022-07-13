c2c8a63894aea07783639625adf8fc5c44f18c6e
anyline <server@anyline.org>
2022-07-13 18:57:49 +0800

    compatible

--- anyline-compatible-default/src/main/java/org/anyline/compatible/DefaultCompatible.java
+++ anyline-compatible-default/src/main/java/org/anyline/compatible/DefaultCompatible.java
@@ -13,83 +13,58 @@ import java.util.Map;
 
 @Component("anyline.compatible.default")
 public class DefaultCompatible implements Compatible{
-    private static Map<String,String> tables = new HashMap<>();
-    private static Map<String,String> columns = new HashMap<>();
+    private static Map<String,String> class2table = new HashMap<>();  // class.name > table.name
+    private static Map<String,String> field2column = new HashMap<>(); // class.name:field.name > column.name
+    private static Map<String,Field> column2field = new HashMap<>();  // column.name > field
     @Override
     public String table(Class clazz) {
         String key = clazz.getName();
-        Object name = tables.get(key);
+        //1.缓存
+        String name = class2table.get(key);
         if(BasicUtil.isNotEmpty(name)){
-            return name.toString();
+            return name;
         }
-        name = ClassUtil.parseAnnotationFieldValue(clazz, "table", "name");
+        //2.注解
+        name = parseAnnotationFieldValue(clazz, "table:name", "table:value", "tableName:name", "tableName:value");
         if(BasicUtil.isNotEmpty(name)){
-            tables.put(key, name.toString());
-            return name.toString();
+            class2table.put(key, name.toString());
+            return name;
         }
-        name = ClassUtil.parseAnnotationFieldValue(clazz, "table", "value");
-        if(BasicUtil.isNotEmpty(name)){
-            tables.put(key, name.toString());
-            return name.toString();
-        }
-        name = ClassUtil.parseAnnotationFieldValue(clazz, "tableName", "name");
-        if(BasicUtil.isNotEmpty(name)){
-            tables.put(key, name.toString());
-            return name.toString();
-        }
-        name = ClassUtil.parseAnnotationFieldValue(clazz, "tableName", "value");
-        if(BasicUtil.isNotEmpty(name)){
-            tables.put(key, name.toString());
-            return name.toString();
-        }
+        //3.类名
         name = clazz.getSimpleName();
-        tables.put(key, name.toString());
-        return name.toString();
+        class2table.put(key, name.toString());
+        return name;
     }
 
     @Override
     public List<String> columns(Class clazz) {
         List<String> columns = new ArrayList<>();
-        List<Field> fields = BeanUtil.getFields(clazz);
+        List<Field> fields = ClassUtil.getFields(clazz);
         for(Field field:fields){
-            String name = field.getName();
-            String column = BeanUtil.camel_(name);
+            String column = column(clazz, field);
             columns.add(column);
         }
         return columns;
     }
 
-    @SuppressWarnings("DuplicatedCode")
     @Override
     public String column(Class clazz, Field field) {
         String key = clazz.getName()+":"+field.getName();
-        Object name = columns.get(field.getName());
+        //1.缓存
+        String name = field2column.get(key);
         if(BasicUtil.isNotEmpty(name)){
-            return name.toString();
+            return name;
         }
-        name = ClassUtil.parseAnnotationFieldValue(field, "column", "name");
+        //2.注解
+        name = parseAnnotationFieldValue(field, "column:name", "column:value", "TableField:name","TableField:value");
         if(BasicUtil.isNotEmpty(name)){
-            columns.put(key, name.toString());
-            return name.toString();
+            field2column.put(key, name.toString());
+            return name;
         }
-        name = ClassUtil.parseAnnotationFieldValue(field, "column", "value");
-        if(BasicUtil.isNotEmpty(name)){
-            columns.put(key, name.toString());
-            return name.toString();
-        }
-        name = ClassUtil.parseAnnotationFieldValue(field, "TableField", "name");
-        if(BasicUtil.isNotEmpty(name)){
-            columns.put(key, name.toString());
-            return name.toString();
-        }
-        name = ClassUtil.parseAnnotationFieldValue(field, "TableField", "value");
-        if(BasicUtil.isNotEmpty(name)){
-            columns.put(key, name.toString());
-            return name.toString();
-        }
+        //3.属性名
         name = field.getName();
-        columns.put(key, name.toString());
-        return name.toString();
+        field2column.put(key, name.toString());
+        return name;
     }
 
     @Override
@@ -112,4 +87,44 @@ public class DefaultCompatible implements Compatible{
         T entity = BeanUtil.map2object(map, clazz, false, true, true);
         return entity;
     }
+
+    /**
+     * 根据注解名与注解类属性 获取指定类上的注解值
+     * @param clazz clazz上的注解
+     * @param configs 注册名:注解属性名
+     * @return String
+     */
+    private String parseAnnotationFieldValue(Class clazz, String ... configs){
+        for(String config:configs){
+            String[] tmps = config.split(":");
+            if(tmps.length <2){
+                continue;
+            }
+            Object name = ClassUtil.parseAnnotationFieldValue(clazz, tmps[0], tmps[1]);
+            if(BasicUtil.isNotEmpty(name)){
+                return name.toString();
+            }
+        }
+        return null;
+    }
+    /**
+     * 根据注解名与注解类属性 获取指定属性上的注解值
+     * @param field field上的注解
+     * @param configs 注册名:注解属性名
+     * @return String
+     */
+    private String parseAnnotationFieldValue(Field field, String ... configs){
+        for(String config:configs){
+            String[] tmps = config.split(":");
+            if(tmps.length <2){
+                continue;
+            }
+            Object name = ClassUtil.parseAnnotationFieldValue(field, tmps[0], tmps[1]);
+            if(BasicUtil.isNotEmpty(name)){
+                return name.toString();
+            }
+        }
+        return null;
+    }
+
 }
--- anyline-compatible/src/main/java/org/anyline/compatible/Compatible.java
+++ anyline-compatible/src/main/java/org/anyline/compatible/Compatible.java
@@ -5,11 +5,55 @@ import java.util.List;
 import java.util.Map;
 
 public interface Compatible {
+    /**
+     * 获取指定类关联的表名
+     * @param clazz 类
+     * @return String
+     */
     public String table(Class clazz);
+
+    /**
+     * 获取指定类的列名s
+     * @param clazz 类
+     * @return List
+     */
     public List<String> columns(Class clazz);
+
+    /**
+     * 获取指定类.属性关联的列名
+     * @param clazz 类
+     * @param field 属性
+     * @return String
+     */
     public String column(Class clazz, Field field);
+
+    /**
+     * 根据类与列名 获取相关的属性
+     * @param clazz 类
+     * @param column 列名
+     * @return Field
+     */
     public Field field(Class clazz, String column);
+
+    /**
+     * 获取clazz类相关的主键
+     * @param clazz 类
+     * @return String
+     */
     public String primary(Class clazz);
+    /**
+     * 获取clazz类相关的主键s
+     * @param clazz 类
+     * @return List
+     */
     public List<String> primarys(Class clazz);
+
+    /**
+     * map结构转换成clazz对象
+     * @param clazz 类
+     * @param map map
+     * @return T
+     * @param <T> T
+     */
     public <T> T entity(Class<T> clazz, Map<String,Object> map);
 }
