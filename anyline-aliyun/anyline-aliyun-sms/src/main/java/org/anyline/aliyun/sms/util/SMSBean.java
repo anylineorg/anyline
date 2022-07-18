677aaedd9d6ad8a5169d0bee9d8d6e2c142a0d5a
anyline <server@anyline.org>
2022-07-18 09:54:39 +0800

    sms.bean

--- anyline-aliyun/anyline-aliyun-sms/src/main/java/org/anyline/aliyun/sms/util/SMSBean.java
+++ anyline-aliyun/anyline-aliyun-sms/src/main/java/org/anyline/aliyun/sms/util/SMSBean.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
-@Component
+@Component("anyline.sms.bean")
 public class SMSBean {
 
     @Value("${anyline.aliyun.sms.key:}")
