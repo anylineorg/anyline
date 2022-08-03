6f3a34d7af0344b939e651fb548465085d8c2cfa
anyline <server@anyline.org>
2022-08-03 13:39:43 +0800

    百度

---
+++ anyline-baidu/anyline-baidu-map/src/main/java/org/anyline/baidu/map/util/BaiduMapConfig.java
@@ -0,0 +1,80 @@
+package org.anyline.baidu.map.util;
+
+import org.anyline.entity.DataRow;
+import org.anyline.util.AnylineConfig;
+import org.anyline.util.BasicUtil;
+import org.anyline.util.ConfigTable;
+
+import java.util.Hashtable;
+
+public class BaiduMapConfig extends AnylineConfig {
+    public static String CONFIG_NAME = "anyline-qq-mp.xml";
+    private static Hashtable<String,AnylineConfig> instances = new Hashtable<String,AnylineConfig>();
+
+    public static String DEFAULT_AK = ""				;
+    public static String DEFAULT_SK = ""				;
+
+
+    public String AK			 = DEFAULT_AK				;
+    public String SK 			 = DEFAULT_SK				;
+    static{
+        init();
+        debug();
+    }
+    /**
+     * 解析配置文件内容
+     * @param content 配置文件内容
+     */
+    public static void parse(String content){
+        parse(BaiduMapConfig.class, content, instances ,compatibles);
+    }
+    /**
+     * 初始化默认配置文件
+     */
+    public static void init() {
+        //加载配置文件 
+        load();
+    }
+
+    public static BaiduMapConfig getInstance(){
+        return getInstance(DEFAULT_INSTANCE_KEY);
+    }
+    public static BaiduMapConfig getInstance(String key){
+        if(BasicUtil.isEmpty(key)){
+            key = DEFAULT_INSTANCE_KEY;
+        }
+
+        if(ConfigTable.getReload() > 0 && (System.currentTimeMillis() - BaiduMapConfig.lastLoadTime)/1000 > ConfigTable.getReload() ){
+            //重新加载 
+            load();
+        }
+        return (BaiduMapConfig)instances.get(key);
+    }
+    /**
+     * 加载配置文件 
+     * 首先加载anyline-config.xml 
+     * 然后加载anyline开头的xml文件并覆盖先加载的配置 
+     */
+    private synchronized static void load() {
+        load(instances, BaiduMapConfig.class, CONFIG_NAME);
+        BaiduMapConfig.lastLoadTime = System.currentTimeMillis();
+    }
+    private static void debug(){
+    }
+    public static BaiduMapConfig register(String instance, DataRow row){
+        BaiduMapConfig config = parse(BaiduMapConfig.class, instance, row, instances, compatibles);
+        return config;
+    }
+    public static BaiduMapConfig register(DataRow row){
+        return register(DEFAULT_INSTANCE_KEY, row);
+    }
+    public static BaiduMapConfig register(String instance,  String ak, String sk){
+        DataRow row = new DataRow();
+        row.put("AK", ak);
+        row.put("SK", sk);
+        return register(instance, row);
+    }
+    public static BaiduMapConfig register(String ak, String sk){
+        return register(DEFAULT_INSTANCE_KEY, ak, sk);
+    }
+}
---
+++ anyline-baidu/anyline-baidu-map/src/main/java/org/anyline/baidu/map/util/BaiduMapUtil.java
@@ -0,0 +1,79 @@
+package org.anyline.baidu.map.util;
+
+import org.anyline.entity.DataRow;
+import org.anyline.entity.MapPoint;
+import org.anyline.net.HttpUtil;
+import org.anyline.util.BasicUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Hashtable;
+import java.util.Map;
+public class BaiduMapUtil {
+    private static Logger log = LoggerFactory.getLogger(BaiduMapUtil.class);
+    public BaiduMapConfig config = null;
+    private static Hashtable<String, BaiduMapUtil> instances = new Hashtable<>();
+
+
+    public BaiduMapConfig getConfig(){
+        return config;
+    }
+    public static BaiduMapUtil getInstance() {
+        return getInstance("default");
+    }
+
+    public static BaiduMapUtil getInstance(String key) {
+        if (BasicUtil.isEmpty(key)) {
+            key = "default";
+        }
+        BaiduMapUtil util = instances.get(key);
+        if (null == util) {
+            BaiduMapConfig config = BaiduMapConfig.getInstance(key);
+            if(null != config) {
+                util = new BaiduMapUtil();
+                util.config = config;
+                instances.put(key, util);
+            }
+        }
+        return util;
+    }
+
+    public MapPoint regeo(String lng, String lat){
+        MapPoint point = null;
+        String url = "https://api.map.baidu.com/reverse_geocoding/v3/?ak="+config.AK+"&location="+lat+","+lng+"&extensions_town=true&output=json";
+        String txt = HttpUtil.get(url).getText();
+        DataRow row = DataRow.parseJson(txt);
+        if(null != row){
+            int status = row.getInt("status",-1);
+            if(status != 0){
+                log.warn("[逆地理编码][执行失败][code:{}][info:{}]", status, row.getString("message"));
+                return null;
+            }else{
+                point = new MapPoint(lng, lat);
+                point.setAddress(row.getString("formatted_address"));
+                DataRow adr = row.getRow("addressComponent");
+                if(null != adr) {
+                    String adcode = adr.getString("adcode");
+                    String provinceCode = adcode.substring(0,2);
+                    String cityCode = adcode.substring(0,4);
+                    point.setProvinceCode(provinceCode);
+                    point.setProvinceName(adr.getString("province"));
+                    point.setCityCode(cityCode);
+                    point.setCityName(adr.getString("city"));
+                    point.setDistrictName(adr.getString("district"));
+                    point.setDistrictCode(adr.getString("adcode"));
+                    point.setTownCode(adr.getString("town_code"));
+                    point.setTownName(adr.getString("town"));
+                }
+
+            }
+        }
+        return point;
+    }
+    public String sign(String api, Map<?,?> params){
+        return null;
+    }
+
+
+
+}
