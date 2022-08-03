7bd131d0e2b50a8baca6b6cb3585d4147cca92a8
anyline <server@anyline.org>
2022-08-03 18:18:46 +0800

    腾讯地图

---
+++ anyline-qq-map/src/main/java/org/anyline/qq/map/util/QQMapBean.java
@@ -0,0 +1,30 @@
+package org.anyline.qq.map.util;
+
+import org.anyline.util.BasicUtil;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component("anyline.QQ.map.load.bean")
+public class QQMapBean implements InitializingBean {
+
+    @Value("${anyline.qq.map.key:}")
+    private String KEY		;
+    @Value("${anyline.qq.map.secret:}")
+    private String SECRET 	;
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        KEY = BasicUtil.evl(KEY, QQMapConfig.DEFAULT_KEY);
+        if(BasicUtil.isEmpty(KEY)){
+            return;
+        }
+        QQMapConfig.register(KEY, BasicUtil.evl(SECRET, QQMapConfig.DEFAULT_SECRET));
+    }
+    @Bean("anyline.qq.map.init.util")
+    public QQMapUtil instance(){
+        return QQMapUtil.getInstance();
+    }
+}
---
+++ anyline-qq-map/src/main/java/org/anyline/qq/map/util/QQMapUtil.java
@@ -0,0 +1,99 @@
+package org.anyline.qq.map.util;
+
+import org.anyline.entity.DataRow;
+import org.anyline.entity.MapPoint;
+import org.anyline.net.HttpUtil;
+import org.anyline.util.BasicUtil;
+import org.anyline.util.BeanUtil;
+import org.anyline.util.MD5Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class QQMapUtil {
+    private static Logger log = LoggerFactory.getLogger(QQMapUtil.class);
+    public QQMapConfig config = null;
+    private static Hashtable<String, QQMapUtil> instances = new Hashtable<>();
+
+
+    public QQMapConfig getConfig(){
+        return config;
+    }
+    public static QQMapUtil getInstance() {
+        return getInstance("default");
+    }
+
+    public static QQMapUtil getInstance(String key) {
+        if (BasicUtil.isEmpty(key)) {
+            key = "default";
+        }
+        QQMapUtil util = instances.get(key);
+        if (null == util) {
+            QQMapConfig config = QQMapConfig.getInstance(key);
+            if(null != config) {
+                util = new QQMapUtil();
+                util.config = config;
+                instances.put(key, util);
+            }
+        }
+        return util;
+    }
+
+
+    public MapPoint regeo(double lng, double lat){
+        return regeo(lng+"", lat+"");
+    }
+    private String sign(String api, Map<String, Object> params){
+        String sign = null;
+        String src = api + "?" + BeanUtil.map2string(params, true, true)+config.SECRET;
+        sign = MD5Util.crypto(src);
+        return sign;
+    }
+    public MapPoint regeo(String lng, String lat){
+        MapPoint point = null;
+        String api = "/ws/geocoder/v1";
+        Map<String, Object> params = new HashMap<>();
+        params.put("location", lat+","+lng);
+        params.put("key", config.KEY);
+        String sign = sign(api, params);
+        String url = QQMapConfig.HOST + api+"?"+BeanUtil.map2string(params, false,true)+"&sig="+sign;
+
+        String txt = HttpUtil.get(url).getText();
+        DataRow row = DataRow.parseJson(txt);
+        if(null != row){
+            int status = row.getInt("status",-1);
+            if(status != 0){
+                log.warn("[逆地理编码][执行失败][code:{}][info:{}]", status, row.getString("message"));
+                return null;
+            }else{
+                point = new MapPoint(lng, lat);
+                point.setAddress(row.getString("formatted_address"));
+                DataRow adr = row.getRow("result","address_component");
+                if(null != adr) {
+                    point.setProvinceName(adr.getString("province"));
+                    point.setCityName(adr.getString("city"));
+                    point.setDistrictName(adr.getString("district"));
+                }
+                adr = row.getRow("result","ad_info");
+                if(null != adr) {
+                    String adcode = adr.getString("adcode");
+                    String provinceCode = adcode.substring(0,2);
+                    String cityCode = adcode.substring(0,4);
+                    point.setProvinceCode(provinceCode);
+                    point.setCityCode(cityCode);
+                    point.setDistrictCode(adcode);
+                }
+                adr = row.getRow("result","address_reference","town");
+                if(null != adr){
+                    point.setTownCode(adr.getString("id"));
+                    point.setTownName(adr.getString("title"));
+                }
+            }
+        }
+        return point;
+    }
+
+}
