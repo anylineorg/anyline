9e299512204398c836d46125c67e95449cfb6bda
anyline <server@anyline.org>
2022-08-05 08:50:36 +0800

    Compare

--- anyline-core/src/main/java/org/anyline/entity/operator/Compare.java
+++ anyline-core/src/main/java/org/anyline/entity/operator/Compare.java
@@ -3,8 +3,11 @@ package org.anyline.entity.operator;
 import java.util.List;
 
 public interface Compare {
+    public Compare setString(String value);
     public Compare setValue(Object value);
     public Compare addValue(Object value);
     public Compare setValues(List<Object> values);
     public boolean compare(Object value);
+    public Object getValue();
+    public Object getString();
 }
