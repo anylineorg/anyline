e51f8fa21cae82226696e35ad396884f1233e08f
anyline <server@anyline.org>
2022-08-20 14:07:20 +0800

    adapter proxy

--- anyline-core/src/main/java/org/anyline/util/AdapterProxy.java
+++ anyline-core/src/main/java/org/anyline/util/AdapterProxy.java
@@ -1,7 +1,10 @@
 package org.anyline.util;
 
 import org.anyline.entity.DataRow;
+import org.anyline.entity.DataSet;
 import org.anyline.entity.EntityAdapter;
+import org.anyline.entity.EntitySet;
+import org.anyline.entity.adapter.KeyAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -59,153 +62,246 @@ public class AdapterProxy {
 
     /**
      * 获取指定类关联的表名
+     * @param adapter adapter
      * @param clazz 类
      * @return String
      */
-    public static String table(Class clazz){
+    public static String table(EntityAdapter adapter, Class clazz){
         if(null != adapter){
             return adapter.table(clazz);
         }
         return null;
     }
+    public static String table(Class clazz){
+        return table(adapter, clazz);
+    }
 
     /**
      * 获取指定类的列名s
+     * @param adapter adapter
      * @param clazz 类
      * @return List
      */
-    public static List<String> columns(Class clazz){
+    public static List<String> columns(EntityAdapter adapter, Class clazz){
         if(null != adapter){
             return adapter.columns(clazz);
         }
         return null;
     }
 
+    public static List<String> columns(Class clazz){
+        return columns(adapter, clazz);
+    }
     /**
      * 获取指定类.属性关联的列名
+     * @param adapter adapter
      * @param clazz 类
      * @param field 属性
      * @return String
      */
-    public static String column(Class clazz, Field field){
+    public static String column(EntityAdapter adapter, Class clazz, Field field){
         if(null != adapter){
             return adapter.column(clazz, field);
         }
         return null;
     }
+    public static String column(Class clazz, Field field){
+        return column(adapter, clazz, field);
+    }
 
     /**
      * 根据类与列名 获取相关的属性
+     * @param adapter adapter
      * @param clazz 类
      * @param column 列名
      * @return Field
      */
-    public static Field field(Class clazz, String column){
+    public static Field field(EntityAdapter adapter, Class clazz, String column){
         if(null != adapter){
             return adapter.field(clazz, column);
         }
         return null;
     }
+    public static Field field(Class clazz, String column){
+        return field(adapter, clazz, column);
+    }
 
     /**
      * 获取clazz类相关的主键
+     * @param adapter adapter
      * @param clazz 类
      * @return String
      */
-    public static String primaryKey(Class clazz){
+    public static String primaryKey(EntityAdapter adapter, Class clazz){
         if(null != adapter){
             return adapter.primaryKey(clazz);
         }
         return null;
     }
+    public static String primaryKey(Class clazz){
+        return primaryKey(adapter, clazz);
+    }
 
     /**
      * 获取clazz类相关的主键s
+     * @param adapter adapter
      * @param clazz 类
      * @return List
      */
-    public static List<String> primaryKeys(Class clazz){
+    public static List<String> primaryKeys(EntityAdapter adapter, Class clazz){
         if(null != adapter){
             return adapter.primaryKeys(clazz);
         }
         return null;
     }
+    public static List<String> primaryKeys(Class clazz){
+        return primaryKeys(adapter, clazz);
+    }
 
     /**
      * 主键值
+     * @param adapter adapter
      * @param obj obj
      * @return String
      */
-    public static Map<String,Object> primaryValue(Object obj){
+    public static Map<String,Object> primaryValue(EntityAdapter adapter, Object obj){
         if(null != adapter){
             return adapter.primaryValue(obj);
         }
         return null;
     }
+    public static Map<String,Object> primaryValue(Object obj){
+        return primaryValue(adapter, obj);
+    }
     /**
      * 主键值
+     * @param adapter adapter
      * @param obj obj
      * @return Map
      */
-    public static Map<String,Object> primaryValues(Object obj){
+    public static Map<String,Object> primaryValues(EntityAdapter adapter, Object obj){
         if(null != adapter){
             return adapter.primaryValues(obj);
         }
         return null;
     }
 
+    public static Map<String,Object> primaryValues(Object obj){
+        return primaryValues(adapter, obj);
+    }
+
     /**
      * 生成主键值
+     * @param adapter adapter
      * @param obj obj
      */
-    public static void createPrimaryValue(Object obj){
+    public static void createPrimaryValue(EntityAdapter adapter, Object obj){
         if(null != adapter){
             adapter.createPrimaryValue(obj);
         }
     }
+    public static void createPrimaryValue(Object obj){
+        createPrimaryValue(adapter, obj);
+    }
     /**
      * DataRow转换成entity时调用  如果有实现则不再执行 DataRow.entity
      * 如果不实现当前可以返回null,将继续执行默认处理方式
+     * @param adapter adapter
+     * @param entity 在此基础上执行，如果不提供则新创建
      * @param clazz 类
      * @param map map
      * @return T
      * @param <T> T
      */
-    public static <T> T entity(Class<T> clazz, Map<String,Object> map){
+    public static <T> T entity(EntityAdapter adapter, T entity,  Class<T> clazz, Map<String,Object> map){
         if(null != adapter){
-        return adapter.entity(clazz, map);
-    }
+            return adapter.entity(entity, clazz, map);
+        }
         return null;
-}
+    }
+    public static <T> T entity(Class<T> clazz, Map<String,Object> map){
+        T entity =  entity(adapter, null, clazz, map);
+        for(EntityAdapter item:adapters.values()){
+            entity = entity(adapter, entity, clazz, map);
+        }
+        return entity;
+    }
+
+    public static <T> EntitySet<T> entitys(Class<T> clazz, DataSet set){
+        EntitySet<T> entitys = new EntitySet<>();
+        if(null != set){
+            for(DataRow row:set){
+                T entity = entity(clazz, row);
+                entitys.add(entity);
+            }
+            entitys.setNavi(set.getNavi());
+        }
+        return entitys;
+    }
 
 
     /**
      * entity转换成DataRow时调用 如果有实现则不再执行DataRow.parse
      * 如果不实现当前可以返回null,将继续执行默认处理方式
+     * @param adapter adapter
      * @param obj obj
      * @param keys keys
      * @return DataRow
      */
-    public static DataRow parse(Object obj, String ... keys){
-        if(null != adapter){
-            return adapter.parse(obj, keys);
+    public static DataRow row(EntityAdapter adapter, DataRow row,  Object obj, String ... keys){
+        if(null == row){
+            row = new DataRow();
         }
-        return null;
+        if(null != adapter){
+            row = adapter.row(row, obj, keys);
+        }
+        return row;
+    }
+    public static DataRow row(Object obj, String ... keys){
+        return row(null, obj, keys);
+    }
+    public static DataRow row(DataRow row, Object obj, String ... keys){
+        if(!ConfigTable.getBoolean("IS_DISABLED_DEFAULT_ENTITY_ADAPTER", false)){
+            row = DataRow.parse(row, KeyAdapter.KEY_CASE.CONFIG, obj, keys);
+        }
+        if(null != adapter){
+            row = row(adapter, row, obj, keys);
+        }
+        for(EntityAdapter item:adapters.values()){
+            row = row(item, row, obj, keys);
+        }
+        return row;
+    }
+    public static DataSet set(EntitySet entitys, String ... keys){
+        DataSet set = new DataSet();
+        if(null != entitys){
+            for(Object obj:entitys){
+                DataRow row = row(obj, keys);
+                set.addRow(row);
+            }
+            set.setNavi(entitys.getNavi());
+        }
+        return set;
     }
 
 
     /**
      * 列名转换成http参数时调用
      * 如果不实现当前可以返回null,将继续执行默认处理方式
+     * @param adapter adapter
      * @param metadata metadata
      * @return List
      *
      */
-    public static List<String> metadata2param(List<String> metadata){
+    public static List<String> metadata2param(EntityAdapter adapter, List<String> metadata){
         if(null != adapter){
             return adapter.metadata2param(metadata);
         }
         return null;
+    }
+
+    public static List<String> metadata2param(List<String> metadata){
+        return metadata2param(adapter, metadata);
     }
 
 
