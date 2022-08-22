65fe9ed60e8521dfb6b7890372badf462a483c17
anyline <server@anyline.org>
2022-08-22 11:23:45 +0800

    gbasedbt

---
+++ anyline-jdbc-dialect/anyline-jdbc-gbasedbt/src/main/java/org/anyline/jdbc/config/db/impl/gbasedbt/SQLCreaterImpl.java
@@ -0,0 +1,48 @@
+package org.anyline.jdbc.config.db.impl.gbasedbt;
+
+import org.anyline.entity.OrderStore;
+import org.anyline.entity.PageNavi;
+import org.anyline.jdbc.config.db.SQLCreater;
+import org.anyline.jdbc.config.db.impl.BasicSQLCreaterImpl;
+import org.anyline.jdbc.config.db.run.RunSQL;
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.jdbc.creater.gbasedbt")
+public class SQLCreaterImpl extends BasicSQLCreaterImpl implements SQLCreater{
+ 
+	public DB_TYPE type(){ 
+		return DB_TYPE.GBase;
+	} 
+	public SQLCreaterImpl(){ 
+		delimiterFr = "";
+		delimiterTo = "";
+	}
+
+	@Override
+	public String parseFinalQueryTxt(RunSQL run){
+		String sql = run.getBaseQueryTxt();
+		String cols = run.getFetchColumns();
+		if(!"*".equals(cols)){
+			String reg = "(?i)^select[\\s\\S]+from";
+			sql = sql.replaceAll(reg,"SELECT "+cols+" FROM ");
+		}
+		OrderStore orders = run.getOrderStore();
+		if(null != orders){
+			sql += orders.getRunText(getDelimiterFr()+getDelimiterTo());
+		}
+		PageNavi navi = run.getPageNavi();
+		if(null != navi){
+			int limit = navi.getLastRow() - navi.getFirstRow() + 1;
+			if(limit < 0){
+				limit = 0;
+			}
+			sql += " LIMIT " + navi.getFirstRow() + "," + limit;
+		}
+		sql = sql.replaceAll("WHERE\\s*1=1\\s*AND", "WHERE");
+		return sql;
+	}
+
+	public String concat(String ... args){
+		return concatFun(args);
+	}
+} 
