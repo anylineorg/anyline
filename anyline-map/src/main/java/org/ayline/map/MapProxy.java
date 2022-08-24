7940b5cb575baac39569cc4c266843bd4a5e384c
anyline <server@anyline.org>
2022-08-24 22:05:09 +0800

    MapProxy

--- anyline-map/src/main/java/org/ayline/map/MapProxy.java
+++ anyline-map/src/main/java/org/ayline/map/MapProxy.java
@@ -5,7 +5,9 @@ import org.anyline.baidu.map.util.BaiduMapUtil;
 import org.anyline.entity.MapPoint;
 import org.anyline.exception.AnylineException;
 import org.anyline.qq.map.util.QQMapUtil;
+import org.anyline.util.BasicUtil;
 import org.anyline.util.DateUtil;
+import org.anyline.util.GISUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,8 +20,8 @@ public class MapProxy {
     private static QQMapUtil qmap;
     private static BaiduMapUtil bmap;
     public static Map<String,String> over_limits = new HashMap<>();
-    public static MapPoint regeo(double lng, double lat){
-        return regeo(lng+"", lat+"");
+    public static MapPoint regeo(GISUtil.COORD_TYPE coord, String lng, String lat){
+        return regeo(coord, BasicUtil.parseDouble(lng, 0d), BasicUtil.parseDouble(lat, 0d));
     }
     private static boolean enable(String type, String platform){
         String ymd = over_limits.get(type+"_"+platform);
@@ -32,12 +34,22 @@ public class MapProxy {
         over_limits.remove(type+"_"+platform);
         return true;
     }
-    public static MapPoint regeo(String lng, String lat){
+
+    /**
+     *
+     * @param coord 坐标系
+     * @param lng 经度
+     * @param lat 纬度
+     * @return MapPoint
+     */
+    public static MapPoint regeo(GISUtil.COORD_TYPE coord, double lng, double lat){
         MapPoint point = null;
         String type = "regeo";
+        double[] location = null;
         if(null != amap && enable(type, "amap")){
             try{
-                point = amap.regeo(lng, lat);
+                location = GISUtil.convert(coord, lng, lat, GISUtil.COORD_TYPE.GCJ02LL);
+                point = amap.regeo(location[0], location[1]);
             }catch (AnylineException e){
                 if("API_OVER_LIMIT".equals(e.getCode())){
                     over_limits.put(type+"_amap", DateUtil.format("yyyy-MM-dd"));
@@ -46,7 +58,8 @@ public class MapProxy {
         }
         if(null == point && null != bmap && enable(type,"bmap")){
             try{
-                point = bmap.regeo(lng, lat);
+                location = GISUtil.convert(coord, lng, lat, GISUtil.COORD_TYPE.BD09LL);
+                point = bmap.regeo(location[0], location[1]);
             }catch (AnylineException e){
                 if("API_OVER_LIMIT".equals(e.getCode())){
                     over_limits.put(type+"_bmap", DateUtil.format("yyyy-MM-dd"));
@@ -55,7 +68,8 @@ public class MapProxy {
         }
         if(null == point && null != qmap && enable(type,"qmap")){
             try{
-                point = qmap.regeo(lng, lat);
+                location = GISUtil.convert(coord, lng, lat, GISUtil.COORD_TYPE.GCJ02LL);
+                point = qmap.regeo(location[0], location[1]);
             }catch (AnylineException e){
                 if("API_OVER_LIMIT".equals(e.getCode())){
                     over_limits.put(type+"_qmap", DateUtil.format("yyyy-MM-dd"));
