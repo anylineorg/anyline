e44e3447baa92419bbd852321174b3de81028397
anyline <server@anyline.org>
2022-08-28 17:04:26 +0800

    influxdb

---
+++ anyline-jdbc-dialect/anyline-jdbc-influxdb/src/main/java/org/anyline/jdbc/config/db/impl/influxdb/SQLCreaterImpl.java
@@ -0,0 +1,70 @@
+ 
+package org.anyline.jdbc.config.db.impl.influxdb;
+
+import org.anyline.entity.DataSet;
+import org.anyline.entity.OrderStore;
+import org.anyline.entity.PageNavi;
+import org.anyline.jdbc.config.db.SQLCreater;
+import org.anyline.jdbc.config.db.impl.BasicSQLCreaterImpl;
+import org.anyline.jdbc.config.db.run.RunSQL;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("anyline.jdbc.creater.influxdb")
+public class SQLCreaterImpl extends BasicSQLCreaterImpl implements SQLCreater, InitializingBean {
+ 
+	public DB_TYPE type(){ 
+		return DB_TYPE.InfluxDB;
+	} 
+	public SQLCreaterImpl(){ 
+		delimiterFr = "\"";
+		delimiterTo = "\"";
+	}
+	@Value("${anyline.jdbc.delimiter.influxdb:}")
+	private String delimiter;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		setDelimiter(delimiter);
+	}
+
+	@Override 
+	public String parseFinalQueryTxt(RunSQL run){ 
+		String sql = run.getBaseQueryTxt(); 
+		String cols = run.getFetchColumns(); 
+		if(!"*".equals(cols)){ 
+			String reg = "(?i)^select[\\s\\S]+from"; 
+			sql = sql.replaceAll(reg,"SELECT "+cols+" FROM "); 
+		} 
+		OrderStore orders = run.getOrderStore(); 
+		if(null != orders){ 
+			sql += orders.getRunText(getDelimiterFr()+getDelimiterTo());
+		} 
+		PageNavi navi = run.getPageNavi(); 
+		if(null != navi){ 
+			int limit = navi.getLastRow() - navi.getFirstRow() + 1; 
+			if(limit < 0){ 
+				limit = 0; 
+			} 
+			sql += " LIMIT " + limit + " OFFSET " + navi.getFirstRow(); 
+		} 
+		sql = sql.replaceAll("WHERE\\s*1=1\\s*AND", "WHERE"); 
+		return sql; 
+	} 
+ 
+ 
+	public String concat(String ... args){
+		return concatOr(args);
+	}
+
+
+	public RunSQL createInsertTxt(String dest, Object obj, boolean checkParimary, String ... columns){
+		return null;
+	}
+	public void createInsertsTxt(StringBuilder builder, String dest, DataSet set, List<String> keys){
+		return;
+	}
+} 
