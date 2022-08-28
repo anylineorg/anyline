37c2c0c65674bbcf65182a709a91ddfcd7dd8e18
anyline <server@anyline.org>
2022-08-28 17:04:36 +0800

    timescaledb

--- anyline-jdbc-dialect/anyline-jdbc-timescaledb/src/main/java/org/anyline/jdbc/config/db/impl/timescaledb/SQLCreaterImpl.java
+++ anyline-jdbc-dialect/anyline-jdbc-timescaledb/src/main/java/org/anyline/jdbc/config/db/impl/timescaledb/SQLCreaterImpl.java
@@ -1,7 +1,18 @@
 package org.anyline.jdbc.config.db.impl.timescaledb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyline.jdbc.creater.timescaledb")
 public class SQLCreaterImpl extends org.anyline.jdbc.config.db.impl.postgresql.SQLCreaterImpl{
+    public DB_TYPE type(){
+        return DB_TYPE.TimescaleDB;
+    }
+    @Value("${anyline.jdbc.delimiter.timescaledb:}")
+    private String delimiter;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        setDelimiter(delimiter);
+    }
 } 
