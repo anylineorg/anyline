43870a3f5b2be890d9e0580583f04838a94c9576
anyline <server@anyline.org>
2022-09-04 21:23:24 +0800

    table

--- anyline-jdbc/src/main/java/org/anyline/jdbc/entity/Table.java
+++ anyline-jdbc/src/main/java/org/anyline/jdbc/entity/Table.java
@@ -27,6 +27,22 @@ public class Table {
     private String selfReferencingColumn;
     private String refGeneration;
 
+    private Table update;
+
+    public Table update(){
+        update = new Table();
+        return update;
+    }
+
+    public Table getUpdate() {
+        return update;
+    }
+
+    public Table setUpdate(Table update) {
+        this.update = update;
+        return this;
+    }
+
     private List<Column> columns = new ArrayList<>();
 
     public Table addColumn(Column column){
@@ -37,87 +53,98 @@ public class Table {
         return cat;
     }
 
-    public void setCat(String cat) {
+    public Table setCat(String cat) {
         this.cat = cat;
+        return this;
     }
 
     public String getSchema() {
         return schema;
     }
 
-    public void setSchema(String schema) {
+    public Table setSchema(String schema) {
         this.schema = schema;
+        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public Table setName(String name) {
         this.name = name;
+        return this;
     }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public Table setType(String type) {
         this.type = type;
+        return this;
     }
 
     public String getRemarks() {
         return remarks;
     }
 
-    public void setRemarks(String remarks) {
+    public Table setRemarks(String remarks) {
         this.remarks = remarks;
+        return this;
     }
 
     public String getTypeCat() {
         return typeCat;
     }
 
-    public void setTypeCat(String typeCat) {
+    public Table setTypeCat(String typeCat) {
         this.typeCat = typeCat;
+        return this;
     }
 
     public String getTypeSchema() {
         return typeSchema;
     }
 
-    public void setTypeSchema(String typeSchema) {
+    public Table setTypeSchema(String typeSchema) {
         this.typeSchema = typeSchema;
+        return this;
     }
 
     public String getTypeName() {
         return typeName;
     }
 
-    public void setTypeName(String typeName) {
+    public Table setTypeName(String typeName) {
         this.typeName = typeName;
+        return this;
     }
 
     public String getSelfReferencingColumn() {
         return selfReferencingColumn;
     }
 
-    public void setSelfReferencingColumn(String selfReferencingColumn) {
+    public Table setSelfReferencingColumn(String selfReferencingColumn) {
         this.selfReferencingColumn = selfReferencingColumn;
+        return this;
     }
 
     public String getRefGeneration() {
         return refGeneration;
     }
 
-    public void setRefGeneration(String refGeneration) {
+    public Table setRefGeneration(String refGeneration) {
         this.refGeneration = refGeneration;
+        return this;
     }
 
     public List<Column> getColumns() {
         return columns;
     }
 
-    public void setColumns(List<Column> columns) {
+    public Table setColumns(List<Column> columns) {
         this.columns = columns;
+        return this;
     }
 }
