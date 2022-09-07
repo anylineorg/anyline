7fe5196897736150a86565e29e84f26f5d9ab47d
anyline <server@anyline.org>
2022-09-07 18:16:41 +0800

    primary key

--- anyline-jdbc/src/main/java/org/anyline/jdbc/entity/Table.java
+++ anyline-jdbc/src/main/java/org/anyline/jdbc/entity/Table.java
@@ -1,20 +1,11 @@
 package org.anyline.jdbc.entity;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Table {
-    /* TABLE_CAT String => 表类别（可为 null）
-     * TABLE_SCHEMA String => 表模式（可为 null）
-     * TABLE_NAME String => 表名称
-     * TABLE_TYPE String => 表类型。"TABLE"、"VIEW"、"SYSTEM TABLE"、"GLOBAL TEMPORARY"、"LOCAL TEMPORARY"、"ALIAS" 和 "SYNONYM"。
-     * REMARKS String => 表的解释性注释
-     * TYPE_CAT String => 类型的类别（可为 null）
-     * TYPE_SCHEMA String => 类型模式（可为 null）
-     * TYPE_NAME String => 类型名称（可为 null）
-     * SELF_REFERENCING_COL_NAME String => 有类型表的指定 "identifier" 列的名称（可为 null）
-     * REF_GENERATION String  => 指定在 SELF_REFERENCING_COL_NAME 中创建值的方式。这些值为 "SYSTEM"、"USER" 和 "DERIVED"。（可能为 null）
-     */
     private String catalog;
     private String schema;
     private String name;
@@ -26,9 +17,25 @@ public class Table {
     private String typeName;
     private String selfReferencingColumn;
     private String refGeneration;
+    private List<Column> columns = new ArrayList<>();
 
     private Table update;
 
+    private List<Column> getPrimaryKeys(){
+        List<Column> pks = new ArrayList<>();
+        for(Column column:columns){
+            if(column.isPrimaryKey()){
+                pks.add(column);
+            }
+        }
+        Collections.sort(pks, new Comparator<Column>() {
+            @Override
+            public int compare(Column o1, Column o2) {
+                return o1.getPrimaryKeyIndex() > o2.getPrimaryKeyIndex() ? 1:-1;
+            }
+        });
+        return pks;
+    }
     public Table update(){
         update = new Table();
         return update;
@@ -43,7 +50,6 @@ public class Table {
         return this;
     }
 
-    private List<Column> columns = new ArrayList<>();
 
     public Table addColumn(Column column){
         columns.add(column);
