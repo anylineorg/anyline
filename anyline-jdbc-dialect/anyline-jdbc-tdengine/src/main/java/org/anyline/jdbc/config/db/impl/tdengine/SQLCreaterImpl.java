a1dc8ef207aef100801250b759f2267af730f4c4
anyline <server@anyline.org>
2022-09-14 23:04:43 +0800

    tdengine creater

--- anyline-jdbc-dialect/anyline-jdbc-tdengine/src/main/java/org/anyline/jdbc/config/db/impl/tdengine/SQLCreaterImpl.java
+++ anyline-jdbc-dialect/anyline-jdbc-tdengine/src/main/java/org/anyline/jdbc/config/db/impl/tdengine/SQLCreaterImpl.java
@@ -1,13 +1,23 @@
 package org.anyline.jdbc.config.db.impl.tdengine;
 
+import org.anyline.entity.DataSet;
 import org.anyline.entity.OrderStore;
 import org.anyline.entity.PageNavi;
 import org.anyline.jdbc.config.db.SQLCreater;
 import org.anyline.jdbc.config.db.impl.BasicSQLCreaterImpl;
 import org.anyline.jdbc.config.db.run.RunSQL;
+import org.anyline.jdbc.entity.Column;
+import org.anyline.jdbc.entity.Table;
+import org.anyline.jdbc.entity.Tag;
+import org.anyline.util.BasicUtil;
+import org.anyline.util.SQLUtil;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 @Repository("anyline.jdbc.creater.tdengine")
 public class SQLCreaterImpl extends BasicSQLCreaterImpl implements SQLCreater, InitializingBean {
@@ -41,20 +51,204 @@ public class SQLCreaterImpl extends BasicSQLCreaterImpl implements SQLCreater, I
 		if(null != orders){ 
 			sql += orders.getRunText(getDelimiterFr()+getDelimiterTo());
 		} 
-		PageNavi navi = run.getPageNavi(); 
-		if(null != navi){ 
-			int limit = navi.getLastRow() - navi.getFirstRow() + 1; 
-			if(limit < 0){ 
-				limit = 0; 
+		PageNavi navi = run.getPageNavi();
+		if(null != navi){
+			int limit = navi.getLastRow() - navi.getFirstRow() + 1;
+			if(limit < 0){
+				limit = 0;
 			}
-			sql += " LIMIT " + (navi.getLastRow()-navi.getFirstRow());
-		} 
+			sql += " LIMIT " + navi.getFirstRow() + "," + limit;
+		}
 		sql = sql.replaceAll("WHERE\\s*1=1\\s*AND", "WHERE"); 
 		return sql; 
 	} 
  
 	public String concat(String ... args){
 		return concatFun(args);
+	}
+
+	/**
+	 * 内置函数
+	 * @param value SQL_BUILD_IN_VALUE
+	 * @return String
+	 */
+	public String buildInValue(SQL_BUILD_IN_VALUE value){
+		if(value == SQL_BUILD_IN_VALUE.CURRENT_TIME){
+			return "NOW";
+		}
+		return null;
+	}
+
+	/**
+	 * 查询超表
+	 * @param catalog catalog
+	 * @param schema schema
+	 * @param pattern pattern
+	 * @param types types
+	 * @return String
+	 */
+	public String buildQuerySTableRunSQL(String catalog, String schema, String pattern, String types){
+		String sql = "SHOW STABLES";
+		if(BasicUtil.isNotEmpty(pattern)){
+			sql += " LIKE '" + pattern + "'";
+		}
+		return sql;
+	}
+
+	/**
+	 * 从查询结果中提取出超表名
+	 * @param set 查询结果
+	 * @return List
+	 */
+	public List<String> stables(DataSet set){
+		return set.getStrings("stable_name");
+	}
+	@Override
+	public String buildCreateRunSQL(Table table){
+		LinkedHashMap<String,Tag> tags = table.getTags();
+		StringBuilder builder = new StringBuilder();
+		if(null == tags || tags.size()==0){
+			return buildCreateRunSQL(builder, table, "TABLE").toString();
+		}
+		buildCreateRunSQL(builder, table, "STABLE");
+		builder.append("(");
+		int idx = 0;
+		for(Tag tag:tags.values()){
+			if(idx > 0){
+				builder.append(",");
+			}
+			SQLUtil.delimiter(builder, tag.getName(), getDelimiterFr(), getDelimiterTo()).append(" ");
+			type(builder, tag);
+			comment(builder, tag);
+			idx ++;
+		}
+		builder.append(")");
+		return builder.toString();
+	}
+
+	/**
+	 * 修改表名
+	 * 子类实现
+	 * 一般不直接调用,如果需要由buildAlterRunSQL内部统一调用
+	 * @param table table
+	 * @return String
+	 */
+	@Override
+	public String buildRenameRunSQL(Table table) {
+		return null;
+	}
+
+	/**
+	 * 修改列名
+	 * 子类实现
+	 * 一般不直接调用,如果需要由buildAlterRunSQL内部统一调用
+	 * @param column column
+	 * @return String
+	 */
+	@Override
+	public String buildRenameRunSQL(Column column) {
+		return null;
+	}
+
+	/**
+	 * 修改默认值
+	 * 子类实现
+	 * 一般不直接调用,如果需要由buildAlterRunSQL内部统一调用
+	 * @param column column
+	 * @return String
+	 */
+	public String buildChangeDefaultRunSQL(Column column){
+		return null;
+	}
+
+	/**
+	 * 修改非空限制
+	 * 子类实现
+	 * 一般不直接调用,如果需要由buildAlterRunSQL内部统一调用
+	 * @param column column
+	 * @return String
+	 */
+	public String buildChangeNullableRunSQL(Column column){
+		return null;
+	}
+	/**
+	 * 修改备注
+	 * 子类实现
+	 * 一般不直接调用,如果需要由buildAlterRunSQL内部统一调用
+	 * @param column column
+	 * @return String
+	 */
+	public String buildChangeCommentRunSQL(Column column){
+		return null;
+	}
+
+
+	/**
+	 * 修改备注
+	 * 子类实现
+	 * 一般不直接调用,如果需要由buildAlterRunSQL内部统一调用
+	 * @param table table
+	 * @return String
+	 */
+	public String buildChangeCommentRunSQL(Table table){
+		return null;
+	}
+	/**
+	 * 修改数据类型
+	 * 子类实现
+	 * 一般不直接调用,如果需要由buildAlterRunSQL内部统一调用
+	 * @param column column
+	 * @return sql
+	 */
+	public List<String> buildChangeTypeRunSQL(Column column){
+		return null;
+	}
+	/**
+	 * 更新行事件
+	 * 子类实现
+	 * @param builder builder
+	 * @param column column
+	 * @return builder
+	 */
+	public StringBuilder onupdate(StringBuilder builder, Column column){
+		return builder;
+	}
+	/**
+	 * 自增长列
+	 * 子类实现
+	 * @param builder builder
+	 * @param column column
+	 * @return builder
+	 */
+	public StringBuilder increment(StringBuilder builder, Column column){
+		return builder;
+	}
+
+	/**
+	 * 位置
+	 * 子类实现
+	 * @param builder builder
+	 * @param column column
+	 * @return builder
+	 */
+	public StringBuilder position(StringBuilder builder, Column column){
+		return builder;
+	}
+
+	/**
+	 * 备注
+	 * 子类实现
+	 * @param builder builder
+	 * @param column column
+	 * @return builder
+	 */
+	@Override
+	public StringBuilder comment(StringBuilder builder, Column column){
+		String comment = column.getComment();
+		if(BasicUtil.isNotEmpty(comment)){
+			builder.append(" COMMENT '").append(comment).append("'");
+		}
+		return builder;
 	}
 
 }
