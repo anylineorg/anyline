23461ec215a060794896f350820db59e8b5ab363
anyline <server@anyline.org>
2022-09-16 01:00:12 +0800

    STABLE

--- anyline-jdbc/src/main/java/org/anyline/jdbc/entity/STable.java
+++ anyline-jdbc/src/main/java/org/anyline/jdbc/entity/STable.java
@@ -2,12 +2,30 @@ package org.anyline.jdbc.entity;
 
 
 import org.anyline.entity.DataRow;
+import org.anyline.listener.impl.DefaulDDtListener;
 
 import java.util.LinkedHashMap;
 
 public class STable extends Table {
     protected String keyword = "STABLE"            ;
     private LinkedHashMap<String,Table> tables     ; //子表
+
+    public STable(){
+        this.listener = new DefaulDDtListener();
+    }
+    public STable(String name){
+        this(null, name);
+    }
+    public STable(String schema, String table){
+        this(null, schema, table);
+    }
+    public STable(String catalog, String schema, String name){
+        this.catalog = catalog;
+        this.schema = schema;
+        this.name = name;
+        this.listener = new DefaulDDtListener();
+    }
+
 
     public String getKeyword() {
         return this.keyword;
