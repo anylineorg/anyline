9dd7eac8f7c92023df01682c4993d1ae431da893
anyline <server@anyline.org>
2022-12-27 17:11:41 +0800

    sbc2dbc

--- anyline-core/src/main/java/org/anyline/util/CharUtil.java
+++ anyline-core/src/main/java/org/anyline/util/CharUtil.java
@@ -213,4 +213,50 @@ public class CharUtil {
 		cs[0] = Character.toUpperCase(cs[0]);
 		return String.valueOf(cs);
 	}
+	/**
+	 * 半角转全角的函数(SBC case)
+	 * 全角空格为12288,半角空格为32，其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+	 *
+	 * @param input 任意字符串
+	 * @return 全角字符串
+	 */
+	public static String dbc2sbc(String input) {
+		if (BasicUtil.isEmpty(input)) {
+			return input;
+		}
+		char[] c = input.toCharArray();
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] == 32) {
+				c[i] = (char) 12288;
+				continue;
+			}
+			if (c[i] < 127) {
+				c[i] = (char) (c[i] + 65248);
+			}
+		}
+		return new String(c);
+	}
+
+	/**
+	 * 全角转半角的函数(DBC case)
+	 * 全角空格为12288，半角空格为32 其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+	 * @param input 任意字符串
+	 * @return 半角字符串
+	 */
+	public static String sbc2dbc(String input) {
+		if (BasicUtil.isEmpty(input)) {
+			return input;
+		}
+		char[] c = input.toCharArray();
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] == 12288) {
+				c[i] = (char) 32;
+				continue;
+			}
+			if (c[i] > 65280 && c[i] < 65375) {
+				c[i] = (char) (c[i] - 65248);
+			}
+		}
+		return new String(c);
+	}
 }
