f9d80330d0337c991c3a12afcbdbe3da72673cbd
anyline <server@anyline.org>
2023-01-09 15:04:52 +0800

    LDAPProperty

---
+++ anyline-boot/src/main/java/org/anyline/boot/ldap/LDAPProperty.java
@@ -0,0 +1,82 @@
+package org.anyline.boot.ldap;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "anyline.ldap")
+public class LDAPProperty {
+
+    private String address					 	;
+    private int port 							;
+    private String domain						;
+    private String root							;
+    private String securityAuthentication		;
+    private String url							; // ldap:{ADDRESS}:{PORT}
+    private int connectTimeout 				    ;
+    private int readTimeout 					;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getRoot() {
+        return root;
+    }
+
+    public void setRoot(String root) {
+        this.root = root;
+    }
+
+    public String getSecurityAuthentication() {
+        return securityAuthentication;
+    }
+
+    public void setSecurityAuthentication(String securityAuthentication) {
+        this.securityAuthentication = securityAuthentication;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(int connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public int getReadTimeout() {
+        return readTimeout;
+    }
+
+    public void setReadTimeout(int readTimeout) {
+        this.readTimeout = readTimeout;
+    }
+}
