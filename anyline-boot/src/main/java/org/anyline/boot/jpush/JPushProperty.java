7fa3397507a4441198bd9b90505154a50f13e97c
anyline <server@anyline.org>
2023-01-09 15:04:43 +0800

    JPushProperty

---
+++ anyline-boot/src/main/java/org/anyline/boot/jpush/JPushProperty.java
@@ -0,0 +1,27 @@
+package org.anyline.boot.jpush;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "anyline.jpush")
+public class JPushProperty {
+    private String appKey;
+    private String masterSecret;
+
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public void setAppKey(String appKey) {
+        this.appKey = appKey;
+    }
+
+    public String getMasterSecret() {
+        return masterSecret;
+    }
+
+    public void setMasterSecret(String masterSecret) {
+        this.masterSecret = masterSecret;
+    }
+}
