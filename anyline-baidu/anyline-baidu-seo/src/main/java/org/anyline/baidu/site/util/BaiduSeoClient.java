c25fb2973ffc48c3b4ad4873183cdcecb7cb29e0
anyline <server@anyline.org>
2023-01-09 15:04:11 +0800

    BaiduSeoClient

--- anyline-baidu/anyline-baidu-seo/src/main/java/org/anyline/baidu/site/util/BaiduSeoClient.java
+++ anyline-baidu/anyline-baidu-seo/src/main/java/org/anyline/baidu/site/util/BaiduSeoClient.java
@@ -13,38 +13,38 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.util.*;
 
-public class BaiduSiteClient {
-    private static Logger log = LoggerFactory.getLogger(BaiduSiteClient.class);
+public class BaiduSeoClient {
+    private static Logger log = LoggerFactory.getLogger(BaiduSeoClient.class);
 
-    public BaiduSiteConfig config = null;
-    private static Hashtable<String, BaiduSiteClient> instances = new Hashtable<>();
+    public BaiduSeoConfig config = null;
+    private static Hashtable<String, BaiduSeoClient> instances = new Hashtable<>();
 
     static {
-        Hashtable<String, AnylineConfig> configs = BaiduSiteConfig.getInstances();
+        Hashtable<String, AnylineConfig> configs = BaiduSeoConfig.getInstances();
         for(String key:configs.keySet()){
             instances.put(key, getInstance(key));
         }
     }
-    public static Hashtable<String, BaiduSiteClient> getInstances(){
+    public static Hashtable<String, BaiduSeoClient> getInstances(){
         return instances;
     }
 
-    public BaiduSiteConfig getConfig(){
+    public BaiduSeoConfig getConfig(){
         return config;
     }
-    public static BaiduSiteClient getInstance() {
+    public static BaiduSeoClient getInstance() {
         return getInstance("default");
     }
 
-    public static BaiduSiteClient getInstance(String key) {
+    public static BaiduSeoClient getInstance(String key) {
         if (BasicUtil.isEmpty(key)) {
             key = "default";
         }
-        BaiduSiteClient client = instances.get(key);
+        BaiduSeoClient client = instances.get(key);
         if (null == client) {
-            BaiduSiteConfig config = BaiduSiteConfig.getInstance(key);
+            BaiduSeoConfig config = BaiduSeoConfig.getInstance(key);
             if(null != config) {
-                client = new BaiduSiteClient();
+                client = new BaiduSeoClient();
                 client.config = config;
                 instances.put(key, client);
             }
