745c0eb1ef675f6b983f50a308f0594f980988c8
anyline <server@anyline.org>
2023-01-09 15:05:42 +0800

    MapProperty

---
+++ anyline-boot/src/main/java/org/anyline/boot/baidu/MapProperty.java
@@ -0,0 +1,27 @@
+package org.anyline.boot.baidu;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "anyline.baidu.map")
+public class MapProperty {
+    private String ak;
+    private String sk;
+
+    public String getAk() {
+        return ak;
+    }
+
+    public void setAk(String ak) {
+        this.ak = ak;
+    }
+
+    public String getSk() {
+        return sk;
+    }
+
+    public void setSk(String sk) {
+        this.sk = sk;
+    }
+}
