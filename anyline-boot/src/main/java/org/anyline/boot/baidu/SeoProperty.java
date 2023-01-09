c9f9915c9fd0129d3b349027bee3d8745db4847a
anyline <server@anyline.org>
2023-01-09 15:08:22 +0800

    SeoProperty

---
+++ anyline-boot/src/main/java/org/anyline/boot/baidu/SeoProperty.java
@@ -0,0 +1,27 @@
+package org.anyline.boot.baidu;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "anyline.baidu.seo")
+public class SeoProperty {
+    private String site;
+    private String token;
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
