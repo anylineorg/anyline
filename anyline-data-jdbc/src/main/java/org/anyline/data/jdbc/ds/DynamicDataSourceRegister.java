e614ef6f11866385f472959cd2e693537b8236de
anyline <server@anyline.org>
2023-04-01 21:43:29 +0800

    DynamicDataSourceRegister

--- anyline-data-jdbc/src/main/java/org/anyline/data/jdbc/ds/DynamicDataSourceRegister.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/jdbc/ds/DynamicDataSourceRegister.java
@@ -1,8 +1,6 @@
 package org.anyline.data.jdbc.ds;
 
-import org.anyline.util.BasicUtil;
-import org.anyline.util.BeanUtil;
-import org.anyline.util.ClassUtil;
+import org.anyline.data.jdbc.util.DataSourceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.MutablePropertyValues;
@@ -14,9 +12,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotationMetadata;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 系统启动时解析多数据源配置
@@ -26,19 +21,21 @@ import java.util.Map;
 //@Component
 //@Import(DynamicDataSourceRegister.class)
 public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar, EnvironmentAware {
+    //启动类以Import形式注入时 激活，激活后禁用JDBCHolder
+    public static boolean ACTIVE = false;
     private Logger log = LoggerFactory.getLogger(DynamicDataSourceRegister.class);
     //指定默认数据源(springboot2.0默认数据源是hikari如何想使用其他数据源可以自己配置)
-    private static final String DATASOURCE_TYPE_DEFAULT = "com.zaxxer.hikari.HikariDataSource";
 
 
     @Override
     public void setEnvironment(Environment environment) {
+        ACTIVE = true;
         initDefaultDataSource(environment);
         initSpringDataSources(environment);
     }
     private void initDefaultDataSource(Environment env) {
         // 读取主数据源
-        DataSource ds = buildDataSource("spring.datasource",env);
+        DataSource ds = DataSourceUtil.buildDataSource("spring.datasource",env);
         DynamicDataSource.setDefaultDatasource(ds);
     }
     private void initSpringDataSources(Environment env) {
@@ -47,7 +44,7 @@ public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar,
         if(null != prefixs){
             for (String prefix : prefixs.split(",")) {
                 // 多个数据源
-                DataSource ds = buildDataSource("spring.datasource."+prefix,env);
+                DataSource ds = DataSourceUtil.buildDataSource("spring.datasource."+prefix,env);
                 DynamicDataSource.addDataSource(prefix, ds);
                 DataSourceHolder.reg(prefix);
                 log.info("[创建数据源][prefix:{}]",prefix);
@@ -76,61 +73,6 @@ public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar,
         //注册 - BeanDefinitionRegistry
         beanDefinitionRegistry.registerBeanDefinition("dataSource", beanDefinition);
 
-    }
-
-
-    @SuppressWarnings("unchecked")
-    public static DataSource buildDataSource(String prefix, Environment env) {
-        try {
-            if(BasicUtil.isNotEmpty(prefix) && !prefix.endsWith(".")){
-                prefix += ".";
-            }
-            String type = BeanUtil.value(prefix, env, "type");
-            if(null == type){
-                type = BeanUtil.value("spring.datasource.", env, "type");
-            }
-            if (type == null) {
-                type = DATASOURCE_TYPE_DEFAULT;
-            }
-
-            Class<? extends DataSource> dataSourceType = (Class<? extends DataSource>) Class.forName(type);
-            String driverClassName = BeanUtil.value(prefix, env, "driver","driver-class","driver-class-name");
-            String url = BeanUtil.value(prefix, env, "url","jdbc-url");
-            String username = BeanUtil.value(prefix, env,"user","username","user-name");
-            String password = BeanUtil.value(prefix, env, "password");
-
-            DataSource ds =  dataSourceType.newInstance();
-            Map<String,Object> map = new HashMap<String,Object>();
-            map.put("url", url);
-            map.put("jdbcUrl", url);
-            map.put("driver",driverClassName);
-            map.put("driverClass",driverClassName);
-            map.put("driverClassName",driverClassName);
-            map.put("user",username);
-            map.put("username",username);
-            map.put("password",password);
-            BeanUtil.setFieldsValue(ds, map);
-            return ds;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * 根据配置文件设置对象属性值
-     * @param obj 对象
-     * @param prefix 前缀
-     * @param env 配置文件环境
-     */
-    private static void setFieldsValue(Object obj, String prefix, Environment env ){
-        List<String> fields = ClassUtil.getFieldsName(obj.getClass());
-        for(String field:fields){
-            String value = BeanUtil.value(prefix, env, field);
-            if(BasicUtil.isNotEmpty(value)) {
-                BeanUtil.setFieldValue(obj, field, value);
-            }
-        }
     }
 
 
