95bb844685b1cd39d8c6fffd9e41116f8626f001
anyline <server@anyline.org>
2023-04-10 20:13:39 +0800

    Variable

--- anyline-data-jdbc/src/main/java/org/anyline/data/prepare/Variable.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/prepare/Variable.java
@@ -29,22 +29,33 @@ import java.util.List;
  
 public interface Variable extends Cloneable,Serializable{
 	public static final int VAR_TYPE_INDEX			= 0;		// 根据下标区分 CD = ? 
-	public static final int VAR_TYPE_KEY			= 1;		// 根据KEY区分  CD = :CD 
-	public static final int VAR_TYPE_KEY_REPLACE	= 2;		// 字符替换 CD=':CD' 
-	public static final int VAR_TYPE_REPLACE		= 3;		// 字符替换 CD=::CD
+	public static final int VAR_TYPE_KEY			= 1;		// 根据KEY区分  CD = :CD  CD = #{CD}
+	public static final int VAR_TYPE_KEY_REPLACE	= 2;		// 字符替换 CD=':CD'  符合1但需要替换 如在''内
+	public static final int VAR_TYPE_REPLACE		= 3;		// 字符替换 CD=::CD   CD = ${CD}
 
 	public static final int KEY_TYPE_SIGN_V1				= 1; // 以:或::区分
-	public static final int KEY_TYPE_SIGN_V2				= 2; // 以{}或${}或#{}区分 (8.5之后用${})
-	
+	public static final int KEY_TYPE_SIGN_V2				= 2; // 以{}或${}或#{}区分 (8.5之后不要用{}避免与json格式冲突)
+
+
+	/**
+	 * key前缀 : :: $ #
+	 * @return String
+	 */
+	public String getKeyPrefix();
+	public void setKeyPrefix(String prefix);
 	public int getSignType();
 	public void setSignType(int signType); 
 	public void init(); 
 	public Compare getCompare() ; 
 	public void setCompare(Compare compare) ; 
-	public void addValue(Object value); 
-	public String getKey() ; 
-	public void setKey(String key) ; 
-	 
+	public void addValue(Object value);
+	//ID
+	public String getKey() ;
+	public void setKey(String key) ;
+
+	//完整KEY :CD ::CD {CD} ${CD} #{CD} 8.5之后不用{CD}避免与json冲突
+	public String getFullKey();
+	public void setFullKey(String gkey);
 	public List<Object> getValues() ;
 	public void setValue(Object value) ;
 	public void setValue(boolean chkNull, Object value) ; 
