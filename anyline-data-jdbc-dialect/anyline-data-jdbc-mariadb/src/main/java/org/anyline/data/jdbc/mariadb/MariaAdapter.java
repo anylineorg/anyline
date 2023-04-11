c883079d72ca870d3fdf0000f959a7b0b9f0d3ee
anyline <server@anyline.org>
2023-04-12 00:47:30 +0800

    MariaDB DROP PRIMARY KEY

--- anyline-data-jdbc-dialect/anyline-data-jdbc-mariadb/src/main/java/org/anyline/data/jdbc/mariadb/MariaAdapter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-mariadb/src/main/java/org/anyline/data/jdbc/mariadb/MariaAdapter.java
@@ -1167,7 +1167,24 @@ public class MariaAdapter extends SQLAdapter implements JDBCAdapter, Initializin
 	 */
 	@Override
 	public String buildAddRunSQL(PrimaryKey primary) throws Exception{
-		return super.buildAddRunSQL(primary);
+		StringBuilder builder = new StringBuilder();
+		Map<String,Column> columns = primary.getColumns();
+		if(columns.size()>0) {
+			builder.append("ALTER TABLE ");
+			name(builder, primary.getTable());
+			builder.append(" ADD PRIMARY KEY (");
+			boolean first = true;
+			for(Column column:columns.values()){
+				SQLUtil.delimiter(builder, column.getName(), getDelimiterFr(), getDelimiterTo());
+				if(!first){
+					builder.append(",");
+				}
+				first = false;
+			}
+			builder.append(")");
+
+		}
+		return builder.toString();
 	}
 	/**
 	 * 修改主键
@@ -1187,7 +1204,11 @@ public class MariaAdapter extends SQLAdapter implements JDBCAdapter, Initializin
 	 */
 	@Override
 	public String buildDropRunSQL(PrimaryKey primary) throws Exception{
-		return super.buildDropRunSQL(primary);
+		StringBuilder builder = new StringBuilder();
+		builder.append("ALTER TABLE ");
+		name(builder, primary.getTable());
+		builder.append(" DROP PRIMARY KEY");
+		return builder.toString();
 	}
 	/**
 	 * 修改主键名
