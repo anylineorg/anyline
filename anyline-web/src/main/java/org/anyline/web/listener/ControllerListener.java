f4bdaad2f236a46dc9367388a255becaf0e19627
anyline <server@anyline.org>
2023-04-14 14:37:17 +0800

    ControllerListener

--- anyline-web/src/main/java/org/anyline/web/listener/ControllerListener.java
+++ anyline-web/src/main/java/org/anyline/web/listener/ControllerListener.java
@@ -5,5 +5,11 @@ import org.anyline.data.param.ConfigStore;
 import javax.servlet.http.HttpServletRequest;
 
 public interface ControllerListener {
+    /**
+     * 封装完查询条件后调用
+     * @param request rquest
+     * @param configs 查询条件
+     * @return ConfigStore
+     */
     public ConfigStore after(HttpServletRequest request, ConfigStore configs);
 }
