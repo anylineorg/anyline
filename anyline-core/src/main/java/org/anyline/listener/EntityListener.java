b01ca0d753c4680ed02f86638f47bd8dc0061157
anyline <server@anyline.org>
2023-04-14 14:37:40 +0800

    EntityListener

--- anyline-core/src/main/java/org/anyline/listener/EntityListener.java
+++ anyline-core/src/main/java/org/anyline/listener/EntityListener.java
@@ -7,7 +7,7 @@ public interface EntityListener {
     /**
      * entity创建完成后调用 AbstractBasicController.entity后调用过
      * @param env 上下文 如request
-     * @param entity entity
+     * @param entity entity或DataRow
      */
     public void after(Object env, Object entity);
 
