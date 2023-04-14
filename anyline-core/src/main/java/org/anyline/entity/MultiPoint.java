71b2113451a53fc7d94ed789ab73ca913a3d850c
anyline <server@anyline.org>
2023-04-14 17:19:45 +0800

    MultiPoint SQL

--- anyline-core/src/main/java/org/anyline/entity/MultiPoint.java
+++ anyline-core/src/main/java/org/anyline/entity/MultiPoint.java
@@ -10,6 +10,18 @@ public class MultiPoint {
         points.add(point);
         return this;
     }
+    public MultiPoint add(MultiPoint points){
+        if(null != points){
+            points.add(points.getPoints());
+        }
+        return this;
+    }
+    public MultiPoint add(List<Point> points){
+        if(null != points) {
+            points.addAll(points);
+        }
+        return this;
+    }
     public MultiPoint clear(){
         //points.clear();
         points = new ArrayList<>();
@@ -17,5 +29,28 @@ public class MultiPoint {
     }
     public List<Point> getPoints(){
         return points;
+    }
+
+    /**
+     * sql格式
+     * @param tag 是否包含tag<br/>
+     *             false:((1 1),(2 2))<br/>
+     *             true: MULTIPOINT((1 1),(2 2))
+     * @return String
+     */
+    public String sql(boolean tag){
+        StringBuilder builder = new StringBuilder();
+        if(tag) {
+            builder.append("MULTIPOINT");
+        }
+        builder.append("(");
+        for(Point point:points){
+            builder.append(point.sql(false));
+        }
+        builder.append(")");
+        return builder.toString();
+    }
+    public String sql(){
+        return sql(true);
     }
 }
