7c9f308a8b47453433d43ee449ed57909de597e1
anyline <server@anyline.org>
2023-04-25 15:06:47 +0800

    View

---
+++ anyline-core/src/main/java/org/anyline/entity/data/View.java
@@ -0,0 +1,4 @@
+package org.anyline.entity.data;
+
+public interface View extends Table {
+}
---
+++ anyline-data-jdbc/src/main/java/org/anyline/data/entity/View.java
@@ -0,0 +1,5 @@
+package org.anyline.data.entity;
+
+public class View extends Table implements org.anyline.entity.data.View{
+    protected String keyword = "VIEW"            ;
+}
