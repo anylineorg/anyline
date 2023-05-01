6f291838dc4616de3d696e756b56a379cdc4dc37
anyline <server@anyline.org>
2023-05-02 06:47:14 +0800

    GbasetAdapter

--- anyline-data-jdbc-dialect/anyline-data-jdbc-gbasedbt/src/main/java/org/anyline/data/jdbc/gbasedb/GbasetAdapter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-gbasedbt/src/main/java/org/anyline/data/jdbc/gbasedb/GbasetAdapter.java
@@ -1,10 +1,11 @@
 package org.anyline.data.jdbc.gbasedb;
 
-import org.anyline.entity.OrderStore;
-import org.anyline.entity.PageNavi;
 import org.anyline.data.adapter.JDBCAdapter;
 import org.anyline.data.adapter.SQLAdapter;
 import org.anyline.data.run.Run;
+import org.anyline.entity.OrderStore;
+import org.anyline.entity.PageNavi;
+import org.anyline.entity.data.DatabaseType;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -12,8 +13,8 @@ import org.springframework.stereotype.Repository;
 @Repository("anyline.data.jdbc.adapter.gbasedb")
 public class GbasetAdapter extends SQLAdapter implements JDBCAdapter, InitializingBean {
  
-	public DB_TYPE type(){
-		return DB_TYPE.GBase;
+	public DatabaseType type(){
+		return DatabaseType.GBase;
 	} 
 	public GbasetAdapter(){
 		delimiterFr = "";
