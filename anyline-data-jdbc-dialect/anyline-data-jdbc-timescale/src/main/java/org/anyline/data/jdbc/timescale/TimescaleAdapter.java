4951623077f642811d2b2786f0d46959f65c6fac
anyline <server@anyline.org>
2023-05-01 23:04:16 +0800

    TimescaleAdapter

--- anyline-data-jdbc-dialect/anyline-data-jdbc-timescale/src/main/java/org/anyline/data/jdbc/timescale/TimescaleAdapter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-timescale/src/main/java/org/anyline/data/jdbc/timescale/TimescaleAdapter.java
@@ -1,13 +1,14 @@
 package org.anyline.data.jdbc.timescale;
 
 import org.anyline.data.jdbc.postgresql.PostgresqlAdapter;
+import org.anyline.entity.data.DatabaseType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyline.data.jdbc.adapter.timescale")
 public class TimescaleAdapter extends PostgresqlAdapter {
-    public DB_TYPE type(){
-        return DB_TYPE.Timescale;
+    public DatabaseType type(){
+        return DatabaseType.Timescale;
     }
     @Value("${anyline.jdbc.delimiter.timescale:}")
     private String delimiter;
