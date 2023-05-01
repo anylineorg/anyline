7f8fa5ef3587093d30439e6043ad72b466d272d8
anyline <server@anyline.org>
2023-05-01 23:07:07 +0800

    PolarAdapter

--- anyline-data-jdbc-dialect/anyline-data-jdbc-polardb/src/main/java/org/anyline/data/jdbc/polardb/PolarAdapter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-polardb/src/main/java/org/anyline/data/jdbc/polardb/PolarAdapter.java
@@ -1,10 +1,11 @@
 package org.anyline.data.jdbc.polardb;
- 
-import org.anyline.entity.PageNavi;
-import org.anyline.entity.OrderStore;
+
 import org.anyline.data.adapter.JDBCAdapter;
 import org.anyline.data.adapter.SQLAdapter;
 import org.anyline.data.run.Run;
+import org.anyline.entity.OrderStore;
+import org.anyline.entity.PageNavi;
+import org.anyline.entity.data.DatabaseType;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -12,8 +13,8 @@ import org.springframework.stereotype.Repository;
 @Repository("anyline.data.jdbc.adapter.polardb")
 public class PolarAdapter extends SQLAdapter implements JDBCAdapter, InitializingBean {
  
-	public DB_TYPE type(){
-		return DB_TYPE.PolarDB;
+	public DatabaseType type(){
+		return DatabaseType.PolarDB;
 	} 
 	public PolarAdapter(){
 		delimiterFr = "`";
