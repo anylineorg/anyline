b48c411684b68954887c537c3739d5e49014a7b9
anyline <server@anyline.org>
2023-05-01 22:59:34 +0800

    DataType

--- anyline-core/src/main/java/org/anyline/entity/metadata/DataType.java
+++ anyline-core/src/main/java/org/anyline/entity/metadata/DataType.java
@@ -2,13 +2,9 @@ package org.anyline.entity.metadata;
 
 public interface DataType {
     public static DataType ILLEGAL = new DataType() {
-        @Override
-        public String type() {
-            return null;
-        }
 
         @Override
-        public Object read(Object value, Class clazz) {
+        public Object read(Object value, Object def, Class clazz) {
             return null;
         }
 
@@ -16,6 +12,17 @@ public interface DataType {
         public Object write(Object value, Object def, boolean placeholder) {
             return null;
         }
+
+        @Override
+        public DataType convert(Convert convert) {
+            return null;
+        }
+
+        @Override
+        public Convert convert(Class clazz) {
+            return null;
+        }
+
 
         @Override
         public boolean ignorePrecision() {
@@ -30,20 +37,20 @@ public interface DataType {
         public boolean support() {
             return false;
         }
-    };
-    /**
-     * 数据类型名称,为兼容不同数据库书写习惯,name有可能是别名与数据库不一致,别名中的所有方法调用原类型方法
-     * @return String
-     */
-    public abstract String type();
 
+        @Override
+        public Class supportClass() {
+            return null;
+        }
+    };
     /**
      * 从数据库中读取数据,常用的基本类型可以自动转换,不常用的如json/point/polygon/blob等转换成anyline对应的类型
      * @param value value
+     * @param def 默认值
      * @param clazz 目标数据类型(给entity赋值时可以根据class, DataRow赋值时可以指定class，否则按检测metadata类型转换 转换不不了的原样返回)
      * @return Object
      */
-    public abstract Object read(Object value, Class clazz);
+    public abstract Object read(Object value, Object def, Class clazz);
     /**
      * 写入数据库前类型转换<br/>
      * 如果有占位符成数据库可接受的Java数据类型<br/>
@@ -55,6 +62,8 @@ public interface DataType {
      */
     public abstract Object write(Object value, Object def, boolean placeholder);
 
+    public abstract DataType convert(Convert convert);
+    public abstract Convert convert(Class clazz);
 
 
     //public abstract JavaType getJavaType();
@@ -67,4 +76,5 @@ public interface DataType {
     public abstract boolean ignorePrecision();
     public abstract boolean ignoreScale();
     public abstract boolean support();
+    public abstract Class supportClass();
 }
