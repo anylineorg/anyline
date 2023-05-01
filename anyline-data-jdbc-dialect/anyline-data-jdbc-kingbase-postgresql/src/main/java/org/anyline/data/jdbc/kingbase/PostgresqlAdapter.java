b887954b28901446139f0aab241b4043accb867c
anyline <server@anyline.org>
2023-05-01 23:06:47 +0800

    PostgresqlAdapter

--- anyline-data-jdbc-dialect/anyline-data-jdbc-kingbase-postgresql/src/main/java/org/anyline/data/jdbc/kingbase/PostgresqlAdapter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-kingbase-postgresql/src/main/java/org/anyline/data/jdbc/kingbase/PostgresqlAdapter.java
@@ -1,11 +1,12 @@
  
 package org.anyline.data.jdbc.kingbase;
- 
-import org.anyline.entity.PageNavi;
-import org.anyline.entity.OrderStore;
+
 import org.anyline.data.adapter.JDBCAdapter;
 import org.anyline.data.adapter.SQLAdapter;
 import org.anyline.data.run.Run;
+import org.anyline.entity.OrderStore;
+import org.anyline.entity.PageNavi;
+import org.anyline.entity.data.DatabaseType;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -13,8 +14,8 @@ import org.springframework.stereotype.Repository;
 @Repository("anyline.data.jdbc.adapter.kingbase")
 public class PostgresqlAdapter extends SQLAdapter implements JDBCAdapter, InitializingBean {
  
-	public DB_TYPE type(){
-		return DB_TYPE.KingBase;
+	public DatabaseType type(){
+		return DatabaseType.KingBase;
 	} 
 	public PostgresqlAdapter(){
 		delimiterFr = "\"";
