fb623641a9d467a6743a69267ced778cf45e1f6a
anyline <server@anyline.org>
2023-05-01 23:06:02 +0800

    RandomGenerator

--- anyline-data-jdbc/src/main/java/org/anyline/data/generator/init/RandomGenerator.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/generator/init/RandomGenerator.java
@@ -1,7 +1,7 @@
 package org.anyline.data.generator.init;
 
 import org.anyline.data.generator.PrimaryGenerator;
-import org.anyline.data.adapter.JDBCAdapter;
+import org.anyline.entity.data.DatabaseType;
 import org.anyline.util.BasicUtil;
 import org.anyline.util.BeanUtil;
 import org.anyline.util.ConfigTable;
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class RandomGenerator implements PrimaryGenerator {
     @Override
-    public Object create(Object entity, JDBCAdapter.DB_TYPE type, String table, List<String> columns, String other) {
+    public Object create(Object entity, DatabaseType type, String table, List<String> columns, String other) {
         for(String column:columns){
             String prefix = ConfigTable.PRIMARY_GENERATOR_PREFIX;
             int len = ConfigTable.PRIMARY_GENERATOR_RANDOM_LENGTH - prefix.length();
