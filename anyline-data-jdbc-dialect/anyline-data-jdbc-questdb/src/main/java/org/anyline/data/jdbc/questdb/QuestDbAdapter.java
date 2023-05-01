123cb25dc5032864c4de1e326e4e39d60e9169df
anyline <server@anyline.org>
2023-05-01 23:06:09 +0800

    QuestDbAdapter

--- anyline-data-jdbc-dialect/anyline-data-jdbc-questdb/src/main/java/org/anyline/data/jdbc/questdb/QuestDbAdapter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-questdb/src/main/java/org/anyline/data/jdbc/questdb/QuestDbAdapter.java
@@ -1,13 +1,14 @@
 package org.anyline.data.jdbc.questdb;
 
 import org.anyline.data.jdbc.postgresql.PostgresqlAdapter;
+import org.anyline.entity.data.DatabaseType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyline.data.jdbc.adapter.questdb")
 public class QuestDbAdapter extends PostgresqlAdapter {
-    public DB_TYPE type(){
-        return DB_TYPE.QuestDB;
+    public DatabaseType type(){
+        return DatabaseType.QuestDB;
     }
     @Value("${anyline.jdbc.delimiter.questdb:}")
     private String delimiter;
