0a7f5f97d6748d76cfcad1bf8b4472c50d67ab87
anyline <server@anyline.org>
2023-05-01 22:58:26 +0800

    Convert interface

---
+++ anyline-core/src/main/java/org/anyline/entity/metadata/Convert.java
@@ -0,0 +1,8 @@
+package org.anyline.entity.metadata;
+
+public interface Convert {
+
+    public Class getOrigin();
+    public Class getTarget();
+    public Object exe(Object value, Object def);
+}
