59e44466a44625127fec017141372bf900fc976e
anyline <server@anyline.org>
2023-05-06 16:49:22 +0800

    JavaTypeAdapter

--- anyline-core/src/main/java/org/anyline/adapter/init/JavaTypeAdapter.java
+++ anyline-core/src/main/java/org/anyline/adapter/init/JavaTypeAdapter.java
@@ -1,9 +1,7 @@
 package org.anyline.adapter.init;
 
 
-import org.anyline.entity.metadata.Convert;
 import org.anyline.entity.metadata.DataType;
-import org.anyline.entity.metadata.init.DefaultConvert;
 import org.anyline.entity.metadata.init.DefaultJavaType;
 
 import java.util.Hashtable;
@@ -25,14 +23,6 @@ public class JavaTypeAdapter {
         for(DefaultJavaType type:DefaultJavaType.values()){
             Class clazz = type.supportClass();
             types.put(clazz, type);
-        }
-        //内置转换器
-        for (Convert convert : DefaultConvert.values()) {
-            Class origin = convert.getOrigin();
-            DataType type = types.get(origin);
-            if(null != type){
-                type.convert(convert);
-            }
         }
     }
 }
