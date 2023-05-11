9a65f133375d2ff5fb6627289ac69517c462c85d
anyline <server@anyline.org>
2023-05-11 21:55:32 +0800

    buildChangeDefaultRunSQL

--- anyline-data-jdbc-dialect/anyline-data-jdbc-oracle/src/main/java/org/anyline/data/jdbc/oracle/OracleAdapter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-oracle/src/main/java/org/anyline/data/jdbc/oracle/OracleAdapter.java
@@ -1299,7 +1299,12 @@ public class OracleAdapter extends SQLAdapter implements JDBCAdapter, Initializi
 	 */
 	@Override
 	public String buildChangeDefaultRunSQL(Column column) throws Exception{
-		Object def = column.getDefaultValue();
+		Object def = null;
+		if(null != column.getUpdate()){
+			def = column.getUpdate().getDefaultValue();
+		}else {
+			def = column.getDefaultValue();
+		}
 		StringBuilder builder = new StringBuilder();
 		builder.append("ALTER TABLE ");
 		name(builder, column.getTable()).append(" MODIFY ");
@@ -1360,7 +1365,12 @@ public class OracleAdapter extends SQLAdapter implements JDBCAdapter, Initializi
 	 */
 	@Override
 	public String buildChangeCommentRunSQL(Column column) throws Exception{
-		String comment = column.getComment();
+		String comment = null;
+		if(null != column.getUpdate()){
+			comment = column.getUpdate().getComment();
+		}else {
+			comment = column.getComment();
+		}
 		if(BasicUtil.isNotEmpty(comment)) {
 			StringBuilder builder = new StringBuilder();
 			builder.append("COMMENT ON COLUMN ");
