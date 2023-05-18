b73d7869778fdb19638087c85e461ff0eda52d61
anyline <server@anyline.org>
2023-05-18 18:31:27 +0800

    EntityAdapterProxy

--- anyline-core/src/main/java/org/anyline/proxy/EntityAdapterProxy.java
+++ anyline-core/src/main/java/org/anyline/proxy/EntityAdapterProxy.java
@@ -40,6 +40,12 @@ public class EntityAdapterProxy {
         }
         return null;
     }
+
+    /**
+     * Entity对应的表名
+     * @param clazz 类
+     * @return 表
+     */
     public static Table table(Class clazz){
         return table(adapter, clazz);
     }
