a56dac56b09ecbd295313cc22f6f7392aa53ca54
anyline <server@anyline.org>
2023-05-22 17:29:15 +0800

    DatabaseType

--- anyline-core/src/main/java/org/anyline/entity/data/DatabaseType.java
+++ anyline-core/src/main/java/org/anyline/entity/data/DatabaseType.java
@@ -4,8 +4,11 @@ import org.anyline.adapter.KeyAdapter;
 import org.anyline.adapter.KeyAdapter.KEY_CASE;
 
 public enum DatabaseType {
+        AntDB			    ("AntDB"	, KEY_CASE.SRC,""),
         Cassandra			("CASSANDRA"	, KEY_CASE.SRC,""),
         ClickHouse			( "CLICKHOUSE"	, KEY_CASE.SRC, "ru.yandex.clickhouse.ClickHouseDriver"),
+        ChinaMobileDB   	("磐维数据库"    , KEY_CASE.SRC,""),
+        ChinaUnicomDB   	("中国联通"    , KEY_CASE.SRC,""),
         Citus   			("Citus"	    , KEY_CASE.SRC,""),
         CockroachDB			("CockroachDB"	, KEY_CASE.SRC,""),
         DB2					("db2"			, KEY_CASE.SRC,"com.ibm.db2.jcc.DB2Driver"),
@@ -16,15 +19,20 @@ public enum DatabaseType {
         GaiaDB  			("百度GaiaDB-X"	, KEY_CASE.SRC,""),
         GreatDB  			("万里数据库"	, KEY_CASE.SRC,""),
         H2  				("H2"			, KEY_CASE.SRC,"org.h2.Driver"),
+        HashData  			("酷克数据"	    , KEY_CASE.SRC,""),
         HighGo				("瀚高"			, KEY_CASE.SRC,"com.highgo.jdbc.Driver"),
+        HotDB  			    ("热璞"         , KEY_CASE.SRC,""),
         HSQLDB  			("HSQLDB"		, KEY_CASE.SRC,"org.hsqldb.jdbcDriver"),
         InfluxDB			("InfluxDB"	, KEY_CASE.SRC,""),
+        Informix			("Informix"	, KEY_CASE.SRC,""),
         KingBase			("人大金仓"		, KEY_CASE.UPPER,"com.kingbase8.Driver"),
+        KunDB			    ("星环"		    , KEY_CASE.UPPER,""),
         LightDB			    ("LightDB"		, KEY_CASE.UPPER,""),
         MariaDB				("MariaDB"		, KEY_CASE.SRC,"org.mariadb.jdbc.Driver"),
         MongoDB				("MongoDB"		, KEY_CASE.SRC,""),
         MogoDB				("云和恩墨"		, KEY_CASE.SRC,""),
         MSSQL				("SQL Server"	, KEY_CASE.SRC,"com.microsoft.sqlserver.jdbc.SQLServerDriver"),
+        MuDB				("沐融信息科技"	, KEY_CASE.SRC,""),
         MYSQL				("MySQL"		, KEY_CASE.SRC,"com.mysql.cj.jdbc.Driver"),
         Neo4j  				("Neo4j"		, KEY_CASE.SRC,"org.neo4j.jdbc.Driver"),
         OceanBase 			("OceanBase"	, KEY_CASE.SRC,"com.oceanbase.jdbc.Driver"),
@@ -35,13 +43,18 @@ public enum DatabaseType {
         PostgreSQL 			("PostgreSQL"	, KEY_CASE.SRC,"org.postgresql.Driver"),
         QuestDB 			("QuestDB"		, KEY_CASE.SRC,"org.postgresql.Driver"),
         RethinkDB  			("RethinkDB"	, KEY_CASE.SRC,""),
+        SinoDB  			("星瑞格"	    , KEY_CASE.SRC,""),
         SQLite  			("SQLite"		, KEY_CASE.SRC,"org.sqlite.JDBC"),
+        StarDB  			("京东StarDB"	, KEY_CASE.SRC,""),
         TDengine  			("TDengine"	, KEY_CASE.SRC,"com.taosdata.jdbc.TSDBDriver"),
         UXDB			    ("优炫数据库"	, KEY_CASE.SRC,"com.uxsino.uxdb.Driver"),
+        UbiSQL    			("平安科技"	    , KEY_CASE.SRC,""),
         TiDB    			("TiDB"	    , KEY_CASE.SRC,""),
         TDSQL    			("TDSQL"	    , KEY_CASE.SRC,""),
         Timescale			("Timescale"	, KEY_CASE.SRC,"org.postgresql.Driver"),
-        Vastbase			("Timescale"	, KEY_CASE.SRC,"org.postgresql.Driver");
+        Vastbase			("Timescale"	, KEY_CASE.SRC,""),
+        xigemaDB			("华胜信泰"	    , KEY_CASE.SRC,""),
+        YiDB			    ("天翼数智"	    , KEY_CASE.SRC,"");
         private DatabaseType(String title, KeyAdapter.KEY_CASE objectNameCase, String driver){
                 this.title = title;
                 this.driver = driver;
