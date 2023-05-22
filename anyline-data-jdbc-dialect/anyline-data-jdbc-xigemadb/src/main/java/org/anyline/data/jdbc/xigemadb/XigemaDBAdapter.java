92b44376235a2325a771909650166ff086c1f9a1
anyline <server@anyline.org>
2023-05-22 17:28:15 +0800

    xigemadb

---
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-xigemadb/src/main/java/org/anyline/data/jdbc/xigemadb/XigemaDBAdapter.java
@@ -0,0 +1,21 @@
+package org.anyline.data.jdbc.xigemadb;
+
+import org.anyline.data.jdbc.informix.InformixAdapter;
+import org.anyline.entity.data.DatabaseType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.data.jdbc.adapter.xigemadb")
+public class XigemaDBAdapter extends InformixAdapter {
+    
+    public DatabaseType type(){
+        return DatabaseType.xigemaDB;
+    }
+    @Value("${anyline.data.jdbc.delimiter.xigemadb:}")
+    private String delimiter;
+
+    @Override
+    public void afterPropertiesSet()  {
+        setDelimiter(delimiter);
+    }
+} 
