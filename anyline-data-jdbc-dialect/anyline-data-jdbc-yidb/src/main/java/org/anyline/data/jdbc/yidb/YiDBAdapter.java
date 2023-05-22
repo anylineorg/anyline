d313bb059b53613151ac587b1a52a1c30f35bbfc
anyline <server@anyline.org>
2023-05-22 17:28:20 +0800

    yidb

---
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-yidb/src/main/java/org/anyline/data/jdbc/yidb/YiDBAdapter.java
@@ -0,0 +1,21 @@
+package org.anyline.data.jdbc.yidb;
+
+import org.anyline.data.jdbc.tidb.TiDBAdapter;
+import org.anyline.entity.data.DatabaseType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.data.jdbc.adapter.yidb")
+public class YiDBAdapter extends TiDBAdapter {
+    
+    public DatabaseType type(){
+        return DatabaseType.YiDB;
+    }
+    @Value("${anyline.data.jdbc.delimiter.yidb:}")
+    private String delimiter;
+
+    @Override
+    public void afterPropertiesSet()  {
+        setDelimiter(delimiter);
+    }
+} 
