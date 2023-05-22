8d1e091c5844dbe02255252c1efafd15a121bbf2
anyline <server@anyline.org>
2023-05-22 15:17:40 +0800

    antdb

---
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-antdb/src/main/java/org/anyline/data/jdbc/antdb/AntDBAdapter.java
@@ -0,0 +1,21 @@
+package org.anyline.data.jdbc.antdb;
+
+import org.anyline.data.jdbc.postgresql.PostgresqlAdapter;
+import org.anyline.entity.data.DatabaseType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.data.jdbc.adapter.antdb")
+public class AntDBAdapter extends PostgresqlAdapter {
+    
+    public DatabaseType type(){
+        return DatabaseType.AntDB;
+    }
+    @Value("${anyline.data.jdbc.delimiter.antdb:}")
+    private String delimiter;
+
+    @Override
+    public void afterPropertiesSet()  {
+        setDelimiter(delimiter);
+    }
+} 
