d57aa07043a277f64a1e27a6e3a8bec5812c1c47
anyline <server@anyline.org>
2023-05-22 17:27:20 +0800

    HashData

---
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-hashdata/src/main/java/org/anyline/data/jdbc/hashdata/HashDataAdapter.java
@@ -0,0 +1,21 @@
+package org.anyline.data.jdbc.hashdata;
+
+import org.anyline.data.jdbc.postgresql.PostgresqlAdapter;
+import org.anyline.entity.data.DatabaseType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.data.jdbc.adapter.hashdata")
+public class HashDataAdapter extends PostgresqlAdapter {
+    
+    public DatabaseType type(){
+        return DatabaseType.HashData;
+    }
+    @Value("${anyline.data.jdbc.delimiter.hashdata:}")
+    private String delimiter;
+
+    @Override
+    public void afterPropertiesSet()  {
+        setDelimiter(delimiter);
+    }
+} 
