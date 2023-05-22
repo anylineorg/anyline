3291fbb2b1409f32b7ffe95603666c9728ef989d
anyline <server@anyline.org>
2023-05-22 17:27:56 +0800

    sinodb

---
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-sinodb/src/main/java/org/anyline/data/jdbc/sinodb/SinoDBAdapter.java
@@ -0,0 +1,21 @@
+package org.anyline.data.jdbc.sinodb;
+
+import org.anyline.data.jdbc.tidb.TiDBAdapter;
+import org.anyline.entity.data.DatabaseType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.data.jdbc.adapter.sinodb")
+public class SinoDBAdapter extends TiDBAdapter {
+    
+    public DatabaseType type(){
+        return DatabaseType.SinoDB;
+    }
+    @Value("${anyline.data.jdbc.delimiter.sinodb:}")
+    private String delimiter;
+
+    @Override
+    public void afterPropertiesSet()  {
+        setDelimiter(delimiter);
+    }
+} 
