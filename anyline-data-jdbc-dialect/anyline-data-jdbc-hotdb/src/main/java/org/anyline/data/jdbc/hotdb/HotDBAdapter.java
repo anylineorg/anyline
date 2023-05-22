aa33d2aa0feae090783a277bffbce3393709258b
anyline <server@anyline.org>
2023-05-22 17:27:28 +0800

    HotDB

---
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-hotdb/src/main/java/org/anyline/data/jdbc/hotdb/HotDBAdapter.java
@@ -0,0 +1,21 @@
+package org.anyline.data.jdbc.hotdb;
+
+import org.anyline.data.jdbc.mysql.MySQLAdapter;
+import org.anyline.entity.data.DatabaseType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.data.jdbc.adapter.hotdb")
+public class HotDBAdapter extends MySQLAdapter {
+    
+    public DatabaseType type(){
+        return DatabaseType.HotDB;
+    }
+    @Value("${anyline.data.jdbc.delimiter.hotdb:}")
+    private String delimiter;
+
+    @Override
+    public void afterPropertiesSet()  {
+        setDelimiter(delimiter);
+    }
+} 
