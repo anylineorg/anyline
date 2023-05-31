5c3c5e4bb7b68bd89870d1376fa1917c216e2e74
anyline <server@anyline.org>
2023-05-31 14:45:01 +0800

    GeometryType

---
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-mysql/src/main/java/org/anyline/data/jdbc/mysql/GeometryType.java
@@ -0,0 +1,17 @@
+package org.anyline.data.jdbc.mysql;
+
+import org.anyline.entity.geometry.*;
+
+public enum GeometryType {
+    point( Point.class),
+    LineString( Line.class),
+    Polygon(Polygon.class),
+    MultiPoint(MultiPoint.class),
+    MultiLineString( MultiLine.class),
+    MultiPolygon(MultiPolygon.class),
+    GeometryCollection(GeometryCollection.class);
+     private Class clazz;
+    GeometryType(Class clazz){
+        this.clazz = clazz;
+    }
+}
