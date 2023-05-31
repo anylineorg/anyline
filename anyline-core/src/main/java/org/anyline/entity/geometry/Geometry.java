5a9a2a92a6123107b9a855d2864c2ce3d7f0ca2a
anyline <server@anyline.org>
2023-05-31 22:43:13 +0800

    GeometryType

--- anyline-core/src/main/java/org/anyline/entity/geometry/Geometry.java
+++ anyline-core/src/main/java/org/anyline/entity/geometry/Geometry.java
@@ -2,21 +2,17 @@ package org.anyline.entity.geometry;
 
 public class Geometry {
     public enum Type{
-        point(1, Point.class),
-        Line(2, Line.class),
-        Polygon(3, Polygon.class),
-        MultiPoint(4, MultiPoint.class),
-        MultiLine(5, MultiLine.class),
-        MultiPolygon(6, MultiPolygon.class),
-        GeometryCollection(7, GeometryCollection.class);
-        private Integer header;
+        Point(Point.class),
+        Line(Line.class),
+        Polygon(Polygon.class),
+        MultiPoint(MultiPoint.class),
+        MultiLine(MultiLine.class),
+        MultiPolygon(MultiPolygon.class),
+        GeometryCollection(GeometryCollection.class);
+
         private Class clazz;
-        Type(Integer header, Class clazz){
-            this.header = header;
-            this.clazz = clazz;
-        }
-        public int getHeader(){
-            return header;
+        Type(Class clazz){
+             this.clazz = clazz;
         }
     }
 }
