228c2364c6fd919cab91b929e759718a950f1505
anyline <server@anyline.org>
2023-05-31 14:57:28 +0800

    double2Bytes

--- anyline-core/src/main/java/org/anyline/util/NumberUtil.java
+++ anyline-core/src/main/java/org/anyline/util/NumberUtil.java
@@ -824,11 +824,18 @@ public class NumberUtil {
 			value |= ((long) (arr[start+i] & 0xff)) << (8 * i);
 		}
 		return Double.longBitsToDouble(value);
-
 	}
 	public static byte[] double2bytes(double d) {
 		long value = Double.doubleToRawLongBits(d);
 		byte[] byteRet = new byte[8];
+		for (int i = 0; i < 8; i++) {
+			byteRet[i] = (byte) ((value >> 8 * i) & 0xff);
+		}
+		return byteRet;
+	}
+	public static Byte[] double2Bytes(double d) {
+		long value = Double.doubleToRawLongBits(d);
+		Byte[] byteRet = new Byte[8];
 		for (int i = 0; i < 8; i++) {
 			byteRet[i] = (byte) ((value >> 8 * i) & 0xff);
 		}
