79737bf53b1caf49cf271eb434430f7bfacfe1bc
anyline <server@anyline.org>
2023-06-01 11:34:45 +0800

    Polygon

--- anyline-core/src/main/java/org/anyline/entity/geometry/Polygon.java
+++ anyline-core/src/main/java/org/anyline/entity/geometry/Polygon.java
@@ -1,4 +1,74 @@
 package org.anyline.entity.geometry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Polygon extends Geometry{
+    private List<Point> points = new ArrayList<>();
+    public Polygon add(Point point){
+        points.add(point);
+        return this;
+    }
+    public List<Point> points(){
+        return points;
+    }
+    public Polygon(){}
+    public Polygon(List<Point> points) {
+        this.points = points;
+    }
+
+    public String toString(){
+        return toString(true);
+    }
+    public String toString(boolean tag){
+        StringBuilder builder = new StringBuilder();
+        if(tag){
+            builder.append("Polygon");
+        }
+        builder.append("(");
+        boolean first = true;
+        for(Point point:points){
+            if(!first){
+                builder.append(", ");
+            }
+            builder.append(point.toString(false));
+            first = false;
+        }
+        builder.append(")");
+        return builder.toString();
+    }
+
+    /**
+     * sql格式
+     * @param tag 是否包含tag<br/>
+     *             false:(120 36)<br/>
+     *             true: Point(120 36)
+     * @param bracket 是否包含()
+     * @return String
+     */
+    public String sql(boolean tag, boolean bracket){
+        StringBuilder builder = new StringBuilder();
+        if(tag){
+            builder.append("Polygon");
+        }
+        if(bracket){
+            builder.append("(");
+        }
+
+        boolean first = true;
+        for(Point point:points){
+            if(!first){
+                builder.append(", ");
+            }
+            builder.append(point.sql(false, false));
+            first = false;
+        }
+        if(bracket){
+            builder.append(")");
+        }
+        return builder.toString();
+    }
+    public String sql(){
+        return sql(true, true);
+    }
 }
