79cf678a802ae0bdb99a973033fbb43da9f155a1
anyline <server@anyline.org>
2023-06-05 14:04:31 +0800

    Geometry writer

--- anyline-data-jdbc-dialect/anyline-data-jdbc-mysql/src/main/java/org/anyline/data/jdbc/mysql/MySQLWriter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-mysql/src/main/java/org/anyline/data/jdbc/mysql/MySQLWriter.java
@@ -2,8 +2,7 @@ package org.anyline.data.jdbc.mysql;
 
 import org.anyline.adapter.DataWriter;
 import org.anyline.data.metadata.StandardColumnType;
-import org.anyline.entity.geometry.Line;
-import org.anyline.entity.geometry.Point;
+import org.anyline.entity.geometry.*;
 
 public enum MySQLWriter {
 
@@ -14,14 +13,24 @@ public enum MySQLWriter {
             if(value instanceof Point) {
                 point = (Point) value;
             }else if(value instanceof double[]){
-                double[] doubles = (double[]) value;
-                if(doubles.length >= 2){
-                    point = new Point(doubles[0], doubles[1]);
+                double[] xy = (double[]) value;
+                if(xy.length >= 2){
+                    point = new Point(xy[0], xy[1]);
                 }
             }else if(value instanceof Double[]){
-                Double[] doubles = (Double[]) value;
-                if(doubles.length >= 2){
-                    point = new Point(doubles[0], doubles[1]);
+                Double[] xy = (Double[]) value;
+                if(xy.length >= 2){
+                    point = new Point(xy[0], xy[1]);
+                }
+            }else if(value instanceof int[]){
+                int[] xy = (int[]) value;
+                if(xy.length >= 2){
+                    point = new Point(xy[0], xy[1]);
+                }
+            }else if(value instanceof Integer[]){
+                Integer[] xy = (Integer[]) value;
+                if(xy.length >= 2){
+                    point = new Point(xy[0], xy[1]);
                 }
             }
             if(null != point) {
@@ -48,7 +57,83 @@ public enum MySQLWriter {
             }
             return value;
         }
+    }),
+
+    PolygonWriter(new Object[]{Polygon.class, StandardColumnType.POLYGON}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof Polygon) {
+                Polygon polygon = (Polygon) value;
+                if (placeholder) {
+                    return MySQLGeometryAdapter.wkb(polygon);
+                } else {
+                    return MySQLGeometryAdapter.sql(polygon);
+                }
+            }
+            return value;
+        }
+    }),
+
+    MultiPointWriter(new Object[]{MultiPoint.class, StandardColumnType.MULTIPOINT}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof MultiPoint) {
+                MultiPoint multiPoint = (MultiPoint) value;
+                if (placeholder) {
+                    return MySQLGeometryAdapter.wkb(multiPoint);
+                } else {
+                    return MySQLGeometryAdapter.sql(multiPoint);
+                }
+            }
+            return value;
+        }
+    }),
+
+    MultiLineWriter(new Object[]{MultiLine.class, StandardColumnType.MULTILINESTRING}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof MultiLine) {
+                MultiLine multiLine = (MultiLine) value;
+                if (placeholder) {
+                    return MySQLGeometryAdapter.wkb(multiLine);
+                } else {
+                    return MySQLGeometryAdapter.sql(multiLine);
+                }
+            }
+            return value;
+        }
+    }),
+
+    MultiPolygonWriter(new Object[]{MultiPolygon.class, StandardColumnType.MULTIPOLYGON}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof MultiPolygon) {
+                MultiPolygon multiPolygon = (MultiPolygon) value;
+                if (placeholder) {
+                    return MySQLGeometryAdapter.wkb(multiPolygon);
+                } else {
+                    return MySQLGeometryAdapter.sql(multiPolygon);
+                }
+            }
+            return value;
+        }
+    }),
+
+    GeometryCollectionWriter(new Object[]{GeometryCollection.class, StandardColumnType.GEOMETRYCOLLECTION}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof GeometryCollection) {
+                GeometryCollection collection = (GeometryCollection) value;
+                if (placeholder) {
+                    return MySQLGeometryAdapter.wkb(collection);
+                } else {
+                    return MySQLGeometryAdapter.sql(collection);
+                }
+            }
+            return value;
+        }
     })
+
     ;
     public Object[] supports(){
         return supports;
