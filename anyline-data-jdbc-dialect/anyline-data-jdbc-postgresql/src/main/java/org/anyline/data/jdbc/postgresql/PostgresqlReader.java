053ca5e225dbcafa6f0e2b85a235439ded24f83f
anyline <server@anyline.org>
2023-06-05 16:56:56 +0800

    PostgresqlReader

--- anyline-data-jdbc-dialect/anyline-data-jdbc-postgresql/src/main/java/org/anyline/data/jdbc/postgresql/PostgresqlReader.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-postgresql/src/main/java/org/anyline/data/jdbc/postgresql/PostgresqlReader.java
@@ -1,23 +1,72 @@
 package org.anyline.data.jdbc.postgresql;
 
 import org.anyline.adapter.DataReader;
-import org.anyline.entity.geometry.Point;
-import org.postgresql.geometric.PGline;
-import org.postgresql.geometric.PGpoint;
+import org.postgresql.geometric.*;
 
 public enum PostgresqlReader {
     PointReader(new Object[]{PGpoint.class}, new DataReader() {
         @Override
         public Object read(Object value) {
-            PGpoint point = (PGpoint)value;
-            return new Point(point.x, point.y);
+            if(value instanceof PGpoint) {
+                value = PostgresqlGeometryAdapter.parsePoint((PGpoint) value);
+            }
+            return value;
         }
     }),
+    LineSegmentReader(new Object[]{PGlseg.class}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            if(value instanceof PGlseg) {
+                value = PostgresqlGeometryAdapter.parseLineSegment((PGlseg) value);
+            }
+            return value;
+        }
+    }),
+    PathReader(new Object[]{PGpath.class}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            if(value instanceof PGpath) {
+                value = PostgresqlGeometryAdapter.parsePath((PGpath) value);
+            }
+            return value;
+        }
+    }),
+    PolygonReader(new Object[]{PGpolygon.class}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            if(value instanceof PGpolygon) {
+                value = PostgresqlGeometryAdapter.parsePolygon((PGpolygon) value);
+            }
+            return value;
+        }
+    }),
+    CircleReader(new Object[]{PGcircle.class}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            if(value instanceof PGcircle) {
+                value = PostgresqlGeometryAdapter.parseCircle((PGcircle) value);
+            }
+            return value;
+        }
+    }),
+    //直线
     LineReader(new Object[]{PGline.class}, new DataReader() {
         @Override
         public Object read(Object value) {
-            PGline pg = (PGline)value;
-            return pg;
+            if(value instanceof PGline) {
+                value = PostgresqlGeometryAdapter.parseLine((PGline) value);
+            }
+            return value;
+        }
+    }),
+    //长方形
+    BoxReader(new Object[]{PGbox.class}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            if(value instanceof PGbox) {
+                value = PostgresqlGeometryAdapter.parseBox((PGbox) value);
+            }
+            return value;
         }
     }),
     ;
