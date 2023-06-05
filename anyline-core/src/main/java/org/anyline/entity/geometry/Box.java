6324d53c719845cb5dc94237f6c97fa47e6835f5
anyline <server@anyline.org>
2023-06-05 16:56:08 +0800

    box

--- anyline-core/src/main/java/org/anyline/entity/geometry/Box.java
+++ anyline-core/src/main/java/org/anyline/entity/geometry/Box.java
@@ -9,6 +9,39 @@ public class Box extends Geometry{
         this.p1 = p1;
         this.p2 = p2;
     }
+
+    public Point p1() {
+        return p1;
+    }
+
+    public void p1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public Point p2() {
+        return p2;
+    }
+
+    public void p2(Point p2) {
+        this.p2 = p2;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public void setP2(Point p2) {
+        this.p2 = p2;
+    }
+
     @Override
     public String toString() {
         return toString(true);
