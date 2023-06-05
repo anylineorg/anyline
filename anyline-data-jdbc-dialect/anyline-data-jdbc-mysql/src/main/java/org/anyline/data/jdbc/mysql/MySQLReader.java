588704e80fe5e93330007b14baa98f917c20182a
anyline <server@anyline.org>
2023-06-05 14:04:23 +0800

    Geometry reader

--- anyline-data-jdbc-dialect/anyline-data-jdbc-mysql/src/main/java/org/anyline/data/jdbc/mysql/MySQLReader.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-mysql/src/main/java/org/anyline/data/jdbc/mysql/MySQLReader.java
@@ -24,6 +24,41 @@ public enum MySQLReader {
             byte[] bytes = (byte[]) value;
             return MySQLGeometryAdapter.parseLine(bytes);
         }
+    }),
+    PolygonReader(new Object[]{StandardColumnType.POLYGON}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            byte[] bytes = (byte[]) value;
+            return MySQLGeometryAdapter.parsePolygon(bytes);
+        }
+    }),
+    MultiPointReader(new Object[]{StandardColumnType.MULTIPOINT}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            byte[] bytes = (byte[]) value;
+            return MySQLGeometryAdapter.parseMultiPoint(bytes);
+        }
+    }),
+    MultiLineReader(new Object[]{StandardColumnType.MULTILINESTRING}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            byte[] bytes = (byte[]) value;
+            return MySQLGeometryAdapter.parseMultiLine(bytes);
+        }
+    }),
+    MultiPolygonReader(new Object[]{StandardColumnType.MULTIPOLYGON}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            byte[] bytes = (byte[]) value;
+            return MySQLGeometryAdapter.parseMultiPolygon(bytes);
+        }
+    }),
+    GeometryCollectionReader(new Object[]{StandardColumnType.GEOMETRYCOLLECTION}, new DataReader() {
+        @Override
+        public Object read(Object value) {
+            byte[] bytes = (byte[]) value;
+            return MySQLGeometryAdapter.parseGeometryCollection(bytes);
+        }
     })
     ;
     public Object[] supports(){
