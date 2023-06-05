0100e8e362b27de3bec8fdc33053fc6aafa87444
anyline <server@anyline.org>
2023-06-05 09:55:27 +0800

    GeometryCollection

--- anyline-core/src/main/java/org/anyline/entity/geometry/GeometryCollection.java
+++ anyline-core/src/main/java/org/anyline/entity/geometry/GeometryCollection.java
@@ -1,4 +1,15 @@
 package org.anyline.entity.geometry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GeometryCollection extends Geometry{
+    private List<Geometry> list = new ArrayList<>();
+    public List<Geometry> list(){
+        return list;
+    }
+    public GeometryCollection add(Geometry geometry){
+        list.add(geometry);
+        return this;
+    }
 }
