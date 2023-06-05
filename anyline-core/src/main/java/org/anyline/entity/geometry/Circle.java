9f1ccab7872693003ae05daf01a54579f705c8f7
anyline <server@anyline.org>
2023-06-05 16:25:32 +0800

    circle

--- anyline-core/src/main/java/org/anyline/entity/geometry/Circle.java
+++ anyline-core/src/main/java/org/anyline/entity/geometry/Circle.java
@@ -9,6 +9,38 @@ public class Circle extends Geometry{
         this.center = center;
         this.radius = radius;
     }
+
+    public Point center() {
+        return center;
+    }
+
+    public void center(Point center) {
+        this.center = center;
+    }
+
+    public Double radius() {
+        return radius;
+    }
+
+    public void radius(Double radius) {
+        this.radius = radius;
+    }
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
     @Override
     public String toString() {
         return toString(true);
