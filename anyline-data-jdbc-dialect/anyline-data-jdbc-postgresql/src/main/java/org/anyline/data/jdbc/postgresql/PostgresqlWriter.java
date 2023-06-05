c75d0b5426c8f2fa59b7173a8ff08325549512ee
anyline <server@anyline.org>
2023-06-05 16:57:04 +0800

    PostgresqlWriter

--- anyline-data-jdbc-dialect/anyline-data-jdbc-postgresql/src/main/java/org/anyline/data/jdbc/postgresql/PostgresqlWriter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-postgresql/src/main/java/org/anyline/data/jdbc/postgresql/PostgresqlWriter.java
@@ -2,8 +2,8 @@ package org.anyline.data.jdbc.postgresql;
 
 import org.anyline.adapter.DataWriter;
 import org.anyline.data.metadata.StandardColumnType;
-import org.anyline.entity.geometry.Point;
-import org.postgresql.geometric.PGpoint;
+import org.anyline.entity.geometry.*;
+import org.postgresql.geometric.*;
 
 public enum PostgresqlWriter {
     PointWriter(new Object[]{Point.class, StandardColumnType.POINT}, new DataWriter() {
@@ -11,10 +11,86 @@ public enum PostgresqlWriter {
         public Object write(Object value, boolean placeholder) {
             if(value instanceof Point) {
                 Point point = (Point) value;
+                PGpoint pg = PostgresqlGeometryAdapter.convert(point);
                 if (placeholder) {
-                    return new PGpoint(point.x(), point.y());
+                    return pg;
                 } else {
-                    return "POINT(" + point.x() + "," + point.y() + ")";
+                    return pg.getValue();
+                }
+            }
+            return value;
+        }
+    }),
+    LineSegmentWriter(new Object[]{LineSegment.class, StandardColumnType.LSEG}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof LineSegment) {
+                LineSegment segment = (LineSegment) value;
+                PGlseg pg = PostgresqlGeometryAdapter.convert(segment);
+                if (placeholder) {
+                    return pg;
+                } else {
+                    return pg.getValue();
+                }
+            }
+            return value;
+        }
+    }),
+    PathWriter(new Object[]{LineString.class, StandardColumnType.PATH}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof LineString) {
+                LineString string = (LineString) value;
+                PGpath pg = PostgresqlGeometryAdapter.convert(string);
+                if (placeholder) {
+                    return pg;
+                } else {
+                    return pg.getValue();
+                }
+            }
+            return value;
+        }
+    }),
+    LineWriter(new Object[]{Line.class, StandardColumnType.LINE}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof Line) {
+                Line line = (Line) value;
+                PGline pg = PostgresqlGeometryAdapter.convert(line);
+                if (placeholder) {
+                    return pg;
+                } else {
+                    return pg.getValue();
+                }
+            }
+            return value;
+        }
+    }),
+    BoxWriter(new Object[]{Box.class, StandardColumnType.BOX}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof Box) {
+                Box box = (Box) value;
+                PGbox pg = PostgresqlGeometryAdapter.convert(box);
+                if (placeholder) {
+                    return pg;
+                } else {
+                    return pg.getValue();
+                }
+            }
+            return value;
+        }
+    }),
+    CircleWriter(new Object[]{Circle.class, StandardColumnType.CIRCLE}, new DataWriter() {
+        @Override
+        public Object write(Object value, boolean placeholder) {
+            if(value instanceof Circle) {
+                Circle circle = (Circle) value;
+                PGcircle pg = PostgresqlGeometryAdapter.convert(circle);
+                if (placeholder) {
+                    return pg;
+                } else {
+                    return pg.getValue();
                 }
             }
             return value;
