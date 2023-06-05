e6ce6be69376473677208bfc53fee23910022327
anyline <server@anyline.org>
2023-06-05 16:56:22 +0800

    LineSegment

--- anyline-core/src/main/java/org/anyline/entity/geometry/LineSegment.java
+++ anyline-core/src/main/java/org/anyline/entity/geometry/LineSegment.java
@@ -8,6 +8,38 @@ public class LineSegment extends Geometry{
         this.p1 = p1;
         this.p2 = p2;
     }
+
+    public Point p1() {
+        return p1;
+    }
+
+    public void p1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public Point p2() {
+        return p2;
+    }
+
+    public void p2(Point p2) {
+        this.p2 = p2;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public void setP2(Point p2) {
+        this.p2 = p2;
+    }
     @Override
     public String toString() {
         return toString(true);
@@ -50,4 +82,5 @@ public class LineSegment extends Geometry{
     public String sql() {
         return sql(true, true);
     }
+
 }
