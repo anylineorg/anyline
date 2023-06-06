45068401726a4dcaeb52691f8b722ca1d5a08156
anyline <server@anyline.org>
2023-06-06 18:00:18 +0800

    trigger

---
+++ anyline-data-jdbc/src/main/java/org/anyline/data/entity/Trigger.java
@@ -0,0 +1,7 @@
+package org.anyline.data.entity;
+
+public class Trigger implements org.anyline.entity.data.Trigger{
+    private String name;
+    private String definition;
+    private String option;
+}
