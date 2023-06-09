2e670adca3e6cc4c9ad485d8f9207faedb711e0a
anyline <server@anyline.org>
2023-06-09 13:57:35 +0800

    Trigger

--- anyline-core/src/main/java/org/anyline/entity/data/Trigger.java
+++ anyline-core/src/main/java/org/anyline/entity/data/Trigger.java
@@ -1,4 +1,23 @@
 package org.anyline.entity.data;
 
 public interface Trigger {
+    enum EVENT{INSERT,DELETE,UPDATE}
+    enum TIME{BEFORE,AFTER}
+
+    Table getTable();
+
+    org.anyline.entity.data.Trigger setTable(Table table);
+    org.anyline.entity.data.Trigger setTable(String table);
+
+
+    String getName();
+    Trigger setName(String name) ;
+    String getDefinition() ;
+    Trigger setDefinition(String definition);
+    TIME getTime();
+    Trigger setTime(TIME time) ;
+    EVENT getEvent() ;
+    Trigger setEvent(EVENT event);
+    boolean isEach() ;
+    Trigger setEach(boolean each) ;
 }
--- anyline-data-jdbc/src/main/java/org/anyline/data/entity/Trigger.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/entity/Trigger.java
@@ -2,6 +2,68 @@ package org.anyline.data.entity;
 
 public class Trigger implements org.anyline.entity.data.Trigger{
     private String name;
+    private org.anyline.entity.data.Table table;
     private String definition;
-    private String option;
+    private TIME time;
+    private EVENT event;
+    private boolean each; //每行触发发
+
+    public org.anyline.entity.data.Table getTable() {
+        return table;
+    }
+
+    @Override
+    public org.anyline.entity.data.Trigger setTable(org.anyline.entity.data.Table table) {
+        this.table = table;
+        return this;
+    }
+    public org.anyline.entity.data.Trigger setTable(String table) {
+        this.table = new Table(table);
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public org.anyline.entity.data.Trigger setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public org.anyline.entity.data.Trigger setDefinition(String definition) {
+        this.definition = definition;
+        return this;
+    }
+
+    public TIME getTime() {
+        return time;
+    }
+
+    public org.anyline.entity.data.Trigger setTime(TIME time) {
+        this.time = time;
+        return this;
+    }
+
+    public EVENT getEvent() {
+        return event;
+    }
+
+    public org.anyline.entity.data.Trigger setEvent(EVENT event) {
+        this.event = event;
+        return this;
+    }
+
+    public boolean isEach() {
+        return each;
+    }
+
+    public org.anyline.entity.data.Trigger setEach(boolean each) {
+        this.each = each;
+        return this;
+    }
 }
