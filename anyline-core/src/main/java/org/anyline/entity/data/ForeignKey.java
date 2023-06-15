6d1d80ccc8fd1416f24996a1983c3bf6957b6a1f
anyline <server@anyline.org>
2023-06-15 09:33:35 +0800

    ForeignKey update/rename

--- anyline-core/src/main/java/org/anyline/entity/data/ForeignKey.java
+++ anyline-core/src/main/java/org/anyline/entity/data/ForeignKey.java
@@ -1,12 +1,19 @@
 package org.anyline.entity.data;
 
 
+import org.anyline.util.BeanUtil;
+
+import java.util.LinkedHashMap;
+
 public class ForeignKey extends Constraint{
     public boolean isForeign(){
         return true;
     }
 
     private Table reference;
+
+    private ForeignKey update;
+
     public ForeignKey(){}
     public ForeignKey(String name){
         this.setName(name);
@@ -25,7 +32,47 @@ public class ForeignKey extends Constraint{
         addColumn(column, rcolumn);
     }
 
+    public ForeignKey getUpdate() {
+        return update;
+    }
+
+    public ForeignKey setNewName(String newName){
+        return setNewName(newName, true, true);
+    }
+
+    public ForeignKey setNewName(String newName, boolean setmap, boolean getmap) {
+        if(null == update){
+            update(setmap, getmap);
+        }
+        update.setName(newName);
+        return update;
+    }
+
+    public ForeignKey update(){
+        return update(true, true);
+    }
+
+    public ForeignKey update(boolean setmap, boolean getmap){
+        this.setmap = setmap;
+        this.getmap = getmap;
+        update = clone();
+        update.update = null;
+        return update;
+    }
+
+    public ForeignKey setUpdate(ForeignKey update, boolean setmap, boolean getmap) {
+        this.update = update;
+        this.setmap = setmap;
+        this.getmap = getmap;
+        update.update = null;
+        return this;
+    }
+
     public ForeignKey setReference(Table reference){
+        if(setmap && null != update){
+            update.setReference(reference);
+            return this;
+        }
         this.reference = reference;
         return this;
     }
@@ -35,11 +82,18 @@ public class ForeignKey extends Constraint{
      * @return ForeignKey
      */
     public ForeignKey setReference(String reference){
+        if(setmap && null != update){
+            update.setReference(reference);
+            return this;
+        }
         this.reference = new Table(reference);
         return this;
     }
 
     public Table getReference() {
+        if(getmap && null != update){
+            return update.reference;
+        }
         return reference;
     }
 
@@ -49,6 +103,10 @@ public class ForeignKey extends Constraint{
      * @return ForeignKey
      */
     public ForeignKey addColumn(Column column){
+        if(setmap && null != update){
+            update.addColumn(column);
+            return this;
+        }
         super.addColumn(column);
         return this;
     }
@@ -61,6 +119,10 @@ public class ForeignKey extends Constraint{
      * @return ForeignKey
      */
     public ForeignKey addColumn(String column, String table, String reference){
+        if(setmap && null != update){
+            update.addColumn(column, table, reference);
+            return this;
+        }
         this.reference = new Table(table);
         addColumn(new Column(column).setReference(reference));
         return this;
@@ -72,7 +134,29 @@ public class ForeignKey extends Constraint{
      * @return ForeignKey
      */
     public ForeignKey addColumn(String column,  String reference){
+        if(setmap && null != update){
+            update.addColumn(column, reference);
+            return this;
+        }
         addColumn(new Column(column).setReference(reference));
         return this;
+    }
+
+    public ForeignKey clone(){
+        ForeignKey copy = new ForeignKey();
+
+        BeanUtil.copyFieldValueNvl(copy, this);
+
+        copy.reference = this.reference.clone();
+        LinkedHashMap<String,Column> cols = new LinkedHashMap<>();
+        for(Column column:this.columns.values()){
+            Column col = column.clone();
+            cols.put(col.getName().toUpperCase(), col);
+        }
+        copy.columns = cols;
+        copy.update = null;
+        copy.setmap = false;
+        copy.getmap = false;
+        return copy;
     }
 }
