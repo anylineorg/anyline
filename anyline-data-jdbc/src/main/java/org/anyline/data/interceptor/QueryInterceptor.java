682edc9abd0278af0ca0750f9aef93470995ca75
anyline <server@anyline.org>
2023-06-18 12:23:14 +0800

    QueryInterceptor

--- anyline-data-jdbc/src/main/java/org/anyline/data/interceptor/QueryInterceptor.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/interceptor/QueryInterceptor.java
@@ -5,6 +5,10 @@ import org.anyline.data.param.ConfigStore;
 import org.anyline.data.prepare.RunPrepare;
 import org.anyline.data.run.Run;
 import org.anyline.entity.PageNavi;
+import org.anyline.entity.data.Parameter;
+import org.anyline.entity.data.Procedure;
+
+import java.util.List;
 
 public interface QueryInterceptor extends DMInterceptor{
 
@@ -16,8 +20,13 @@ public interface QueryInterceptor extends DMInterceptor{
      * @param conditions  查询条件
      * @return RESULT
      */
-    SWITCH prepare(JDBCRuntime runtime, RunPrepare prepare, ConfigStore configs, String ... conditions);
+    default SWITCH prepare(JDBCRuntime runtime, RunPrepare prepare, ConfigStore configs, String ... conditions){
+        return SWITCH.CONINUE;
+    }
 
+    default SWITCH prepare(JDBCRuntime runtime, Procedure procedure, PageNavi navi){
+        return SWITCH.CONINUE;
+    }
     /**
      * 合计总数之前调用，到这一步SQL已创建完成
      * @param runtime datasource/adapter/jdbctemplate
@@ -25,15 +34,25 @@ public interface QueryInterceptor extends DMInterceptor{
      * @param navi 分页
      * @return RESULT
      */
-    SWITCH before(JDBCRuntime runtime, Run run, PageNavi navi);
+    default SWITCH before(JDBCRuntime runtime, Run run, PageNavi navi){
+        return SWITCH.CONINUE;
+    }
+    default SWITCH before(JDBCRuntime runtime, Procedure procedure, List<Parameter> inputs, List<Parameter> outputs, PageNavi navi){
+        return SWITCH.CONINUE;
+    }
     /**
      * 合计总数之前调用，到这一步SQL已创建完成
      * @param runtime datasource/adapter/jdbctemplate
-     * @param exe 查询SQL是否执行过
+     * @param success 查询SQL是否执行成功
      * @param run 查询SQL(包含SQL体，查询条件，查询参数值)
      * @param navi 分页
      * @param millis 耗时
      * @return RESULT
      */
-    SWITCH after(JDBCRuntime runtime, Run run, boolean exe, Object result, PageNavi navi, long millis);
+    default SWITCH after(JDBCRuntime runtime, Run run, boolean success, Object result, PageNavi navi, long millis){
+        return SWITCH.CONINUE;
+    }
+    default SWITCH after(JDBCRuntime runtime, Procedure procedure, List<Parameter> inputs, List<Parameter> outputs, PageNavi navi, boolean success, Object result, long millis){
+        return SWITCH.CONINUE;
+    }
 }
