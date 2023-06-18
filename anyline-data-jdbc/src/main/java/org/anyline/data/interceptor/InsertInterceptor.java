d9cb50155425f1907a96fd550e6fbc4cf85f288d
anyline <server@anyline.org>
2023-06-18 12:22:43 +0800

    InsertInterceptor

---
+++ anyline-data-jdbc/src/main/java/org/anyline/data/interceptor/InsertInterceptor.java
@@ -0,0 +1,42 @@
+package org.anyline.data.interceptor;
+
+import org.anyline.data.jdbc.ds.JDBCRuntime;
+import org.anyline.data.run.Run;
+
+import java.util.List;
+
+public interface InsertInterceptor extends DMInterceptor{
+
+    /**
+     * 创建update SQL之前，可以在这一步修改查询条件
+     * @param runtime datasource/adapter/jdbctemplate
+     * @param dest 表
+     * @param data  对象
+     * @param columns  需要更新的列
+     * @return RESULT
+     */
+    default SWITCH prepare(JDBCRuntime runtime, String dest, Object data, boolean checkPrimary, List<String> columns){
+        return SWITCH.CONINUE;
+    }
+
+    /**
+     * 合计总数之前调用，到这一步SQL已创建完成
+     * @param runtime datasource/adapter/jdbctemplate
+     * @param run 查询SQL(包含SQL体，查询条件，查询参数值)
+     * @return RESULT
+     */
+    default SWITCH before(JDBCRuntime runtime, Run run, String dest, Object data, boolean checkPrimary, List<String> columns){
+        return SWITCH.CONINUE;
+    }
+    /**
+     * 合计总数之前调用，到这一步SQL已创建完成
+     * @param runtime datasource/adapter/jdbctemplate
+     * @param result 影响行数
+     * @param run 查询SQL(包含SQL体，查询条件，查询参数值)
+     * @param millis 耗时
+     * @return RESULT
+     */
+    default SWITCH after(JDBCRuntime runtime, Run run, String dest, Object data, boolean checkPrimary, List<String> columns, boolean success, int result, long millis){
+        return SWITCH.CONINUE;
+    }
+}
