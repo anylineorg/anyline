682139cfc38366a17672966c33ec3e36c1764a41
anyline <server@anyline.org>
2023-06-18 20:10:21 +0800

    DDListener

--- anyline-data-jdbc/src/main/java/org/anyline/data/listener/DDListener.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/listener/DDListener.java
@@ -1,52 +1,53 @@
 package org.anyline.data.listener;
 
-import org.anyline.data.adapter.JDBCAdapter;
+import org.anyline.data.jdbc.ds.JDBCRuntime;
 import org.anyline.entity.data.*;
-import org.anyline.service.AnylineService;
 
 import java.util.Collection;
 
 public interface DDListener {
 
-    boolean beforeAdd(Column column);
-
-    void afterAdd(Column column, boolean result);
-
+    default boolean beforeAdd(JDBCRuntime runtime, Column column){
+        return true;
+    }
+    
+    default void afterAdd(JDBCRuntime runtime, Column column, boolean result){}
+    
     /**
      * 修改列之前触发
      * @param column 列
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean beforeAlter(Column column);
-    void afterAlter(Column column, boolean result);
+    default boolean beforeAlter(JDBCRuntime runtime, Column column){return true;}
+    default void afterAlter(JDBCRuntime runtime, Column column, boolean result){}
 
     /**
      * 修改列之后触发
      * 触发之后如果返回true dao将再执行一次 alter column
-     * 一般在此事件中处理 发生类型转换时(如String to Number) 修改表内容
+     * 一般在此事件中处理 发生类型转换时(JDBCRuntime runtime, 如String to Number) 修改表内容
      * @param table 表
      * @param column 列
      * @param exception exception
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean afterAlterColumnException(Table table, Column column, Exception exception);
+    default boolean afterAlterColumnException(JDBCRuntime runtime, Table table, Column column, Exception exception){return true;}
     /**
      * 修改列之后触发
      * 触发之后如果返回true dao将再执行一次 alter column
-     * 一般在此事件中处理 发生类型转换时(如String to Number) 修改表内容
+     * 一般在此事件中处理 发生类型转换时(JDBCRuntime runtime, 如String to Number) 修改表内容
      * @param table 表
      * @param column 列
-     * @param rows rows 整个表中行数(超出ConfigTable.AFTER_ALTER_COLUMN_EXCEPTION_ACTION时调用)
+     * @param rows rows 整个表中行数(JDBCRuntime runtime, 超出ConfigTable.AFTER_ALTER_COLUMN_EXCEPTION_ACTION时调用)
      * @param exception exception
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean afterAlterColumnException(Table table, Column column, int rows, Exception exception);
+    default boolean afterAlterColumnException(JDBCRuntime runtime, Table table, Column column, int rows, Exception exception){return true;}
 
-    boolean beforeDrop(Column column);
-    void afterDrop(Column column, boolean result);
+    default boolean beforeDrop(JDBCRuntime runtime, Column column){return true;}
+    default void afterDrop(JDBCRuntime runtime, Column column, boolean result){}
 
-    boolean beforeRename(Column column);
-    void afterRename(Column column, boolean result);
+    default boolean beforeRename(JDBCRuntime runtime, Column column){return true;}
+    default void afterRename(JDBCRuntime runtime, Column column, boolean result){}
 
 
     /**
@@ -54,17 +55,17 @@ public interface DDListener {
      * @param table
      * @return
      */
-    boolean beforeCreate(Table table);
-    void afterCreate(Table table, boolean result);
-    boolean beforeAlter(Table table);
-    boolean beforeAlter(Table table, Collection<Column> columns);
-    void afterAlter(Table table, boolean result);
-    void afterAlter(Table table, Collection<Column> columns, boolean result);
+    default boolean beforeCreate(JDBCRuntime runtime, Table table){return true;}
+    default void afterCreate(JDBCRuntime runtime, Table table, boolean result){}
+    default boolean beforeAlter(JDBCRuntime runtime, Table table){return true;}
+    default boolean beforeAlter(JDBCRuntime runtime, Table table, Collection<Column> columns){return true;}
+    default void afterAlter(JDBCRuntime runtime, Table table, boolean result){}
+    default void afterAlter(JDBCRuntime runtime, Table table, Collection<Column> columns, boolean result){}
 
-    boolean beforeDrop(Table table);
-    void afterDrop(Table table, boolean result);
-    boolean beforeRename(Table table);
-    void afterRename(Table table, boolean result);
+    default boolean beforeDrop(JDBCRuntime runtime, Table table){return true;}
+    default void afterDrop(JDBCRuntime runtime, Table table, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, Table table){return true;}
+    default void afterRename(JDBCRuntime runtime, Table table, boolean result){}
 
 
     /**
@@ -72,14 +73,14 @@ public interface DDListener {
      * @param view view
      * @return boolean
      */
-    boolean beforeCreate(View view);
-    void afterCreate(View view, boolean result);
-    boolean beforeAlter(View view);
-    void afterAlter(View view, boolean result);
-    boolean beforeDrop(View view);
-    void afterDrop(View view, boolean result);
-    boolean beforeRename(View view);
-    void afterRename(View view, boolean result);
+    default boolean beforeCreate(JDBCRuntime runtime, View view){return true;}
+    default void afterCreate(JDBCRuntime runtime, View view, boolean result){}
+    default boolean beforeAlter(JDBCRuntime runtime, View view){return true;}
+    default void afterAlter(JDBCRuntime runtime, View view, boolean result){}
+    default boolean beforeDrop(JDBCRuntime runtime, View view){return true;}
+    default void afterDrop(JDBCRuntime runtime, View view, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, View view){return true;}
+    default void afterRename(JDBCRuntime runtime, View view, boolean result){}
 
 
     /**
@@ -87,137 +88,131 @@ public interface DDListener {
      * @param table table
      * @return boolean
      */
-    boolean beforeCreate(MasterTable table);
-    void afterCreate(MasterTable table, boolean result);
-    boolean beforeAlter(MasterTable table);
-    void afterAlter(MasterTable table, boolean result);
-    boolean beforeDrop(MasterTable table);
-    void afterDrop(MasterTable table, boolean result);
-    boolean beforeRename(MasterTable table);
-    void afterRename(MasterTable table, boolean result);
+    default boolean beforeCreate(JDBCRuntime runtime, MasterTable table){return true;}
+    default void afterCreate(JDBCRuntime runtime, MasterTable table, boolean result){}
+    default boolean beforeAlter(JDBCRuntime runtime, MasterTable table){return true;}
+    default void afterAlter(JDBCRuntime runtime, MasterTable table, boolean result){}
+    default boolean beforeDrop(JDBCRuntime runtime, MasterTable table){return true;}
+    default void afterDrop(JDBCRuntime runtime, MasterTable table, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, MasterTable table){return true;}
+    default void afterRename(JDBCRuntime runtime, MasterTable table, boolean result){}
 
     /**
      * 创建 PartitionTable 之前调用
      * @param table table
      * @return boolean
      */
-    boolean beforeCreate(PartitionTable table);
-    void afterCreate(PartitionTable table, boolean result);
-    boolean beforeAlter(PartitionTable table);
-    void afterAlter(PartitionTable table, boolean result);
-    boolean beforeDrop(PartitionTable table);
-    void afterDrop(PartitionTable table, boolean result);
-    boolean beforeRename(PartitionTable table);
-    void afterRename(PartitionTable table, boolean result);
+    default boolean beforeCreate(JDBCRuntime runtime, PartitionTable table){return true;}
+    default void afterCreate(JDBCRuntime runtime, PartitionTable table, boolean result){}
+    default boolean beforeAlter(JDBCRuntime runtime, PartitionTable table){return true;}
+    default void afterAlter(JDBCRuntime runtime, PartitionTable table, boolean result){}
+    default boolean beforeDrop(JDBCRuntime runtime, PartitionTable table){return true;}
+    default void afterDrop(JDBCRuntime runtime, PartitionTable table, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, PartitionTable table){return true;}
+    default void afterRename(JDBCRuntime runtime, PartitionTable table, boolean result){}
 
     /**
      * 创建 index 之前触发
      * @param index index
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean beforeAdd(Index index);
-    void afterAdd(Index index, boolean result);
+    default boolean beforeAdd(JDBCRuntime runtime, Index index){return true;}
+    default void afterAdd(JDBCRuntime runtime, Index index, boolean result){}
 
-    boolean beforeAlter(Index index);
-    void afterAlter(Index index, boolean result);
+    default boolean beforeAlter(JDBCRuntime runtime, Index index){return true;}
+    default void afterAlter(JDBCRuntime runtime, Index index, boolean result){}
 
-    boolean beforeDrop(Index index);
-    void afterDrop(Index index, boolean result);
-    boolean beforeRename(Index index);
-    void afterRename(Index index, boolean result);
+    default boolean beforeDrop(JDBCRuntime runtime, Index index){return true;}
+    default void afterDrop(JDBCRuntime runtime, Index index, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, Index index){return true;}
+    default void afterRename(JDBCRuntime runtime, Index index, boolean result){}
 
     /**
      * 创建 primary 之前触发
      * @param primary primary
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean beforeAdd(PrimaryKey primary);
-    void afterAdd(PrimaryKey primary, boolean result);
+    default boolean beforeAdd(JDBCRuntime runtime, PrimaryKey primary){return true;}
+    default void afterAdd(JDBCRuntime runtime, PrimaryKey primary, boolean result){}
 
-    boolean beforeAlter(PrimaryKey primary);
-    void afterAlter(PrimaryKey primary, boolean result);
+    default boolean beforeAlter(JDBCRuntime runtime, PrimaryKey primary){return true;}
+    default void afterAlter(JDBCRuntime runtime, PrimaryKey primary, boolean result){}
 
-    boolean beforeDrop(PrimaryKey primary);
-    void afterDrop(PrimaryKey primary, boolean result);
-    boolean beforeRename(PrimaryKey primary);
-    void afterRename(PrimaryKey primary, boolean result);
+    default boolean beforeDrop(JDBCRuntime runtime, PrimaryKey primary){return true;}
+    default void afterDrop(JDBCRuntime runtime, PrimaryKey primary, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, PrimaryKey primary){return true;}
+    default void afterRename(JDBCRuntime runtime, PrimaryKey primary, boolean result){}
     /**
      * 创建 foreign 之前触发
      * @param foreign foreign
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean beforeAdd(ForeignKey foreign);
-    void afterAdd(ForeignKey foreign, boolean result);
+    default boolean beforeAdd(JDBCRuntime runtime, ForeignKey foreign){return true;}
+    default void afterAdd(JDBCRuntime runtime, ForeignKey foreign, boolean result){}
 
-    boolean beforeAlter(ForeignKey foreign);
-    void afterAlter(ForeignKey foreign, boolean result);
+    default boolean beforeAlter(JDBCRuntime runtime, ForeignKey foreign){return true;}
+    default void afterAlter(JDBCRuntime runtime, ForeignKey foreign, boolean result){}
 
-    boolean beforeDrop(ForeignKey foreign);
-    void afterDrop(ForeignKey foreign, boolean result);
-    boolean beforeRename(ForeignKey foreign);
-    void afterRename(ForeignKey foreign, boolean result);
+    default boolean beforeDrop(JDBCRuntime runtime, ForeignKey foreign){return true;}
+    default void afterDrop(JDBCRuntime runtime, ForeignKey foreign, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, ForeignKey foreign){return true;}
+    default void afterRename(JDBCRuntime runtime, ForeignKey foreign, boolean result){}
 
 
     /**
      * 创建constraint之前触发
      * @param constraint constraint
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean beforeAdd(Constraint constraint);
-    void afterAdd(Constraint constraint, boolean result);
-    boolean beforeAlter(Constraint constraint);
-    void afterAlter(Constraint constraint, boolean result);
-    boolean beforeDrop(Constraint constraint);
-    void afterDrop(Constraint constraint, boolean result);
-    boolean beforeRename(Constraint constraint);
-    void afterRename(Constraint constraint, boolean result);
+    default boolean beforeAdd(JDBCRuntime runtime, Constraint constraint){return true;}
+    default void afterAdd(JDBCRuntime runtime, Constraint constraint, boolean result){}
+    default boolean beforeAlter(JDBCRuntime runtime, Constraint constraint){return true;}
+    default void afterAlter(JDBCRuntime runtime, Constraint constraint, boolean result){}
+    default boolean beforeDrop(JDBCRuntime runtime, Constraint constraint){return true;}
+    default void afterDrop(JDBCRuntime runtime, Constraint constraint, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, Constraint constraint){return true;}
+    default void afterRename(JDBCRuntime runtime, Constraint constraint, boolean result){}
 
     /**
      * 创建procedure之前触发
      * @param procedure procedure
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean beforeCreate(Procedure procedure);
-    void afterCreate(Procedure procedure, boolean result);
-    boolean beforeAlter(Procedure procedure);
-    void afterAlter(Procedure procedure, boolean result);
-    boolean beforeDrop(Procedure procedure);
-    void afterDrop(Procedure procedure, boolean result);
-    boolean beforeRename(Procedure procedure);
-    void afterRename(Procedure procedure, boolean result);
+    default boolean beforeCreate(JDBCRuntime runtime, Procedure procedure){return true;}
+    default void afterCreate(JDBCRuntime runtime, Procedure procedure, boolean result){}
+    default boolean beforeAlter(JDBCRuntime runtime, Procedure procedure){return true;}
+    default void afterAlter(JDBCRuntime runtime, Procedure procedure, boolean result){}
+    default boolean beforeDrop(JDBCRuntime runtime, Procedure procedure){return true;}
+    default void afterDrop(JDBCRuntime runtime, Procedure procedure, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, Procedure procedure){return true;}
+    default void afterRename(JDBCRuntime runtime, Procedure procedure, boolean result){}
     /**
      * 创建 function 之前触发
      * @param function function
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean beforeCreate(Function function);
-    void afterCreate(Function function, boolean result);
-    boolean beforeAlter(Function function);
-    void afterAlter(Function function, boolean result);
-    boolean beforeDrop(Function function);
-    void afterDrop(Function function, boolean result);
-    boolean beforeRename(Function function);
-    void afterRename(Function function, boolean result);
+    default boolean beforeCreate(JDBCRuntime runtime, Function function){return true;}
+    default void afterCreate(JDBCRuntime runtime, Function function, boolean result){}
+    default boolean beforeAlter(JDBCRuntime runtime, Function function){return true;}
+    default void afterAlter(JDBCRuntime runtime, Function function, boolean result){}
+    default boolean beforeDrop(JDBCRuntime runtime, Function function){return true;}
+    default void afterDrop(JDBCRuntime runtime, Function function, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, Function function){return true;}
+    default void afterRename(JDBCRuntime runtime, Function function, boolean result){}
 
 
     /**
      * 创建 trigger 之前触发
      * @param trigger trigger
-     * @return boolean  如果返回false则中断执行
+     * @return default boolean  如果返回false则中断执行
      */
-    boolean beforeCreate(Trigger trigger);
-    void afterCreate(Trigger trigger, boolean result);
-    boolean beforeAlter(Trigger trigger);
-    void afterAlter(Trigger trigger, boolean result);
-    boolean beforeDrop(Trigger trigger);
-    void afterDrop(Trigger trigger, boolean result);
-    boolean beforeRename(Trigger trigger);
-    void afterRename(Trigger trigger, boolean result);
-
-    void setService(AnylineService srvice);
-    AnylineService getService();
-    void setAdapter(JDBCAdapter adapter);
-
-
+    default boolean beforeCreate(JDBCRuntime runtime, Trigger trigger){return true;}
+    default void afterCreate(JDBCRuntime runtime, Trigger trigger, boolean result){}
+    default boolean beforeAlter(JDBCRuntime runtime, Trigger trigger){return true;}
+    default void afterAlter(JDBCRuntime runtime, Trigger trigger, boolean result){}
+    default boolean beforeDrop(JDBCRuntime runtime, Trigger trigger){return true;}
+    default void afterDrop(JDBCRuntime runtime, Trigger trigger, boolean result){}
+    default boolean beforeRename(JDBCRuntime runtime, Trigger trigger){return true;}
+    default void afterRename(JDBCRuntime runtime, Trigger trigger, boolean result){}
 
 }
