810889707317fe18b7c8521f71209b4cc1b80ae0
anyline <server@anyline.org>
2023-06-18 12:26:12 +0800

    DDListener

--- anyline-data-jdbc/src/main/java/org/anyline/data/listener/DDListener.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/listener/DDListener.java
@@ -8,17 +8,17 @@ import java.util.Collection;
 
 public interface DDListener {
 
-    public boolean beforeAdd(Column column);
+    boolean beforeAdd(Column column);
 
-    public void afterAdd(Column column, boolean result);
+    void afterAdd(Column column, boolean result);
 
     /**
      * 修改列之前触发
      * @param column 列
      * @return boolean  如果返回false则中断执行
      */
-    public boolean beforeAlter(Column column);
-    public void afterAlter(Column column, boolean result);
+    boolean beforeAlter(Column column);
+    void afterAlter(Column column, boolean result);
 
     /**
      * 修改列之后触发
@@ -29,7 +29,7 @@ public interface DDListener {
      * @param exception exception
      * @return boolean  如果返回false则中断执行
      */
-    public boolean afterAlterColumnException(Table table, Column column, Exception exception);
+    boolean afterAlterColumnException(Table table, Column column, Exception exception);
     /**
      * 修改列之后触发
      * 触发之后如果返回true dao将再执行一次 alter column
@@ -40,13 +40,13 @@ public interface DDListener {
      * @param exception exception
      * @return boolean  如果返回false则中断执行
      */
-    public boolean afterAlterColumnException(Table table, Column column, int rows, Exception exception);
+    boolean afterAlterColumnException(Table table, Column column, int rows, Exception exception);
 
-    public boolean beforeDrop(Column column);
-    public void afterDrop(Column column, boolean result);
+    boolean beforeDrop(Column column);
+    void afterDrop(Column column, boolean result);
 
-    public boolean beforeRename(Column column);
-    public void afterRename(Column column, boolean result);
+    boolean beforeRename(Column column);
+    void afterRename(Column column, boolean result);
 
 
     /**
@@ -54,17 +54,17 @@ public interface DDListener {
      * @param table
      * @return
      */
-    public boolean beforeCreate(Table table);
-    public void afterCreate(Table table, boolean result);
-    public boolean beforeAlter(Table table);
-    public boolean beforeAlter(Table table, Collection<Column> columns);
-    public void afterAlter(Table table, boolean result);
-    public void afterAlter(Table table, Collection<Column> columns, boolean result);
+    boolean beforeCreate(Table table);
+    void afterCreate(Table table, boolean result);
+    boolean beforeAlter(Table table);
+    boolean beforeAlter(Table table, Collection<Column> columns);
+    void afterAlter(Table table, boolean result);
+    void afterAlter(Table table, Collection<Column> columns, boolean result);
 
-    public boolean beforeDrop(Table table);
-    public void afterDrop(Table table, boolean result);
-    public boolean beforeRename(Table table);
-    public void afterRename(Table table, boolean result);
+    boolean beforeDrop(Table table);
+    void afterDrop(Table table, boolean result);
+    boolean beforeRename(Table table);
+    void afterRename(Table table, boolean result);
 
 
     /**
@@ -72,14 +72,14 @@ public interface DDListener {
      * @param view view
      * @return boolean
      */
-    public boolean beforeCreate(View view);
-    public void afterCreate(View view, boolean result);
-    public boolean beforeAlter(View view);
-    public void afterAlter(View view, boolean result);
-    public boolean beforeDrop(View view);
-    public void afterDrop(View view, boolean result);
-    public boolean beforeRename(View view);
-    public void afterRename(View view, boolean result);
+    boolean beforeCreate(View view);
+    void afterCreate(View view, boolean result);
+    boolean beforeAlter(View view);
+    void afterAlter(View view, boolean result);
+    boolean beforeDrop(View view);
+    void afterDrop(View view, boolean result);
+    boolean beforeRename(View view);
+    void afterRename(View view, boolean result);
 
 
     /**
@@ -87,75 +87,75 @@ public interface DDListener {
      * @param table table
      * @return boolean
      */
-    public boolean beforeCreate(MasterTable table);
-    public void afterCreate(MasterTable table, boolean result);
-    public boolean beforeAlter(MasterTable table);
-    public void afterAlter(MasterTable table, boolean result);
-    public boolean beforeDrop(MasterTable table);
-    public void afterDrop(MasterTable table, boolean result);
-    public boolean beforeRename(MasterTable table);
-    public void afterRename(MasterTable table, boolean result);
+    boolean beforeCreate(MasterTable table);
+    void afterCreate(MasterTable table, boolean result);
+    boolean beforeAlter(MasterTable table);
+    void afterAlter(MasterTable table, boolean result);
+    boolean beforeDrop(MasterTable table);
+    void afterDrop(MasterTable table, boolean result);
+    boolean beforeRename(MasterTable table);
+    void afterRename(MasterTable table, boolean result);
 
     /**
      * 创建 PartitionTable 之前调用
      * @param table table
      * @return boolean
      */
-    public boolean beforeCreate(PartitionTable table);
-    public void afterCreate(PartitionTable table, boolean result);
-    public boolean beforeAlter(PartitionTable table);
-    public void afterAlter(PartitionTable table, boolean result);
-    public boolean beforeDrop(PartitionTable table);
-    public void afterDrop(PartitionTable table, boolean result);
-    public boolean beforeRename(PartitionTable table);
-    public void afterRename(PartitionTable table, boolean result);
+    boolean beforeCreate(PartitionTable table);
+    void afterCreate(PartitionTable table, boolean result);
+    boolean beforeAlter(PartitionTable table);
+    void afterAlter(PartitionTable table, boolean result);
+    boolean beforeDrop(PartitionTable table);
+    void afterDrop(PartitionTable table, boolean result);
+    boolean beforeRename(PartitionTable table);
+    void afterRename(PartitionTable table, boolean result);
 
     /**
      * 创建 index 之前触发
      * @param index index
      * @return boolean  如果返回false则中断执行
      */
-    public boolean beforeAdd(Index index);
-    public void afterAdd(Index index, boolean result);
+    boolean beforeAdd(Index index);
+    void afterAdd(Index index, boolean result);
 
-    public boolean beforeAlter(Index index);
-    public void afterAlter(Index index, boolean result);
+    boolean beforeAlter(Index index);
+    void afterAlter(Index index, boolean result);
 
-    public boolean beforeDrop(Index index);
-    public void afterDrop(Index index, boolean result);
-    public boolean beforeRename(Index index);
-    public void afterRename(Index index, boolean result);
+    boolean beforeDrop(Index index);
+    void afterDrop(Index index, boolean result);
+    boolean beforeRename(Index index);
+    void afterRename(Index index, boolean result);
 
     /**
      * 创建 primary 之前触发
      * @param primary primary
      * @return boolean  如果返回false则中断执行
      */
-    public boolean beforeAdd(PrimaryKey primary);
-    public void afterAdd(PrimaryKey primary, boolean result);
+    boolean beforeAdd(PrimaryKey primary);
+    void afterAdd(PrimaryKey primary, boolean result);
 
-    public boolean beforeAlter(PrimaryKey primary);
-    public void afterAlter(PrimaryKey primary, boolean result);
+    boolean beforeAlter(PrimaryKey primary);
+    void afterAlter(PrimaryKey primary, boolean result);
 
-    public boolean beforeDrop(PrimaryKey primary);
-    public void afterDrop(PrimaryKey primary, boolean result);
-    public boolean beforeRename(PrimaryKey primary);
-    public void afterRename(PrimaryKey primary, boolean result);
+    boolean beforeDrop(PrimaryKey primary);
+    void afterDrop(PrimaryKey primary, boolean result);
+    boolean beforeRename(PrimaryKey primary);
+    void afterRename(PrimaryKey primary, boolean result);
     /**
      * 创建 foreign 之前触发
      * @param foreign foreign
      * @return boolean  如果返回false则中断执行
      */
-    public boolean beforeAdd(ForeignKey foreign);
-    public void afterAdd(ForeignKey foreign, boolean result);
+    boolean beforeAdd(ForeignKey foreign);
+    void afterAdd(ForeignKey foreign, boolean result);
 
-    public boolean beforeAlter(ForeignKey foreign);
-    public void afterAlter(ForeignKey foreign, boolean result);
+    boolean beforeAlter(ForeignKey foreign);
+    void afterAlter(ForeignKey foreign, boolean result);
 
-    public boolean beforeDrop(ForeignKey foreign);
-    public void afterDrop(ForeignKey foreign, boolean result);
-    public boolean beforeRename(ForeignKey foreign);
-    public void afterRename(ForeignKey foreign, boolean result);
+    boolean beforeDrop(ForeignKey foreign);
+    void afterDrop(ForeignKey foreign, boolean result);
+    boolean beforeRename(ForeignKey foreign);
+    void afterRename(ForeignKey foreign, boolean result);
 
 
     /**
@@ -163,41 +163,41 @@ public interface DDListener {
      * @param constraint constraint
      * @return boolean  如果返回false则中断执行
      */
-    public boolean beforeAdd(Constraint constraint);
-    public void afterAdd(Constraint constraint, boolean result);
-    public boolean beforeAlter(Constraint constraint);
-    public void afterAlter(Constraint constraint, boolean result);
-    public boolean beforeDrop(Constraint constraint);
-    public void afterDrop(Constraint constraint, boolean result);
-    public boolean beforeRename(Constraint constraint);
-    public void afterRename(Constraint constraint, boolean result);
+    boolean beforeAdd(Constraint constraint);
+    void afterAdd(Constraint constraint, boolean result);
+    boolean beforeAlter(Constraint constraint);
+    void afterAlter(Constraint constraint, boolean result);
+    boolean beforeDrop(Constraint constraint);
+    void afterDrop(Constraint constraint, boolean result);
+    boolean beforeRename(Constraint constraint);
+    void afterRename(Constraint constraint, boolean result);
 
     /**
      * 创建procedure之前触发
      * @param procedure procedure
      * @return boolean  如果返回false则中断执行
      */
-    public boolean beforeCreate(Procedure procedure);
-    public void afterCreate(Procedure procedure, boolean result);
-    public boolean beforeAlter(Procedure procedure);
-    public void afterAlter(Procedure procedure, boolean result);
-    public boolean beforeDrop(Procedure procedure);
-    public void afterDrop(Procedure procedure, boolean result);
-    public boolean beforeRename(Procedure procedure);
-    public void afterRename(Procedure procedure, boolean result);
+    boolean beforeCreate(Procedure procedure);
+    void afterCreate(Procedure procedure, boolean result);
+    boolean beforeAlter(Procedure procedure);
+    void afterAlter(Procedure procedure, boolean result);
+    boolean beforeDrop(Procedure procedure);
+    void afterDrop(Procedure procedure, boolean result);
+    boolean beforeRename(Procedure procedure);
+    void afterRename(Procedure procedure, boolean result);
     /**
      * 创建 function 之前触发
      * @param function function
      * @return boolean  如果返回false则中断执行
      */
-    public boolean beforeCreate(Function function);
-    public void afterCreate(Function function, boolean result);
-    public boolean beforeAlter(Function function);
-    public void afterAlter(Function function, boolean result);
-    public boolean beforeDrop(Function function);
-    public void afterDrop(Function function, boolean result);
-    public boolean beforeRename(Function function);
-    public void afterRename(Function function, boolean result);
+    boolean beforeCreate(Function function);
+    void afterCreate(Function function, boolean result);
+    boolean beforeAlter(Function function);
+    void afterAlter(Function function, boolean result);
+    boolean beforeDrop(Function function);
+    void afterDrop(Function function, boolean result);
+    boolean beforeRename(Function function);
+    void afterRename(Function function, boolean result);
 
 
     /**
@@ -205,18 +205,18 @@ public interface DDListener {
      * @param trigger trigger
      * @return boolean  如果返回false则中断执行
      */
-    public boolean beforeCreate(Trigger trigger);
-    public void afterCreate(Trigger trigger, boolean result);
-    public boolean beforeAlter(Trigger trigger);
-    public void afterAlter(Trigger trigger, boolean result);
-    public boolean beforeDrop(Trigger trigger);
-    public void afterDrop(Trigger trigger, boolean result);
-    public boolean beforeRename(Trigger trigger);
-    public void afterRename(Trigger trigger, boolean result);
+    boolean beforeCreate(Trigger trigger);
+    void afterCreate(Trigger trigger, boolean result);
+    boolean beforeAlter(Trigger trigger);
+    void afterAlter(Trigger trigger, boolean result);
+    boolean beforeDrop(Trigger trigger);
+    void afterDrop(Trigger trigger, boolean result);
+    boolean beforeRename(Trigger trigger);
+    void afterRename(Trigger trigger, boolean result);
 
-    public void setService(AnylineService srvice);
-    public AnylineService getService();
-    public void setAdapter(JDBCAdapter adapter);
+    void setService(AnylineService srvice);
+    AnylineService getService();
+    void setAdapter(JDBCAdapter adapter);
 
 
 
