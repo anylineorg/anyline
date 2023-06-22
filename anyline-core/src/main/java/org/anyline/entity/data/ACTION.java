21dd43d184caa2d47eb1265c8d2975aec0d09bc6
anyline <server@anyline.org>
2023-06-22 11:04:04 +0800

    ddl action

--- anyline-core/src/main/java/org/anyline/entity/data/ACTION.java
+++ anyline-core/src/main/java/org/anyline/entity/data/ACTION.java
@@ -17,10 +17,60 @@ public interface ACTION {
         TABLE_RENAME    ("表重命名"),
         TABLE_COMMENT   ("表修改备注"),
 
+        VIEW_CREATE   ("视图创建"),
+        VIEW_ALTER    ("视图修改"),
+        VIEW_DROP     ("视图删除"),
+        VIEW_RENAME   ("视图重命名"),
+
+        MASTER_TABLE_CREATE   ("超表创建"),
+        MASTER_TABLE_ALTER    ("超表修改"),
+        MASTER_TABLE_DROP     ("超表删除"),
+        MASTER_TABLE_RENAME   ("超表重命名"),
+
+        PARTITION_TABLE_CREATE   ("子表(分区表)创建"),
+        PARTITION_TABLE_ALTER    ("子表(分区表)修改"),
+        PARTITION_TABLE_DROP     ("子表(分区表)删除"),
+        PARTITION_TABLE_RENAME   ("子表(分区表)重命名"),
+
         COLUMN_CREATE   ("列创建"),
         COLUMN_ALTER    ("列结构修改"),
         COLUMN_DROP     ("列删除"),
         COLUMN_RENAME   ("列重命名"),
+
+        PRIMARY_CREATE   ("主键创建"),
+        PRIMARY_ALTER    ("主键修改"),
+        PRIMARY_DROP     ("主键删除"),
+        PRIMARY_RENAME   ("主键重命名"),
+
+        FOREIGN_CREATE   ("外键创建"),
+        FOREIGN_ALTER    ("外键修改"),
+        FOREIGN_DROP     ("外键删除"),
+        FOREIGN_RENAME   ("外键重命名"),
+
+        INDEX_CREATE   ("索引创建"),
+        INDEX_ALTER    ("索引修改"),
+        INDEX_DROP     ("索引删除"),
+        INDEX_RENAME   ("索引重命名"),
+
+        CONSTRAINT_CREATE   ("约束创建"),
+        CONSTRAINT_ALTER    ("约束修改"),
+        CONSTRAINT_DROP     ("约束删除"),
+        CONSTRAINT_RENAME   ("约束重命名"),
+
+        PROCEDURE_CREATE   ("存储过程创建"),
+        PROCEDURE_ALTER    ("存储过程修改"),
+        PROCEDURE_DROP     ("存储过程删除"),
+        PROCEDURE_RENAME   ("存储过程重命名"),
+
+        FUNCTION_CREATE   ("函数创建"),
+        FUNCTION_ALTER    ("函数修改"),
+        FUNCTION_DROP     ("函数删除"),
+        FUNCTION_RENAME   ("函数重命名"),
+
+        TRIGGER_CREATE   ("触发器创建"),
+        TRIGGER_ALTER    ("触发器修改"),
+        TRIGGER_DROP     ("触发器删除"),
+        TRIGGER_RENAME   ("触发器重命名"),
         ;
         private final String title;
         DDL(String title){
@@ -36,9 +86,10 @@ public interface ACTION {
      * after1
      * after2
      * after3
+     * 
      * */
     enum SWITCH {
-        CONINUE("正常执行"),
+        CONTINUE("正常执行"),
         SKIP("跳过后续prepare/before/after"), //before1返回SKIP则跳过before2,before3,但不影响after2,after3,
         BREAK("中断主流程执行")
         ;
