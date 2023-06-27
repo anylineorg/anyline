8f96c23cec8cfd1b5ae51ef4303fd7461389b76c
anyline <server@anyline.org>
2023-06-27 17:17:53 +0800

    Aggregation

--- anyline-core/src/main/java/org/anyline/entity/Aggregation.java
+++ anyline-core/src/main/java/org/anyline/entity/Aggregation.java
@@ -12,16 +12,18 @@ public enum Aggregation {
     MAX_FLOAT	    ("MAX_FLOAT"      ,"最大"),
     MAX_INT			("MAX_INT"        ,"最大"),
     AVG		        ("AVG"  			,"平均"),
+    AVGA		    ("AVGA"  			,"平均(空值参与)"),
     SUM			    ("SUM"  			,"合计"),
     COUNT		    ("COUNT"  		,"数量"),
+    COUNTA		    ("COUNT"  		,"数量(空值参与)"),
     STDEV 		    ("STDEV"  		,"标准偏差"),
-    STDEVA 		    ("STDEVA" 		,"标准偏差"),
+    STDEVA 		    ("STDEVA" 		,"标准偏差(空值参与)"),
     STDEVP		    ("STDEVP"      	,"标准偏差"),
-    STDEVPA		    ("STDEVPA"      	,"标准偏差"),
+    STDEVPA		    ("STDEVPA"      	,"标准偏差(空值参与)"),
     VAR		        ("VAR"            ,"方差"),
-    VARA		    ("VARA"   	    ,"方差"),
+    VARA		    ("VARA"   	    ,"方差(空值参与)"),
     VARP		    ("VARP"  		    ,"方差"),
-    VARPA		    ("VARPA"  		,"方差");
+    VARPA		    ("VARPA"  		,"方差(空值参与)");
     final String code;
     final String name;
     Aggregation(String code, String name){
