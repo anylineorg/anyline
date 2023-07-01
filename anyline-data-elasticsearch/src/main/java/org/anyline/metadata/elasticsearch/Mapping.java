5acc86a7128571ff526e190d3199680578a01731
anyline <server@anyline.org>
2023-07-01 21:27:26 +0800

    Mapping

---
+++ anyline-data-elasticsearch/src/main/java/org/anyline/metadata/elasticsearch/Mapping.java
@@ -0,0 +1,19 @@
+package org.anyline.metadata.elasticsearch;
+
+import java.util.LinkedHashMap;
+
+public class Mapping {
+    protected LinkedHashMap<String,Property> properties;
+
+    /*_all
+_field_names
+_index
+_parent
+_routing
+_source
+_timestamp
+date_detection
+dynamic_date_formats
+properties
+*/
+}
