a1a2f0cbb825f93f603faef5d3620267cfd245d2
anyline <server@anyline.org>
2023-07-04 16:24:59 +0800

    MixUtil

--- anyline-core/src/main/java/org/anyline/util/MixUtil.java
+++ anyline-core/src/main/java/org/anyline/util/MixUtil.java
@@ -4,25 +4,32 @@ import org.anyline.util.encrypt.MD5Util;
 
 public class MixUtil {
 
-    public static String mix(String seed, String value){
-        return mix(seed, 6, 8, value);
+    public static String mix(String seed, String origin){
+        return mix(seed, 6, 8, origin);
     }
-    public static String mix(String value){
-        return mix(ConfigTable.MIX_DEFAULT_SEED, 6, 8, value);
+    public static String mix(String origin){
+        return mix(ConfigTable.MIX_DEFAULT_SEED, 6, 8, origin);
     }
-    public static String mix(String seed, int begin, int end, String value){
-        String result = MD5Util.crypto(seed + MD5Util.crypto(value).substring(begin) + value).substring(begin, end);
+    public static String mix(String seed, int begin, int end, String origin){
+        String result = MD5Util.crypto(seed + MD5Util.crypto(origin).substring(begin) + origin).substring(begin, end);
         return result;
     }
 
-    public static boolean verify(String verify, String seed, String value){
-        return verify(verify, seed, 6, 8, value);
+    /**
+     *
+     * @param verify 验证码
+     * @param seed 加密种子
+     * @param origin 源
+     * @return boolean
+     */
+    public static boolean verify(String verify, String seed, String origin){
+        return verify(verify, seed, 6, 8, origin);
     }
-    public static boolean verify(String verify, String value){
-        return verify(verify,ConfigTable.MIX_DEFAULT_SEED, 6, 8, value);
+    public static boolean verify(String verify, String origin){
+        return verify(verify,ConfigTable.MIX_DEFAULT_SEED, 6, 8, origin);
     }
-    public static boolean verify(String verify, String seed, int begin, int end, String value){
-        String result = MD5Util.crypto(seed + MD5Util.crypto(value).substring(begin) + value).substring(begin, end);
+    public static boolean verify(String verify, String seed, int begin, int end, String origin){
+        String result = MD5Util.crypto(seed + MD5Util.crypto(origin).substring(begin) + origin).substring(begin, end);
         return result.equals(verify);
     }
 }
