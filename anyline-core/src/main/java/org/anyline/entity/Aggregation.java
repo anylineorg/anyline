3ddbc49a2e1b2005b327580c63d29d87faf0df50
anyline <server@anyline.org>
2023-07-21 13:52:01 +0800

    Aggregation

--- anyline-core/src/main/java/org/anyline/entity/Aggregation.java
+++ anyline-core/src/main/java/org/anyline/entity/Aggregation.java
@@ -16,14 +16,14 @@ public enum Aggregation {
     SUM			    ("SUM"  			, "合计"),
     COUNT		    ("COUNT"  		, "数量"),
     COUNTA		    ("COUNT"  		, "数量(空值参与)"),
-    STDEV 		    ("STDEV"  		, "标准偏差"),
-    STDEVA 		    ("STDEVA" 		, "标准偏差(空值参与)"),
-    STDEVP		    ("STDEVP"      	, "标准偏差"),
-    STDEVPA		    ("STDEVPA"      	, "标准偏差(空值参与)"),
-    VAR		        ("VAR"            , "方差"),
-    VARA		    ("VARA"   	    , "方差(空值参与)"),
-    VARP		    ("VARP"  		    , "方差"),
-    VARPA		    ("VARPA"  		, "方差(空值参与)");
+    STDEV 		    ("STDEV"  		, "抽样标准偏差"),
+    STDEVA 		    ("STDEVA" 		, "抽样标准偏差(空值参与)"),
+    STDEVP		    ("STDEVP"      	, "总体标准偏差"),
+    STDEVPA		    ("STDEVPA"      	, "总体标准偏差(空值参与)"),
+    VAR		        ("VAR"            , "抽样方差"),
+    VARA		    ("VARA"   	    , "抽样方差(空值参与)"),
+    VARP		    ("VARP"  		    , "总体方差"),
+    VARPA		    ("VARPA"  		, "总体方差(空值参与)");
     final String code;
     final String name;
     Aggregation(String code, String name){
