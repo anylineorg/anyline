c064248a851e88ba2ef4cf51d969f5ae9ccc1f1b
anyline <server@anyline.org>
2023-08-03 22:42:20 +0800

    InformixAdapter

--- anyline-data-jdbc-dialect/anyline-data-jdbc-informix/src/main/java/org/anyline/data/jdbc/informix/InformixAdapter.java
+++ anyline-data-jdbc-dialect/anyline-data-jdbc-informix/src/main/java/org/anyline/data/jdbc/informix/InformixAdapter.java
@@ -5,6 +5,7 @@ import org.anyline.data.adapter.JDBCAdapter;
 import org.anyline.data.adapter.init.SQLAdapter;
 import org.anyline.data.run.Run;
 import org.anyline.data.run.SimpleRun;
+import org.anyline.data.runtime.DataRuntime;
 import org.anyline.entity.DataRow;
 import org.anyline.entity.DataSet;
 import org.anyline.entity.OrderStore;
@@ -24,7 +25,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
-import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.util.*;
 
@@ -98,7 +98,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 		} 
 		PageNavi navi = run.getPageNavi(); 
 		if(null != navi){
-			int limit = navi.getLastRow() - navi.getFirstRow() + 1;
+			long limit = navi.getLastRow() - navi.getFirstRow() + 1;
 			if(limit < 0){
 				limit = 0;
 			}
@@ -157,7 +157,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 		sql = sql.replaceAll("WHERE\\s*1=1\\s*AND", "WHERE");
 		return sql;
 	}
-	protected void createPrimaryValue(JdbcTemplate template, Collection list, String seq){
+	protected void createPrimaryValue(DataRuntime runtime, Collection list, String seq){
 		StringBuilder builder = new StringBuilder();
 		builder.append("SELECT ").append(seq).append(" AS ID FROM(\n");
 		int size = list.size();
@@ -168,7 +168,8 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 			}
 		}
 		builder.append(") M");
-		List<Map<String,Object>> ids = template.queryForList(builder.toString());
+		JdbcTemplate jdbc = jdbc(runtime);
+		List<Map<String,Object>> ids = jdbc.queryForList(builder.toString());
 		int i=0;
 		for(Object obj:list){
 			Object value = ids.get(i++).get("ID");
@@ -186,13 +187,13 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * 		UNION ALL SELECT    'A2' FROM DUAL
 	 * 		UNION ALL SELECT    'A3' FROM DUAL
 	 * ) M
-	 * @param template JdbcTemplate
+	 * @param runtime runtime
 	 * @param run run
 	 * @param dest dest
 	 * @param keys keys
 	 */
 	@Override
-	public void createInserts(JdbcTemplate template, Run run, String dest, DataSet set, List<String> keys){
+	public void createInserts(DataRuntime runtime, Run run, String dest, DataSet set, List<String> keys){
 		if(null == set || set.size() ==0){
 			return;
 		}
@@ -208,7 +209,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 						str = str.substring(2, str.length() - 1);
 					}
 					if(IS_GET_SEQUENCE_VALUE_BEFORE_INSERT) {
-						createPrimaryValue(template, set, str);
+						createPrimaryValue(runtime, set, str);
 					}else {
 						seqs.put(key, str);
 					}
@@ -241,12 +242,12 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 				}
 				//createPrimaryValue(row, type(),dest.replace(getDelimiterFr(), "").replace(getDelimiterTo(), ""), pks, null);
 			}
-			insertValue(template, run, row, false, false,true, keys);
+			insertValue(runtime, run, row, false, false,true, keys);
 			builder.append(";");
 		}
 	}
 	@Override
-	public void createInserts(JdbcTemplate template, Run run, String dest, Collection list, List<String> keys){
+	public void createInserts(DataRuntime runtime, Run run, String dest, Collection list, List<String> keys){
 		if(null == list || list.isEmpty()){
 			return;
 		}
@@ -257,7 +258,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 		}
 		if(list instanceof DataSet){
 			DataSet set = (DataSet) list;
-			createInserts(template, run, dest, set, keys);
+			createInserts(runtime, run, dest, set, keys);
 			return;
 		}
 
@@ -272,7 +273,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 						str = str.substring(2, str.length() - 1);
 					}
 					if(IS_GET_SEQUENCE_VALUE_BEFORE_INSERT) {
-						createPrimaryValue(template, list, str);
+						createPrimaryValue(runtime, list, str);
 					}else {
 						seqs.put(key, str);
 					}
@@ -340,7 +341,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 				builder.append("\n\tUNION ALL");
 			}
 			builder.append("\n\tSELECT ");
-			insertValue(template, run, obj, true, true,false, keys);
+			insertValue(runtime, run, obj, true, true,false, keys);
 			builder.append(" FROM DUAL ");
 			col ++;
 		}
@@ -349,7 +350,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 
 	/**
 	 * 执行 insert
-	 * @param template JdbcTemplate
+	 * @param runtime runtime
 	 * @param random random
 	 * @param data entity|DataRow|DataSet
 	 * @param sql sql
@@ -358,18 +359,19 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * @throws Exception 异常
 	 */
 	@Override
-	public int insert(JdbcTemplate template, String random, Object data, String sql, List<Object> values, String[] pks) throws Exception{
+	public int insert(DataRuntime runtime, String random, Object data, String sql, List<Object> values, String[] pks) throws Exception{
 		int cnt = 0;
 		if(data instanceof Collection) {
+			JdbcTemplate jdbc = jdbc(runtime);
 			if (null == values || values.isEmpty()) {
-				cnt = template.update(sql);
+				cnt = jdbc.update(sql);
 			} else {
 				int size = values.size();
 				Object[] params = new Object[size];
 				for (int i = 0; i < size; i++) {
 					params[i] = values.get(i);
 				}
-				cnt = template.update(sql, params);
+				cnt = jdbc.update(sql, params);
 			}
 		}else{
 			//单行的可以返回序列号
@@ -379,7 +381,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 			}else{
 				pks = null;
 			}
-			cnt = super.insert(template, random, data, sql, values, pks);
+			cnt = super.insert(runtime, random, data, sql, values, pks);
 		}
 		return cnt;
 	}
@@ -422,7 +424,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * List<Run> buildQueryTableRunSQL(String catalog, String schema, String pattern, String types)
 	 * List<Run> buildQueryTableCommentRunSQL(String catalog, String schema, String pattern, String types)
 	 * <T extends Table> LinkedHashMap<String, T> tables(int index, boolean create, String catalog, String schema, LinkedHashMap<String, T> tables, DataSet set) throws Exception
-	 * <T extends Table> LinkedHashMap<String, T> tables(boolean create, LinkedHashMap<String, T> tables, DatabaseMetaData dbmd, String catalog, String schema, String pattern, String ... types) throws Exception
+	 * <T extends Table> LinkedHashMap<String, T> tables(boolean create, LinkedHashMap<String, T> tables, DataRuntime runtime, String catalog, String schema, String pattern, String ... types) throws Exception
 	 * <T extends Table> LinkedHashMap<String, T> comments(int index, boolean create, String catalog, String schema, LinkedHashMap<String, T> tables, DataSet set) throws Exception
 	 * List<Run> buildQueryDDLRunSQL(Table table) throws Exception
 	 * public List<String> ddl(int index, Table table, List<String> ddls, DataSet set)
@@ -478,8 +480,8 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 		return tables;
 	}
 	@Override
-	public <T extends Table> LinkedHashMap<String, T> tables(boolean create, LinkedHashMap<String, T> tables, DatabaseMetaData dbmd, String catalog, String schema, String pattern, String ... types) throws Exception{
-		return super.tables(create, tables, dbmd, catalog, schema, pattern, types);
+	public <T extends Table> LinkedHashMap<String, T> tables(boolean create, LinkedHashMap<String, T> tables, DataRuntime runtime, String catalog, String schema, String pattern, String ... types) throws Exception{
+		return super.tables(create, tables, runtime, catalog, schema, pattern, types);
 	}
 
 	/* *****************************************************************************************************************
@@ -487,7 +489,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * -----------------------------------------------------------------------------------------------------------------
 	 * List<Run> buildQueryMasterTableRunSQL(String catalog, String schema, String pattern, String types);
 	 * <T extends MasterTable> LinkedHashMap<String, T> mtables(int index, boolean create, String catalog, String schema, LinkedHashMap<String, T> tables, DataSet set) throws Exception;
-	 * <T extends MasterTable> LinkedHashMap<String, T> mtables(boolean create, LinkedHashMap<String, T> tables, DatabaseMetaData dbmd, String catalog, String schema, String pattern, String ... types) throws Exception;
+	 * <T extends MasterTable> LinkedHashMap<String, T> mtables(boolean create, LinkedHashMap<String, T> tables, DataRuntime runtime, String catalog, String schema, String pattern, String ... types) throws Exception;
 	 ******************************************************************************************************************/
 
 	/**
@@ -509,12 +511,12 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * @param create 上一步没有查到的,这一步是否需要新创建
 	 * @param catalog catalog
 	 * @param schema schema
-	 * @param dbmd DatabaseMetaData
+	 * @param runtime runtime
 	 * @return List
 	 */
 	@Override
-	public <T extends MasterTable> LinkedHashMap<String, T> mtables(boolean create, LinkedHashMap<String, T> tables, DatabaseMetaData dbmd, String catalog, String schema, String pattern, String ... types) throws Exception{
-		return super.mtables(create, tables, dbmd, catalog, schema, pattern, types);
+	public <T extends MasterTable> LinkedHashMap<String, T> mtables(boolean create, LinkedHashMap<String, T> tables, DataRuntime runtime, String catalog, String schema, String pattern, String ... types) throws Exception{
+		return super.mtables(create, tables, runtime, catalog, schema, pattern, types);
 	}
 
 
@@ -586,13 +588,13 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * @param catalog catalog
 	 * @param schema schema
 	 * @param tables 上一步查询结果
-	 * @param dbmd DatabaseMetaData
+	 * @param runtime runtime
 	 * @return tables
 	 * @throws Exception 异常
 	 */
 	@Override
-	public <T extends PartitionTable> LinkedHashMap<String,T> ptables(boolean create, LinkedHashMap<String, T> tables, DatabaseMetaData dbmd, String catalog, String schema, MasterTable master) throws Exception{
-		return super.ptables(create, tables, dbmd, catalog, schema, master);
+	public <T extends PartitionTable> LinkedHashMap<String,T> ptables(boolean create, LinkedHashMap<String, T> tables, DataRuntime runtime, String catalog, String schema, MasterTable master) throws Exception{
+		return super.ptables(create, tables, runtime, catalog, schema, master);
 	}
 
 
@@ -602,7 +604,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * List<Run> buildQueryColumnRunSQL(Table table, boolean metadata);
 	 * <T extends Column> LinkedHashMap<String, T> columns(int index, boolean create, Table table, LinkedHashMap<String, T> columns, DataSet set) throws Exception;
 	 * <T extends Column> LinkedHashMap<String, T> columns(boolean create, LinkedHashMap<String, T> columns, Table table, SqlRowSet set) throws Exception;
-	 * <T extends Column> LinkedHashMap<String, T> columns(boolean create, LinkedHashMap<String, T> columns, DatabaseMetaData dbmd, Table table, String pattern) throws Exception;
+	 * <T extends Column> LinkedHashMap<String, T> columns(boolean create, LinkedHashMap<String, T> columns, DataRuntime runtime, Table table, String pattern) throws Exception;
 	 ******************************************************************************************************************/
 
 	/**
@@ -691,8 +693,8 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 		return super.columns(create, columns, table, set);
 	}
 	@Override
-	public <T extends Column> LinkedHashMap<String, T> columns(boolean create, LinkedHashMap<String, T> columns, DatabaseMetaData dbmd, Table table, String pattern) throws Exception{
-		return super.columns(create, columns, dbmd, table, pattern);
+	public <T extends Column> LinkedHashMap<String, T> columns(boolean create, LinkedHashMap<String, T> columns, DataRuntime runtime, Table table, String pattern) throws Exception{
+		return super.columns(create, columns, runtime, table, pattern);
 	}
 
 
@@ -702,7 +704,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * List<Run> buildQueryTagRunSQL(Table table, boolean metadata);
 	 * <T extends Tag> LinkedHashMap<String, T> tags(int index, boolean create, Table table, LinkedHashMap<String, T> tags, DataSet set) throws Exception;
 	 * <T extends Tag> LinkedHashMap<String, T> tags(boolean create, Table table, LinkedHashMap<String, T> tags, SqlRowSet set) throws Exception;
-	 * <T extends Tag> LinkedHashMap<String, T> tags(boolean create, LinkedHashMap<String, T> tags, DatabaseMetaData dbmd, Table table, String pattern) throws Exception;
+	 * <T extends Tag> LinkedHashMap<String, T> tags(boolean create, LinkedHashMap<String, T> tags, DataRuntime runtime, Table table, String pattern) throws Exception;
 	 ******************************************************************************************************************/
 	/**
 	 *
@@ -734,8 +736,8 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 		return super.tags(create, table, tags, set);
 	}
 	@Override
-	public <T extends Tag> LinkedHashMap<String, T> tags(boolean create, LinkedHashMap<String, T> tags, DatabaseMetaData dbmd, Table table, String pattern) throws Exception{
-		return super.tags(create, tags, dbmd, table, pattern);
+	public <T extends Tag> LinkedHashMap<String, T> tags(boolean create, LinkedHashMap<String, T> tags, DataRuntime runtime, Table table, String pattern) throws Exception{
+		return super.tags(create, tags, runtime, table, pattern);
 	}
 
 	/* *****************************************************************************************************************
@@ -829,7 +831,7 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 	 * List<Run> buildQueryIndexRunSQL(Table table, boolean metadata);
 	 * <T extends Index> LinkedHashMap<String, T> indexs(int index, boolean create, Table table, LinkedHashMap<String, T> indexs, DataSet set) throws Exception;
 	 * <T extends Index> LinkedHashMap<String, T> indexs(boolean create, Table table, LinkedHashMap<String, T> indexs, SqlRowSet set) throws Exception;
-	 * <T extends Index> LinkedHashMap<String, T> indexs(boolean create, LinkedHashMap<String, T> indexs, DatabaseMetaData dbmd, Table table, boolean unique, boolean approximate) throws Exception;
+	 * <T extends Index> LinkedHashMap<String, T> indexs(boolean create, LinkedHashMap<String, T> indexs, DataRuntime runtime, Table table, boolean unique, boolean approximate) throws Exception;
 	 ******************************************************************************************************************/
 	/**
 	 * 查询表上的列
@@ -861,8 +863,8 @@ public class InformixAdapter extends SQLAdapter implements JDBCAdapter, Initiali
 		return super.indexs(create, table, indexs, set);
 	}
 	@Override
-	public <T extends Index> LinkedHashMap<String, T> indexs(boolean create, LinkedHashMap<String, T> indexs, DatabaseMetaData dbmd, Table table, boolean unique, boolean approximate) throws Exception{
-		return super.indexs(create, indexs, dbmd, table, unique, approximate);
+	public <T extends Index> LinkedHashMap<String, T> indexs(boolean create, LinkedHashMap<String, T> indexs, DataRuntime runtime, Table table, boolean unique, boolean approximate) throws Exception{
+		return super.indexs(create, indexs, runtime, table, unique, approximate);
 	}
 
 
