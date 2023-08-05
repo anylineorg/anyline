3c0c99cd5cef39145337c81c900ff6b6068e7016
anyline <server@anyline.org>
2023-08-05 23:12:11 +0800

    TimestampGenerator

--- anyline-core/src/main/java/org/anyline/entity/generator/init/TimestampGenerator.java
+++ anyline-core/src/main/java/org/anyline/entity/generator/init/TimestampGenerator.java
@@ -1,6 +1,7 @@
 package org.anyline.entity.generator.init;
 
 import org.anyline.entity.DataRow;
+import org.anyline.metadata.Column;
 import org.anyline.metadata.type.DatabaseType;
 import org.anyline.entity.generator.PrimaryGenerator;
 import org.anyline.proxy.EntityAdapterProxy;
@@ -8,6 +9,7 @@ import org.anyline.util.BasicUtil;
 import org.anyline.util.BeanUtil;
 import org.anyline.util.ConfigTable;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class TimestampGenerator implements PrimaryGenerator {
@@ -24,12 +26,33 @@ public class TimestampGenerator implements PrimaryGenerator {
             if(null != BeanUtil.getFieldValue(entity, column)) {
                 continue;
             }
-            String value = System.currentTimeMillis()+"";
-            if(ConfigTable.PRIMARY_GENERATOR_TIME_SUFFIX_LENGTH > 0){
-                value += BasicUtil.getRandomNumberString(ConfigTable.PRIMARY_GENERATOR_TIME_SUFFIX_LENGTH);
-            }
-            BeanUtil.setFieldValue(entity, column, value, true);
+            create(entity, type, table, column, other);
         }
+        return true;
+    }
+    @Override
+    public boolean create(Object entity, DatabaseType type, String table, LinkedHashMap<String, Column> columns, String other) {
+        if(null == columns){
+            if(entity instanceof DataRow){
+                columns = ((DataRow)entity).getPrimaryColumns();
+            }else{
+                columns = EntityAdapterProxy.primaryKeys(entity.getClass());
+            }
+        }
+        for(Column column:columns.values()){
+            if(null != BeanUtil.getFieldValue(entity, column.getName())) {
+                continue;
+            }
+            create(entity, type, table, column.getName(), other);
+        }
+        return true;
+    }
+    public boolean create(Object entity, DatabaseType type, String table, String column, String other) {
+        String value = System.currentTimeMillis()+"";
+        if(ConfigTable.PRIMARY_GENERATOR_TIME_SUFFIX_LENGTH > 0){
+            value += BasicUtil.getRandomNumberString(ConfigTable.PRIMARY_GENERATOR_TIME_SUFFIX_LENGTH);
+        }
+        BeanUtil.setFieldValue(entity, column, value, true);
         return true;
     }
 }
