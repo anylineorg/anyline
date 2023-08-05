83cab5f19d6a7ca9eb62f938c2d2bb09d41154f2
anyline <server@anyline.org>
2023-08-05 23:13:25 +0800

    RunValue

--- anyline-data/src/main/java/org/anyline/data/run/RunValue.java
+++ anyline-data/src/main/java/org/anyline/data/run/RunValue.java
@@ -1,12 +1,22 @@
 package org.anyline.data.run;
 
-import org.anyline.util.BeanUtil;
+import org.anyline.metadata.Column;
 
 public class RunValue {
     private String key;
     private Object value;
+    private Column column;
 
     public RunValue(){}
+    public RunValue(Column column, Object value){
+        if(null == column){
+            this.key = "none";
+        }else{
+            this.key = column.getName();
+        }
+        this.column = column;
+        this.value = value;
+    }
     public RunValue(String key, Object value){
         this.key = key;
         this.value = value;
@@ -25,5 +35,13 @@ public class RunValue {
 
     public void setValue(Object value) {
         this.value = value;
+    }
+
+    public Column getColumn() {
+        return column;
+    }
+
+    public void setColumn(Column column) {
+        this.column = column;
     }
 }
