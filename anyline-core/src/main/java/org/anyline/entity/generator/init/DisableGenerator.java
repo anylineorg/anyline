2825ada6e94a15c005aeb6a93b321b4052c879f7
anyline <server@anyline.org>
2023-08-05 23:25:04 +0800

    DisableGenerator

--- anyline-core/src/main/java/org/anyline/entity/generator/init/DisableGenerator.java
+++ anyline-core/src/main/java/org/anyline/entity/generator/init/DisableGenerator.java
@@ -1,8 +1,10 @@
 package org.anyline.entity.generator.init;
 
+import org.anyline.metadata.Column;
 import org.anyline.metadata.type.DatabaseType;
 import org.anyline.entity.generator.PrimaryGenerator;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -11,6 +13,10 @@ import java.util.List;
 public class DisableGenerator implements PrimaryGenerator {
     @Override
     public boolean create(Object entity, DatabaseType type, String table, List<String> columns,  String other) {
-       return false;
+        return false;
+    }
+    @Override
+    public boolean create(Object entity, DatabaseType type, String table, LinkedHashMap<String, Column> columns, String other) {
+        return false;
     }
 }
