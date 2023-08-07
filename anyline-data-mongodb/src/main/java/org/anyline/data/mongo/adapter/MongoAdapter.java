5458f0487a1827228efd5480a42640e390b64ff9
anyline <server@anyline.org>
2023-08-07 22:01:19 +0800

    mongodb condition

--- anyline-data-mongodb/src/main/java/org/anyline/data/mongo/adapter/MongoAdapter.java
+++ anyline-data-mongodb/src/main/java/org/anyline/data/mongo/adapter/MongoAdapter.java
@@ -12,9 +12,10 @@ import org.anyline.data.param.ConfigStore;
 import org.anyline.data.prepare.Condition;
 import org.anyline.data.prepare.ConditionChain;
 import org.anyline.data.prepare.RunPrepare;
-import org.anyline.data.prepare.Variable;
+import org.anyline.data.prepare.auto.AutoCondition;
 import org.anyline.data.prepare.auto.TablePrepare;
 import org.anyline.data.run.Run;
+import org.anyline.data.run.RunValue;
 import org.anyline.data.run.TableRun;
 import org.anyline.data.runtime.DataRuntime;
 import org.anyline.data.util.ThreadConfig;
@@ -81,7 +82,7 @@ public class MongoAdapter extends DefaultDriverAdapter implements DriverAdapter 
         PrimaryGenerator generator = checkPrimaryGenerator(type(),dest.replace(getDelimiterFr(), "").replace(getDelimiterTo(), ""));
 
         int from = 1;
-         DataRow row = null;
+        DataRow row = null;
         if(obj instanceof Map){
             obj = new DataRow((Map)obj);
         }
@@ -89,14 +90,14 @@ public class MongoAdapter extends DefaultDriverAdapter implements DriverAdapter 
             row = (DataRow)obj;
             if(row.hasPrimaryKeys() && null != generator){
                 generator.create(row, type(),dest.replace(getDelimiterFr(), "").replace(getDelimiterTo(), ""), row.getPrimaryKeys(), null);
-             }
+            }
         }else{
             from = 2;
             boolean create = EntityAdapterProxy.createPrimaryValue(obj, columns);
             LinkedHashMap<String,Column> pks = EntityAdapterProxy.primaryKeys(obj.getClass());
             if(!create && null != generator){
                 generator.create(obj, type(),dest.replace(getDelimiterFr(), "").replace(getDelimiterTo(), ""), pks, null);
-             }
+            }
         }
         run.setFrom(from);
 
@@ -158,7 +159,7 @@ public class MongoAdapter extends DefaultDriverAdapter implements DriverAdapter 
             }
             if(ConfigTable.IS_THROW_SQL_UPDATE_EXCEPTION){
                 SQLUpdateException ex = new SQLUpdateException("insert异常:"+e.toString(),e);
-                 throw ex;
+                throw ex;
             }else{
                 if(ConfigTable.IS_SHOW_SQL_WHEN_ERROR){
                     log.error("{}[{}][table:\n{}\n]\n[param:{}]", random, LogUtil.format("插入异常:", 33)+e, run.getTable());
@@ -201,18 +202,69 @@ public class MongoAdapter extends DefaultDriverAdapter implements DriverAdapter 
     @Override
     protected void createQueryContent(DataRuntime runtime, TableRun run){
         Bson bson = Filters.empty();
-        ConfigStore configs = run.getConfigStore();
         ConditionChain chain = run.getConditionChain();
-        List<Condition> conditions = chain.getConditions();
-        for(Condition condition:conditions){
-            List<Variable> vars = condition.getVariables();
-            for(Variable var:vars){
-                bson =Filters.and(bson, Filters.eq(var.getKey(), var.getValues()));
-            }
-        }
+        bson = parseCondition(bson, chain);
         run.setFilter(bson);
     }
+    private Bson parseCondition(Bson bson, Condition condition){
+        if(condition instanceof ConditionChain){
+            ConditionChain chain = (ConditionChain)condition;
+            bson = parseCondition(bson, chain);
+        }else{
+            if(condition instanceof AutoCondition){
+                AutoCondition auto = (AutoCondition)condition;
+                List<RunValue> values = condition.getRunValues();
+                String column = condition.getId();
+                String join = condition.getJoin();
+                Bson create = bson(auto.getCompare(), column, values);
+                if(null != create) {
+                    if ("or".equalsIgnoreCase(join)) {
+                        bson = Filters.or(create);
+                    } else {
+                        bson = Filters.and(create);
+                    }
+                }
+            }
+        }
+        return bson;
+    }
 
+    private Bson parseCondition(Bson bson, ConditionChain chain){
+        String join = chain.getJoin();
+        Bson child = Filters.empty();
+        List<Condition> conditions = chain.getConditions();
+        for(Condition con:conditions){
+            child = parseCondition(child, con);
+        }
+        if("or".equalsIgnoreCase(join)){
+            bson = Filters.or(bson, child);
+        }else{
+            bson = Filters.and(bson, child);
+        }
+        return bson;
+    }
+    private Bson bson(Compare compare, String column, List<RunValue> values){
+        Bson bson = null;
+        if(null != values && !values.isEmpty()) {
+            Object value = null;
+            boolean multiple = compare.isMultipleValue();
+            if (multiple) {
+                List<Object> list = new ArrayList<>();
+                for (RunValue rv : values) {
+                    list.add(rv.getValue());
+                }
+                value = list;
+            } else {
+                value = values.get(0).getValue();
+            }
+            if(compare == Compare.EQUAL){
+                bson = Filters.eq(column, value);
+            }else if(compare == Compare.GREAT){
+                bson = Filters.gt(column, value);
+            }
+        }
+        return bson;
+    }
     @Override
     public DataSet select(DataRuntime runtime, String random, boolean system, String table, Run run) {
         long fr = System.currentTimeMillis();
@@ -223,20 +275,22 @@ public class MongoAdapter extends DefaultDriverAdapter implements DriverAdapter 
         //根据这一步中的JDBC结果集检测类型不准确,如:实际POINT 返回 GEOMETRY 如果要求准确 需要开启到自动检测
         //在DataRow中 如果检测到准确类型 JSON XML POINT 等 返回相应的类型,不返回byte[]（所以需要开启自动检测）
         //Entity中 JSON XML POINT 等根据属性类型返回相应的类型（所以不需要开启自动检测）
-       // LinkedHashMap<String,Column> columns = new LinkedHashMap<>();
+        // LinkedHashMap<String,Column> columns = new LinkedHashMap<>();
 
         if(!system && ThreadConfig.check(runtime.getKey()).IS_AUTO_CHECK_METADATA() && null != table){
-           // columns = columns(runtime, false, new Table(null, null, table), null);
+            // columns = columns(runtime, false, new Table(null, null, table), null);
         }
         try{
-            //final LinkedHashMap<String, Column> metadatas = new LinkedHashMap<>();
-            //metadatas.putAll(columns);
-            //set.setMetadatas(metadatas);
-
-            //set.setDatalink(runtime.datasource());
             MongoDatabase database = (MongoDatabase)runtime.getClient();
             Bson bson = (Bson)run.getFilter();
+            if(ConfigTable.IS_SHOW_SQL && log.isInfoEnabled()){
+                log.info("{}[collection:{}][filter:{}]", random, run.getTable(), bson);
+            }
             FindIterable<DataRow> rows = database.getCollection(run.getTable(), DataRow.class).find(bson);
+            PageNavi navi = run.getPageNavi();
+            if(null != navi){
+                rows.skip((int)navi.getFirstRow()).limit(navi.getPageRows());
+            }
             for(DataRow row:rows){
                 set.add(row);
             }
