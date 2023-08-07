2c514f965907ba0cdd4ce196dc3f4736a3ea43ae
anyline <server@anyline.org>
2023-08-07 15:47:22 +0800

    EnvironmentListener

--- anyline-data-jdbc/src/main/java/org/anyline/data/jdbc/listener/EnvironmentListener.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/jdbc/listener/EnvironmentListener.java
@@ -28,30 +28,48 @@ public class EnvironmentListener implements ApplicationContextAware {
         factory = (DefaultListableBeanFactory) context.getAutowireCapableBeanFactory();
         SpringContextUtil.init(context);
         JDBCRuntimeHolder.init(factory);
-        load();
+        int qty = load("spring.datasource");
+        qty += load("anyline.datasource");
+        if(qty > 0) {
+            loadDefault();
+        }
+    }
+    private void loadDefault(){
+
+        //默认数据源 有多个数据源的情况下 再注册anyline.service.default
+        //如果单个数据源 只通过@serveri注解 注册一个anyline.service
+        //anyline.service.default 用来操作主数据源
+        //anyline.service.sso 用来操作sso数据源
+        //anyline.service.common 用来操作所有数据源
+        JdbcTemplate template = (JdbcTemplate)SpringContextUtil.getBean("jdbcTemplate");
+        if(null != template) {
+            //注册一个默认运行环境
+            JDBCRuntimeHolder.reg("default", template, null);
+            if(ConfigTable.IS_OPEN_PRIMARY_TRANSACTION_MANAGER){
+                //注册一个主事务管理器
+                DataSourceHolder.regTransactionManager("primary", template.getDataSource(), true);
+            }
+            //注册一个通用运行环境(可切换数据源)
+            DataRuntime runtime = new JDBCRuntime("common", template, null);
+            JDBCRuntimeHolder.reg("common", runtime);
+        }else{
+            //加载anyline.datasource.url格式定义
+        }
     }
     //加载配置文件
-    private void load(){
+    private int load(String head){
+        int qty = 0;
         Environment env = context.getEnvironment();
         // 读取配置文件获取更多数据源
-        String prefixs = env.getProperty("spring.datasource.list");
-        boolean multiple = false;
+        String prefixs = env.getProperty(head + ".list");
         if(null != prefixs){
             for (String prefix : prefixs.split(",")) {
                 // 多个数据源
                 try {
                     //返回bean id
-                    String ds = DataSourceHolder.reg(prefix, "spring.datasource." + prefix, env);
+                    String ds = DataSourceHolder.reg(prefix, head + "." + prefix, env);
                     if(null != ds) {
-                      /*  //注册事务管理器
-                        DataSourceHolder.regTransactionManager(prefix, ds);
-                        //记录数据源列表
-                        DataSourceHolder.reg(prefix);
-                        //注册运行环境
-                        RuntimeHolder.reg(prefix, ds);
-                        //以上几行在reg内部已经执行了
-                        */
-                        multiple = true;
+                        qty ++;
                         log.info("[创建数据源][prefix:{}][bean:{}]", prefix, ds);
                     }
                 }catch (Exception e){
@@ -59,25 +77,7 @@ public class EnvironmentListener implements ApplicationContextAware {
                 }
             }
         }
-        //默认数据源 有多个数据源的情况下 再注册anyline.service.default
-        //如果单个数据源 只通过@serveri注解 注册一个anyline.service
-        //anyline.service.default 用来操作主数据源
-        //anyline.service.sso 用来操作sso数据源
-        //anyline.service.common 用来操作所有数据源
-        JdbcTemplate template = SpringContextUtil.getBean(JdbcTemplate.class);
-        if(null != template) {
-            if(multiple) {
-                //注册一个默认运行环境
-                JDBCRuntimeHolder.reg("default", template, null);
-                if(ConfigTable.IS_OPEN_PRIMARY_TRANSACTION_MANAGER){
-                    //注册一个主事务管理器
-                    DataSourceHolder.regTransactionManager("primary", template.getDataSource(), true);
-                }
-            }
-            //注册一个通用运行环境(可切换数据源)
-            DataRuntime runtime = new JDBCRuntime("common", template, null);
-            JDBCRuntimeHolder.reg("common", runtime);
-        }
+        return qty;
     }
 
 
