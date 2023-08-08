3183833d8f371524dd08d8a9543d48e315db012f
anyline <server@anyline.org>
2023-08-08 11:18:46 +0800

    RuntimeHolderProxy

--- anyline-data/src/main/java/org/anyline/proxy/RuntimeHolderProxy.java
+++ anyline-data/src/main/java/org/anyline/proxy/RuntimeHolderProxy.java
@@ -14,10 +14,21 @@ public class RuntimeHolderProxy {
     public static void reg(Class calzz, RuntimeHolder holder){
         holders.put(calzz, holder);
     }
-    public static DataRuntime runtime(String key, Object source, DriverAdapter adapter) throws Exception{
+
+
+    /**
+     * 临时数据源
+     * @param key 数据源标识,切换数据源时根据key,输出日志时标记当前数据源
+     * @param datasource 数据源,如DruidDataSource,MongoClient
+     * @param database 数据库,jdbc类型数据源不需要
+     * @param adapter 如果确认数据库类型可以提供如 new MySQLAdapter() ,如果不提供则根据ds检测
+     * @return DataRuntime
+     * @throws Exception 异常 Exception
+     */
+    public static DataRuntime runtime(String key, Object datasource, String database, DriverAdapter adapter) throws Exception{
         DataRuntime runtime = null;
-        if(null != source){
-            Class clazz = source.getClass();
+        if(null != datasource){
+            Class clazz = datasource.getClass();
             //类型相同
             RuntimeHolder holder = holders.get(clazz);
             //子类
@@ -32,7 +43,7 @@ public class RuntimeHolderProxy {
                 }
             }
             if(null != holder){
-                runtime = holder.runtime(key, source, adapter);
+                runtime = holder.runtime(key, datasource, database, adapter);
             }
         }
         return runtime;
