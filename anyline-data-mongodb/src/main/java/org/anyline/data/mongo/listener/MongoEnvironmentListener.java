7b9d2ccb2ee55aa21fda89a7e664fae215eef9b4
anyline <server@anyline.org>
2023-08-08 11:19:30 +0800

    MongoRuntime

--- anyline-data-mongodb/src/main/java/org/anyline/data/mongo/listener/MongoEnvironmentListener.java
+++ anyline-data-mongodb/src/main/java/org/anyline/data/mongo/listener/MongoEnvironmentListener.java
@@ -1,6 +1,7 @@
 package org.anyline.data.mongo.listener;
 
-import org.anyline.data.mongo.runtime.DatabaseHolder;
+import com.mongodb.client.MongoDatabase;
+import org.anyline.data.mongo.runtime.MongoClientHolder;
 import org.anyline.data.mongo.runtime.MongoRuntimeHolder;
 import org.anyline.listener.EnvironmentListener;
 import org.anyline.util.SpringContextUtil;
@@ -27,50 +28,48 @@ public class MongoEnvironmentListener implements ApplicationContextAware {
         MongoRuntimeHolder.init(factory);
         int qty = load("spring.datasource");
         qty += load("anyline.datasource");
-        if(qty > 0) {
-            loadDefault();
-        }
-     }
-    private void loadDefault(){
-
+        load(qty > 0);
+    }
+    private void load(boolean multiple){
+        MongoDatabase database =  SpringContextUtil.getBean(MongoDatabase.class);
         //默认数据源 有多个数据源的情况下 再注册anyline.service.default
         //如果单个数据源 只通过@serveri注解 注册一个anyline.service
         //anyline.service.default 用来操作主数据源
         //anyline.service.sso 用来操作sso数据源
         //anyline.service.common 用来操作所有数据源
-      /*  JdbcTemplate template = (JdbcTemplate)SpringContextUtil.getBean("jdbcTemplate");
-        if(null != template) {
+        if(null != database) {
             //注册一个默认运行环境
-            JDBCRuntimeHolder.reg("default", template, null);
-            if(ConfigTable.IS_OPEN_PRIMARY_TRANSACTION_MANAGER){
-                //注册一个主事务管理器
-                DataSourceHolder.regTransactionManager("primary", template.getDataSource(), true);
+            if(multiple) {
+                //MongoRuntimeHolder.reg("mongo", database, null);
             }
             //注册一个通用运行环境(可切换数据源)
-            DataRuntime runtime = new JDBCRuntime("common", template, null);
-            JDBCRuntimeHolder.reg("common", runtime);
+/*
+            DataRuntime runtime = new MongoRuntime("mongo.common", database, null);
+            MongoRuntimeHolder.reg("mongo.common", runtime);*/
         }else{
             //加载anyline.datasource.url格式定义
-        }*/
+        }
     }
     //加载配置文件
     private int load(String head){
         int qty = 0;
         Environment env = context.getEnvironment();
-        // 读取配置文件获取更多数据源
+        // 读取配置文件获取更多数据源 anyline.datasource.list
         String prefixs = env.getProperty(head + ".list");
         if(null == prefixs){
+            //anyline.datasource-list
             prefixs = env.getProperty(head + "-list");
         }
         if(null != prefixs){
             for (String prefix : prefixs.split(",")) {
                 // 多个数据源
                 try {
-                    //返回bean id
-                    String ds = DatabaseHolder.reg(prefix, head + "." + prefix, env);
-                    if(null != ds) {
+                    //返回bean id(MongoDatabase)
+                    //sso, anyline.datasource.sso, env
+                    String client = MongoClientHolder.reg(prefix, head + "." + prefix, env);
+                    if(null != client) {
                         qty ++;
-                        log.info("[创建数据源][prefix:{}][bean:{}]", prefix, ds);
+                        log.info("[创建数据源][prefix:{}][client:{}]", prefix, client);
                     }
                 }catch (Exception e){
                     log.error("[创建数据源失败][prefix:{}][msg:{}]", prefix, e.toString());
