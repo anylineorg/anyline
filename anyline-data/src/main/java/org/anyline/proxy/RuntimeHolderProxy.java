94ae84e3782e4ccf07b803978885ea41d63271a9
anyline <server@anyline.org>
2023-08-14 10:22:01 +0800

    temporary service(datasource)

--- anyline-data/src/main/java/org/anyline/proxy/RuntimeHolderProxy.java
+++ anyline-data/src/main/java/org/anyline/proxy/RuntimeHolderProxy.java
@@ -4,17 +4,18 @@ import org.anyline.data.adapter.DriverAdapter;
 import org.anyline.data.runtime.DataRuntime;
 import org.anyline.data.runtime.RuntimeHolder;
 import org.anyline.util.ClassUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RuntimeHolderProxy {
-
+    protected static Logger log = LoggerFactory.getLogger(RuntimeHolderProxy.class);
     private static Map<Class, RuntimeHolder> holders = new HashMap<>();
     public static void reg(Class calzz, RuntimeHolder holder){
         holders.put(calzz, holder);
     }
-
 
     /**
      * 临时数据源
@@ -25,27 +26,45 @@ public class RuntimeHolderProxy {
      * @return DataRuntime
      * @throws Exception 异常 Exception
      */
-    public static DataRuntime runtime(String key, Object datasource, String database, DriverAdapter adapter) throws Exception{
+    public static DataRuntime temporary(String key, Object datasource, String database, DriverAdapter adapter) throws Exception{
         DataRuntime runtime = null;
         if(null != datasource){
             Class clazz = datasource.getClass();
             //类型相同
-            RuntimeHolder holder = holders.get(clazz);
-            //子类
-            if(null == holder){
-                for(Class c: holders.keySet()){
-                    if(ClassUtil.isInSub(clazz, c)){
-                        RuntimeHolder h = holders.get(c);
-                        holders.put(clazz, h);
-                        holder = h;
-                        break;
-                    }
-                }
-            }
+            RuntimeHolder holder = holder(clazz);
             if(null != holder){
-                runtime = holder.runtime(key, datasource, database, adapter);
+                runtime = holder.regTemporary(key, datasource, database, adapter);
             }
         }
         return runtime;
+    }
+    public static void destroy(String key){
+        DataRuntime runtime = RuntimeHolder.getRuntime(key);
+        if(null == runtime){
+            log.warn("[注销运行环境][{}不存在在]", key);
+            return;
+        }
+        Object client = runtime.getClient();
+        Class clazz = client.getClass();
+        RuntimeHolder holder = holder(clazz);
+        if(null != holder){
+            holder.exeDestroy(key);
+        }
+    }
+
+    public static RuntimeHolder holder(Class clazz){
+        RuntimeHolder holder = holders.get(clazz);
+        //子类
+        if(null == holder){
+            for(Class c: holders.keySet()){
+                if(ClassUtil.isInSub(clazz, c)){
+                    RuntimeHolder h = holders.get(c);
+                    holders.put(clazz, h);
+                    holder = h;
+                    break;
+                }
+            }
+        }
+        return holder;
     }
 }
