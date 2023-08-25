14d06f5bb754bac18acf0536446db9b0e4c0b4ac
anyline <server@anyline.org>
2023-08-25 10:02:07 +0800

    DataSourceProperty

--- anyline-boot/src/main/java/org/anyline/boot/datasource/DataSourceProperty.java
+++ anyline-boot/src/main/java/org/anyline/boot/datasource/DataSourceProperty.java
@@ -19,7 +19,9 @@ package org.anyline.boot.datasource;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
-
+//不要加载 有些类没有
+//@Configuration("anyline.boot.datasource")
+//@ConfigurationProperties(prefix = "anyline.datasource")
 public class DataSourceProperty extends DataSourceProperties {
 
     /**
@@ -28,33 +30,72 @@ public class DataSourceProperty extends DataSourceProperties {
     private String database;
 
     /**
-     * [MongoDB]Authentication database name
+     * Default port used when the configured port is {@code null}.
+     */
+    public static final int DEFAULT_PORT = 27017;
+
+    /**
+     * Default URI used when the configured URI is {@code null}.
+     */
+    public static final String DEFAULT_URI = "mongodb://localhost/test";
+
+    /**
+     * Mongo server host. Cannot be set with URI.
+     */
+    private String host;
+
+    /**
+     * Mongo server port. Cannot be set with URI.
+     */
+    private Integer port = null;
+
+    /**
+     * Mongo database URI. Cannot be set with host, port, credentials and replica set
+     * name.
+     */
+    private String uri;
+
+
+    /**
+     * Authentication database name.
      */
     private String authenticationDatabase;
 
     private final MongoProperties.Gridfs gridfs = new MongoProperties.Gridfs();
 
+    /**
+     * Login user of the mongo server. Cannot be set with URI.
+     */
+    private String username;
 
 
 
     /**
-     * [MongoDB]Required replica set name for the cluster. Cannot be set with URI
+     * Required replica set name for the cluster. Cannot be set with URI.
      */
     private String replicaSetName;
 
     /**
-     * [MongoDB]Fully qualified name of the FieldNamingStrategy to use
+     * Fully qualified name of the FieldNamingStrategy to use.
      */
     private Class<?> fieldNamingStrategy;
 
 
     /**
-     * [MongoDB]Whether to enable auto-index creation
+     * Whether to enable auto-index creation.
      */
     private Boolean autoIndexCreation;
 
+    public String getHost() {
+        return this.host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
     public String getDatabase() {
-        return database;
+        return this.database;
     }
 
     public void setDatabase(String database) {
@@ -62,19 +103,24 @@ public class DataSourceProperty extends DataSourceProperties {
     }
 
     public String getAuthenticationDatabase() {
-        return authenticationDatabase;
+        return this.authenticationDatabase;
     }
 
     public void setAuthenticationDatabase(String authenticationDatabase) {
         this.authenticationDatabase = authenticationDatabase;
     }
 
-    public MongoProperties.Gridfs getGridfs() {
-        return gridfs;
+    public String getUsername() {
+        return this.username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
     public String getReplicaSetName() {
-        return replicaSetName;
+        return this.replicaSetName;
     }
 
     public void setReplicaSetName(String replicaSetName) {
@@ -82,18 +128,77 @@ public class DataSourceProperty extends DataSourceProperties {
     }
 
     public Class<?> getFieldNamingStrategy() {
-        return fieldNamingStrategy;
+        return this.fieldNamingStrategy;
     }
 
     public void setFieldNamingStrategy(Class<?> fieldNamingStrategy) {
         this.fieldNamingStrategy = fieldNamingStrategy;
     }
 
-    public Boolean getAutoIndexCreation() {
-        return autoIndexCreation;
+
+
+    public String getUri() {
+        return this.uri;
+    }
+
+    public String determineUri() {
+        return (this.uri != null) ? this.uri : DEFAULT_URI;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public Integer getPort() {
+        return this.port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public MongoProperties.Gridfs getGridfs() {
+        return this.gridfs;
+    }
+
+    public Boolean isAutoIndexCreation() {
+        return this.autoIndexCreation;
     }
 
     public void setAutoIndexCreation(Boolean autoIndexCreation) {
         this.autoIndexCreation = autoIndexCreation;
+    }
+
+    public static class Gridfs {
+
+        /**
+         * GridFS database name.
+         */
+        private String database;
+
+        /**
+         * GridFS bucket name.
+         */
+        private String bucket;
+
+        public String getDatabase() {
+            return this.database;
+        }
+
+        public void setDatabase(String database) {
+            this.database = database;
+        }
+
+        public String getBucket() {
+            return this.bucket;
+        }
+
+        public void setBucket(String bucket) {
+            this.bucket = bucket;
+        }
+    }
+
+    public Boolean getAutoIndexCreation() {
+        return autoIndexCreation;
     }
 }
