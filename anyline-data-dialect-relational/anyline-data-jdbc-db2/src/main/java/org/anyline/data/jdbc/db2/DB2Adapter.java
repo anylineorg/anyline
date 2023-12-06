89963f68de9df5b464266efffb7123d53962dc75
server@anyline.org <server@anyline.org>
2023-12-06 19:22:36 +0800

    db2 primary ddl

--- anyline-data-dialect-relational/anyline-data-jdbc-db2/src/main/java/org/anyline/data/jdbc/db2/DB2Adapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-db2/src/main/java/org/anyline/data/jdbc/db2/DB2Adapter.java
@@ -3182,11 +3182,17 @@ public class DB2Adapter extends OracleGenusAdapter implements JDBCAdapter, Initi
 	 */
 	@Override
 	public StringBuilder primary(DataRuntime runtime, StringBuilder builder, Table meta){
-		List<Column> pks = meta.primarys();
-		if(pks.size()>0){
+		PrimaryKey primary = meta.getPrimaryKey();
+		LinkedHashMap<String, Column> pks = null;
+		if(null != primary){
+			pks = primary.getColumns();
+		}else{
+			pks = meta.primarys();
+		}
+		if(!pks.isEmpty()){
 			builder.append(",PRIMARY KEY (");
 			boolean first = true;
-			for(Column pk:pks){
+			for(Column pk:pks.values()){
 				if(!first){
 					builder.append(",");
 				}
