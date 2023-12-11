3584774cd6a75ae3117d204abb337e6c640e23c9
server@anyline.org <server@anyline.org>
2023-12-11 18:08:49 +0800

    OpenGaussAdapter

--- anyline-data-dialect-relational/anyline-data-jdbc-opengauss/src/main/java/org/anyline/data/jdbc/opengauss/OpenGaussAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-opengauss/src/main/java/org/anyline/data/jdbc/opengauss/OpenGaussAdapter.java
@@ -4297,7 +4297,7 @@ public class OpenGaussAdapter extends PostgresGenusAdapter implements JDBCAdapte
 	 */
 	@Override
 	public boolean alter(DataRuntime runtime, Table table, Tag meta, boolean trigger) throws Exception{
-		return super.alter(runtime, table, meta);
+		return super.alter(runtime, table, meta, trigger);
 	}
 
 
