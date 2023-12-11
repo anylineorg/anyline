0c3637157f73bd91dd17b1d48b5f7c39026d3a78
server@anyline.org <server@anyline.org>
2023-12-11 18:38:16 +0800

    MySQLAdapter

--- anyline-data-dialect-relational/anyline-data-jdbc-mysql/src/main/java/org/anyline/data/jdbc/mysql/MySQLAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-mysql/src/main/java/org/anyline/data/jdbc/mysql/MySQLAdapter.java
@@ -4290,7 +4290,7 @@ public class MySQLAdapter extends MySQLGenusAdapter implements JDBCAdapter, Init
 	 */
 	@Override
 	public boolean alter(DataRuntime runtime, Table table, Tag meta, boolean trigger) throws Exception{
-		return super.alter(runtime, table, meta);
+		return super.alter(runtime, table, meta, trigger);
 	}
 
 
