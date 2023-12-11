bafa10929fecf4d6b3ac2b1c61396d600859dcfa
server@anyline.org <server@anyline.org>
2023-12-11 17:22:16 +0800

    alter tag

--- anyline-data-dialect-relational/anyline-data-jdbc-sqlite/src/main/java/org/anyline/data/jdbc/sqlite/SQLiteAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-sqlite/src/main/java/org/anyline/data/jdbc/sqlite/SQLiteAdapter.java
@@ -4291,7 +4291,7 @@ public class SQLiteAdapter extends DefaultJDBCAdapter implements JDBCAdapter, In
 	 */
 	@Override
 	public boolean alter(DataRuntime runtime, Table table, Tag meta, boolean trigger) throws Exception{
-		return super.alter(runtime, table, meta);
+		return super.alter(runtime, table, meta, trigger);
 	}
 
 
