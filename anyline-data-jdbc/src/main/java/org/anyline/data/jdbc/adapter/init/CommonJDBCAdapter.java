1a134ac765a74e7c35414eb016cee9ee397ea1a6
server@anyline.org <server@anyline.org>
2023-12-20 18:44:35 +0800

    CommonJDBCAdapter

---
+++ anyline-data-jdbc/src/main/java/org/anyline/data/jdbc/adapter/init/CommonJDBCAdapter.java
@@ -0,0 +1,7 @@
+package org.anyline.data.jdbc.adapter.init;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("anyline.data.jdbc.adapter.common")
+public class CommonJDBCAdapter extends DefaultJDBCAdapter{
+}
