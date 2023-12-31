f4b0306fcfa70505064475efbad0a9f8f49c890d
Administrator <server@anyline.org>
2023-12-31 20:14:27 +0800

    SQLiteAdapter

--- anyline-data-dialect-relational/anyline-data-jdbc-sqlite/src/main/java/org/anyline/data/jdbc/sqlite/SQLiteAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-sqlite/src/main/java/org/anyline/data/jdbc/sqlite/SQLiteAdapter.java
@@ -96,7 +96,7 @@ public class SQLiteAdapter extends DefaultJDBCAdapter implements JDBCAdapter, In
 	 * public void fillInsertContent(DataRuntime runtime, Run run, Table dest, Collection list, ConfigStore configs, LinkedHashMap<String, Column> columns)
 	 * public LinkedHashMap<String, Column> confirmInsertColumns(DataRuntime runtime, String dest, Object obj, ConfigStore configs, List<String> columns, boolean batch)
 	 * public String batchInsertSeparator()
-	 * public boolean supportInsertPlaceholder ()
+	 * public boolean supportInsertPlaceholder()
 	 * protected Run createInsertRun(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns)
 	 * protected Run createInsertRunFromCollection(DataRuntime runtime, int batch, Table dest, Collection list, ConfigStore configs, List<String> columns)
 	 * public String generatedKey()
@@ -209,7 +209,7 @@ public class SQLiteAdapter extends DefaultJDBCAdapter implements JDBCAdapter, In
 	 * @return String
 	 */
 	@Override
-	public String batchInsertSeparator (){
+	public String batchInsertSeparator(){
 		return ",";
 	}
 
@@ -219,7 +219,7 @@ public class SQLiteAdapter extends DefaultJDBCAdapter implements JDBCAdapter, In
 	 * @return boolean
 	 */
 	@Override
-	public boolean supportInsertPlaceholder (){
+	public boolean supportInsertPlaceholder(){
 		return true;
 	}
 	/**
