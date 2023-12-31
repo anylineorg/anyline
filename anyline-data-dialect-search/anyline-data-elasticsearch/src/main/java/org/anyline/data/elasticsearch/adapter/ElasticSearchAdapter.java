633dd3fd14afdb904f21fdcdc25c8218fca7a1fc
Administrator <server@anyline.org>
2023-12-31 20:13:26 +0800

    ElasticSearchAdapter

--- anyline-data-dialect-search/anyline-data-elasticsearch/src/main/java/org/anyline/data/elasticsearch/adapter/ElasticSearchAdapter.java
+++ anyline-data-dialect-search/anyline-data-elasticsearch/src/main/java/org/anyline/data/elasticsearch/adapter/ElasticSearchAdapter.java
@@ -68,7 +68,7 @@ public class ElasticSearchAdapter extends DefaultDriverAdapter implements Driver
      * public void fillInsertContent(DataRuntime runtime, Run run, Table dest, Collection list, ConfigStore configs, LinkedHashMap<String, Column> columns)
      * public LinkedHashMap<String, Column> confirmInsertColumns(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns, boolean batch)
      * public String batchInsertSeparator()
-     * public boolean supportInsertPlaceholder ()
+     * public boolean supportInsertPlaceholder()
      * protected Run createInsertRun(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns)
      * protected Run createInsertRunFromCollection(DataRuntime runtime, int batch, Table dest, Collection list, ConfigStore configs, List<String> columns)
      * public String generatedKey()
@@ -182,7 +182,7 @@ public class ElasticSearchAdapter extends DefaultDriverAdapter implements Driver
      * @return String
      */
     @Override
-    public String batchInsertSeparator (){
+    public String batchInsertSeparator(){
         return ",";
     }
 
@@ -192,7 +192,7 @@ public class ElasticSearchAdapter extends DefaultDriverAdapter implements Driver
      * @return boolean
      */
     @Override
-    public boolean supportInsertPlaceholder (){
+    public boolean supportInsertPlaceholder(){
         return true;
     }
     /**
