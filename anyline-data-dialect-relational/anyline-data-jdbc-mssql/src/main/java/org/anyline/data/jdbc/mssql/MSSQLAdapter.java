867770573d272faca033ada3628c2653f250cc24
Administrator <server@anyline.org>
2023-12-31 20:18:13 +0800

    MSSQLAdapter

--- anyline-data-dialect-relational/anyline-data-jdbc-mssql/src/main/java/org/anyline/data/jdbc/mssql/MSSQLAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-mssql/src/main/java/org/anyline/data/jdbc/mssql/MSSQLAdapter.java
@@ -121,7 +121,7 @@ public class MSSQLAdapter extends DefaultJDBCAdapter implements JDBCAdapter, Ini
 	 * public void fillInsertContent(DataRuntime runtime, Run run, Table dest, Collection list, ConfigStore configs, LinkedHashMap<String, Column> columns)
 	 * public LinkedHashMap<String, Column> confirmInsertColumns(DataRuntime runtime, String dest, Object obj, ConfigStore configs, List<String> columns, boolean batch)
 	 * public String batchInsertSeparator()
-	 * public boolean supportInsertPlaceholder ()
+	 * public boolean supportInsertPlaceholder()
 	 * protected Run createInsertRun(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns)
 	 * protected Run createInsertRunFromCollection(DataRuntime runtime, int batch, Table dest, Collection list, ConfigStore configs, List<String> columns)
 	 * public String generatedKey()
@@ -234,7 +234,7 @@ public class MSSQLAdapter extends DefaultJDBCAdapter implements JDBCAdapter, Ini
 	 * @return String
 	 */
 	@Override
-	public String batchInsertSeparator (){
+	public String batchInsertSeparator(){
 		return ",";
 	}
 
@@ -244,7 +244,7 @@ public class MSSQLAdapter extends DefaultJDBCAdapter implements JDBCAdapter, Ini
 	 * @return boolean
 	 */
 	@Override
-	public boolean supportInsertPlaceholder (){
+	public boolean supportInsertPlaceholder(){
 		return true;
 	}
 	/**
