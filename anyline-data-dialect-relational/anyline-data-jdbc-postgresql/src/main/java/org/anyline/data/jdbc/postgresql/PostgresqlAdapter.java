efea5a01076607d481b2953a622be14e261c772e
Administrator <server@anyline.org>
2023-12-31 20:15:06 +0800

    PostgresqlAdapter

--- anyline-data-dialect-relational/anyline-data-jdbc-postgresql/src/main/java/org/anyline/data/jdbc/postgresql/PostgresqlAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-postgresql/src/main/java/org/anyline/data/jdbc/postgresql/PostgresqlAdapter.java
@@ -102,7 +102,7 @@ public class PostgresqlAdapter extends PostgresGenusAdapter implements JDBCAdapt
 	 * public void fillInsertContent(DataRuntime runtime, Run run, Table dest, Collection list, ConfigStore configs, LinkedHashMap<String, Column> columns)
 	 * public LinkedHashMap<String, Column> confirmInsertColumns(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns, boolean batch)
 	 * public String batchInsertSeparator()
-	 * public boolean supportInsertPlaceholder ()
+	 * public boolean supportInsertPlaceholder()
 	 * protected Run createInsertRun(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns)
 	 * protected Run createInsertRunFromCollection(DataRuntime runtime, int batch, Table dest, Collection list, ConfigStore configs, List<String> columns)
 	 * public String generatedKey()
@@ -215,7 +215,7 @@ public class PostgresqlAdapter extends PostgresGenusAdapter implements JDBCAdapt
 	 * @return String
 	 */
 	@Override
-	public String batchInsertSeparator (){
+	public String batchInsertSeparator(){
 		return ",";
 	}
 
@@ -225,7 +225,7 @@ public class PostgresqlAdapter extends PostgresGenusAdapter implements JDBCAdapt
 	 * @return boolean
 	 */
 	@Override
-	public boolean supportInsertPlaceholder (){
+	public boolean supportInsertPlaceholder(){
 		return true;
 	}
 	/**
