7e34c66f6e75e34385839112ee71d131e7884b5a
Administrator <server@anyline.org>
2023-12-31 20:15:40 +0800

    H2Adapter

--- anyline-data-dialect-relational/anyline-data-jdbc-h2/src/main/java/org/anyline/data/jdbc/h2/H2Adapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-h2/src/main/java/org/anyline/data/jdbc/h2/H2Adapter.java
@@ -91,7 +91,7 @@ public class H2Adapter extends DefaultJDBCAdapter implements JDBCAdapter, Initia
 	 * public void fillInsertContent(DataRuntime runtime, Run run, Table dest, Collection list, ConfigStore configs, LinkedHashMap<String, Column> columns)
 	 * public LinkedHashMap<String, Column> confirmInsertColumns(DataRuntime runtime, String dest, Object obj, ConfigStore configs, List<String> columns, boolean batch)
 	 * public String batchInsertSeparator()
-	 * public boolean supportInsertPlaceholder ()
+	 * public boolean supportInsertPlaceholder()
 	 * protected Run createInsertRun(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns)
 	 * protected Run createInsertRunFromCollection(DataRuntime runtime, int batch, Table dest, Collection list, ConfigStore configs, List<String> columns)
 	 * public String generatedKey()
@@ -204,7 +204,7 @@ public class H2Adapter extends DefaultJDBCAdapter implements JDBCAdapter, Initia
 	 * @return String
 	 */
 	@Override
-	public String batchInsertSeparator (){
+	public String batchInsertSeparator(){
 		return ",";
 	}
 
@@ -214,7 +214,7 @@ public class H2Adapter extends DefaultJDBCAdapter implements JDBCAdapter, Initia
 	 * @return boolean
 	 */
 	@Override
-	public boolean supportInsertPlaceholder (){
+	public boolean supportInsertPlaceholder(){
 		return true;
 	}
 	/**
