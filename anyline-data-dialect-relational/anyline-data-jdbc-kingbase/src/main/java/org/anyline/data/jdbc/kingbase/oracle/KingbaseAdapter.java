609abc61ab04b9eae76ab3144f697859092118c7
Administrator <server@anyline.org>
2023-12-31 20:17:38 +0800

    KingbaseAdapter

--- anyline-data-dialect-relational/anyline-data-jdbc-kingbase/src/main/java/org/anyline/data/jdbc/kingbase/oracle/KingbaseAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-kingbase/src/main/java/org/anyline/data/jdbc/kingbase/oracle/KingbaseAdapter.java
@@ -139,7 +139,7 @@ public class KingbaseAdapter extends OracleGenusAdapter implements JDBCAdapter, 
 	 * public void fillInsertContent(DataRuntime runtime, Run run, Table dest, Collection list, ConfigStore configs, LinkedHashMap<String, Column> columns)
 	 * public LinkedHashMap<String, Column> confirmInsertColumns(DataRuntime runtime, String dest, Object obj, ConfigStore configs, List<String> columns, boolean batch)
 	 * public String batchInsertSeparator()
-	 * public boolean supportInsertPlaceholder ()
+	 * public boolean supportInsertPlaceholder()
 	 * protected Run createInsertRun(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns)
 	 * protected Run createInsertRunFromCollection(DataRuntime runtime, int batch, Table dest, Collection list, ConfigStore configs, List<String> columns)
 	 * public String generatedKey()
@@ -252,7 +252,7 @@ public class KingbaseAdapter extends OracleGenusAdapter implements JDBCAdapter, 
 	 * @return String
 	 */
 	@Override
-	public String batchInsertSeparator (){
+	public String batchInsertSeparator(){
 		return ",";
 	}
 
@@ -262,7 +262,7 @@ public class KingbaseAdapter extends OracleGenusAdapter implements JDBCAdapter, 
 	 * @return boolean
 	 */
 	@Override
-	public boolean supportInsertPlaceholder (){
+	public boolean supportInsertPlaceholder(){
 		return true;
 	}
 	/**
--- anyline-data-dialect-relational/anyline-data-jdbc-kingbase/src/main/java/org/anyline/data/jdbc/kingbase/postgres/KingbaseAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-kingbase/src/main/java/org/anyline/data/jdbc/kingbase/postgres/KingbaseAdapter.java
@@ -145,7 +145,7 @@ public class KingbaseAdapter extends PostgresGenusAdapter implements JDBCAdapter
 	 * public void fillInsertContent(DataRuntime runtime, Run run, Table dest, Collection list, ConfigStore configs, LinkedHashMap<String, Column> columns)
 	 * public LinkedHashMap<String, Column> confirmInsertColumns(DataRuntime runtime, String dest, Object obj, ConfigStore configs, List<String> columns, boolean batch)
 	 * public String batchInsertSeparator()
-	 * public boolean supportInsertPlaceholder ()
+	 * public boolean supportInsertPlaceholder()
 	 * protected Run createInsertRun(DataRuntime runtime, Table dest, Object obj, ConfigStore configs, List<String> columns)
 	 * protected Run createInsertRunFromCollection(DataRuntime runtime, int batch, Table dest, Collection list, ConfigStore configs, List<String> columns)
 	 * public String generatedKey()
@@ -258,7 +258,7 @@ public class KingbaseAdapter extends PostgresGenusAdapter implements JDBCAdapter
 	 * @return String
 	 */
 	@Override
-	public String batchInsertSeparator (){
+	public String batchInsertSeparator(){
 		return ",";
 	}
 
@@ -268,7 +268,7 @@ public class KingbaseAdapter extends PostgresGenusAdapter implements JDBCAdapter
 	 * @return boolean
 	 */
 	@Override
-	public boolean supportInsertPlaceholder (){
+	public boolean supportInsertPlaceholder(){
 		return true;
 	}
 	/**
