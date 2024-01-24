4b7a7bfe058d7b27ef338b51eabf87f23bb02b5c
server@anyline.org <server@anyline.org>
2024-01-24 16:07:17 +0800

    yidb

--- anyline-data-dialect-relational-extends/anyline-data-jdbc-yidb/src/main/java/org/anyline/data/jdbc/yidb/YiDBAdapter.java
+++ anyline-data-dialect-relational-extends/anyline-data-jdbc-yidb/src/main/java/org/anyline/data/jdbc/yidb/YiDBAdapter.java
@@ -17,13 +17,13 @@
 
 package org.anyline.data.jdbc.yidb;
 
-import org.anyline.data.jdbc.tidb.TiDBAdapter;
+import org.anyline.data.jdbc.adapter.init.MySQLGenusAdapter;
 import org.anyline.metadata.type.DatabaseType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyline.data.jdbc.adapter.yidb")
-public class YiDBAdapter extends TiDBAdapter {
+public class YiDBAdapter extends MySQLGenusAdapter {
     
     public DatabaseType type(){
         return DatabaseType.YiDB;
