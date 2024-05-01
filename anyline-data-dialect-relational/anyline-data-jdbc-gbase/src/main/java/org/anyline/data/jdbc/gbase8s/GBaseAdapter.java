7857077f01cac0caeffd6296b226705665c188a5
Administrator <server@anyline.org>
2024-05-02 00:57:28 +0800

    init

--- anyline-data-dialect-relational/anyline-data-jdbc-gbase/src/main/java/org/anyline/data/jdbc/gbase8s/GBaseAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-gbase/src/main/java/org/anyline/data/jdbc/gbase8s/GBaseAdapter.java
@@ -3448,7 +3448,7 @@ public class GBaseAdapter extends InformixGenusAdapter implements JDBCAdapter {
 	/**
 	 *
 	 * 根据 catalog, schema, name检测tables集合中是否存在
-	 * @param tables tables
+	 * @param metas metas
 	 * @param catalog catalog
 	 * @param schema schema
 	 * @param name name
