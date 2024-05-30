4bf4e241881c2cd6bff5e0761bb6d83cde6b33f2
server@anyline.org <server@anyline.org>
2024-05-30 18:30:09 +0800

    doc

--- anyline-data-jdbc/src/main/java/org/anyline/data/jdbc/adapter/init/InformixGenusAdapter.java
+++ anyline-data-jdbc/src/main/java/org/anyline/data/jdbc/adapter/init/InformixGenusAdapter.java
@@ -2464,7 +2464,7 @@ public abstract class InformixGenusAdapter extends AbstractJDBCAdapter {
      * column[命令合成]<br/>
      * 查询表上的列
      * @param runtime 运行环境主要包含驱动适配器 数据源或客户端
-     * @param table 表
+     * @param tables 表
      * @param metadata 是否根据metadata(true:SELECT * FROM T WHERE 1=0,false:查询系统表)
      * @return sqls
      */
