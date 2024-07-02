908d9d41a439357696a6be9e42aaf0efa6715e45
Administrator <server@anyline.org>
2024-07-03 00:46:38 +0800

    ElasticSearchFilter

--- anyline-data-dialect-search/anyline-data-elasticsearch/src/main/java/org/anyline/data/elasticsearch/metadata/ElasticSearchFilter.java
+++ anyline-data-dialect-search/anyline-data-elasticsearch/src/main/java/org/anyline/data/elasticsearch/metadata/ElasticSearchFilter.java
@@ -1,13 +1,24 @@
 package org.anyline.data.elasticsearch.metadata;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
+/**
+ * 对应index.setting[.index].analysis.filter
+ */
 public class ElasticSearchFilter {
+    private String key;
     private String type;
     private String synonymsPath;   // 同义词文件path
     private String synonymsSet;
-    private int updateable;
-    private LinkedHashMap<String, String> synonyms = new LinkedHashMap<>(); //同义词
+    private Boolean updateAble;
+    private Boolean expand;
+    private Boolean lenient;
+    private List<String> stopWords;
+    private List<String> synonyms = new ArrayList<>(); //同义词 ["pc => personal computer", "computer, pc, laptop"]
+    private List<String> filters = new ArrayList<>(); //Multiplexer token filter
+
 
     public String getType() {
         return type;
@@ -27,31 +38,139 @@ public class ElasticSearchFilter {
         return this;
     }
 
-    public LinkedHashMap<String, String> getSynonyms() {
+    public String getSynonymsSet() {
+        return synonymsSet;
+    }
+
+    public ElasticSearchFilter setSynonymsSet(String synonymsSet) {
+        this.synonymsSet = synonymsSet;
+        return this;
+    }
+
+    public Boolean getUpdateAble() {
+        return updateAble;
+    }
+
+    public ElasticSearchFilter setUpdateAble(Boolean updateAble) {
+        this.updateAble = updateAble;
+        return this;
+    }
+
+    public Boolean getExpand() {
+        return expand;
+    }
+
+    public ElasticSearchFilter setExpand(Boolean expand) {
+        this.expand = expand;
+        return this;
+    }
+
+    public Boolean getLenient() {
+        return lenient;
+    }
+
+    public ElasticSearchFilter setLenient(Boolean lenient) {
+        this.lenient = lenient;
+        return this;
+    }
+
+    public List<String> getStopWords() {
+        return stopWords;
+    }
+
+    public ElasticSearchFilter setStopWords(List<String> stopWords) {
+        this.stopWords = stopWords;
+        return this;
+    }
+
+    public List<String> getSynonyms() {
         return synonyms;
     }
 
-    public ElasticSearchFilter setSynonyms(LinkedHashMap<String, String> synonyms) {
+    public ElasticSearchFilter setSynonyms(List<String> synonyms) {
         this.synonyms = synonyms;
+        return this;
+    }
+
+    public List<String> getFilters() {
+        return filters;
+    }
+
+    public ElasticSearchFilter setFilters(List<String> filters) {
+        this.filters = filters;
         return this;
     }
 
     /**
      * 添加同义词
-     * @param origin 原文
-     * @param synonym 同义词
+     * @param synonym 同义词  ["pc => personal computer", "computer, pc, laptop"]
      * @return this
      */
-    public ElasticSearchFilter addSynonym(String origin, String synonym){
-        synonyms.put(origin, synonym);
+    public ElasticSearchFilter addSynonym(String synonym){
+        synonyms.add(synonym);
         return this;
     }
 
-    public int getUpdateable() {
-        return updateable;
+
+    public String getKey() {
+        return key;
     }
 
-    public void setUpdateable(int updateable) {
-        this.updateable = updateable;
+    public ElasticSearchFilter setKey(String key) {
+        this.key = key;
+        return this;
     }
-}
+
+    public LinkedHashMap<String, Object> map(){
+        LinkedHashMap<String, Object> map =new LinkedHashMap<>();
+        if(null != type){
+            map.put("type", type);
+        }
+        if(null != synonymsPath){
+            map.put("synonyms_path", synonymsPath);
+        }
+        if(null != synonymsSet){
+            map.put("synonyms_set", synonymsSet);
+        }
+        if(null != updateAble){
+            map.put("updateable", updateAble);
+        }
+        if(null != synonyms && !synonyms.isEmpty()){
+            map.put("synonyms", synonyms);
+        }
+        if(null != lenient){
+            map.put("lenient", lenient);
+        }
+        if(null != expand){
+            map.put("expand", expand);
+        }
+        if(null != filters && !filters.isEmpty()){
+            map.put("filters", filters);
+        }
+        return map;
+    }
+}/*
+"settings": {
+    "analysis": {
+        "filter": {
+            "us_synonym_filter": {
+              "type": "synonym_graph",
+              "synonyms_path": "analysis/us_synonym.txt"
+            }
+        },
+          "analyzer": {
+            "us_max_word": {
+              "tokenizer": "ik_max_word",
+              "filter": [
+                "us_synonym_filter"
+              ]
+            },
+            "us_smart": {
+              "tokenizer": "ik_smart",
+              "filter": [
+                "us_synonym_filter"
+              ]
+            }
+        }
+    }
+  }*/
