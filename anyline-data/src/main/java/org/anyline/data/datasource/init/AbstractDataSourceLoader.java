50eefbae290ac0932488f98199357aa9896c4d1a
Administrator <server@anyline.org>
2024-07-13 22:00:44 +0800

    AbstractDataSourceLoader

--- anyline-data/src/main/java/org/anyline/data/datasource/init/AbstractDataSourceLoader.java
+++ anyline-data/src/main/java/org/anyline/data/datasource/init/AbstractDataSourceLoader.java
@@ -19,6 +19,58 @@
 package org.anyline.data.datasource.init;
 
 import org.anyline.data.datasource.DataSourceLoader;
+import org.anyline.data.runtime.DataRuntime;
+import org.anyline.util.ConfigTable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class AbstractDataSourceLoader implements DataSourceLoader {
+
+    /**
+     *
+     * @param head 前缀
+     * @param loadDefault 是否加载默认数据源
+     * @return keys
+     */
+    protected List<String> load(String head, boolean loadDefault) {
+        //加载成功的前缀 crm, sso
+        List<String> list = new ArrayList<>();
+        if(loadDefault) {
+            //上下文初始化前后会调用 两次第二次就不执行加载了
+            if(!ConfigTable.worker.containsBean(DataRuntime.ANYLINE_SERVICE_BEAN_PREFIX+"default")) {
+                String def = holder().create("default", head);
+                if (null != def) {
+                    list.add(def);
+                }
+            }
+        }
+        //默认数据源
+        //多数据源
+        // 读取配置文件获取更多数据源 anyline.datasource.list
+        String prefixs = ConfigTable.environment().string(null, head + ".list");
+        if(null == prefixs) {
+            //anyline.datasource-list
+            prefixs = ConfigTable.environment().string(null,head + "-list");
+        }
+        if(null != prefixs) {
+            for (String prefix : prefixs.split(",")) {
+                // 多个数据源
+                try {
+                    //返回 datasource的bean id
+                    // sso, anyline.datasource.sso, env
+                    //上下文初始化前后会调用 两次第二次就不执行加载了
+                    if(!ConfigTable.worker.containsBean(DataRuntime.ANYLINE_SERVICE_BEAN_PREFIX+prefix)) {
+                        String datasource =  holder().create(prefix, head + "." + prefix);
+                        if(null != datasource) {
+                            list.add(datasource);
+                        }
+                    }
+                }catch (Exception e) {
+                    log.error("[注入数据源失败][type:Nebula][key:{}][msg:{}]", prefix, e.toString());
+                }
+            }
+        }
+        return list;
+    }
 }
