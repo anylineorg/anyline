05f1c1d4cbf3c0911afd4499b3683f44d4a6c102
server@anyline.org <server@anyline.org>
2024-08-14 11:15:55 +0800

    oracle11-

--- anyline-data-dialect-relational/anyline-data-jdbc-oracle/src/main/java/org/anyline/data/jdbc/oracle/Oracle11Adapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-oracle/src/main/java/org/anyline/data/jdbc/oracle/Oracle11Adapter.java
@@ -20,8 +20,6 @@ import org.anyline.annotation.Component;
 import org.anyline.data.jdbc.adapter.JDBCAdapter;
 import org.anyline.data.run.Run;
 import org.anyline.data.runtime.DataRuntime;
-import org.anyline.entity.OrderStore;
-import org.anyline.entity.PageNavi;
 import org.anyline.util.BasicUtil;
 import org.anyline.util.ConfigTable;
 import org.anyline.util.regular.RegularUtil;
@@ -78,36 +76,6 @@ public class Oracle11Adapter extends OracleAdapter implements JDBCAdapter {
 
     @Override
     public String mergeFinalQuery(DataRuntime runtime, Run run) {
-        StringBuilder builder = new StringBuilder();
-        String cols = run.getQueryColumn();
-        PageNavi navi = run.getPageNavi();
-        String sql = run.getBaseQuery();
-        OrderStore orders = run.getOrderStore();
-        long first = 0;
-        long last = 0;
-        String order = "";
-        if(null != orders) {
-            order = orders.getRunText(getDelimiterFr()+getDelimiterTo());
-        }
-        if(null != navi) {
-            first = navi.getFirstRow();
-            last = navi.getLastRow();
-        }
-        if(null == navi) {
-            builder.append(sql).append("\n").append(order);
-        }else{
-            // 分页
-            builder.append("SELECT "+cols+" FROM( \n");
-            builder.append("SELECT TAB_I.*, ROWNUM AS PAGE_ROW_NUMBER_ \n");
-            builder.append("FROM( \n");
-            builder.append(sql);
-            builder.append("\n").append(order);
-            builder.append(")  TAB_I \n");
-            builder.append(")  TAB_O WHERE PAGE_ROW_NUMBER_ >= "+(first+1)+" AND PAGE_ROW_NUMBER_ <= "+(last+1));
-
-        }
-
-        return builder.toString();
-
+        return super.pageRowNum(runtime, run);
     }
 }
