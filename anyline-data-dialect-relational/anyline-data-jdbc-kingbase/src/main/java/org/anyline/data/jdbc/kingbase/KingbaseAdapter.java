631445676074aedb58125cf6ed4eca789ee8fb6c
server@anyline.org <server@anyline.org>
2024-08-15 15:37:46 +0800

    doc

--- anyline-data-dialect-relational/anyline-data-jdbc-kingbase/src/main/java/org/anyline/data/jdbc/kingbase/KingbaseAdapter.java
+++ anyline-data-dialect-relational/anyline-data-jdbc-kingbase/src/main/java/org/anyline/data/jdbc/kingbase/KingbaseAdapter.java
@@ -83,7 +83,7 @@ public class KingbaseAdapter extends PostgresGenusAdapter implements JDBCAdapter
 	/**
 	 * 验证运行环境与当前适配器是否匹配<br/>
 	 * 默认不连接只根据连接参数<br/>
-	 * 只有同一个库区分不同版本(如mmsql2000/mssql2005)或不同模式(如kingbase的oracle/pg模式)时才需要单独实现
+	 * 只有同一个种区分不同版本(如mmsql2000/mssql2005)或不同模式(如kingbase的oracle/pg模式)时才需要单独实现
 	 * @param runtime 运行环境主要包含驱动适配器 数据源或客户端
 	 * @param compensate 是否补偿匹配，第一次失败后，会再匹配一次，第二次传入true
 	 * @return boolean
