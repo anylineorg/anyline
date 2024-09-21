09f2b84ac43df4652a9c6a0400e7042dc062aa3b
server@anyline.org <server@anyline.org>
2024-09-21 12:57:53 +0800

    SystemFunction

---
+++ anyline-core/src/main/java/org/anyline/metadata/SystemFunction.java
@@ -0,0 +1,34 @@
+package org.anyline.metadata;
+
+import org.anyline.metadata.type.DatabaseType;
+
+public interface SystemFunction {
+    enum META{
+        CONCAT("拼接String", 9);
+        META(String title, int cnt){
+            this.title = title;
+            this.paramCount = cnt;
+        }
+        private final String title;
+        private final int paramCount;
+        /**
+         * 支持参数数量 0:没有参数 IS NULL/IS EMPTY 1:一个参数 2:两个参数 BETWEEN 9:多个参数  IN()
+         * @return boolean
+         */
+        public int paramCount() {
+            return paramCount;
+        }
+        public String title() {
+            return title;
+        }
+    }
+    DatabaseType database();
+    META meta();
+    String formula(boolean placeholder, boolean unicode, Object ... args);
+    default String formula(Object ... args) {
+        return formula(false, false, args);
+    }
+    default String formula(boolean placeholder, Object ... args) {
+        return formula(placeholder, false, args);
+    }
+}
---
+++ anyline-data-jdbc/src/main/java/org/anyline/data/jdbc/adapter/init/function/MySQLGenusFunction.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2006-2023 www.anyline.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.anyline.data.jdbc.adapter.init.function;
+
+import org.anyline.metadata.SystemFunction;
+import org.anyline.metadata.type.DatabaseType;
+
+public enum MySQLGenusFunction implements SystemFunction {
+    CONCAT(META.CONCAT) {
+        @Override
+        public String formula(boolean placeholder, boolean unicode, Object... args) {
+            return "";
+        }
+    };
+    MySQLGenusFunction(META meta){
+        this.meta = meta;
+    }
+    private final SystemFunction.META meta;
+    public DatabaseType database() {
+        return null;
+    }
+    public META meta() {
+        return meta;
+    }
+}
