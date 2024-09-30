b74b93949031328a23798e999b8032cc1ef93674
server@anyline.org <server@anyline.org>
2024-09-30 14:37:51 +0800

    EasySearch

---
+++ anyline-data-dialect-search/anyline-data-easysearch/src/main/java/org/anyline/data/easysearch/EasySearchAdapter.java
@@ -0,0 +1,7 @@
+package org.anyline.data.easysearch;
+
+import org.anyline.annotation.Component;
+
+@Component("anyline.environment.data.driver.actuator.easysearch")
+public class EasySearchAdapter {
+}
