a88a9a808e5f8493a95a521a118de63a54ab5b58
anyline <server@anyline.org>
2025-05-25 15:55:29 +0800

    StandardTypeMetadata

--- anyline-core/src/main/java/org/anyline/metadata/type/init/StandardTypeMetadata.java
+++ anyline-core/src/main/java/org/anyline/metadata/type/init/StandardTypeMetadata.java
@@ -880,7 +880,7 @@ public enum StandardTypeMetadata implements TypeMetadata {
      */
     private String formula;
     private Class transfer                  ; //中间转换类型 转换成其他格式前先转换成transfer类型
-    private final Class compatible          ; //从数据库中读写数据的类型
+    private Class compatible          ; //从数据库中读写数据的类型
     private final int ignoreLength;
     private final int ignorePrecision;
     private final int ignoreScale;
@@ -934,7 +934,9 @@ public enum StandardTypeMetadata implements TypeMetadata {
         }
         return CATEGORY_GROUP.NONE;
     }
-
+    public void compatible(Class compatible){
+        this.compatible = compatible;
+    }
     @Override
     public Object convert(Object value, Object def) {
         return convert(value, null, def);
