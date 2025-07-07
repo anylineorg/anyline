d0cf73ae140d5f4f62d9c522b72f31e0186babc1
anyline <server@anyline.org>
2025-07-07 14:36:28 +0800

    Replace

--- anyline-extends/anyline-web/src/main/java/org/anyline/web/tag/Replace.java
+++ anyline-extends/anyline-web/src/main/java/org/anyline/web/tag/Replace.java
@@ -26,9 +26,9 @@ import java.io.IOException;
 
 public class Replace extends BaseBodyTag implements Cloneable{
 	private static final long serialVersionUID = 1L; 
-	private String from;
+	private String target;
 	private String separate;
-	private String to;
+	private String replacement;
 
 	 public int doEndTag() throws JspException {
 		 if(BasicUtil.isNotEmpty(var)) {
@@ -38,23 +38,23 @@ public class Replace extends BaseBodyTag implements Cloneable{
 		 if(BasicUtil.isEmpty(src)) {
 			 return EVAL_BODY_INCLUDE;
 		 }
-		 if(null == from || from.length()==0) {
+		 if(null == target || target.isEmpty()) {
 			 return EVAL_BODY_INCLUDE;
 		 }
-		if(BasicUtil.isEmpty(to)) {
-			to = "";
+		if(BasicUtil.isEmpty(replacement)) {
+			replacement = "";
 		}
 		try {
 			String result = "";
 			if(null!= separate) {
 				/**
-				 * 以separate分隔from,每个条目换成to,如果to中也有separate并且条目长度与from一致则按顺序替换
-				 * value = "ABC123"  from = "ABC" to="a" result = "a123"
-				 * value = "ABC123" from = "A,B,C"  separate="," to = "a" result = "aaa123"
-				 * value = "ABC123" from = "A,B,C"  separate="," to = "a,b,c" result = "abc123"
+				 * 以separate分隔target,每个条目换成replacement,如果replacement中也有separate并且条目长度与 target 一致则按顺序替换
+				 * value = "ABC123"  target = "ABC" to="a" result = "a123"
+				 * value = "ABC123" target = "A,B,C"  separate="," replacement = "a" result = "aaa123"
+				 * value = "ABC123" target = "A,B,C"  separate="," replacement = "a,b,c" result = "abc123"
 				 */
-				String froms[] = from.split(separate,-1);
-				String tos[] = to.split(separate,-1);
+				String froms[] = target.split(separate,-1);
+				String tos[] = replacement.split(separate,-1);
 
 				result = src;
 				if(tos.length == froms.length) {
@@ -64,11 +64,11 @@ public class Replace extends BaseBodyTag implements Cloneable{
 					}
 				}else {
 					for (String item : froms) {
-						result = result.replace(item, to);
+						result = result.replace(item, replacement);
 					}
 				}
 			}else {
-				result = src.replace(from, to);
+				result = src.replaceAll(target, replacement);
 			}
 			if(null != result) {
 				if(BasicUtil.isNotEmpty(var)) {
@@ -91,8 +91,8 @@ public class Replace extends BaseBodyTag implements Cloneable{
 	public void release() {
 		super.release();
 		value = null;
-		from = null;
-		to = null;
+		target = null;
+		replacement = null;
 		separate = null;
 	} 
 	@Override 
@@ -108,23 +108,23 @@ public class Replace extends BaseBodyTag implements Cloneable{
 		this.separate = separate;
 	}
 
-	public String getFrom() {
-		return from;
+	public String getTarget() {
+		return target;
 	}
 
 
-	public void setFrom(String from) {
-		this.from = from;
+	public void setTarget(String target) {
+		this.target = target;
 	}
 
 
-	public String getTo() {
-		return to;
+	public String getReplacement() {
+		return replacement;
 	}
 
 
-	public void setTo(String to) {
-		this.to = to;
+	public void setReplacement(String replacement) {
+		this.replacement = replacement;
 	}
 
 }
