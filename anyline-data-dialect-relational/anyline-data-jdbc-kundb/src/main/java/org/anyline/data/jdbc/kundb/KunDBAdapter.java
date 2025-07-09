55c9e8bfb27368703d523f7479dc7679304d3b60
anyline <server@anyline.org>
2025-07-09 09:55:18 +0800

    adapter

---
+++ anyline-data-dialect-relational/anyline-data-jdbc-kundb/src/main/java/org/anyline/data/jdbc/kundb/KunDBAdapter.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2006-2025 www.anyline.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.anyline.data.jdbc.kundb;
+
+import org.anyline.annotation.AnylineComponent;
+import org.anyline.data.jdbc.adapter.init.MySQLGenusAdapter;
+import org.anyline.metadata.type.DatabaseType;
+@AnylineComponent("anyline.data.jdbc.adapter.kundb")
+public class KunDBAdapter extends MySQLGenusAdapter {
+    
+    public DatabaseType type() {
+        return DatabaseType.KunDB;
+    }
+    
+    private String delimiter;
+
+} 
